da47e7cbebf7236497355f9efca252126cf0515a
Michal Moskal <michal.moskal@microsoft.com>
2016-01-26 09:01:36 -0800

    Recursive install

--- yelmlib/main.ts
+++ yelmlib/main.ts
@@ -6,11 +6,18 @@ namespace yelm {
         name: string;
         description?: string;
         dependencies: Util.StringMap<string>;
+        files: string[];
     }
-    
+
     export interface Package {
-        _packageBrand:any;
-        [s:string]: string;
+        _packageBrand: any;
+        [s: string]: string;
+    }
+
+    export function getConfig(p: Package) {
+        let cfg = <PackageConfig>JSON.parse(p["yelm.config"])
+        Util.assert(typeof cfg.name == "string")
+        return cfg
     }
 
     var pkgPrefix = "ptr-yelm-"
@@ -29,19 +36,35 @@ namespace yelm {
         throw new Error("bad version spec: " + n)
     }
     
-    export function installPkgAsync(cfg: PackageConfig){
-        let res:Util.StringMap<Package> = {}
-        return Promise.join(Object.keys(cfg.dependencies).map(n => {
-            return depVersionAsync(n, cfg.dependencies[n])
-                .then(v => Cloud.privateRequestAsync({ url: v + "/text" }))
-                .then(resp => {
-                    let v = JSON.parse(resp.text)
-                    if (typeof v["yelm.json"] == "string")
-                        v["yelm.json"] = JSON.parse(v["yelm.json"])
-                    res[n] = v                      
-                })              
-        }))
-        .then(() => res)   
+    export function buildPkgAsync(pkgs:Util.StringMap<Package>)
+    {
+        
+    }
+
+    export function installPkgAsync(cfg: PackageConfig) {
+        let res: Util.StringMap<Package> = {}
+        let versions: Util.StringMap<string> = {}
+
+        function recAsync(cfg: PackageConfig):Promise<any> {
+            return Promise.join(Object.keys(cfg.dependencies).map(n => {
+                return depVersionAsync(n, cfg.dependencies[n])
+                    .then(v => {
+                        if (versions.hasOwnProperty(n)) {
+                            if (versions[n] != v)
+                                throw new Error("Version mismatch on " + n)
+                        } else {
+                            versions[n] = v
+                            return Cloud.privateRequestAsync({ url: v + "/text" })
+                                .then(resp => {
+                                    let pkg = <Package>JSON.parse(resp.text)
+                                    res[n] = pkg
+                                    return recAsync(getConfig(pkg))
+                                })
+                        }
+                    })
+            }))
+        }
+        return recAsync(cfg).then(() => res)
     }
 
 
