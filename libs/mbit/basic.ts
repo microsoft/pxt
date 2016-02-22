30439666c3346d3ca4661a0ca9765c763d7ba6fc
Peli de Halleux <jhalleux@microsoft.com>
2016-02-22 13:06:17 -0800

    ordering functions/gap update

--- libs/mbit/basic.ts
+++ libs/mbit/basic.ts
@@ -2,18 +2,6 @@
  * Provides access to basic micro:bit functionality.
  */
 namespace basic {
-    /**
-     * Display text on the display, one character at a time, and shift by one column each ``interval`` milliseconds. If the string fits on the screen (i.e. is one letter), does not scroll.
-     * @param text the text to scroll on the screen, eg: Hello!
-     * @param interval how fast to shift characters; eg: 150, 100, 200, -100
-     */
-    //% help=functions/show-string 
-    //% weight=87
-    //% shim=micro_bit::scrollString async 
-    //% block="show|string %text=2" icon="\uf031" 
-    //% async
-    //% blockId=device_print_message
-    export function showString(text: string, interval: number = 150): void { }
 
     /**
      * Scroll a number on the screen and shift by one column every ``interval`` milliseconds. If the number fits on the screen (i.e. is a single digit), does not scroll.
@@ -25,6 +13,40 @@ namespace basic {
     //% blockId=device_show_number block="show|number %number=2" blockGap=8 icon="\uf1ec"
     //% async
     export function showNumber(value: number, interval: number = 150): void { }
+
+    /**
+     * Draws an image on the LED screen and pauses for the given milliseconds.
+     * @param leds TODO
+     * @param interval TODO
+     */
+    //% help=functions/show-leds 
+    //% weight=95 blockGap=8
+    //% shim=micro_bit::showLeds 
+    //% imageLiteral async
+    //% blockId=device_show_leds
+    //% block="show leds" icon="\uf00a"
+    export function showLeds(leds: string, interval: number = 400) : void {}
+
+    /**
+     * Display text on the display, one character at a time, and shift by one column each ``interval`` milliseconds. If the string fits on the screen (i.e. is one letter), does not scroll.
+     * @param text the text to scroll on the screen, eg: Hello!
+     * @param interval how fast to shift characters; eg: 150, 100, 200, -100
+     */
+    //% help=functions/show-string 
+    //% weight=87 blockGap=8
+    //% shim=micro_bit::scrollString async 
+    //% block="show|string %text=2" icon="\uf031" 
+    //% async
+    //% blockId=device_print_message
+    export function showString(text: string, interval: number = 150): void { }
+
+    /**
+     * Turn off all LEDs
+     */
+    //% help=functions/clear-screen weight=79
+    //% shim=micro_bit::clearScreen
+    //% blockId=device_clear_display block="clear screen" icon="\uf12d"
+    export function clearScreen(): void { }
     
     /**
      * Shows a sequence of LED screens as an animation with an ``interval`` delay between each frame
@@ -35,39 +57,17 @@ namespace basic {
     export function showAnimation(leds: string, interval: number = 400) : void {}
 
     /**
-     * Draws an image on the LED screen and pauses for the given milliseconds.
-     * @param leds TODO
-     * @param interval TODO
-     */
-    //% help=functions/show-leds 
-    //% weight=95 
-    //% shim=micro_bit::showLeds 
-    //% imageLiteral async
-    //% blockId=device_show_leds
-    //% block="show leds" icon="\uf00a"
-    export function showLeds(leds: string, interval: number = 400) : void {}
-
-    /**
      * Draws an image on the LED screen.
      * @param leds TODO
      */
     //% help=functions/plot-leds weight=80 shim=micro_bit::plotLeds imageLiteral
     export function plotLeds(leds: string) : void {}
 
-
-    /**
-     * Turn off all LEDs
-     */
-    //% help=functions/clear-screen weight=79
-    //% shim=micro_bit::clearScreen
-    //% blockId=device_clear_display block="clear screen" icon="\uf12d"
-    export function clearScreen(): void { }
-
     /**
      * Repeats the code forever in the background. On each iteration, allows other codes to run.
      * @param body TODO
      */
-    //% help=functions/forever weight=55
+    //% help=functions/forever weight=55 blockGap=8
     //% blockId=device_forever block="forever" icon="\uf01e" shim=micro_bit::forever
     export function forever(body:() => void) : void {
     }
