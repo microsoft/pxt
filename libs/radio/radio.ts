3133018be381265ebe84b89a3fe3c29143d00110
Peli de Halleux <jhalleux@microsoft.com>
2016-02-29 14:10:01 -0800

    fixing annotation of radio

--- libs/radio/radio.ts
+++ libs/radio/radio.ts
@@ -2,12 +2,8 @@
 namespace radio {
     /**
      * Sends a message id to other micro:bit in the group using radio.
-     * {namespace:radio}
-     * {weight:80}
-     * {help:/functions/broadcast-message}
-     * {shim:micro_bit::broadcastMessage}
      */
-    //% weight=70
+    //% weight=70 shim="micro_bit::broadcastMessage" help="/functions/broadcast-message"
     //% blockId=radio_broadcast block="broadcast message %MESSAGE" icon="\uf1d8" blockGap=12
     export function broadcastMessage(message: number) : void {
         /*
@@ -20,11 +16,9 @@ namespace radio {
 
     /**
      * Registers code to run when a particular message is received from another micro:bit.
-     * {namespace:radio}
-     * {weight:79}
-     * {help:/functions/on-message-received}
-     * {shim:micro_bit::onBroadcastMessageReceived}
      */
+    //% help=/functions/on-message-received
+    //% shim=micro_bit::onBroadcastMessageReceived
     //% weight=69
     //% blockId=radio_broadcast_received_event block="on message received %message" icon="\uf1d8"
     export function onMessageReceived(message: number, body:Action) : void {
@@ -38,22 +32,18 @@ namespace radio {
 
     /**
      * Broadcasts a number over radio to any connected micro:bit in the group.
-     * {help:/functions/send-number}
-     * {namespace:radio}
-     * {shim:micro_bit::datagramSendNumber}
-     * {weight:78}
      */    
+    //% help=/functions/send-number
+    //% shim=micro_bit::datagramSendNumber
     //% weight=60
     //% blockId=radio_datagram_send block="send number %MESSAGE" blockGap=8
     export function sendNumber(value: number) : void { }
 
     /**
      * Reads the next packet as a number from the radio queue.
-     * {help:/functions/receive-number}
-     * {weight:77}
-     * {namespace:radio}
-     * {shim:micro_bit::datagramReceiveNumber}
      */
+    //% help=/functions/receive-number
+    //% shim=micro_bit::datagramReceiveNumber
     //% weight=59
     //% blockId=radio_datagram_receive block="receive number" blockGap=12
     export function receiveNumber() : number
@@ -62,21 +52,11 @@ namespace radio {
         return 0;
     }
 
-    /*
-    function noBleWarning() : void
-    {
-        if (TD.simulator.isBleRequired()) {
-            TD.simulator.warning("*radio* functions cannot be used with *devices* function or BLE enabled");
-        }
-    }*/
-
     /**
      * Registers code to run when a packet is received over radio.
-     * {namespace:radio}
-     * {weight:30}
-     * {help:/functions/on-data-received}
-     * {shim:micro_bit::onDatagramReceived}
      */
+    //% help=/functions/on-data-received
+    //% shim=micro_bit::onDatagramReceived
     //% weight=58
     //% blockId=radio_datagram_received_event block="on data received" blockGap=8
     export function onDataReceived(body:Action) : void
@@ -86,11 +66,9 @@ namespace radio {
 
     /**
      * Broadcasts 4 numbers over radio to any connected micro:bit in the group.
-     * {help:/functions/send-numbers}
-     * {namespace:radio}
-     * {shim:micro_bit::datagramSendNumbers}
-     * {weight:20}
      */
+    //% help=/functions/send-numbers
+    //% shim=micro_bit::datagramSendNumbers
     //% weight=57
     //% blockId=radio_datagram_send_numbers block="send numbers|0: %VALUE0|1: %VALUE1|2: %VALUE2|3: %VALUE3" blockGap=8
     export function sendNumbers(value_0: number, value_1: number, value_2: number, value_3: number) : void
@@ -100,12 +78,10 @@ namespace radio {
 
     /**
      * Reads a number at a given index, between ``0`` and ``3``, from the packet received by ``receive number``. Not supported in simulator.
-     * {namespace:radio}
-     * {help:/functions/received-number-at}
-     * {weight:19}
-     * {shim:micro_bit::datagramGetNumber}
-     * {hints:index:0,1,2,3}
      */
+    //% help=/functions/received-number-at
+    //% shim=micro_bit::datagramGetNumber
+    //% hints=index:0,1,2,3
     //% weight=56
     //% blockId=radio_datagram_received_number_at block="receive number|at %VALUE" blockGap=8
     export function receivedNumberAt(index: number) : number {
@@ -117,36 +93,24 @@ namespace radio {
 
     /**
      * Gets the received signal strength indicator (RSSI) from the packet received by ``receive number``. Not supported in simulator.
-     * {namespace:radio}
-     * {help:/functions/received-signal-strength}
-     * {weight:10}
-     * {shim:micro_bit::datagramGetRSSI}
-     * {weight:18}
+     * namespace=radio
      */
+    //% help=/functions/received-signal-strength
+    //% shim=micro_bit::datagramGetRSSI
     //% weight=55
     //% blockId=radio_datagram_rssi block="received signal strength"
-    export function receivedSignalStrength() : number
-        {
+    export function receivedSignalStrength() : number {
         return 0;
     }
 
     /**
      * Sets the group id for radio communications. A micro:bit can only listen to one group ID at any time.
      * @ param id the group id between ``0`` and ``255``, eg: 1
-     * {namespace:radio}
-     * {weight:10}
-     * {help:/functions/set-group}
-     * {shim:micro_bit::setGroup}
      */
+    //% help=/functions/set-group
+    //% shim=micro_bit::setGroup
     //% weight=10
     //% blockId=radio_set_group block="set group %ID"
-    export function setGroup(id: number) : void
-        {
-        /*
-        if (id < 0 || id > 255) {
-            TD.simulator.warning("id should be between 0 and 255.");
-        }
-        noBleWarning();
-        */
+    export function setGroup(id: number) : void {
     }
 }
