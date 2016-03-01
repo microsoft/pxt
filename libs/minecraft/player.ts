2554d727236da4f25aad4ee6a02ce0f9db9f91b5
Peli de Halleux <jhalleux@microsoft.com>
2016-02-29 23:41:13 -0800

    adding player.getPos and world.setBlock

---
+++ libs/minecraft/player.ts
@@ -0,0 +1,10 @@
+//% color=176 weight=32
+namespace player {
+    /**
+     * Gets the current player position
+     */
+    //% blockId=mcpiPlayerGetPos block="position"
+    export function getPos() : number[] {
+        return [0,0,0];
+    }   
+}
---
+++ libs/minecraft/world.ts
@@ -0,0 +1,14 @@
+/**
+ * World manipulation
+ */
+//% color=270 weight=80
+namespace world {
+    /**
+     * Sets a block in the world.
+     */
+    //% weight=60
+    //% blockId=mcpiWorldSetBlock block="set block|x: %x|y: %y|z: %z|block: %blockid|state: %state"
+    export function setBlock(x : number, y : number, z: number, blockid:number, state : number = 0)  {
+                
+    }
+}
