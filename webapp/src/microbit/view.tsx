b2437f1b10a7cd762e9a21d48025fef126e00e4d
Peli de Halleux <jhalleux@microsoft.com>
2016-03-01 22:23:46 -0800

    refactoring simulator to runtime

--- webapp/src/microbit/view.tsx
+++ webapp/src/microbit/view.tsx
@@ -1,13 +1,14 @@
+/// <reference path="../../../built/yelmlib.d.ts" />
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as simsvg from "./simsvg";
 import * as sui from "../sui"
 
 import rt = yelm.rt;
 import U = yelm.U;
 
-export class BoardView extends React.Component<simsvg.IBoardProps, {}> {
-    view: simsvg.MicrobitBoardSvg;
+export class BoardView extends React.Component<yelm.rt.micro_bit.IBoardProps, {}> {
+    view: yelm.rt.micro_bit.MicrobitBoardSvg;
 
     componentDidUpdate() {
         if (this.view && !$(this.view.element).parentsUntil("body").length) {
@@ -25,20 +26,9 @@ export class BoardView extends React.Component<simsvg.IBoardProps, {}> {
         if (!this.view || this.view.board !== runtime.board) {
             let props = U.flatClone(this.props)
             if (!props.theme)
-                props.theme = simsvg.randomTheme();
-            this.view = new simsvg.MicrobitBoardSvg(props)
+                props.theme = yelm.rt.micro_bit.randomTheme();
+            this.view = new yelm.rt.micro_bit.MicrobitBoardSvg(props)
         }
-
-        var events = ["shake", "logo up", "logo down", "screen up", "screendown"];
-        var eid = events[0];
-        /*
-        <div>                
-            <sui.Dropdown class='selection search' value={eid}>
-                {events.map(h => <sui.Item key={h} value={h} text={h} />) }
-            </sui.Dropdown>
-            <sui.Button class="secondary" icon="lab" popup="Simulate event."/>
-        </div>
-        */
 
         return (
             <div>
