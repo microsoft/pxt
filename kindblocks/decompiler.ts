c46354b8d1a4404f306d2d6769887f2e8da3f1e5
Peli de Halleux <jhalleux@microsoft.com>
2016-03-17 08:27:50 -0700

    support for decompiling string/number/boolean literals

--- kindblocks/decompiler.ts
+++ kindblocks/decompiler.ts
@@ -1,42 +1,55 @@
 namespace ks.blocks {
     let SK = ts.SyntaxKind;
 
-    export function toBlocks(stmts: ts.Statement[]) : string {
+    export function toBlocks(stmts: ts.Statement[]): string {
         let output = ""
 
         stmts.forEach(emit)
 
         return `<xml xmlns="http://www.w3.org/1999/xhtml">
 ${output}</xml>`;
-        
-        function write(s:string) {
+
+        function write(s: string) {
             output += s + "\n"
         }
 
         function emit(n: ts.Node) {
             switch (n.kind) {
                 case SK.ExpressionStatement:
-                    emit((n as ts.ExpressionStatement).expression)
-                    break;
+                    emit((n as ts.ExpressionStatement).expression); break;
                 case SK.CallExpression:
-                    emitCallExpression(n as ts.CallExpression)
-                    break;
+                    emitCallExpression(n as ts.CallExpression); break;
                 case SK.StringLiteral:
-                    emitStringLiteral(n as ts.StringLiteral)
-                    break;                    
+                    emitStringLiteral(n as ts.StringLiteral); break;
+                case SK.NullKeyword:
+                    // don't emit anything
+                    break;
+                case SK.NumericLiteral:
+                    emitNumberExpression(n as ts.LiteralExpression); break;
+                case SK.TrueKeyword:
+                case SK.FalseKeyword:                
+                    emitBooleanExpression(n as ts.LiteralExpression); break;
                 default:
                     console.warn("Unhandled emit:", ts.ks.stringKind(n))
             }
         }
-        
-        function emitStringLiteral(n:ts.StringLiteral) {
+
+        function emitStringLiteral(n: ts.StringLiteral) {
             write(`<block type="text"><field name="TEXT">${U.htmlEscape(n.text)}</field></block>`)
+        }
+        
+        function emitNumberExpression(n: ts.LiteralExpression) {
+            write(`<block type="math_number"><field name="NUM">${U.htmlEscape(n.text)}</field></block>`)
+        }
+        
+        function emitBooleanExpression(n: ts.LiteralExpression) {
+            write(`<block type="logic_boolean"><field name="BOOL">${U.htmlEscape(n.kind == ts.SyntaxKind.TrueKeyword ? 'TRUE' : 'FALSE')}</field></block>`)
         }
 
         function emitCallExpression(node: ts.CallExpression) {
             let info: ts.ks.CallInfo = (node as any).callInfo
-            let argNames:string[] = []
-            info.attrs.block.replace(/%(\w+)/g, (f,n) => {
+            let argNames: string[] = []
+            info.attrs.block.replace(/%(\w+)/g, (f, n) => {
                 argNames.push(n)
                 return ""
             })
