8171c0a4754a8ac3da92d801619b45506da4aa1a
Peli de Halleux <jhalleux@microsoft.com>
2016-03-19 20:03:22 -0700

    simplify kind.json editor

--- webapp/src/kindjson.tsx
+++ webapp/src/kindjson.tsx
@@ -32,66 +32,53 @@ export class Editor extends srceditor.Editor {
         }
         let card = c.card || {};
         let mainFile = pkg.mainEditorPkg().getMainFile();
-                
+
+        /*
+                                <div className="two fields">
+                         <sui.Input inputLabel={lf("Power")} type="number" value={(card.power || 0).toString()} onChange={v => {
+                             initCard();
+                             let vi = Math.max(0, parseInt(v) || 0)
+                             update(c.card.power = vi)
+                         }} /> /
+                         <sui.Input inputLabel={lf("Toughness")} type="number" value={(card.toughness || 0).toString()} onChange={v => {
+                             initCard();
+                             let vi = Math.max(0, parseInt(v) || 0)
+                             update(c.card.toughness = vi)
+                         }} />
+                         </div>
+ */
+
         return (
-            <div className="ui four column grid">
-                <div className="ui column">
-                    <div className="ui segment">                    
-                        <codecard.CodeCardView 
-                            url="kind.sc/abc" 
-                            header="me" 
-                            name={c.name}
-                            description={c.description} 
-                            card={c.card}
-                            target={this.parent.appTarget.id}
-                            />
+            <div className="ui segment form text" style={{ backgroundColor: "white" }}>
+                <sui.Field>
+                    <div className="ui toggle checkbox ">
+                        <input type="checkbox" name="public" checked={c.public}
+                            onChange={() => update(c.public = !c.public) } />
+                        <label>{lf("Public package (library)") }</label>
                     </div>
-                </div>
-                <div className="ui three column">
-                    <div className="ui segment form text container" style={{backgroundColor: "white"}}>
-                        <sui.Field>
-                            <div className="ui toggle checkbox ">
-                                <input type="checkbox" name="public" checked={c.public}
-                                    onChange={() => update(c.public = !c.public) } />
-                                <label>{lf("Public package (library)") }</label>
-                            </div>
-                        </sui.Field>
-                        <sui.Input label={lf("Name") } value={c.name} onChange={v => update(c.name = v) } />
-                        <sui.Input label={lf("Description") } lines={3} value={c.description} onChange={v => update(c.description = v) } />
-                        <sui.Input label={lf("Picture or video (YouTube, Vimeo, Instagram)")} value={card.promoUrl || ""} onChange={v => {
-                            initCard();
-                            update(c.card.promoUrl = v)
-                        }} />
-                        <div className="three fields">
-                        <sui.Input inputLabel={lf("Any")} type="number" value={(card.any || 0).toString()} onChange={v => {
-                            initCard();
-                            let vi = Math.max(0, parseInt(v) || 0)
-                            update(c.card.any = vi)
-                        }} />
-                        <sui.Input inputLabel={lf("Hardware")} type="number" value={(card.hardware || 0).toString()} onChange={v => {
-                            initCard();
-                            let vi = Math.max(0, parseInt(v) || 0)
-                            update(c.card.hardware = vi)
-                        }} />
-                        <sui.Input inputLabel={lf("Software")} type="number" value={(card.software || 0).toString()} onChange={v => {
-                            initCard();
-                            let vi = Math.max(0, parseInt(v) || 0)
-                            update(c.card.software = vi)
-                        }} />
-                        </div>
-                        <div className="two fields">
-                        <sui.Input inputLabel={lf("Power")} type="number" value={(card.power || 0).toString()} onChange={v => {
-                            initCard();
-                            let vi = Math.max(0, parseInt(v) || 0)
-                            update(c.card.power = vi)
-                        }} /> /
-                        <sui.Input inputLabel={lf("Toughness")} type="number" value={(card.toughness || 0).toString()} onChange={v => {
-                            initCard();
-                            let vi = Math.max(0, parseInt(v) || 0)
-                            update(c.card.toughness = vi)
-                        }} />
-                        </div>
-                    </div>
+                </sui.Field>
+                <sui.Input label={lf("Name") } value={c.name} onChange={v => update(c.name = v) } />
+                <sui.Input label={lf("Description") } lines={3} value={c.description} onChange={v => update(c.description = v) } />
+                <sui.Input label={lf("Picture or video (YouTube, Vimeo, Instagram)") } value={card.promoUrl || ""} onChange={v => {
+                    initCard();
+                    update(c.card.promoUrl = v)
+                } } />
+                <div className="three fields">
+                    <sui.Input inputLabel={lf("Any") } type="number" value={(card.any || 0).toString() } onChange={v => {
+                        initCard();
+                        let vi = Math.max(0, parseInt(v) || 0)
+                        update(c.card.any = vi)
+                    } } />
+                    <sui.Input inputLabel={lf("Hardware") } type="number" value={(card.hardware || 0).toString() } onChange={v => {
+                        initCard();
+                        let vi = Math.max(0, parseInt(v) || 0)
+                        update(c.card.hardware = vi)
+                    } } />
+                    <sui.Input inputLabel={lf("Software") } type="number" value={(card.software || 0).toString() } onChange={v => {
+                        initCard();
+                        let vi = Math.max(0, parseInt(v) || 0)
+                        update(c.card.software = vi)
+                    } } />
                 </div>
             </div>
         )
@@ -103,7 +90,7 @@ export class Editor extends srceditor.Editor {
 
     acceptsFile(file: pkg.File) {
         if (file.name != ks.configName) return false
-        
+
         if (file.isReadonly()) {
             // TODO add read-only support
             return false
@@ -122,12 +109,12 @@ export class Editor extends srceditor.Editor {
         this.config = JSON.parse(file.content)
         this.setDiagnostics(file, this.snapshotState())
     }
-    
+
     menu() {
         return (
             <div className="item">
                 <sui.Button class="button floating" text={lf("Edit Text") } icon="keyboard" onClick={() => this.parent.editText() } />
             </div>
-        )        
+        )
     }
 }
