13915a2ad233f34c63d847209d49a94398a4e19f
Michal Moskal <michal.moskal@microsoft.com>
2016-03-28 19:18:08 -0700

    kind-cli: look for kindscript in target packages

--- kindscript-cli/cli.js
+++ kindscript-cli/cli.js
@@ -6,8 +6,15 @@ let child_process = require("child_process")
 
 function findKindJs() {
     let goUp = (s) => {
-        let f = s + "/node_modules/kindscript/built/kind.js"
-        if (fs.existsSync(f)) return f
+        let mod = s + "/node_modules/"
+        if (fs.existsSync(mod)) {
+            let f = mod + "kindscript/built/kind.js"
+            if (fs.existsSync(f)) return f
+            for (let dir of fs.readdirSync(mod)) {
+                let inner = mod + dir + "/node_modules/kindscript/built/kind.js"
+                if (fs.existsSync(inner)) return inner
+            }
+        }
         f = s + "/built/kind.js" // local build
         if (fs.existsSync(f)) return f
         let s2 = path.resolve(path.join(s, ".."))
@@ -41,7 +48,7 @@ function main() {
         selfInstall()
         path = findKindJs()
     }
-    
+
     if (!path) {
         console.error("cannot find built/kind.js; maybe try 'kind selfinstall'")
         process.exit(1)
