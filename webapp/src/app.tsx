7d7d5a697bfa006922db1f9a6feaac59a729c067
Peli de Halleux <jhalleux@microsoft.com>
2016-03-31 13:03:34 -0700

    show new project if no eader available

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -426,7 +426,7 @@ export class ProjectView extends data.Component<IAppProps, IAppState> {
             let header = this.getData("header:*")[0];
             if (!this.state.header && header) {
                 this.loadHeader(header)
-            }
+            } else this.newProject(true);
         }, 1000)
     }
 
