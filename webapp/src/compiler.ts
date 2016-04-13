dac08e0563cd4605020f360dd34d99c36f465c4d
Michal Moskal <michal.moskal@microsoft.com>
2016-04-13 15:02:51 -0700

    Refactor web worker interface

--- webapp/src/compiler.ts
+++ webapp/src/compiler.ts
@@ -3,29 +3,17 @@ import * as data from "./data";
 import * as pkg from "./package";
 import * as core from "./core";
 import * as srceditor from "./srceditor"
+import * as workeriface from "./workeriface"
+
 
 import Cloud = pxt.Cloud;
 import U = pxt.Util;
 
-let tsWorker: Worker;
-let pendingMsgs: U.StringMap<(v: any) => void> = {}
-let msgId = 0;
-let q: U.PromiseQueue;
+let iface: workeriface.Iface
 
 export function init() {
-    q = new U.PromiseQueue();
-    let initPromise = new Promise<void>((resolve, reject) => {
-        pendingMsgs["ready"] = resolve;
-    })
-    q.enqueue("main", () => initPromise)
-    
-    tsWorker = new Worker(pxt.webConfig.workerjs)
-    tsWorker.onmessage = ev => {
-        if (pendingMsgs.hasOwnProperty(ev.data.id)) {
-            let cb = pendingMsgs[ev.data.id]
-            delete pendingMsgs[ev.data.id]
-            cb(ev.data.result)
-        }
+    if (!iface) {
+        iface = workeriface.make(pxt.webConfig.workerjs)
     }
 }
 
@@ -58,7 +46,7 @@ function setDiagnostics(diagnostics: ts.pxt.KsDiagnostic[]) {
 }
 
 export interface CompileOptions {
-    native?: boolean;    
+    native?: boolean;
     debug?: boolean;
     background?: boolean; // not explicitely requested by user (hint for simulator)
 }
@@ -107,21 +95,8 @@ function decompileCoreAsync(opts: ts.pxt.CompileOptions, fileName: string): Prom
 }
 
 export function workerOpAsync(op: string, arg: ts.pxt.service.OpArg) {
-    return q.enqueue("main", () => new Promise<any>((resolve, reject) => {
-        let id = "" + msgId++
-        pendingMsgs[id] = v => {
-            if (!v) {
-                pxt.reportError("no worker response", null)
-                reject(new Error("no response"))
-            } else if (v.errorMessage) {
-                pxt.reportError("worker response: " + v.errorMessage, null)
-                reject(new Error(v.errorMessage))
-            } else {
-                resolve(v)
-            }
-        }
-        tsWorker.postMessage({ id, op, arg })
-    }))
+    init()
+    return iface.opAsync(op, arg)
 }
 
 var firstTypecheck: Promise<void>;
