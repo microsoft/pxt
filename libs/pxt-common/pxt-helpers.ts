47f5e60bef6085978e1c29d3bc9c19c3532b379b
Peli de Halleux <jhalleux@microsoft.com>
2016-05-10 08:47:19 -0700

    formatting code

--- libs/pxt-common/pxt-helpers.ts
+++ libs/pxt-common/pxt-helpers.ts
@@ -12,7 +12,7 @@ namespace helpers {
 }
 
 namespace Math {
-    export function clamp(min: number, max:number, value:number): number {
+    export function clamp(min: number, max: number, value: number): number {
         return Math.min(max, Math.max(min, value));
     }
 
@@ -21,36 +21,32 @@ namespace Math {
       * For example, the absolute value of -5 is the same as the absolute value of 5.
       * @param x A numeric expression for which the absolute value is needed.
       */
-    export function abs(x: number): number
-    {
+    export function abs(x: number): number {
         return x < 0 ? -x : x;
     }
-    
+
     /**
       * Returns the sign of the x, indicating whether x is positive, negative or zero.
       * @param x The numeric expression to test
       */
-    export function sign(x: number): number
-    {
+    export function sign(x: number): number {
         if (x == 0) return 0;
         if (x > 0) return 1;
         return -1;
     }
-    
+
     /**
       * Returns the larger of two supplied numeric expressions. 
       */
-    export function max(a:number, b:number): number
-    {
+    export function max(a: number, b: number): number {
         if (a >= b) return a;
         return b;
     }
-    
+
     /**
       * Returns the smaller of two supplied numeric expressions. 
       */
-    export function min(a:number, b:number): number
-    {
+    export function min(a: number, b: number): number {
         if (a <= b) return a;
         return b;
     }
