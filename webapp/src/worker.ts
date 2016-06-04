878e61c0e45ccdc0ed626b3a875416cb5ef3571c
Peli de Halleux <jhalleux@microsoft.com>
2016-06-03 23:25:24 -0700

    use typeof to check existence of atob

--- webapp/src/worker.ts
+++ webapp/src/worker.ts
@@ -7,7 +7,7 @@ importScripts(
 let pm: any = postMessage;
 
 // work around safari not providing atob
-if (!atob) {
+if (typeof atob === "undefined") {
     // http://www.rise4fun.com/Bek/base64encode
     ts.pxt.decodeBase64 = function (_input: string): string {
         function _base64(_x: number): number { return ((_x <= 0x19) ? (_x + 0x41) : ((_x <= 0x33) ? (_x + 0x47) : ((_x <= 0x3D) ? (_x - 0x4) : ((_x == 0x3E) ? 0x2B : 0x2F)))); };
