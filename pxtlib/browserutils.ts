b244bca9aa4b1262fe45330ff9b52bca1201ad98
Peli de Halleux <jhalleux@microsoft.com>
2016-06-06 11:23:10 -0700

    fixing downloads in Safari

--- pxtlib/browserutils.ts
+++ pxtlib/browserutils.ts
@@ -6,12 +6,32 @@ namespace pxt.BrowserUtils {
     }
 
     function browserDownloadUInt8Array(buf: Uint8Array, name: string, contentType: string = "application/octet-stream", onError?: (err: any) => void): string {
-        let isMobileBrowser = /mobile/.test(navigator.userAgent);
-        let dataurl = "data:" + contentType + ";base64," + btoa(Util.uint8ArrayToString(buf))
+        const isMobileBrowser = /mobile/.test(navigator.userAgent);
+        const isSafari = /safari/i.test(navigator.userAgent);
+        const isDesktopIE = (<any>window).navigator.msSaveOrOpenBlob && !isMobileBrowser;
+        
+        const dataurl = "data:" + contentType + ";base64," + btoa(Util.uint8ArrayToString(buf))
         try {
-            if ((<any>window).navigator.msSaveOrOpenBlob && !isMobileBrowser) {
+            if (isDesktopIE) {
                 let b = new Blob([buf], { type: contentType })
                 let result = (<any>window).navigator.msSaveOrOpenBlob(b, name);
+            } else if (isSafari) {
+                // For mysterious reasons, the "link" trick closes the
+                // PouchDB database                
+                let iframe = document.getElementById("downloader") as HTMLIFrameElement;
+                if (!iframe) {
+                    console.log('injecting downloader iframe')
+                    iframe = document.createElement("iframe") as HTMLIFrameElement;
+                    iframe.id = "downloader";
+                    iframe.style.position = "absolute";
+                    iframe.style.right = "0";
+                    iframe.style.bottom = "0";
+                    iframe.style.zIndex = "-1";
+                    iframe.style.width= "1px";
+                    iframe.style.height = "1px";
+                    document.body.appendChild(iframe);
+                }
+                iframe.src = dataurl;
             } else {
                 let link = <any>window.document.createElement('a');
                 if (typeof link.download == "string") {
