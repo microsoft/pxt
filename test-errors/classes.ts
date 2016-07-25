a1420b3f7b983f4dda6294545fc855115d45f750
Thomas Denney <me@thomasdenney.co.uk>
2016-07-25 14:17:41 +0100

    Comments on classes

--- test-errors/classes.ts
+++ test-errors/classes.ts
@@ -1,7 +1,7 @@
 class A {
-    x: number = 42 // TS9209
+    x: number = 42 // TS9209 - field initializers
 }
-class B extends A { } // TS9228
+class B extends A { } // TS9228 - inheritence
 interface C { } // Probably should emit some sort of error, just skips it at the moment
 class D implements C { } // TS9228
 class G<T> { } // TS9227
