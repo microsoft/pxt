fb0775766b86b73bbb8fa5f3290acd63315ef766
Thomas Denney <a-thdenn@microsoft.com>
2016-07-28 09:19:00 +0100

    Generics supported, update tests

--- test-errors/classes.ts
+++ test-errors/classes.ts
@@ -4,9 +4,9 @@ class A {
 class B extends A { } // TS9228 - inheritence
 interface C { } // Probably should emit some sort of error, just skips it at the moment
 class D implements C { } // TS9228
-class G<T> { } // TS9227
+class G<T> { } // Generics now supported
 class S {
-    public static x: number 
+    public static x: number
     public static m() { }
 }
 S.x = 42 // TS9202 - can be removed if static field support is added
