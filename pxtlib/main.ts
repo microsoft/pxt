d9ed2f108a493633ea647d329b535888a10dd0eb
Peli de Halleux <jhalleux@microsoft.com>
2016-08-09 09:13:05 -0700

    added description field to target

--- pxtlib/main.ts
+++ pxtlib/main.ts
@@ -167,6 +167,7 @@ namespace pxt {
     export interface AppTarget {
         id: string; // has to match ^[a-z\-]+$; used in URLs and domain names
         name: string;
+        description?: string;
         corepkg: string;
         title?: string;
         cloud?: AppCloud;
