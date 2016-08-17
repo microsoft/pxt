microsoft/pxt

commit 49e9f2c4588d81c0ad4bab6461429156bb54d03f

Support the comma operator; fixes #160

pxtlib/emitter/emitter.ts
@@ -590,7 +590,7 @@ namespace ts.pxt {
                 case ts.SyntaxKind.CatchClause:
                 case ts.SyntaxKind.FinallyKeyword:
                 case ts.SyntaxKind.ThrowStatement:
-                    syntax =  lf("throwing and catching exceptions")
+                    syntax = lf("throwing and catching exceptions")
                     break
                 case ts.SyntaxKind.ClassExpression:
                     syntax = lf("class expressions")
@@ -1693,6 +1693,14 @@ ${lbl}: .short 0xffff
 
             let shim = (n: string) => rtcallMask(n, [node.left, node.right]);
 
+            if (node.operatorToken.kind == SK.CommaToken) {
+                let v = emitIgnored(node.left)
+                if (v)
+                    return ir.op(EK.Sequence, [v, emitExpr(node.right)])
+                else
+                    return emitExpr(node.right)
+            }
+
             if ((lt.flags & TypeFlags.Number) && (rt.flags & TypeFlags.Number)) {
                 let noEq = stripEquals(node.operatorToken.kind)
                 let shimName = simpleInstruction(noEq || node.operatorToken.kind)
@@ -1737,7 +1745,7 @@ ${lbl}: .short 0xffff
                             simpleInstruction(node.operatorToken.kind),
                             [shim("String_::compare"), ir.numlit(0)])
                     default:
-                        unhandled(node.operatorToken, lf("unknown numeric operator"), 9251)
+                        unhandled(node.operatorToken, lf("unknown string operator"), 9251)
                 }
             }
 
@@ -1840,16 +1848,15 @@ ${lbl}: .short 0xffff
             proc.emitLblDirect(l.brk);
         }
 
-        function emitExprAsStmt(node: Expression) {
-            if (!node) return;
+        function emitIgnored(node: Expression) {
+            if (!node) return null;
             switch (node.kind) {
                 case SK.Identifier:
                 case SK.StringLiteral:
                 case SK.NumericLiteral:
                 case SK.NullKeyword:
-                    return; // no-op
+                    return null; // no-op
             }
-            emitBrk(node)
             let v = emitExpr(node);
             let a = typeOf(node)
             if (!(a.flags & TypeFlags.Void)) {
@@ -1858,6 +1865,13 @@ ${lbl}: .short 0xffff
                     v = ir.op(EK.Decr, [v])
                 }
             }
+            return v
+        }
+
+        function emitExprAsStmt(node: Expression) {
+            let v = emitIgnored(node)
+            if (!v) return
+            emitBrk(node)
             proc.emitExpr(v)
             proc.stackEmpty();
         }

libs/lang-test0/lang-test0.ts
@@ -62,6 +62,7 @@ testEnums()
 testForOf()
 testMaps()
 testBufferShiftRotate();
+testComma();
 
 // test some top-level code
 let xsum = 0;
@@ -838,6 +839,17 @@ function testMaps() {
     control.assert(m.getElt("two") == 2, "2")
     //control.assert(mapGet(m, "zzzz") == null, "0")
 }
+
+function testComma() {
+    glb1 = 0
+    let x = (incrBy_2(), 77)
+    assert(x == 77, "x")
+    assert(glb1 == 2, "g")
+    // make sure there are no leaks
+    let y = ("aaa" + "zz", "x" + "yyy")
+    assert(y.length == 4, "y")
+}
+
 
 function testBufferShiftRotate() {
     /* TODO: create buffer?

commit e50b70540bf019a3451244ba7bb52f4cc3cc5ba8

Fix compilation regression from 49e9f2c4588d81c0ad4bab6461429156bb54d03f

pxtlib/emitter/emitter.ts
@@ -1702,11 +1702,12 @@ ${lbl}: .short 0xffff
             let shim = (n: string) => rtcallMask(n, [node.left, node.right]);
 
             if (node.operatorToken.kind == SK.CommaToken) {
-                let v = emitIgnored(node.left)
-                if (v)
-                    return ir.op(EK.Sequence, [v, emitExpr(node.right)])
-                else
+                if (isNoopExpr(node.left))
                     return emitExpr(node.right)
+                else {
+                    let v = emitIgnored(node.left)
+                    return ir.op(EK.Sequence, [v, emitExpr(node.right)])
+                }
             }
 
             if ((lt.flags & TypeFlags.Number) && (rt.flags & TypeFlags.Number)) {
@@ -1856,15 +1857,19 @@ ${lbl}: .short 0xffff
             proc.emitLblDirect(l.brk);
         }
 
-        function emitIgnored(node: Expression) {
-            if (!node) return null;
+        function isNoopExpr(node: Expression) {
+            if (!node) return true;
             switch (node.kind) {
                 case SK.Identifier:
                 case SK.StringLiteral:
                 case SK.NumericLiteral:
                 case SK.NullKeyword:
-                    return null; // no-op
+                    return true; // no-op
             }
+            return false
+        }
+
+        function emitIgnored(node: Expression) {
             let v = emitExpr(node);
             let a = typeOf(node)
             if (!(a.flags & TypeFlags.Void)) {
@@ -1877,9 +1882,9 @@ ${lbl}: .short 0xffff
         }
 
         function emitExprAsStmt(node: Expression) {
-            let v = emitIgnored(node)
-            if (!v) return
+            if (isNoopExpr(node)) return
             emitBrk(node)
+            let v = emitIgnored(node)
             proc.emitExpr(v)
             proc.stackEmpty();
         }