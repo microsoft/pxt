44c976ed9d1c1d874d8bcb7cbfe6304d5846b3dc
Richard Knoll <riknoll@microsoft.com>
2016-09-22 18:09:52 -0700

    Remove option to disable blocks

--- webapp/src/blocks.tsx
+++ webapp/src/blocks.tsx
@@ -196,6 +196,7 @@ export class Editor extends srceditor.Editor {
             trashcan: true,
             collapse: false,
             comments: false,
+            disable: false,
             zoom: {
                 enabled: true,
                 controls: true,
