d36fab7aa97a927b6dbe566c40fc585fe549c367
Richard Knoll <riknoll@users.noreply.github.com>
2016-10-06 14:12:28 -0700

    Handle name collisions in the decompiler (#429)
    Handle name collisions in the decompiler by renaming shadowed variables

---
+++ tests/decompile-test/name_collision_2.ts
@@ -0,0 +1,11 @@
+let i = 0;
+
+for (let i = 0; i <= 5; i++) {
+    i++
+}
+
+i++
+
+for (let i = 0; i <= i; i++) {
+    i++
+}
---
+++ tests/decompile-test/name_collision_9.ts
@@ -0,0 +1,53 @@
+let a = 0;
+let z = 0;
+let A = 0;
+let someName0 = 0;
+a++;
+z++;
+A++;
+someName0++;
+
+{
+    let a = 0;
+    let z = 0;
+    let A = 0;
+    let someName0 = 0;
+    a++;
+    z++;
+    A++;
+    someName0++;
+}
+
+a++;
+z++;
+A++;
+someName0++;
+
+let d = 0;
+let e = 0;
+let f = 0;
+let g = 0;
+let h = 0;
+let i = 0;
+let j = 0;
+let k = 0;
+let l = 0;
+let m = 0;
+let n = 0;
+let o = 0;
+let p = 0;
+let q = 0;
+let r = 0;
+let s = 0;
+let t = 0;
+let u = 0;
+let v = 0;
+let w = 0;
+let x = 0;
+let y = 0;
+
+// c is already taken, should become c2
+let c = 0;
+{
+    let c = 0;
+}
