7ff964ffe68588d26f6084b9f86d27bdada8bdf9
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2016-10-18 17:13:57 -0700

    Move trashcan into the toolbox (#519)
    * Moving trashcan to toolbox, shown on drag.
    
    * Adding blockly methods to blockly.d.ts

--- webapp/src/blocks.tsx
+++ webapp/src/blocks.tsx
@@ -198,7 +198,7 @@ export class Editor extends srceditor.Editor {
             scrollbars: true,
             media: pxt.webConfig.pxtCdnUrl + "blockly/media/",
             sound: true,
-            trashcan: true,
+            trashcan: false,
             collapse: false,
             comments: true,
             disable: false,
