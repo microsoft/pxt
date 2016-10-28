58d52dbf4c51fc99b105f7ab31925ec723bdcfe8
Tom Ball <tball@microsoft.com>
2016-10-27 19:15:12 -0700

    fix load_indirect bug

--- pxtlib/emitter/backavr.ts
+++ pxtlib/emitter/backavr.ts
@@ -196,12 +196,12 @@ namespace ts.pxtc {
 
             // different possibilities for src: r0, r5, sp, r6
             // any indirection we want to do using Y+C, Z+C (recall Y=sp, r6 -> Z)
-            if (src == "r0" || src == "r5") {
+            if (src != "sp") {
                 prelude = `
     movw r30, ${this.rmap_lo[src]}`
                 z_reg = "Z"
             } else
-                z_reg = this.ld_map[src]  // maps to Y or Z
+                z_reg = "Y" // sp -> FP = r29
 
             // different possibilities for off
             if (word || off[0] == "#") {
@@ -337,11 +337,6 @@ namespace ts.pxtc {
         inst_hi: pxt.Map<string> = {
             "adds": "adc",
             "subs": "sbc"
-        }
-
-        ld_map: pxt.Map<string> = {
-            "sp": "Y",
-            "r6": "Z"
         }
     }
 }
