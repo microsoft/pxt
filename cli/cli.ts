0e3393496249f2edb9c1b1555e10ff755e92cbaf
Peli de Halleux <pelikhan@users.noreply.github.com>
2016-10-31 22:14:17 -0700

    Updatelocs (#633)
    * fix bug in crowdin download options
    
    * updated language strings

--- cli/cli.ts
+++ cli/cli.ts
@@ -225,7 +225,7 @@ export function execCrowdinAsync(cmd: string, ...args: string[]): Promise<void> 
         case "download": {
             if (!args[1]) throw new Error("output path missing");
             const fn = path.basename(args[0]);
-            return pxt.crowdin.downloadTranslationsAsync(prj, key, args[0], { translatedOnly: true, validatedOnly: true })
+            return pxt.crowdin.downloadTranslationsAsync(prj, key, args[0], { translatedOnly: true, validatedOnly: false })
                 .then(r => {
                     Object.keys(r).forEach(k => {
                         nodeutil.mkdirP(path.join(args[1], k));
