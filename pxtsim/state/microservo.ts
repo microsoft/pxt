8b29948dc3518533e418273e6067b2fad24f2c8a
Peli de Halleux <jhalleux@microsoft.com>
2016-11-07 15:09:30 -0800

    added simulator support for multiple servos

--- pxtsim/state/microservo.ts
+++ pxtsim/state/microservo.ts
@@ -1,5 +1,22 @@
 namespace pxsim {
     export class MicroServoState {
         angle: number = 0;
+        physicalAngle: number = 0;
+
+        public setAngle(angle: number) {
+            this.angle = Math.max(0, Math.min(180, angle));
+        }
+    }
+
+    export class MicroServosState {
+        public servos: {
+            [index: number]: MicroServoState;
+        } = {};
+
+        public servoState(pin: number): MicroServoState {
+            let state = this.servos[pin];
+            if (!state) state = this.servos[pin] = new MicroServoState();
+            return state;
+        }
     }
 }
