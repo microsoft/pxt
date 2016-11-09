94d36dc22b1ac3b306f2d5ace49a90cede38a064
Guillaume Jenkins <gujenkin@microsoft.com>
2016-11-09 11:41:17 -0800

    Fix for pxtCoreDir path on npm 3+ (#732)

--- cli/nodeutil.ts
+++ cli/nodeutil.ts
@@ -19,7 +19,23 @@ export var pxtCoreDir: string = path.join(targetDir, "node_modules/pxt-core")
 
 export function setTargetDir(dir: string) {
     targetDir = dir;
-    pxtCoreDir = path.join(targetDir, "node_modules/pxt-core");
+    let newPxtCoreDir = path.join(targetDir, "node_modules", "pxt-core");
+
+    if (!fs.existsSync(newPxtCoreDir)) {
+        // Fix for NPM 3: dependencies are flattened, which means pxt-core is not under [target]/node_modules/pxt-core.
+        // In that case, use the pxt-core that nodeutil.js script is running in. It's in pxt-core/built/nodeutil.js, so
+        // go up once to reach root of pxt-core.
+        newPxtCoreDir = path.join(__dirname, "..");
+
+        let packageJsonFile = path.join(newPxtCoreDir, "package.json");
+        if (!fs.existsSync(packageJsonFile) || JSON.parse(fs.readFileSync(packageJsonFile, "utf8")).name !== "pxt-core") {
+            newPxtCoreDir = null;
+        }
+    }
+
+    if (!!newPxtCoreDir && newPxtCoreDir !== pxtCoreDir) {
+        pxtCoreDir = newPxtCoreDir;
+    }
 }
 
 export function readResAsync(g: events.EventEmitter) {
