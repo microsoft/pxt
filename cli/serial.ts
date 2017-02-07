e2a324912db9571e1b1fee10568aef313f13f0b8
Michal Moskal <michal@moskal.me>
2017-02-07 16:41:27 +0000

    Add reset after flash

--- cli/serial.ts
+++ cli/serial.ts
@@ -106,17 +106,20 @@ class Serial {
     }
 }
 
-let samd21flash = [
-    0xb5f02180, 0x68184b1a, 0x681c4b1a, 0x685a4b1a, 0x605a430a, 0x3c014a19,
-    0x7d1dd329, 0x07ed4916, 0x2520d5fb, 0x830d35ff, 0x61cd0845, 0x800d4d14,
-    0x07c97d19, 0x4913d5fc, 0x468c0005, 0x37ff1c57, 0x80194911, 0x07c97d19,
-    0x2100d5fc, 0x506e5856, 0x29403104, 0x4661d1fa, 0x35403240, 0x7d198019,
-    0xd5fc07c9, 0xd1eb4297, 0x30ff3001, 0xbdf0e7d3, 0x20006000, 0x20006004,
-    0x41004000, 0x20006008, 0xffffa502, 0xffffa504, 0xffffa544,
-    // code ends
-    0x20007ff0, // stack
-    0x20008000 - 512 + 1 // start address (+1 for Thumb)
-]
+const samd21 = {
+    flash: [
+        0xb5f02180, 0x68184b1a, 0x681c4b1a, 0x685a4b1a, 0x605a430a, 0x3c014a19,
+        0x7d1dd329, 0x07ed4916, 0x2520d5fb, 0x830d35ff, 0x61cd0845, 0x800d4d14,
+        0x07c97d19, 0x4913d5fc, 0x468c0005, 0x37ff1c57, 0x80194911, 0x07c97d19,
+        0x2100d5fc, 0x506e5856, 0x29403104, 0x4661d1fa, 0x35403240, 0x7d198019,
+        0xd5fc07c9, 0xd1eb4297, 0x30ff3001, 0xbdf0e7d3, 0x20006000, 0x20006004,
+        0x41004000, 0x20006008, 0xffffa502, 0xffffa504, 0xffffa544,
+    ],
+    reset: [
+        0x4b064a05, 0xf3bf601a, 0x4b054a05, 0xf3bf60da, 0x46c0e7fe, 0xf02669ef,
+        0x20007ffc, 0x05fa0004, 0xe000ed00,
+    ]
+}
 
 function sambaCmd(ch: string, addr: number, len?: number) {
     let r = ch + addr.toString(16)
@@ -130,7 +133,6 @@ export function flashSerialAsync(c: commandParser.ParsedCommand) {
 
     let f = fs.readFileSync(c.arguments[0])
     let blocks = pxtc.UF2.parseFile(f as any)
-    let goCmd = ""
     let s: Serial
 
     let writeMemAsync = (addr: number, buf: Buffer) =>
@@ -141,39 +143,53 @@ export function flashSerialAsync(c: commandParser.ParsedCommand) {
         s.writeAsync(sambaCmd("R", 0, 4))
             .then(() => s.readBlockingAsync(4))
 
+    let currApplet: number[] = null
+    let goCmd = ""
+
+    let saveAppletAsync = (appl: number[]) => {
+        if (currApplet == appl) return Promise.resolve()
+        currApplet = appl
+        let writeBuf = new Buffer(appl.length * 4 + 8)
+        for (let i = 0; i < appl.length; i++)
+            pxt.HF2.write32(writeBuf, i * 4, appl[i])
+        let code = 0x20008000 - 512
+        pxt.HF2.write32(writeBuf, appl.length * 4, 0x20007ff0) // stack
+        pxt.HF2.write32(writeBuf, appl.length * 4 + 4, code + 1) // start addr (+1 for Thumb)
+        goCmd = sambaCmd("G", code + writeBuf.length - 8)
+        return writeMemAsync(code, writeBuf)
+    }
+
+    let runAppletAsync = (appl: number[]) =>
+        saveAppletAsync(appl)
+            .then(() => s.writeAsync(goCmd))
+
     let writeBlockAsync = (b: pxtc.UF2.Block) => {
         let hd = new Buffer(8)
         pxt.HF2.write32(hd, 0, b.targetAddr)
         pxt.HF2.write32(hd, 4, 1)
         return writeMemAsync(0x20006000, Buffer.concat([hd, b.data as any]))
-            .then(() => s.writeAsync(goCmd))
+            .then(() => runAppletAsync(samd21.flash))
             .then(pingAsync)
-            .then(() => {
-                // console.log("written at " + b.targetAddr)
-            })
+    }
+
+    let readWordsAsync = (addr: number, len: number) => {
+        return s.writeAsync(sambaCmd("R", addr, len * 4))
+            .then(() => s.readBlockingAsync(len * 4))
     }
 
     return listAsync()
         .then(ports => {
             let p = ports.filter(p => /Arduino|Adafruit/i.test(p.manufacturer))[0]
             s = new Serial(p)
-            return pxt.HF2.onlyChangedBlocksAsync(blocks, (addr, len) => {
-                return s.writeAsync(sambaCmd("R", addr, len * 4))
-                    .then(() => s.readBlockingAsync(len * 4))
-            })
+            return pxt.HF2.onlyChangedBlocksAsync(blocks, readWordsAsync)
                 .then(lessBlocks => {
                     console.log(`flash ${blocks.length} pages -> ${lessBlocks.length} pages`)
-                    let writeBuf = new Buffer(samd21flash.length * 4)
-                    for (let i = 0; i < samd21flash.length; i++)
-                        pxt.HF2.write32(writeBuf, i * 4, samd21flash[i])
-                    let code = samd21flash[samd21flash.length - 1] - 1
-                    goCmd = sambaCmd("G", code + writeBuf.length - 8)
-                    return writeMemAsync(code, writeBuf)
-                        .then(() => Promise.mapSeries(lessBlocks, writeBlockAsync))
+                    return Promise.mapSeries(lessBlocks, writeBlockAsync)
                         .then(() => {
-                            console.log("all done")
-                            s.close()
+                            console.log("all done; resetting...")
+                            return runAppletAsync(samd21.reset)
                         })
+                        .then(() => s.close())
                 })
         })
 }
