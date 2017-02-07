cb63a69419d387571e1d9e90bd4b2b85522ee083
Shigeru KANEMOTO <sgk@switch-science.com>
2017-02-07 23:57:01 +0900

    Fix double slash

--- pxteditor/monaco.ts
+++ pxteditor/monaco.ts
@@ -132,7 +132,7 @@ namespace pxt.vs {
             }
 
             let onGotAmdLoader = () => {
-                (window as any).require.config({ paths: { 'vs': pxt.webConfig.pxtCdnUrl + '/vs' }});
+                (window as any).require.config({ paths: { 'vs': pxt.webConfig.pxtCdnUrl + 'vs' }});
 
                 // Load monaco
                 (window as any).require(['vs/editor/editor.main'], () => {
@@ -146,7 +146,7 @@ namespace pxt.vs {
             if (!(<any>window).require) {
                 let loaderScript = document.createElement('script');
                 loaderScript.type = 'text/javascript';
-                loaderScript.src = pxt.webConfig.pxtCdnUrl + '/vs/loader.js';
+                loaderScript.src = pxt.webConfig.pxtCdnUrl + 'vs/loader.js';
                 loaderScript.addEventListener('load', onGotAmdLoader);
                 document.body.appendChild(loaderScript);
             } else {
