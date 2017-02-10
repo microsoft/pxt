a3430a353d8fb2d645399aa83387d44fc730358f
Peli de Halleux <jhalleux@microsoft.com>
2017-02-10 11:38:15 -0800

    adding flag to support IDE layout

--- pxteditor/shell.ts
+++ pxteditor/shell.ts
@@ -13,16 +13,22 @@ namespace pxt.shell {
         const sandbox = /sandbox=1|#sandbox|#sandboxproject/i.test(window.location.href)
             // in iframe
             || pxt.BrowserUtils.isIFrame();
-
         const nosandbox = /nosandbox=1/i.test(window.location.href);
-        if (nosandbox) {
-            layoutType = EditorLayoutType.Widget;
-        } else if (sandbox) {
-            layoutType = EditorLayoutType.Sandbox;
-        } else {
-            layoutType = EditorLayoutType.IDE;
-        }
+        const layout = /editorlayout=(widget|sandbox|ide)/i.exec(window.location.href);
 
+        layoutType = EditorLayoutType.IDE;
+        if (nosandbox)
+            layoutType = EditorLayoutType.Widget;
+        else if (sandbox)
+            layoutType = EditorLayoutType.Sandbox;
+
+        if (layout) {
+            switch (layout[1].toLowerCase()) {
+                case "widget": layoutType = EditorLayoutType.Widget; break;
+                case "sandbox": layoutType = EditorLayoutType.Sandbox; break;
+                case "ide": layoutType = EditorLayoutType.IDE; break;
+            }
+        }
         pxt.debug(`shell: layout type ${EditorLayoutType[layoutType]}, readonly ${isReadOnly()}`);
     }
 
