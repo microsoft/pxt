bb4666bc899a3a160548ef3d855831902c57a000
Guillaume Jenkins <gujenkin@microsoft.com>
2017-02-10 14:00:07 -0800

    Add Create text with block

---
+++ tests/decompile-test/block_text.ts
@@ -0,0 +1,8 @@
+"abc"
+let a = "abc" + 12 + false
+let b = "abcdef".charAt(2)
+let c = "hello"
+let d = c == "goodbye"
+let e = c.compare("goodbye")
+let f = "abcdef".substr(2, 2)
+//let g = parseInt("42")
