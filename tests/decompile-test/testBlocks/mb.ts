efd6157644c756436e93ba8ad27387393e4a3cae
Richard Knoll <riknoll@microsoft.com>
2017-02-27 11:25:52 -0800

    Decompiler should handle property access expressions that aren't enums

--- tests/decompile-test/testBlocks/mb.ts
+++ tests/decompile-test/testBlocks/mb.ts
@@ -141,4 +141,15 @@ declare namespace basic {
     //% block="show leds" icon="\uf00a"
     //% parts="ledmatrix" interval.defl=400 shim=basic::showLeds
     export function showLeds(leds: string, interval?: number): void;
+
+    /**
+     * Scroll a number on the screen. If the number fits on the screen (i.e. is a single digit), do not scroll.
+     * @param interval speed of scroll; eg: 150, 100, 200, -100
+     */
+    //% help=basic/show-number
+    //% weight=96
+    //% blockId=device_show_number block="show|number %number" blockGap=8
+    //% async
+    //% parts="ledmatrix" interval.defl=150 shim=basic::showNumber
+    export function showNumber(value: number, interval?: number): void;
 }
