249954d4cbf93f090dd9d910867cfaabdbf53c9a
Peli de Halleux <jhalleux@microsoft.com>
2017-03-08 15:06:29 -0800

    support for importing scripts for legacy web site

--- pxteditor/workspace.ts
+++ pxteditor/workspace.ts
@@ -41,5 +41,6 @@ namespace pxt.workspace {
         resetAsync(): Promise<void>;
         // optional screenshot support
         saveScreenshotAsync?: (h: Header, screenshot: string, icon: string) => Promise<void>;
+        importLegacyScriptsAsync?: () => Promise<void>;
     }
 }
