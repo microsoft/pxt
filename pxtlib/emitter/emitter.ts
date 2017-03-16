066cbf5e212e8b057db935eeff39680215bf80d1
Tom Ball <tball@microsoft.com>
2017-03-15 22:46:00 -0700

    revert

--- pxtlib/emitter/emitter.ts
+++ pxtlib/emitter/emitter.ts
@@ -1056,9 +1056,7 @@ namespace ts.pxtc {
             assert(inf.isUsed)
             if (inf.vtable)
                 return inf.vtable
-            let tbl = inf.baseClassInfo ? getVTable(inf.baseClassInfo) : []
-            // make a copy
-            tbl = tbl.length==0 ? [] : tbl.slice(0)
+            let tbl = inf.baseClassInfo ? getVTable(inf.baseClassInfo).slice(0) : []
 
             scope(() => {
                 U.pushRange(typeBindings, inf.bindings)
