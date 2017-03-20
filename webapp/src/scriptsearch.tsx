35ec57ae377d26037963654a1bd553a822a020dc
Peli de Halleux <jhalleux@microsoft.com>
2017-03-20 10:37:59 -0700

    allow to enter shared url in add packages

--- webapp/src/scriptsearch.tsx
+++ webapp/src/scriptsearch.tsx
@@ -43,6 +43,20 @@ export class ScriptSearch extends data.Component<ISettingsProps, ScriptSearchSta
         this.setState({ visible: true, searchFor: '' })
     }
 
+    fetchUrlData(): Cloud.JsonScript[] {
+        if (!this.state.searchFor) return [];
+        let scriptid = pxt.Cloud.parseScriptId(this.state.searchFor)
+        if (scriptid) {
+            let res = this.getData(`cloud-search:${scriptid}`)
+            if (res) {
+                if (res.statusCode !== 404) {
+                    this.prevUrlData = [res];
+                }
+            }
+        }
+        return this.prevUrlData;
+    }
+
     fetchGhData(): pxt.github.GitRepo[] {
         const cloud = pxt.appTarget.cloud || {};
         if (!cloud.packages) return [];
@@ -71,22 +85,19 @@ export class ScriptSearch extends data.Component<ISettingsProps, ScriptSearchSta
     renderCore() {
         const bundles = this.fetchBundled();
         const ghdata = this.fetchGhData();
+        const urldata = this.fetchUrlData();
 
-        const chgHeader = (hdr: pxt.workspace.Header) => {
-            pxt.tickEvent("projects.header");
+        const addUrl = (scr: pxt.Cloud.JsonScript) => {
             this.hide();
-            this.props.parent.loadHeaderAsync(hdr)
+            let p = pkg.mainEditorPkg();
+            return p.addDepAsync(scr.name, scr.id)
+                .then(() => this.props.parent.reloadHeaderAsync());
         }
-        const chgBundle = (scr: pxt.PackageConfig) => {
+        const addBundle = (scr: pxt.PackageConfig) => {
             pxt.tickEvent("packages.bundled", { name: scr.name });
             this.hide();
             addDepIfNoConflict(scr, "*")
                 .done();
-        }
-        const chgGallery = (scr: pxt.CodeCard) => {
-            pxt.tickEvent("projects.gallery", { name: scr.name });
-            this.hide();
-            this.props.parent.newEmptyProject(scr.name.toLowerCase(), scr.url);
         }
         const upd = (v: any) => {
             let str = (ReactDOM.findDOMNode(this.refs["searchInput"]) as HTMLInputElement).value
@@ -163,8 +174,7 @@ export class ScriptSearch extends data.Component<ISettingsProps, ScriptSearchSta
         }
         const isEmpty = () => {
             if (this.state.searchFor) {
-                if (bundles.length > 0
-                    || ghdata.length > 0)
+                if (bundles.length || ghdata.length || urldata.length)
                     return false;
                 return true;
             }
@@ -187,13 +197,23 @@ export class ScriptSearch extends data.Component<ISettingsProps, ScriptSearchSta
                         </div>
                     </div>
                     <div className="ui cards">
+                        {urldata.map(scr =>
+                            <codecard.CodeCardView
+                                key={'url' + scr.id}
+                                name={scr.name}
+                                description={scr.description}
+                                url={"/" + scr.id}
+                                onClick={() => addUrl(scr) }
+                                color="red"
+                                />
+                        ) }
                         {bundles.map(scr =>
                             <codecard.CodeCardView
                                 key={'bundled' + scr.name}
                                 name={scr.name}
                                 description={scr.description}
                                 url={"/" + scr.installedVersion}
-                                onClick={() => chgBundle(scr) }
+                                onClick={() => addBundle(scr) }
                                 />
                         ) }
                         {ghdata.filter(repo => repo.status == pxt.github.GitRepoStatus.Approved).map(scr =>
