2bd2890f4ad763dd054a28e169846f0f12936055
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-04-13 07:52:27 -0700

    support for #projects:... route (#1896)

--- webapp/src/projects.tsx
+++ webapp/src/projects.tsx
@@ -42,8 +42,10 @@ export class Projects extends data.Component<ISettingsProps, ProjectsState> {
         this.setState({ visible: false });
     }
 
-    showOpenProject() {
-        this.setState({ visible: true, tab: MYSTUFF })
+    showOpenProject(tab?: string) {
+        const gals = pxt.appTarget.appTheme.galleries || {};
+        tab = (!tab || !gals[tab]) ? MYSTUFF : tab;
+        this.setState({ visible: true, tab: tab || MYSTUFF })
     }
 
     showOpenTutorials() {
