02a95bb5bb1c68fad39aa1b6abd2f5cec3620ea6
Sam El-Husseini <samelh@microsoft.com>
2017-05-02 08:28:41 -0700

    Tick event

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1956,6 +1956,7 @@ function initTheme() {
     if (pxt.appTarget.experiments) {
         Object.keys(pxt.appTarget.experiments).forEach((key) => {
             if (pxt.shell.inExperiment(key)) {
+                pxt.tickEvent(`app.experiment.${key}`)
                 Util.jsonMergeFrom(theme, pxt.appTarget.experiments[key].appTheme);
             }
         })
