cff052b771472f4cc2b75eba82eede67599b53a1
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2017-06-25 17:36:33 -0700

    Better search interactions (#2351)
    * New search interactions: 300ms debounce, no Jquery, show flyout on click / focus, intercept blockly toolbox key presses for valid characters to switch focus to search input. Fixes #2071
    
    * comment
    
    * update d.ts
    
    * Deal with advanced and switching between blocks and Javascript
    
    * Fix build

--- localtypings/blockly.d.ts
+++ localtypings/blockly.d.ts
@@ -802,6 +802,7 @@ declare namespace Blockly {
             setExpanded(expanded: boolean): void;
             toggle(): void;
             updateRow(): void;
+            onKeyDown(e: any): void;
         }
 
         class TreeControl {
