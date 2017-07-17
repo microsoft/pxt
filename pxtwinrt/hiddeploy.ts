b5f059f32cc1b97de64e5c70216084fd1fe371f4
guillaumejenkins <gujenkin@microsoft.com>
2017-07-17 09:28:47 -0700

    WinRT now supports both 1.22 and 1.23 UF2 bootloader versions simultaneously

--- pxtwinrt/hiddeploy.ts
+++ pxtwinrt/hiddeploy.ts
@@ -2,8 +2,12 @@
 /// <reference path="./winrtrefs.d.ts"/>
 /// <reference path="../built/pxtlib.d.ts"/>
 
+type WHID = Windows.Devices.HumanInterfaceDevice.HidDevice;
+
 namespace pxt.winrt {
     class WindowsRuntimeIO implements pxt.HF2.PacketIO {
+        static uf2Selectors: string[];
+
         onData = (v: Uint8Array) => { };
         onEvent = (v: Uint8Array) => { };
         onError = (e: Error) => { };
@@ -49,43 +53,77 @@ namespace pxt.winrt {
 
         initAsync(): Promise<void> {
             Util.assert(!this.dev, "HID interface not properly reseted");
-            const selector = Windows.Devices.HumanInterfaceDevice.HidDevice.getDeviceSelector(0xFF97, 0x0001);
-            return pxt.winrt.promisify(Windows.Devices.Enumeration.DeviceInformation.findAllAsync(selector, null)
-                .then(devices => {
-                    pxt.debug(`hid enumerate ${devices.length} devices`)
-                    const device = devices[0];
-                    if (device) {
-                        pxt.debug(`hid connect to ${device.name} (${device.id})`);
-                        return Windows.Devices.HumanInterfaceDevice.HidDevice.fromIdAsync(device.id, Windows.Storage.FileAccessMode.readWrite)
-                            .then((r: Windows.Devices.HumanInterfaceDevice.HidDevice) => {
-                                this.dev = r;
-                                if (this.dev) {
-                                    pxt.debug(`hid device version ${this.dev.version}`);
-                                    this.dev.addEventListener("inputreportreceived", (e) => {
-                                        pxt.debug(`input report`)
-                                        const dr = Windows.Storage.Streams.DataReader.fromBuffer(e.report.data);
-                                        const values: number[] = [];
-                                        while (dr.unconsumedBufferLength) {
-                                            values.push(dr.readByte());
-                                        }
-                                        if (values.length == 65 && values[0] === 0) {
-                                            values.shift()
-                                        }
-                                        this.onData(new Uint8Array(values));
-                                    });
-                                    return Promise.resolve();
-                                }
-                                pxt.debug("no hid device found");
-                                return Promise.reject(new Error("no hid device found"));
-                            });
+            const wd = Windows.Devices;
+            const whid = wd.HumanInterfaceDevice.HidDevice;
+            if (!WindowsRuntimeIO.uf2Selectors) {
+                this.initUf2Selectors();
+            }
+            const getDevicesPromise = WindowsRuntimeIO.uf2Selectors.reduce((soFar, currentSelector) => {
+                // Try all selectors, in order, until some devices are found
+                return soFar.then((devices) => {
+                    if (devices && devices.length) {
+                        return Promise.resolve(devices);
                     }
-                    return Promise.reject(new Error("no hid device found"));
-                }))
+                    return wd.Enumeration.DeviceInformation.findAllAsync(currentSelector, null);
+                });
+            }, Promise.resolve<Windows.Devices.Enumeration.DeviceInformationCollection>(null));
+
+            return getDevicesPromise
+                .then((devices) => {
+                    if (!devices || !devices[0]) {
+                        pxt.debug("no hid device found");
+                        return Promise.reject(new Error("no hid device found"));
+                    }
+                    pxt.debug(`hid enumerate ${devices.length} devices`);
+                    const device = devices[0];
+                    pxt.debug(`hid connect to ${device.name} (${device.id})`);
+                    return whid.fromIdAsync(device.id, Windows.Storage.FileAccessMode.readWrite);
+                })
+                .then((r: WHID) => {
+                    this.dev = r;
+                    if (!this.dev) {
+                        pxt.debug("can't connect to hid device");
+                        return Promise.reject(new Error("can't connect to hid device"));
+                    }
+                    pxt.debug(`hid device version ${this.dev.version}`);
+                    this.dev.addEventListener("inputreportreceived", (e) => {
+                        pxt.debug(`input report`)
+                        const dr = Windows.Storage.Streams.DataReader.fromBuffer(e.report.data);
+                        const values: number[] = [];
+                        while (dr.unconsumedBufferLength) {
+                            values.push(dr.readByte());
+                        }
+                        if (values.length == 65 && values[0] === 0) {
+                            values.shift()
+                        }
+                        this.onData(new Uint8Array(values));
+                    });
+                    return Promise.resolve();
+                })
                 .catch((e) => {
                     const err = new Error(U.lf("Device not found"));
                     (<any>err).notifyUser = true;
                     return Promise.reject(err);
                 });
+        }
+
+        private initUf2Selectors() {
+            const whid = Windows.Devices.HumanInterfaceDevice.HidDevice;
+            // Selectors common to all UF2 bootloaders
+            WindowsRuntimeIO.uf2Selectors = [
+                // UF2 Bootloader v1.23.0
+                whid.getDeviceSelector(0xFF97, 0x0001, 0x239A, 0x0018),
+                // UF2 Bootloader v1.22.0
+                whid.getDeviceSelector(0xFF00, 0x0001, 0x239A, 0x0019)
+            ];
+
+            // Target-specified selectors (for User Space)
+            if (pxt.appTarget && pxt.appTarget.compile && pxt.appTarget.compile.hidSelectors) {
+                pxt.appTarget.compile.hidSelectors.forEach((s) => {
+                    const sel = whid.getDeviceSelector(parseInt(s.usagePage), parseInt(s.usageId), parseInt(s.vid), parseInt(s.pid));
+                    WindowsRuntimeIO.uf2Selectors.push(sel);
+                });
+            }
         }
     }
 
