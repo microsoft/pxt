4fbbf2a97d0a2452814856e85b8e115b906d20ef
Guillaume Jenkins <gujenkin@microsoft.com>
2017-07-18 07:17:05 -0700

    WinRT now supports both 1.22 and 1.23 UF2 bootloader versions simulta… (#2469)
    * WinRT now supports both 1.22 and 1.23 UF2 bootloader versions simultaneously
    
    * Remove adafruit-specific HID selectors

--- localtypings/pxtarget.d.ts
+++ localtypings/pxtarget.d.ts
@@ -255,6 +255,7 @@ declare namespace ts.pxtc {
         openocdScript?: string;
         flashChecksumAddr?: number;
         onStartText?: boolean;
+        hidSelectors?: HidSelector[];
     }
 
     interface CompileOptions {
@@ -304,5 +305,13 @@ declare namespace ts.pxtc {
 
     interface HexInfo {
         hex: string[];
+    }
+
+    // HEX values as strings, e.g. "0xFF97"
+    interface HidSelector {
+        vid: string;
+        pid: string;
+        usagePage: string;
+        usageId: string;
     }
 }
