e3e596b7fcd9e2f54fe900b06eec066b4c3dc178
Majeed Kazemitabaar <majeed@umd.edu>
2017-07-19 19:46:59 -0700

    added the realtime webcam for recording the gestures while performing them (as a ground truth)

---
+++ webapp/src/gesture-toolbox/webcam.ts
@@ -0,0 +1,37 @@
+const MediaStreamRecorder = require("msr");
+
+export let nav = navigator as any;
+export let mediaStream: any;
+export let mediaRecorder: any;
+
+export function init(webcam_id: string) {
+    nav.getUserMedia  = nav.getUserMedia || nav.webkitGetUserMedia ||
+                        nav.mozGetUserMedia || nav.msGetUserMedia;
+
+    if (nav.getUserMedia) {
+        nav.getUserMedia({audio: false, video: true},
+            (stream: any) => {
+                let video = document.getElementById(webcam_id) as any;
+                video.autoplay = true;
+                video.src = window.URL.createObjectURL(stream);
+                mediaStream = stream;
+
+                mediaRecorder = new MediaStreamRecorder(stream);
+                mediaRecorder.mimeType = 'video/mp4';
+            }, () => {
+                console.error('unable to initialize webcam');
+            });
+    }
+}
+
+
+// start
+// mediaRecorder.start(60 * 1000);
+
+// stop
+// mediaRecorder.stop();
+
+// onrecord
+// mediaRecorder.ondataavailable = function (blob: any) {
+//     let vid = window.URL.createObjectURL(blob);
+// };
