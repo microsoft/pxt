489c3f9a4bb994ce1fb5234463f80294fb660ced
Majeed Kazemitabaar <majeed@cs.umd.edu>
2017-07-31 12:22:27 -0700

    added the multi-gesture block that worked

--- webapp/src/gesture-toolbox/gesture-block.ts
+++ webapp/src/gesture-toolbox/gesture-block.ts
@@ -56,6 +56,8 @@ namespace Gesture {
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -278,3 +280,295 @@ class SpringAlgorithm {
     }
 }
 `;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+export let multiGesture_v1 = `
+enum GestureName {
+    //% block="Gesture1"
+    Gesture1 = 1,
+    //% block="Gesture2"
+    Gesture2 = 2,
+}
+
+/**
+ * Gesture blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace Gesture {
+    let MY_EVENT_SRC = 12345;
+    let is_initialized = false;
+
+    //% blockId=input_on_gesture_recognized block="on gesture %condition"
+    export function onGesture(condition: GestureName, handler: () => void): void {
+        if (is_initialized == false) {
+            // initialize fiber for gesture1 that will control.raiseEvent(MY_EVENT_SRC, 1);
+
+            control.runInBackground(() => {
+                let circleThreshold = 174508;
+                let circleAvgLength = 25;
+
+                let circlePrototype = [
+                    new Vector(922, 224, -464),
+                    new Vector(880, 246, -547),
+                    new Vector(954, 301, -710),
+                    new Vector(1072, 368, -560),
+                    new Vector(1120, 416, -586),
+                    new Vector(1331, 499, -400),
+                    new Vector(1248, 490, -58),
+                    new Vector(1430, 470, 416),
+                    new Vector(1379, 378, 602),
+                    new Vector(1235, 250, 723),
+                    new Vector(989, 134, 662),
+                    new Vector(944, 128, 624),
+                    new Vector(773, 66, 569),
+                    new Vector(550, -74, 144),
+                    new Vector(498, -171, 128),
+                    new Vector(400, -205, -80),
+                    new Vector(346, -250, -173),
+                    new Vector(320, -221, -339),
+                    new Vector(403, -195, -362),
+                    new Vector(589, -67, -614),
+                    new Vector(739, -19, -736),
+                    new Vector(1142, 166, -867),
+                    new Vector(1421, 285, -797),
+                    new Vector(1477, 448, -499),
+                    new Vector(1421, 451, -128),
+                    new Vector(1267, 490, 58),
+                    new Vector(1144, 365, 421),
+                    new Vector(1114, 262, 541),
+                    new Vector(973, 243, 221),
+                    new Vector(941, 240, 176),
+                    new Vector(986, 250, 163)
+                ];
+
+                // initialize first core
+                let circleCore = new SpringAlgorithm(circlePrototype, circleThreshold, circleAvgLength, EuclideanDistanceFast);
+
+                // initialize second core
+                let crossThreshold = 20447;
+                let crossAvgLength = 18;
+
+                let crossPrototype = [
+                    new Vector(794, -109, -698),
+                    new Vector(736, -125, -755),
+                    new Vector(774, 70, -1142),
+                    new Vector(1331, 630, -1126),
+                    new Vector(1853, 998, 1238),
+                    new Vector(1904, 586, 1510),
+                    new Vector(1770, 390, 1779),
+                    new Vector(1808, 870, 128),
+                    new Vector(1254, 848, -1091),
+                    new Vector(272, 413, -774),
+                    new Vector(-570, -182, -432),
+                    new Vector(-1018, -582, -67),
+                    new Vector(-1181, -522, 19),
+                    new Vector(-1514, 38, -32),
+                    new Vector(-1501, 1725, -448),
+                    new Vector(1853, 2032, -8),
+                    new Vector(2048, 1941, -389),
+                    new Vector(1216, 973, 189),
+                    new Vector(1488, 755, -182),
+                    new Vector(733, 586, 22),
+                    new Vector(1050, 570, -138)
+                ];
+
+                let crossCore = new SpringAlgorithm(crossPrototype, crossThreshold, crossAvgLength, EuclideanDistanceFast);
+
+                while (true) {
+                    let x = input.acceleration(Dimension.X);
+                    let y = input.acceleration(Dimension.Y);
+                    let z = input.acceleration(Dimension.Z);
+
+                    let xt = new Vector(x, y, z);
+
+                    if (circleCore.Feed(xt) == 1)
+                        control.raiseEvent(MY_EVENT_SRC, 1);
+                    else if (crossCore.Feed(xt) == 1)
+                        control.raiseEvent(MY_EVENT_SRC, 2);
+
+                    loops.pause(40);
+                }
+            });
+
+            is_initialized = true;
+        }
+
+        control.onEvent(MY_EVENT_SRC, condition, handler);
+    }
+}
+
+function EuclideanDistanceFast(a: Vector, b: Vector): number {
+    // L2 Norm:
+    return IntegerSqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z));
+}
+
+function IntegerSqrt(n: number) {
+    if (n < 0) return -1;
+
+    let shift = 2;
+    let nShifted = n >> shift;
+
+    while (nShifted != 0 && nShifted != n) {
+        shift += 2;
+        nShifted = n >> shift;
+    }
+
+    shift -= 2;
+
+    let result = 0;
+
+    while (shift >= 0) {
+        result = result << 1;
+        let candidateResult = result + 1;
+
+        if (candidateResult * candidateResult <= n >> shift)
+            result = candidateResult;
+
+        shift -= 2;
+    }
+
+    return result;
+}
+
+class Vector {
+    public X: number;
+    public Y: number;
+    public Z: number;
+
+    constructor(x: number, y: number, z: number) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+}
+
+class SpringAlgorithm {
+    private distFunction: (a: Vector, b: Vector) => number;
+
+    private Y: Vector[];
+    private eps: number;
+    private M: number;
+
+    private minLen: number;
+    private maxLen: number;
+
+    private s: number[];
+    private d: number[];
+    private s2: number[];
+    private d2: number[];
+
+    private dmin: number;
+
+    private t: number;
+    private te: number;
+    private ts: number;
+
+    private report: (dmin: number, ts: number, te: number) => void;
+
+
+    constructor(_input: Vector[], _epsilon: number, avgLen: number, _distFun: (a: Vector, b: Vector) => number) {
+        this.Y = _input;
+        this.eps = _epsilon;
+        this.distFunction = _distFun;
+
+        this.M = _input.length;
+
+        this.minLen = Math.idiv(Math.imul(avgLen, 7), 10);
+        this.maxLen = Math.idiv(Math.imul(avgLen, 13), 10);
+
+        this.d = [];
+        this.s = [];
+        this.d2 = [];
+        this.s2 = [];
+
+        for (let i = 0; i < this.M + 1; i++) {
+            this.d.push(0);
+            this.s.push(0);
+            this.d2.push(0);
+            this.s2.push(0);
+        }
+
+        for (let i = 1; i <= this.M; i++) {
+            this.d[i] = 1e8;
+            this.s[i] = 0;
+        }
+
+        this.dmin = 1e8;
+        this.t = 0;
+        this.ts = 0;
+        this.te = 0;
+    }
+
+
+    public Feed(xt: Vector) {
+        let predicted = 0;
+
+        let t = this.t + 1;
+        let d: number[] = this.d2;
+        let s: number[] = this.s2;
+
+        d[0] = 0;
+        s[0] = t;
+
+        // update M distances (d[] based on dp[]) and M starting points (s[] based on sp[]):
+        for (let i = 1; i <= this.M; i++) {
+            let dist = this.distFunction(this.Y[i - 1], xt);
+            let di_minus1 = d[i - 1];
+            let dip = this.d[i];
+            let dip_minus1 = this.d[i - 1];
+
+            // compute dbest and use that to compute s[i]
+            if (di_minus1 <= dip && di_minus1 <= dip_minus1) {
+                d[i] = dist + di_minus1;
+                s[i] = s[i - 1];
+            } else if (dip <= di_minus1 && dip <= dip_minus1) {
+                d[i] = dist + dip;
+                s[i] = this.s[i];
+            } else {
+                d[i] = dist + dip_minus1;
+                s[i] = this.s[i - 1];
+            }
+        }
+
+        if (this.dmin <= this.eps) {
+            let matchLength = this.te - this.ts;
+
+            if (matchLength > this.minLen && matchLength < this.maxLen) {
+                let condition = true;
+
+                for (let i = 0; i <= this.M; i++)
+                    if (!(d[i] >= this.dmin || s[i] > this.te))
+                        condition = false;
+
+                if (condition) {
+                    predicted = 1;
+                    this.dmin = 1e8;
+
+                    for (let i = 1; i <= this.M; i++) {
+                        if (s[i] <= this.te) {
+                            d[i] = 1e8;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (d[this.M] <= this.eps && d[this.M] < this.dmin) {
+            this.dmin = d[this.M];
+            this.ts = s[this.M];
+            this.te = t;
+        }
+
+        this.d2 = this.d; this.d = d;
+        this.s2 = this.s; this.s = s;
+        this.t = t;
+
+        return predicted;
+    }
+}
+`
