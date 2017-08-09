5cf009c9519f5d09175f00e08d85f8049eb78c8e
Richard Knoll <riknoll@microsoft.com>
2017-08-09 14:58:20 -0700

    Increasing timeout for Karma tests

--- tests/blocklycompiler-test/test.spec.ts
+++ tests/blocklycompiler-test/test.spec.ts
@@ -190,7 +190,9 @@ function blockTestAsync(name: string) {
         }, err => fail('Compiling blocks failed'));
 }
 
-describe("blockly compiler", () => {
+describe("blockly compiler", function() {
+    this.timeout(3000);
+
     describe("compiling lists", () => {
         it("should handle unambiguously typed list generics", done => {
             blockTestAsync("lists_generics1").then(done, done);
