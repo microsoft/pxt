3c222de079d47cbc32619f2cad01a4a21ee64878
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-08-10 08:38:11 -0700

    fixing merge

--- pxtrunner/runner.ts
+++ pxtrunner/runner.ts
@@ -143,6 +143,7 @@ namespace pxt.runner {
         const mlang = /(live)?lang=([a-z]{2,}(-[A-Z]+)?)/i.exec(window.location.href);
         const lang = mlang ? mlang[2] : (cookieValue && cookieValue[1] || pxt.appTarget.appTheme.defaultLocale || navigator.userLanguage || navigator.language);
         const live = !pxt.appTarget.appTheme.disableLiveTranslations || (mlang && !!mlang[1]);
+        const versions = pxt.appTarget.versions;
 
         patchSemantic();
         const cfg = pxt.webConfig
