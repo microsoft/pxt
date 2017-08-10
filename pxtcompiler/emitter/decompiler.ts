67201d14b41791e570d2b68d2c986302b696ca87
Richard Knoll <riknoll@microsoft.com>
2017-08-10 13:26:42 -0700

    Removing regex accidentally added in merge

--- pxtcompiler/emitter/decompiler.ts
+++ pxtcompiler/emitter/decompiler.ts
@@ -5,9 +5,6 @@ namespace ts.pxtc.decompiler {
     const lowerCaseAlphabetStartCode = 97;
     const lowerCaseAlphabetEndCode = 122;
 
-    const validStringRegex = /^[^\f\n\r\t\v\u00a0\u1680\u180e\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]*$/;
-
-
     const numberType = "math_number";
     const stringType = "text";
     const booleanType = "logic_boolean";
