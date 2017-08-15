de9a60bedd4d2c4aaacfe600628f6d6e1ff4eb49
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2017-08-15 15:18:33 -0700

    Fix regression in adding packages with no namespace color set. Fixes #2716 (#2717)

--- pxtrunner/renderer.ts
+++ pxtrunner/renderer.ts
@@ -276,7 +276,7 @@ namespace pxt.runner {
                 });
                 let nsStyleBuffer = '';
                 Object.keys(res).forEach(ns => {
-                    const color = res[ns];
+                    const color = res[ns] || '#dddddd';
                     nsStyleBuffer += `
                         span.docs.${ns.toLowerCase()} {
                             background-color: ${color} !important;
