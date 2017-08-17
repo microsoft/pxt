148b97640702a53db54405049aa113933c400448
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-08-17 09:55:49 -0700

    Initial work

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1887,6 +1887,17 @@ function initLogin() {
     }
 }
 
+let timeout: number;
+let interval: number;
+function startT() {
+    timeout = window.setTimeout(() => interval = window.setInterval(initSerial, 5000), 5000);
+}
+
+function stopT() {
+    window.clearTimeout(timeout);
+    window.clearInterval(interval);
+}
+
 function initSerial() {
     if (!pxt.appTarget.serial || !pxt.winrt.isWinRT() && (!Cloud.isLocalHost() || !Cloud.localToken))
         return;
@@ -1895,6 +1906,8 @@ function initSerial() {
         hidbridge.initAsync()
             .then(dev => {
                 dev.onSerial = (buf, isErr) => {
+                    stopT();
+                    startT();
                     window.postMessage({
                         type: 'serial',
                         id: 'n/a', // TODO
