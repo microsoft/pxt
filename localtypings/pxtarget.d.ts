9a95680c65ba9244ee3bd50f5f3ddf24b17f9aa6
Richard Knoll <riknoll@users.noreply.github.com>
2017-08-17 13:15:43 -0700

    Adding flag to disable reload on app cache download (#2740)

--- localtypings/pxtarget.d.ts
+++ localtypings/pxtarget.d.ts
@@ -208,6 +208,7 @@ declare namespace pxt {
         blockColors?: Map<string>; // block namespace colors, used for build in categories
         socialOptions?: SocialOptions; // show social icons in share dialog, options like twitter handle and org handle
         useStartPage?: boolean;
+        noReloadOnUpdate?: boolean; // do not notify the user or reload the page when a new app cache is downloaded
     }
 
     interface SocialOptions {
