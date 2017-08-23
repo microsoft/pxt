b8772504546f47bd4dd25f098b2747b3a34bcea4
Sam El-Husseini <samelh@microsoft.com>
2017-08-22 18:07:45 -0700

    Update monaco editor to v0.10.0 and pxt-monaco-typescript to v2.3.3

--- webapp/public/vs/language/json/jsonMode.js
+++ webapp/public/vs/language/json/jsonMode.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-json version: 1.3.1(7b38df12a12ed0bf7117b407584b20b786c0ab7d)
+ * monaco-json version: 1.3.2(63d6dadc9cc5928c83a232dd95cdc31403f08974)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-json/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
