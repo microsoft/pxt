319bd7df4bd17b0e18217e19ff09f6d5e9406fd6
Sam El-Husseini <samelh@microsoft.com>
2017-08-27 10:58:59 -0700

    Fix editor load telemetry to use actual window width and height.

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1884,6 +1884,8 @@ function enableAnalytics() {
         stats["screen.height"] = screen.height;
         stats["screen.availwidth"] = screen.availWidth;
         stats["screen.availheight"] = screen.availHeight;
+        stats["window.innerWidth"] = window.innerWidth;
+        stats["window.innerHeight"] = window.innerHeight;
         stats["screen.devicepixelratio"] = pxt.BrowserUtils.devicePixelRatio();
     }
     pxt.tickEvent("editor.loaded", stats);
