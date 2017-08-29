cf942309f6b0d7d1476faa35260c54c1e3c77746
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-08-29 10:28:15 -0700

    basic data flow

--- webapp/src/serialdata.tsx
+++ webapp/src/serialdata.tsx
@@ -12,12 +12,25 @@ import Util = pxt.Util;
 const lf = Util.lf
 
 export class Editor extends srceditor.Editor {
+    points: canvaschart.Point[] = [];
+
     acceptsFile(file: pkg.File) {
-        if (file.name != "serialdata.json") return false
-        return true
+        return file.name === "serialdata.json"
     }
 
     display() {
+        window.addEventListener("message", (ev: MessageEvent) => {
+            let msg = ev.data;
+            console.log("PINEAPPLE " + msg.type);
+            if (msg.type === "serial") {
+                let data = msg.data.slice(0, msg.data.length - 1);
+                let datasplit = data.split(":");
+                let x = datasplit[0];
+                let y = datasplit[1];
+                this.points.push(new canvaschart.Point(Date.now(), y));
+                new canvaschart.CanvasChart().drawChart(document.getElementById("fizzle") as HTMLCanvasElement, this.points);
+            }
+        });
         return (
             <div className="ui content">
                 <canvas id="fizzle">
@@ -25,57 +38,9 @@ export class Editor extends srceditor.Editor {
             </div>
         )
     }
-        /** 
-        let serialDataWrapper = document.createElement("div");
-        serialDataWrapper.className = "ui content";
-        let chartElement = document.createElement("canvas");
-        **/
-        /**
-        let serialDataWrapper = <div></div>
-        let canvasElement = document.createElement("canvas");
-        canvasElement.id = "foo";
-        return ({canvasElement})
-        **/
-        /** 
-        let p: pxsim.Point[] = [];
-        p.push(new pxsim.Point(1, 2));
-        p.push(new pxsim.Point(3, 4));
-        p.push(new pxsim.Point(5, 6));
-        new canvaschart.CanvasChart().drawChart(canvasElement, p);
-        serialDataWrapper.appendChild(canvasElement);
-        //serialDataWrapper.appendChild(chartElement);
-        return serialDataWrapper;
-        **/
-        /**
-        return (
-            <div className="ui content">
-            {}
-                <div className="ui segment form text" style={{ backgroundColor: "white" }}>
-                    <canvas></canvas>
 
-                    <sui.Input label={lf("Name")} value={c.name} onChange={setFileName}/>
-                    {userConfigs.map(uc =>
-                        <sui.Checkbox
-                            key={`userconfig-${uc.description}`}
-                            inputLabel={uc.description}
-                            checked={isUserConfigActive(uc) }
-                            onChange={() => applyUserConfig(uc) } />
-                    ) }
-                    <sui.Field>
-                        <sui.Button text={lf("Save")} class={`green ${this.isSaving ? 'disabled' : ''}`} onClick={() => save()} />
-                        <sui.Button text={lf("Edit Settings As text") } onClick={() => this.editSettingsText() } />
-                    </sui.Field>
-                </div>
-            </div>
-        )
-        **/
-
+    /** 
     domUpdate() {
-        let p: pxsim.Point[] = [];
-        p.push(new pxsim.Point(1, 2));
-        p.push(new pxsim.Point(3, 4));
-        p.push(new pxsim.Point(5, 6));
-        new canvaschart.CanvasChart().drawChart(document.getElementById("fizzle") as HTMLCanvasElement, p);
-        console.log("here!!!");
     }
+    **/
 }
