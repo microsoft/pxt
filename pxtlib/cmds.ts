43ed4aa5cf9c2c1aa96b510ef49e3ee5d9445677
Michał Moskal <michal@moskal.me>
2017-08-31 18:34:18 +0100

    Bring HID support in line with master (#2842)
    * Initial application of HID changes from master
    
    * Bring HID in line with master
    
    * One more HID import
    
    * Add "prepend": ["files"] in tsconfig.json
    
    * Add pxt.commands.saveOnlyAsync
    
    * Add TODO
    
    * Move UF2 utils to pxtlib
    
    * Allow "talk" command in raw HID mode (assuming one-on-one packets)
    
    * Add faster/simpler SHA-like function running on constant size blocks
    
    * Fix typo
    
    * Linting

--- pxtlib/cmds.ts
+++ pxtlib/cmds.ts
@@ -2,4 +2,5 @@ namespace pxt.commands {
     // overriden by targets
     export let deployCoreAsync: (r: ts.pxtc.CompileResult) => Promise<void> = undefined;
     export let browserDownloadAsync: (text: string, name: string, contentType: string) => Promise<void> = undefined;
+    export let saveOnlyAsync: (r: ts.pxtc.CompileResult) => Promise<void> = undefined;
 }
