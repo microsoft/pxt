94e57c0d8dd3c12fcf28d2336a946ce6ff4070a8
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-09-01 16:39:49 -0700

    more progress

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -930,6 +930,7 @@ export class ProjectView
         if (options.filesOverride)
             Util.jsonCopyFrom(files, options.filesOverride)
         files["pxt.json"] = JSON.stringify(cfg, null, 4) + "\n"
+        //TODO this
         files["serial.json"] = JSON.stringify({"pineapple": "banana"}) + "\n"
         return workspace.installAsync({
             name: cfg.name,
@@ -1224,8 +1225,6 @@ export class ProjectView
     }
 
     clearSerial() {
-//        let logs = this.refs["simLogs"] as logview.LogView;
-//        logs.clear();
         this.serialEditor.clear()
     }
 
--- webapp/src/serial.tsx
+++ webapp/src/serial.tsx
@@ -28,9 +28,12 @@ export class Editor extends srceditor.Editor {
     private shouldScroll = false
     private isSim: boolean = true
     private maxEntries: number = 80
-    private maxLineLength: number = 255
+    private maxLineLength: number = 500
     private maxAccValues: number = 500
     private renderFiberId: number
+    private maxConsoleEntries: number = 20
+
+    private active: boolean = true
 
     acceptsFile(file: pkg.File) {
         // TODO hardcoded string
@@ -43,14 +46,14 @@ export class Editor extends srceditor.Editor {
 
     constructor(public parent: pxt.editor.IProjectView) {
         super(parent)
-        // TODO
+        // TODO this not happening
         this.setLabel(this.isSim ? lf("SIM") : lf("DEV"))
         window.addEventListener("message", this.processMessage.bind(this), false)
     }
 
     processMessage(ev: MessageEvent) {
         let msg = ev.data
-        if (msg.type === "serial") {
+        if (this.active && msg.type === "serial") {
             const smsg = msg as pxsim.SimulatorSerialMessage
             const sim = !!smsg.sim || false
             if (sim == this.isSim) {
@@ -68,154 +71,138 @@ export class Editor extends srceditor.Editor {
     }
 
     appendGraphEntry(source: string, data: string, theme: string, sim: boolean) {
+        //TODO consolidate with other regex - have isGraphable return split data
+        let m = /^\s*(([^:]+):)?\s*(-?\d+)/i.exec(data)
+        let variable = m ? (m[2] || ' ') : undefined
+        let nvalue = m ? parseInt(m[3]) : null
+
+        let last: ILogEntryElement = undefined
+        for (let i = 0; i < this.graphEntries.length; ++i) {
+            let graph = this.graphEntries[i]
+            if (graph.source === source && graph.variable === variable) {
+                last = graph
+                break
+            }
+        }
+
+        if (last) {
+            last.dirty = true
+            last.data = data
+            last.accvalues.push({
+                t: Date.now() - last.time,
+                v: nvalue
+            })
+            if (last.accvalues.length > this.maxAccValues) {
+                last.accvalues.shift()
+            }
+        } else {
+            let graph: ILogEntryElement = {
+                dirty: true,
+                theme: theme,
+                variable: variable,
+                data: data,
+                accvalues: [{t: 0, v: nvalue}],
+                time: Date.now(),
+                source: source
+            }
+            this.graphEntries.push(graph)
+        }
+        this.scheduleRender()
     }
 
     appendConsoleEntry(data: string) {
         for (let i = 0; i < data.length; ++i) {
             let ch = data[i]
+            this.consoleBuffer += ch
             if (ch === "\n" || this.consoleBuffer.length > this.maxLineLength) {
                 let consoleEntry: IConsoleEntry = {
-                    data: this.consoleBuffer
+                    data: this.consoleBuffer,
+                    dirty: true
                 }
                 this.consoleEntries.push(consoleEntry)
                 this.consoleBuffer = ""
-                if (this.consoleEntries.length > this.maxAccValues) {
-                    this.consoleEntries.shift()
+                while (this.consoleEntries.length > this.maxConsoleEntries) {
+                    let po = this.consoleEntries.shift();
+                    if (po.element && po.element.parentElement) po.element.parentElement.removeChild(po.element);
                 }
                 this.scheduleRender()
-            } else {
-                this.consoleBuffer += ch
             }
         }
     }
-    /**
-        switch (msg.type || '') {
-            case 'serial':
-                const smsg = msg as pxsim.SimulatorSerialMessage;
-                const sim = !!smsg.sim || false;
-                if (sim == this.isSim) {
-                    const value = smsg.data || '';
-                    const source = smsg.id || '?';
-                    let theme = source.split('-')[0] || '';
-                    if (!/^[a-z]+$/.test(theme)) theme = 'black';
-                    let buffer = this.serialBuffers[source] || '';
-
-                    if (this.isGraphable(value)) {
-
-                    }
-
-                    for (let i = 0; i < value.length; ++i) {
-                        switch (value.charCodeAt(i)) {
-                            case 10: //'\n'
-                                this.appendEntry(source, buffer, theme, sim);
-                                buffer = '';
-                                break;
-                            case 13: //'\r'
-                                break;
-                            default:
-                                buffer += value[i];
-                                if (buffer.length > (this.maxLineLength || 255)) {
-                                    this.appendEntry(source, buffer, theme, sim);
-                                    buffer = ''
-                                }
-                                break;
-                        }
-                    }
-                    this.serialBuffers[source] = buffer;
-                }
-                break;
-        }
-    }
-    **/
-    /**
-    appendEntry(source: string, value: string, theme: string, sim: boolean) {
-         //source: e.g., "red-1234567"
-         //value: e.g., "x:123"
-         //theme: e.g., "red"
-         //sim: e.g., "false"
-        if (this.labelElement && !this.labelElement.parentElement)
-            this.element.insertBefore(this.labelElement, this.element.firstElementChild);
-
-        let ens = this.graphEntries;
-        while (ens.length > this.maxEntries) {
-            let po = ens.shift();
-            if (po.element && po.element.parentElement) po.element.parentElement.removeChild(po.element);
-        }
-        // find the entry with same source
-        let last: ILogEntryElement = undefined;
-        let m = /^\s*(([^:]+):)?\s*(-?\d+)/i.exec(value);
-        let variable = m ? (m[2] || ' ') : undefined;
-        let nvalue = m ? parseInt(m[3]) : null;
-        for (let i = ens.length - 1; i >= 0; --i) {
-            if (ens[i].source == source &&
-                ((i == ens.length - 1 && ens[i].value == value) ||
-                    (variable && ens[i].variable == variable))) {
-                last = ens[i];
-                break;
-            }
-        }
-
-        if (last) {
-            last.value = value;
-            if (last.accvalues) {
-                last.accvalues.push({
-                    t: Date.now() - last.time,
-                    v: nvalue
-                });
-                if (last.accvalues.length > this.maxAccValues)
-                    last.accvalues.shift();
-            } else if (!last.countElement) {
-                last.countElement = document.createElement("span");
-                last.countElement.className = 'ui log counter';
-                last.element.insertBefore(last.countElement, last.element.firstChild);
-            }
-            last.count++;
-            this.scheduleRender(last);
-        }
-        else {
-            let e: ILogEntryElement = {
-                id: Editor.counter++,
-                theme: theme,
-                time: Date.now(),
-                value: value,
-                source: source,
-                count: 1,
-                dirty: true,
-                variable: variable,
-                accvalues: nvalue != null ? [{ t: 0, v: nvalue }] : undefined,
-                element: document.createElement("div"),
-                valueElement: document.createTextNode('')
-            };
-            e.element.className = "ui log " + e.theme;
-            let raiseTrends = false;
-            if (e.accvalues) {
-                e.accvaluesElement = document.createElement("span");
-                e.accvaluesElement.className = "ui log " + e.theme + " gauge"
-                e.chartElement = document.createElement("canvas");
-                new canvaschart.CanvasChart().drawChart(e.chartElement, [new canvaschart.Point(e.accvalues[0].t, e.accvalues[0].v)])
-                e.element.appendChild(e.accvaluesElement);
-                e.element.appendChild(e.chartElement);
-            }
-            e.element.appendChild(e.valueElement);
-            ens.push(e);
-            this.element.appendChild(e.element);
-            this.scheduleRender(e);
-        }
-    }
-    **/
 
     clear() {
         this.graphEntries = [];
         if (this.labelElement && this.labelElement.parentElement)
-            this.labelElement.parentElement.removeChild(this.labelElement);
-        if (this.element)
-            this.element.innerHTML = "";
-        this.consoleBuffer = "";
+            this.labelElement.parentElement.removeChild(this.labelElement)
+        if (this.consoleElement)
+            this.consoleElement.innerHTML = ""
+        if (this.graphElement)
+            this.graphElement.innerHTML = ""
+        this.consoleEntries.forEach(e => {
+            if (e.element && e.element.parentElement) e.element.parentElement.removeChild(e.element)
+        })
+        this.graphEntries.forEach(e => {
+            if (e.dataElement && e.dataElement.parentElement) e.dataElement.parentElement.removeChild(e.dataElement)
+            if (e.canvasElement && e.canvasElement.parentElement) e.canvasElement.parentElement.removeChild(e.canvasElement)
+            if (e.element && e.element.parentElement) e.element.parentElement.removeChild(e.element)
+        })
+        this.consoleBuffer = ""
     }
 
     scheduleRender() {
-        //e.dirty = true;
         if (!this.renderFiberId) this.renderFiberId = setTimeout(() => this.render(), 50);
+    }
+
+    render() {
+        this.consoleEntries.forEach(e => {
+            if (!e.dirty) return
+            let consoleDiv = document.createElement("div")
+            consoleDiv.className = "coconut"
+            consoleDiv.textContent = e.data
+            e.element = consoleDiv
+            this.consoleElement.appendChild(consoleDiv)
+            e.dirty = false
+        })
+
+        this.graphEntries.forEach(e => {
+            if (!e.dirty) return
+            if (!e.element) {
+                let graphDiv = document.createElement("div")
+                graphDiv.className = "guava"
+                let labelDiv = document.createElement("div")
+                labelDiv.className = "lemon"
+                let canvas = document.createElement("canvas")
+                canvas.className = "coconut"
+                //TODO hacky
+                canvas.setAttribute("width", "1000px")
+                graphDiv.appendChild(labelDiv)
+                graphDiv.appendChild(canvas)
+                e.element = graphDiv
+                e.canvasElement = canvas
+                e.dataElement = labelDiv
+                this.graphElement.appendChild(graphDiv)
+            }
+            //TODO dk about this
+            e.canvasElement.innerHTML = ""
+            new canvaschart.CanvasChart().drawChart(e.canvasElement, e.accvalues.map(accvalue => new canvaschart.Point(accvalue.t, accvalue.v)))
+            e.dirty = false
+        })
+        this.renderFiberId = 0
+    }
+
+    display() {
+        return (
+            <div className="serialEditor" ref={(el) => {this.element = el}}>
+                <sui.Button text={lf("Start")} onClick= {() => this.active = true} />
+                <sui.Button text={lf("Stop")} onClick = {() => this.active = false} />
+                <div className="graphs" ref={(el) => {this.graphElement = el}}></div>
+                <div className="console" ref={(el) => {this.consoleElement = el}}></div>
+            </div>
+        )
+    }
+
+    domUpdate() {
+        this.scheduleRender()
     }
 
     setLabel(text: string, theme?: string) {
@@ -231,105 +218,29 @@ export class Editor extends srceditor.Editor {
             this.labelElement.appendChild(document.createTextNode(text))
         }
     }
-    render() {
-        //TODO handle dirty setting, no longer in scheduleRender
-        /**
-        this.graphEntries.forEach(entry => {
-            if (!entry.dirty) return;
-
-            if (entry.countElement) entry.countElement.innerText = entry.count.toString();
-            if (entry.accvaluesElement) entry.accvaluesElement.innerText = entry.value;
-            if (entry.chartElement) new canvaschart.CanvasChart().drawChart(entry.chartElement, entry.accvalues.map(accvalue => new canvaschart.Point(accvalue.t, accvalue.v)))
-                //TODO valueElement obsolete
-            entry.valueElement.textContent = entry.accvalues ? '' : entry.value;
-            entry.dirty = false;
-        });
-        **/
-        /**
-        let console = (
-            <div className="console" ref={(el) => this.consoleElement = el}>
-                {this.consoleEntries.map(e =>
-                    <div className="consoleEntry" ref={(el) => e.consoleElement = el}>{e.data}</div>
-                )}
-            </div>
-        )
-        **/
-        let console = document.createElement("div")
-        console.className = "console"
-        this.consoleEntries.forEach(e => {
-            let consoleEntry = document.createElement("div")
-            consoleEntry.className = "consoleEntry"
-            consoleEntry.textContent = e.data
-            console.appendChild(consoleEntry)
-        })
-        this.element.appendChild(console)
-        this.renderFiberId = 0;
-    }
-
-    display() {
-        return (
-            <div className="serialEditor" ref={(el) => {this.element = el}}>
-            </div>
-        )
-        /**
-        return (
-            <div className="foo"></div>
-        )
-        **/
-        /**
-        return (
-            <div className="serialEditor" ref={(el) => {this.element = el}}>
-                <div className="graphs">
-                    {this.graphEntries.map(e =>
-                        <div>
-                            <span className="graphLabel" ref={(el) => e.accvaluesElement = el}></span>
-                            <canvas className="graphCanvas" ref = {(el) => e.chartElement = el}></canvas>
-                        </div>
-                    )}
-                </div>
-                <div className="console">
-                    {this.consoleEntries.map(e =>
-                        <div className="consoleEntry" ref={(el) => e.consoleElement = el}></div>
-                    )}
-                </div>
-            </div>
-        )
-        **/
-    }
-
-    domUpdate() {
-        this.scheduleRender()
-    }
 }
-/**
-interface ILogProps {
-    isSim?: boolean
-    maxEntries?: number
-    maxLineLength?: number
-    maxAccValues?: number
-}
-**/
+
 interface ILogEntry {
-    id: number
+    //TODO get rid of some of these fields
+    id?: number
     theme: string
     variable?: string
-    accvalues?: { t: number, v: number }[]
+    accvalues: { t: number, v: number }[]
     time: number
-    value: string
+    data: string
     source: string
-    count: number
+    count?: number
 }
 
 interface ILogEntryElement extends ILogEntry {
     dirty: boolean
     element?: HTMLDivElement
-    accvaluesElement?: HTMLElement
-    countElement?: HTMLSpanElement
-    chartElement?: HTMLCanvasElement
-    valueElement?: Text
+    canvasElement?: HTMLCanvasElement
+    dataElement?: HTMLDivElement
 }
 
 interface IConsoleEntry {
     data: string
-    consoleElement?: HTMLElement
+    dirty: boolean
+    element?: HTMLElement
 }
