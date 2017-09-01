5b0e9e78da9d617c0e269b027a3428adbad0d243
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-09-01 16:39:49 -0700

    more progress

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -893,6 +893,7 @@ export class ProjectView
         if (options.filesOverride)
             Util.jsonCopyFrom(files, options.filesOverride)
         files["pxt.json"] = JSON.stringify(cfg, null, 4) + "\n"
+        //TODO this
         files["serial.json"] = JSON.stringify({"pineapple": "banana"}) + "\n"
         return workspace.installAsync({
             name: cfg.name,
@@ -1187,8 +1188,6 @@ export class ProjectView
     }
 
     clearSerial() {
-//        let logs = this.refs["simLogs"] as logview.LogView;
-//        logs.clear();
         this.serialEditor.clear()
     }
 
