0d97cb7f937a6f65d4d61641d9a849835557cbbe
Richard Knoll <riknoll@microsoft.com>
2017-09-08 08:47:33 -0700

    Fixing merge error

--- pxtblocks/blocklycompiler.ts
+++ pxtblocks/blocklycompiler.ts
@@ -59,12 +59,6 @@ namespace pxt.blocks {
 
     const MAX_COMMENT_LINE_LENGTH = 50;
 
-    const reservedWords = ["break", "case", "catch", "class", "const", "continue", "debugger",
-        "default", "delete", "do", "else", "enum", "export", "extends", "false", "finally",
-        "for", "function", "if", "import", "in", "instanceof", "new", "null", "return",
-        "super", "switch", "this", "throw", "true", "try", "typeof", "var", "void", "while",
-        "with"];
-
     let placeholders: Map<Map<any>> = {};
 
     function stringLit(s: string) {
