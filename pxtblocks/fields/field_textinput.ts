59b03f4fa3de13e7a27878ef2a5e9ec8e6ffc27c
Sam El-Husseini <samelh@microsoft.com>
2017-09-19 16:24:23 -0700

    Final fix

--- pxtblocks/fields/field_textinput.ts
+++ pxtblocks/fields/field_textinput.ts
@@ -5,8 +5,8 @@ namespace pxtblockly {
     export class FieldTextInput extends Blockly.FieldTextInput implements Blockly.FieldCustom {
         public isFieldCustom_ = true;
 
-        constructor(value: string, options: Blockly.FieldCustomOptions, validator?: Function) {
-            super(value, validator);
+        constructor(value: string, options: Blockly.FieldCustomOptions, opt_validator?: () => void) {
+            super(value, opt_validator);
         }
     }
 }
