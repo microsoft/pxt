8f4bf5d642cb16dfcb2678eb94cd34c98dad532b
Sam El-Husseini <samelh@microsoft.com>
2017-09-30 18:54:21 -0400

    Update the color picker to be inside of a dropdown div, and make it easy to configure the colours, and size of the picker

--- pxtblocks/fields/field_colour.ts
+++ pxtblocks/fields/field_colour.ts
@@ -3,21 +3,35 @@
 
 namespace pxtblockly {
 
+    export interface FieldColourNumberOptions extends Blockly.FieldCustomOptions {
+        colours?: string; // parsed as a JSON array
+        columns?: string; // parsed as a number
+        className?: string;
+    }
+
     export class FieldColorNumber extends Blockly.FieldColour implements Blockly.FieldCustom {
         public isFieldCustom_ = true;
 
         private colour_: string;
 
-        constructor(text: string, options: Blockly.FieldCustomOptions, opt_validator?: Function) {
+        private colorPicker_: goog.ui.ColorPicker;
+        private className_: string;
+
+        constructor(text: string, params: FieldColourNumberOptions, opt_validator?: Function) {
             super(text, opt_validator);
+
+            if (params.colours) this.setColours(JSON.parse(params.colours));
+            if (params.columns) this.setColumns(parseInt(params.columns));
+            if (params.className) this.className_ = params.className;
         }
 
         /**
          * Return the current colour.
+         * @param {boolean} opt_asHex optional field if the returned value should be a hex
          * @return {string} Current colour in '#rrggbb' format.
          */
-        getValue() {
-            if (this.colour_.indexOf('#') > -1) {
+        getValue(opt_asHex?: boolean) {
+            if (!opt_asHex && this.colour_.indexOf('#') > -1) {
                 return `0x${this.colour_.replace(/^#/, '')}`;
             }
             return this.colour_;
@@ -40,6 +54,12 @@ namespace pxtblockly {
             if (this.sourceBlock_) {
                 this.sourceBlock_.setColour(colour, colour, colour);
             }
+        }
+
+        showEditor_() {
+            super.showEditor_();
+            if (this.className_ && this.colorPicker_)
+                Blockly.utils.addClass((this.colorPicker_.getElement()), this.className_);
         }
     }
 }
