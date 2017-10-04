1d6708e0bc667e266d5b35e2c7f1553363899f4c
Richard Knoll <riknoll@users.noreply.github.com>
2017-10-04 13:05:09 -0700

    SVG helper for horizontal gradients (#3151)
    * SVG helper for horizontal gradients
    
    * PR Feedback

--- pxtsim/svg.ts
+++ pxtsim/svg.ts
@@ -152,9 +152,9 @@ namespace pxsim.svg {
         });
     }
 
-    export function mkLinearGradient(id: string): SVGLinearGradientElement {
+    export function mkLinearGradient(id: string, horizontal = false): SVGLinearGradientElement {
         let gradient = <SVGLinearGradientElement>svg.elt("linearGradient");
-        svg.hydrate(gradient, { id: id, x1: "0%", y1: "0%", x2: "0%", y2: "100%" });
+        svg.hydrate(gradient, { id: id, x1: "0%", y1: "0%", x2: horizontal ? "100%" : "0%", y2: horizontal ? "0%" : "100%"});
         let stop1 = svg.child(gradient, "stop", { offset: "0%" })
         let stop2 = svg.child(gradient, "stop", { offset: "100%" })
         let stop3 = svg.child(gradient, "stop", { offset: "100%" })
@@ -162,8 +162,8 @@ namespace pxsim.svg {
         return gradient;
     }
 
-    export function linearGradient(defs: SVGDefsElement, id: string): SVGLinearGradientElement {
-        let lg = mkLinearGradient(id);
+    export function linearGradient(defs: SVGDefsElement, id: string, horizontal = false): SVGLinearGradientElement {
+        let lg = mkLinearGradient(id, horizontal);
         defs.appendChild(lg);
         return lg;
     }
