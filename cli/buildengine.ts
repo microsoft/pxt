2341ea0301e8070c19da1e01b7c23794ce70504d
Michal Moskal <michal@moskal.me>
2017-10-06 11:27:12 +0100

    Be smarter about constant extraction

--- cli/buildengine.ts
+++ cli/buildengine.ts
@@ -317,6 +317,22 @@ export function buildDalConst(buildEngine: BuildEngine, mainPkg: pxt.MainPackage
     let vals: Map<string> = {}
     let done: Map<string> = {}
 
+    function expandInt(s: string): number {
+        s = s.trim()
+        let existing = U.lookup(vals, s)
+        if (existing != null && existing != "?")
+            s = existing
+        let m = /^\((\w+)\s*\+\s*(\d+)\)$/.exec(s)
+        if (m) {
+            let k = expandInt(m[1])
+            if (k != null)
+                return k + parseInt(m[2])
+        }
+        let pp = parseCppInt(s)
+        if (pp != null) return pp
+        return null
+    }
+
     function isValidInt(v: string) {
         return /^-?(\d+|0[xX][0-9a-fA-F]+)$/.test(v)
     }
@@ -328,8 +344,9 @@ export function buildDalConst(buildEngine: BuildEngine, mainPkg: pxt.MainPackage
         let inEnum = false
         let enumVal = 0
         let defineVal = (n: string, v: string) => {
-            v = v.trim()
-            if (parseCppInt(v) != null) {
+            let parsed = expandInt(v)
+            if (parsed != null) {
+                v = parsed.toString()
                 let curr = U.lookup(vals, n)
                 if (curr == null || curr == v) {
                     vals[n] = v
@@ -343,8 +360,6 @@ export function buildDalConst(buildEngine: BuildEngine, mainPkg: pxt.MainPackage
                     if (dogenerate && !/^MICROBIT_DISPLAY_(ROW|COLUMN)_COUNT$/.test(n))
                         pxt.log(`${fileName}(${lineNo}): #define conflict, ${n}`)
                 }
-            } else {
-                vals[n] = "?" // just in case there's another more valid entry
             }
         }
         src.split(/\r?\n/).forEach(ln => {
@@ -399,11 +414,16 @@ export function buildDalConst(buildEngine: BuildEngine, mainPkg: pxt.MainPackage
         for (let fn of files) {
             if (U.endsWith(fn, "Config.h")) continue
             if (fn.indexOf("/mbed-classic/") >= 0) continue
+            if (fn.indexOf("/mbed-os/") >= 0) continue
             fc[fn] = fs.readFileSync(fn, "utf8")
         }
         files = Object.keys(fc)
 
         // pre-pass - detect conflicts
+        for (let fn of files) {
+            extractConstants(fn, fc[fn])
+        }
+        // stabilize
         for (let fn of files) {
             extractConstants(fn, fc[fn])
         }
