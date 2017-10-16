3cf232956b1e8d557a4a29cadc37ce9dabdd1b97
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-10-15 22:26:54 -0700

    translate list variable

--- webapp/src/toolbox.ts
+++ webapp/src/toolbox.ts
@@ -214,7 +214,7 @@ export default function() {
             <block type="lists_index_get">
                 <value name="LIST">
                     <block type="variables_get">
-                        <field name="VAR">list</field>
+                        <field name="VAR">${lf("list")}</field>
                     </block>
                 </value>
                 <value name="INDEX">
@@ -226,7 +226,7 @@ export default function() {
             <block type="lists_index_set">
                 <value name="LIST">
                     <block type="variables_get">
-                        <field name="VAR">list</field>
+                        <field name="VAR">${lf("list")}</field>
                     </block>
                 </value>
                 <value name="INDEX">
