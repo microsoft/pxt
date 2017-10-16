725af773fd66b8455575aecc2847dd59583df675
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-10-16 09:49:52 -0700

    Fix for loc generation (#3254)
    * fixing logic to generate docs (was dropping instance methods)
    
    * renaming "list" to "{id:var}list"

--- pxtcompiler/emitter/service.ts
+++ pxtcompiler/emitter/service.ts
@@ -213,35 +213,34 @@ namespace ts.pxtc {
         return null;
     }
 
-    export interface GenMarkdownOptions {
+    export interface GenDocsOptions {
         package?: boolean;
         locs?: boolean;
         docs?: boolean;
     }
 
-    export function genMarkdown(pkg: string, apiInfo: ApisInfo, options: GenMarkdownOptions = {}): pxt.Map<string> {
+    export function genDocs(pkg: string, apiInfo: ApisInfo, options: GenDocsOptions = {}): pxt.Map<string> {
+        pxt.debug(`generating docs for ${pkg}`)
+        pxt.debug(JSON.stringify(Object.keys(apiInfo.byQName), null, 2))
+
         const files: pxt.Map<string> = {};
         const infos = Util.values(apiInfo.byQName);
-        const namespaces = infos.filter(si => si.kind == SymbolKind.Module).sort(compareSymbol);
         const enumMembers = infos.filter(si => si.kind == SymbolKind.EnumMember).sort(compareSymbol);
-
-        const calls: pxt.Map<string> = {};
-        infos.filter(si => !!si.qName).forEach(si => calls[si.qName] = renderCall(apiInfo, si));
 
         const locStrings: pxt.Map<string> = {};
         const jsdocStrings: pxt.Map<string> = {};
-        const helpPages: pxt.Map<string> = {};
-        let reference = ""
         const nameToFilename = (n: string) => n.replace(/([A-Z])/g, function (m) { return '-' + m.toLowerCase(); });
-        const writeRef = (s: string) => reference += s + "\n"
         const writeLoc = (si: SymbolInfo) => {
             if (!options.locs || !si.qName) {
                 return;
             }
+            pxt.debug(`loc: ${si.qName}`)
             // must match blockly loader
-            const ns = ts.pxtc.blocksCategory(si);
-            if (ns)
-                locStrings[`{id:category}${ns}`] = ns;
+            if (si.kind != SymbolKind.EnumMember) {
+                const ns = ts.pxtc.blocksCategory(si);
+                if (ns)
+                    locStrings[`{id:category}${ns}`] = ns;
+            }
             if (si.attributes.jsDoc)
                 jsdocStrings[si.qName] = si.attributes.jsDoc;
             if (si.attributes.block)
@@ -251,115 +250,26 @@ namespace ts.pxtc {
                     jsdocStrings[`${si.qName}|param|${pi.name}`] = pi.description;
                 })
         }
-        const sipkg = pkg && pkg != "core" ? `\`\`\`package
-${pkg}
-\`\`\`
-` : '';
-        const writeApi = (ns: SymbolInfo, si: SymbolInfo, call: string) => {
-            if (!options.docs || !si.qName) return;
-            let api =
-                `# ${si.name.replace(/[A-Z]/g, function (m) { return ' ' + m; })}
-
-${si.attributes.jsDoc.split(/\n\./)[0]}
-
-\`\`\`sig
-${call}
-\`\`\`
-
-## Parameters
-${(si.parameters || []).map(p => `
-* **${p.name}**: [${p.type}](/reference/blocks/${p.type}), ${p.description}`)}
-
-## Example
-
-\`\`\`blocks
-${call}
-\`\`\`
-
-## See Also
-
-${ns.namespace ? `[${ns.namespace}](/reference/${nameToFilename(ns.namespace)})` : ``}
-${sipkg}
-`;
-            files[`reference/${nameToFilename(ns.name)}/${nameToFilename(si.name)}.md`] = api;
-        }
         const mapLocs = (m: pxt.Map<string>, name: string) => {
             if (!options.locs) return;
             const locs: pxt.Map<string> = {};
             Object.keys(m).sort().forEach(l => locs[l] = m[l]);
             files[pkg + name + "-strings.json"] = JSON.stringify(locs, null, 2);
         }
-        const writePackage = (w: (s: string) => void) => {
-            if (options.package) {
-                w("");
-                w("```package");
-                w(pkg);
-                w("```");
+        for (const info of infos) {
+            const isNamespace = info.kind == SymbolKind.Module;
+            if (isNamespace) {
+                if (!infos.filter(si => si.namespace == info.name && !!si.attributes.jsDoc)[0])
+                    continue; // nothing in namespace
+                if (!info.attributes.block) info.attributes.block = info.name; // reusing this field to store localized namespace name
             }
-        }
-        const writeHelpPages = (h: pxt.Map<string>, w: (s: string) => void) => {
-            w("");
-            w("### See Also");
-            w("")
-            w(Object.keys(h).map(k => `[${k}](/reference/${h[k]})`).join(', '))
-        }
-
-        writeRef(`# ${pkg} Reference`)
-        writeRef('')
-        writeRef('```namespaces')
-        for (const ns of namespaces) {
-            const nsHelpPages: pxt.Map<string> = {};
-            const syms = infos
-                .filter(si => si.namespace == ns.name && !!si.attributes.jsDoc)
-                .sort(compareSymbol)
-            if (!syms.length) continue;
-
-            if (!ns.attributes.block) ns.attributes.block = ns.name; // reusing this field to store localized namespace name
-            writeLoc(ns);
-            helpPages[ns.name] = ns.name.replace(`\s+`, `-`);
-
-            let nsmd = "";
-            const writeNs = (s: string) => {
-                nsmd += s + "\n"
-            }
-
-            writeNs(`# ${capitalize(ns.name)}`)
-            writeNs('')
-
-            if (ns.attributes.jsDoc) {
-                writeNs(`${ns.attributes.jsDoc}`)
-                writeNs('')
-            }
-
-            writeNs('```cards')
-            syms.forEach((si, i) => {
-                writeLoc(si);
-                if (si.attributes.help)
-                    nsHelpPages[si.name] = si.attributes.help;
-                const call = calls[si.qName];
-                if (i == 0)
-                    writeRef(call);
-                writeNs(call)
-                if (!si.attributes.help)
-                    writeApi(ns, si, call)
-            })
-            writeNs('```')
-            writePackage(writeNs);
-            writeHelpPages(nsHelpPages, writeNs);
-            if (options.docs)
-                files["reference/" + nameToFilename(ns.name) + '.md'] = nsmd;
+            writeLoc(info);
         }
         if (options.locs)
             enumMembers.forEach(em => {
                 if (em.attributes.block) locStrings[`${em.qName}|block`] = em.attributes.block;
                 if (em.attributes.jsDoc) locStrings[em.qName] = em.attributes.jsDoc;
             });
-        writeRef('```');
-        writePackage(writeRef);
-        writeHelpPages(helpPages, writeRef);
-
-        if (options.docs)
-            files[pkg + "-reference.md"] = reference;
         mapLocs(locStrings, "");
         mapLocs(jsdocStrings, "-jsdoc");
         return files;
@@ -370,10 +280,6 @@ ${sipkg}
 
         function capitalize(name: string) {
             return name[0].toUpperCase() + name.slice(1);
-        }
-
-        function urlify(name: string) {
-            return name.replace(/[A-Z]/g, '-$&').toLowerCase();
         }
 
         function compareSymbol(l: SymbolInfo, r: SymbolInfo): number {
