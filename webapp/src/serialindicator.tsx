aaa25a3fd980eddac504de8cfbd2dd631f307b18
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-10-24 16:19:10 -0700

    Replace preview button background with border

--- webapp/src/serialindicator.tsx
+++ webapp/src/serialindicator.tsx
@@ -38,8 +38,8 @@ export class SerialIndicator extends React.Component<SerialIndicatorProps, Seria
 
     render() {
         if (!this.state.active) return <div />;
-        return <div className="ui segment inverted serialindicator" tabIndex={0} onClick={this.props.onClick} onKeyDown={sui.fireClickOnEnter}>
-            <div className="ui label circular">
+        return(
+            <div className="ui label circular" tabIndex={0} onClick={this.props.onClick} onKeyDown={sui.fireClickOnEnter}>
                 <div className="detail">
                     <sui.Icon icon="bar graph"/>
                 </div>
@@ -47,7 +47,6 @@ export class SerialIndicator extends React.Component<SerialIndicatorProps, Seria
                 <div className="detail">
                     {this.props.isSim ? lf("Simulator") : lf("Device") }
                 </div>
-            </div>
-        </div>
+            </div>)
     }
 }
