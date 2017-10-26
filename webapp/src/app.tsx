ac4d838046d528cb67deb42b2a3ae975d2cc0bfd
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-10-26 14:02:05 -0700

    Clean PR for 'close hf2 before reloading' (#3353)

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1019,6 +1019,15 @@ export class ProjectView
         }
     }
 
+    resetWorkspace() {
+        this.reload = true;
+        return workspace.resetAsync()
+        .done(
+            () => window.location.reload(),
+            () => window.location.reload()
+        );
+    }
+
     reset() {
         pxt.tickEvent("reset");
         core.confirmAsync({
@@ -1029,11 +1038,13 @@ export class ProjectView
             agreeIcon: "sign out",
             disagreeLbl: lf("Cancel")
         }).then(r => {
-            if (!r) return;
-            this.reload = true; //Indicate we are goint to reload next.
-            workspace.resetAsync()
-                .done(() => window.location.reload(),
-                () => window.location.reload())
+            if (!r) return Promise.resolve();
+            if (hf2Connection) {
+                return hf2Connection.disconnectAsync()
+                .then(() => this.resetWorkspace())
+            } else {
+                return this.resetWorkspace()
+            }
         });
     }
 
@@ -1981,6 +1992,7 @@ function initLogin() {
 
 let serialConnectionPoller: number;
 let hidPingInterval: number;
+let hf2Connection: pxt.HF2.Wrapper;
 
 function startSerialConnectionPoller() {
     if (serialConnectionPoller == null)
@@ -1999,6 +2011,7 @@ function initSerial() {
     if (hidbridge.shouldUse()) {
         hidbridge.initAsync(true)
             .then(dev => {
+                hf2Connection = dev;
                 // disable poller when connected; otherwise the forceful reconnecting interferes with
                 // flashing; it may also lead to data loss on serial stream
                 stopSerialConnectionPoller()
