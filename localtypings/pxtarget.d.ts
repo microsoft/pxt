9b2899416a561d94a3bf542d03d988941f20f81e
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-10-30 15:54:42 -0700

    updated map issue

--- localtypings/pxtarget.d.ts
+++ localtypings/pxtarget.d.ts
@@ -42,7 +42,7 @@ declare namespace pxt {
     interface ProjectTemplate {
         id: string;
         config: PackageConfig;
-        files: Map<string>;
+        files: pxt.Map<string>;
     }
 
     interface BlockToolboxDefinition {
@@ -50,7 +50,7 @@ declare namespace pxt {
         type: string;
         gap?: number;
         weight?: number;
-        fields?: Map<string>;
+        fields?: pxt.Map<string>;
     }
 
     interface RuntimeOptions {
@@ -305,7 +305,7 @@ declare namespace ts.pxtc {
         target: CompileTarget;
         testMode?: boolean;
         sourceFiles?: string[];
-        jres?: Map<pxt.JRes>;
+        jres?: pxt.Map<pxt.JRes>;
         hexinfo: HexInfo;
         extinfo?: ExtensionInfo;
         noEmit?: boolean;
