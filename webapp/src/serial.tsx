bea7e22605ebea5e2d00c65ecffaf91f6506e82e
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-11-01 21:48:26 -0700

    Map each data source name to a more readable name (#3394)
    * Map each data source name to a more readable name
    
    * Tweaks

--- webapp/src/serial.tsx
+++ webapp/src/serial.tsx
@@ -16,6 +16,7 @@ const lf = Util.lf
 export class Editor extends srceditor.Editor {
     charts: Chart[] = []
     chartIdx: number = 0
+    sourceMap: pxt.Map<string> = {}
     consoleBuffer: string = ""
     isSim: boolean = true
     maxConsoleLineLength: number = 255
@@ -74,12 +75,18 @@ export class Editor extends srceditor.Editor {
         const data = smsg.data || ""
         const source = smsg.id || "?"
 
+        if (!this.sourceMap[source]) {
+            let sourceIdx = Object.keys(this.sourceMap).length + 1
+            this.sourceMap[source] = lf("source") + sourceIdx.toString()
+        }
+        let niceSource = this.sourceMap[source]
+
         const m = /^\s*(([^:]+):)?\s*(-?\d+(\.\d*)?)/i.exec(data);
         if (m) {
             const variable = m[2] || '';
             const nvalue = parseFloat(m[3]);
             if (!isNaN(nvalue)) {
-                this.appendGraphEntry(source, variable, nvalue)
+                this.appendGraphEntry(niceSource, variable, nvalue)
                 return;
             }
         }
