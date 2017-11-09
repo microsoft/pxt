019baf3bf848838ab5b37bf9640abb3eee30454c
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-11-09 01:12:01 +0100

    dump all outputs from checkdocs build in subfolder (#3441)
    * diump all output in subfolder
    
    * dump on failure as well
    
    * reduce verbvosity
    
    * removing jump line

--- cli/cli.ts
+++ cli/cli.ts
@@ -3277,7 +3277,7 @@ function testSnippetsAsync(snippets: CodeSnippet[], re?: string): Promise<void> 
         filenameMatch = new RegExp(pattern);
     }
     catch (e) {
-        console.log(`pattern could not be compiled as a regular expression, ignoring`);
+        pxt.log(`pattern could not be compiled as a regular expression, ignoring`);
         filenameMatch = new RegExp('.*')
     }
     snippets = snippets.filter(snippet => filenameMatch.test(snippet.name));
@@ -3307,6 +3307,15 @@ function testSnippetsAsync(snippets: CodeSnippet[], re?: string): Promise<void> 
             opts.ast = true
             let resp = pxtc.compile(opts)
 
+            if (resp.outfiles && snippet.file) {
+                const dir = snippet.file.replace(/\.ts$/, '');
+                nodeutil.mkdirP(dir);
+                Object.keys(resp.outfiles).forEach(outfile => {
+                    const ofn = path.join(dir, outfile);
+                    pxt.debug(`writing ${ofn}`);
+                    fs.writeFileSync(ofn, resp.outfiles[outfile], 'utf8')
+                })
+            }
             if (resp.success) {
                 if (/^block/.test(snippet.type)) {
                     //Similar to pxtc.decompile but allows us to get blocksInfo for round trip
