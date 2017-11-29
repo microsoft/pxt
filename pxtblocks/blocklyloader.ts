cc45919f2639b89031c91de44cda8523baac35db
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2017-11-29 09:47:49 -0800

    Flip images in RTL languages (#3521)

--- pxtblocks/blocklyloader.ts
+++ pxtblocks/blocklyloader.ts
@@ -3221,7 +3221,7 @@ namespace pxt.blocks {
             pxt.log(`missing jres icon ${id}`)
             return undefined;
         }
-        return new Blockly.FieldImage(url, 56, 56, false, '');
+        return new Blockly.FieldImage(url, 56, 56, Util.isUserLanguageRtl(), '');
     }
 
     function initJresIcons(blockInfo: pxtc.BlocksInfo) {
