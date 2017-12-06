ee470c71f34333a9de220fdca8a5f81fe1a31be5
Tom Ball <tball@microsoft.com>
2017-12-06 16:30:21 -0500

    more structure

--- papers/jsTSblockly/custom.ts
+++ papers/jsTSblockly/custom.ts
@@ -3,10 +3,19 @@ namespace Examples {
 export enum Poke { A, B }
 
 //% block
-export function onButton1() { }
+export function noParam(): void { }
 
 //% block
-export function onButton2(b: Poke) { }
+export function returnVal(): number { return 1; }
+
+//% block
+export function oneParam(b: Poke): void { }
+
+//% block
+export function oneParamWithReturn(b: Poke): number { return b; }
+
+//% block
+export function onEvent(b: Poke, handler: () => void): void { }
 
 }
 
