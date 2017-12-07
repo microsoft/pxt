d905388635e8b407f9cbb2c7d0f1d06e6d166898
Caitlin Hennessy <caitlin.hennessy@microsoft.com>
2017-12-07 13:34:57 -0800

    Restore v0 version of notification.tsx

--- webapp/src/notification.tsx
+++ webapp/src/notification.tsx
@@ -1,128 +1,65 @@
-/// <reference path="../../typings/globals/react/index.d.ts" />
-/// <reference path="../../typings/globals/react-dom/index.d.ts" />
-/// <reference path="../../built/pxtlib.d.ts" />
-
 import * as React from "react";
-import * as data from "./data";
-import * as sui from "./sui";
-import * as core from "./core";
-import * as electron from "./electron";
-
-import Cloud = pxt.Cloud;
+import * as ReactDOM from "react-dom";
+import * as sui from "./sui"
+import * as codecard from "./codecard"
+import * as data from "./data"
 
 type ISettingsProps = pxt.editor.ISettingsProps;
 
-export interface GenericBannerProps extends ISettingsProps {
-    delayTime?: number; //milliseconds - delay before banner is shown
-    displayTime?: number; //milliseconds - duration of banner display
-    sleepTime?: number; //seconds - time to hide banner after it is dismissed
-    bannerType?: string;
-    children?: React.ReactChild;
-    ref?: any;
+const lf = pxt.Util.lf;
+
+interface NotificationState {
 }
 
-export class GenericBanner extends data.Component<GenericBannerProps, {}> {
-    delayTime: number;
-    doneSleeping: boolean;
-    bannerType: string;
-    timer: number;
-
-    constructor(props: GenericBannerProps) {
+export class NotificationDialog extends data.Component<ISettingsProps, NotificationState> {
+    constructor(props: ISettingsProps) {
         super(props);
-        this.delayTime = this.props.delayTime || 0;
-        this.doneSleeping = this.sleepDone();
-        this.bannerType = this.props.bannerType || "default";
+        this.state = {}
     }
 
-    componentDidMount() {
-        if (this.doneSleeping) {
-            this.timer = setTimeout(() => this.show(), this.delayTime);
-        }
+    hide() {
+        this.props.parent.setState({ notification: undefined })
     }
 
-    componentWillUnmount() {
-        clearTimeout(this.timer);
+    readMore(notification: pxt.Notification) {
+        if (!notification.readmore)
+            return;
+        this.hide()
+        window.open(notification.readmore);
     }
 
-    sleepDone() {
-        if (!this.props.sleepTime) {
-            return true;
-        }
-        const lastBannerClosedTime = parseInt(pxt.storage.getLocal("lastBannerClosedTime") || "0");
-        const now = Util.nowSeconds();
-        return (now - lastBannerClosedTime) > this.props.sleepTime;
-    }
-
-    show() {
-        pxt.tickEvent("notificationBanner.show");
-        if (this.props.displayTime) {
-            this.timer = setTimeout(() => this.hide("automatic"), this.delayTime + this.props.displayTime);
-        }
-        this.props.parent.setBanner(true);
-        this.renderCore();
-    }
-
-    hide(mode: string) {
-        pxt.tickEvent("notificationBanner." + mode + "Close");
-        pxt.storage.setLocal("lastBannerClosedTime", Util.nowSeconds().toString());
-        this.props.parent.setBanner(false);
-        this.renderCore();
+    goToURL(notification: pxt.Notification) {
+        if (!notification.url)
+            return;
+        this.hide()
+        window.open(notification.url);
     }
 
     renderCore() {
+        if (!this.props.parent.state.notification) return <div></div>;
+
+        const notification = this.props.parent.state.notification;
+
+        const header = notification.heading || lf("Notification");
+
         return (
-            (this.props.parent.state.bannerVisible  && this.doneSleeping) ?
-            <div id="notificationBanner" className={`ui attached ${this.bannerType} message`}>
-                <div className="bannerLeft">
-                    <div className="content">
-                        {this.props.children}
-                    </div>
+            <sui.Modal open={!!notification} className={`notificationDialog ${notification.type}`} header={header} size="small"
+                onClose={() => this.hide() } dimmer={true}
+                closeIcon={true}
+                >
+                <div>
+                    {notification.message}
                 </div>
-                <div className="bannerRight">
-                    <sui.Icon icon="close" tabIndex={0} onClick={() => {this.hide("manual"); clearTimeout(this.timer)}}/>
+                <div className="ui segment inverted right aligned">
+                    {notification.url ? <sui.Button
+                        text={lf("Go to URL")}
+                        class={`green`}
+                        onClick={() => this.goToURL(notification)} /> : undefined }
+                    <sui.Button
+                        text={lf("Ok")}
+                        onClick={() => this.hide()} />
                 </div>
-            </div> :
-            <div></div>
+            </sui.Modal>
         );
-    }
-}
-
-export class NotificationBanner extends data.Component<ISettingsProps, {}> {
-    renderCore() {
-        const targetTheme = pxt.appTarget.appTheme;
-        const isApp = electron.isElectron || pxt.winrt.isWinRT();
-        const isLocalServe = location.hostname === "localhost";
-        const isExperimentalUrlPath = location.pathname !== "/"
-            && (targetTheme.appPathNames || []).indexOf(location.pathname) === -1;
-        const showExperimentalBanner = !isLocalServe && isApp && isExperimentalUrlPath;
-        const isWindows10 = pxt.BrowserUtils.isWindows10();
-        const targetConfig = this.getData("target-config:") as pxt.TargetConfig;
-        const showWindowsStoreBanner = isWindows10 && Cloud.isOnline() && targetConfig && targetConfig.windowsStoreLink && !isApp;
-
-        if (showWindowsStoreBanner) {
-            return (
-                <GenericBanner parent={this.props.parent} delayTime={10000} displayTime={45000} sleepTime={604800}>
-                    <sui.Link class="link" target="_blank" ariaLabel={lf("View app in the Windows store")} href={targetConfig.windowsStoreLink} onClick={() => pxt.tickEvent("banner.linkClicked")}>
-                        <img className="bannerIcon" src={Util.pathJoin(pxt.webConfig.commitCdnUrl, `images/windowsstorebag.png`)}></img>
-                    </sui.Link>
-                    <sui.Link class="link" target="_blank" ariaLabel={lf("View app in the Windows store")} href={targetConfig.windowsStoreLink} onClick={() => pxt.tickEvent("banner.linkClicked")}>
-                        {lf("Want a faster download? Get the app!")}
-                    </sui.Link>
-                </GenericBanner>
-            );
-        }
-
-        if (showExperimentalBanner) {
-            const liveUrl = pxt.appTarget.appTheme.homeUrl + location.search + location.hash;
-            return (
-                <GenericBanner parent={this.props.parent} bannerType={"negative"} >
-                    <sui.Icon icon="warning circle" />
-                    <div className="header">{lf("You are viewing an experimental version of the editor") }</div>
-                    <sui.Link class="link" ariaLabel={lf("Go back to live editor")} href={liveUrl}>{lf("Take me back")}</sui.Link>
-                </GenericBanner>
-            );
-        }
-
-        return <div></div>;
     }
 }
