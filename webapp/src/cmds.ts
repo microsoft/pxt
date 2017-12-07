8d3674fb7de7ab22e4c81b908fcb15ea9c9f4028
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-12-07 12:02:09 -0800

    add lost check for ios

--- webapp/src/cmds.ts
+++ webapp/src/cmds.ts
@@ -54,7 +54,7 @@ export function browserDownloadDeployCoreAsync(resp: pxtc.CompileResult): Promis
     }
 
     if (resp.saveOnly && userContext) return showUploadInstructionsAsync(fn, url); // save does the same as download as far iOS is concerned
-    if (resp.saveOnly || pxt.BrowserUtils.isBrowserDownloadInSameWindow()) return Promise.resolve();
+    if (resp.saveOnly || pxt.BrowserUtils.isBrowserDownloadInSameWindow() && !userContext) return Promise.resolve();
     else return showUploadInstructionsAsync(fn, url);
 }
 
