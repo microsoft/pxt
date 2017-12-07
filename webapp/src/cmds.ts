615d90998f00cc74c946d490ae06634a21d6e71e
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-12-07 11:21:51 -0800

    fix save button

--- webapp/src/cmds.ts
+++ webapp/src/cmds.ts
@@ -22,7 +22,8 @@ export function browserDownloadDeployCoreAsync(resp: pxtc.CompileResult): Promis
     const ext = pxt.outputName().replace(/[^.]*/, "")
     const out = resp.outfiles[pxt.outputName()]
     const fn = pkg.genFileName(ext);
-    if (pxt.BrowserUtils.isBrowserDownloadWithinUserContext()) {
+    const userContext = pxt.BrowserUtils.isBrowserDownloadWithinUserContext();
+    if (userContext) {
         url = pxt.BrowserUtils.toDownloadDataUri(pxt.isOutputText() ? btoa(out) : out, pxt.appTarget.compile.hexMimeType);
     } else if (!pxt.isOutputText()) {
         pxt.debug('saving ' + fn)
@@ -53,7 +54,8 @@ export function browserDownloadDeployCoreAsync(resp: pxtc.CompileResult): Promis
         }).then(() => { });
     }
 
-    if (resp.saveOnly || pxt.BrowserUtils.isBrowserDownloadInSameWindow() && !pxt.BrowserUtils.isBrowserDownloadWithinUserContext()) return Promise.resolve();
+    if (resp.saveOnly && userContext) return showUploadInstructionsAsync(fn, url); // save does the same as download as far iOS is concerned
+    if (resp.saveOnly || pxt.BrowserUtils.isBrowserDownloadInSameWindow()) return Promise.resolve();
     else return showUploadInstructionsAsync(fn, url);
 }
 
