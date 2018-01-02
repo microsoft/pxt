22a87f03eeef38ad622ba57562714510bf1c4cf9
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-01 22:21:55 -0800

    fix typo

--- tests/compile-test/lang-test0/02numbers.ts
+++ tests/compile-test/lang-test0/02numbers.ts
@@ -66,7 +66,7 @@ function testNums(): void {
     assert(105 % 100 == 5, "perc")
 
     assert((2 ** 3) == 8, "**")
-    assert((z ** 3) = z * z * z, "**")    
+    assert((z ** 3) == z * z * z, "**")
 
     // test number->bool conversion, #1057
     // x==20 here
