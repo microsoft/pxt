e190697fd25afbe68c9e3435056a16a5984c64e6
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-26 13:58:35 -0800

    support for down (true), up(false) (#3772)

--- pxtblocks/fields/field_toggle_updown.ts
+++ pxtblocks/fields/field_toggle_updown.ts
@@ -3,25 +3,35 @@
 
 namespace pxtblockly {
 
-    export interface FieldToggleUpDownOptions extends Blockly.FieldCustomOptions {
-        inverted?: boolean;
-    }
-
     export class FieldToggleUpDown extends FieldToggle implements Blockly.FieldCustom {
         public isFieldCustom_ = true;
-        inverted: boolean;
 
-        constructor(state: string, params: FieldToggleUpDownOptions, opt_validator?: Function) {
+        constructor(state: string, params: Blockly.FieldCustomOptions, opt_validator?: Function) {
             super(state, params, opt_validator);
-            this.inverted = !!params.inverted;
         }
 
         getTrueText() {
-            return this.inverted ? lf("DOWN") : lf("UP");
+            return lf("UP");
         }
 
         getFalseText() {
-            return this.inverted ? lf("UP") : lf("DOWN");
+            return lf("DOWN");
+        }
+    }
+
+    export class FieldToggleDownUp extends FieldToggle implements Blockly.FieldCustom {
+        public isFieldCustom_ = true;
+
+        constructor(state: string, params: Blockly.FieldCustomOptions, opt_validator?: Function) {
+            super(state, params, opt_validator);
+        }
+
+        getTrueText() {
+            return lf("DOWN");
+        }
+
+        getFalseText() {
+            return lf("UP");
         }
     }
 }
