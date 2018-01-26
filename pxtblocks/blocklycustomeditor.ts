ffca343cf2932c7b2bd276145f2b5557f364b183
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-26 13:47:42 -0800

    support for down (true), up(false)

--- pxtblocks/blocklycustomeditor.ts
+++ pxtblocks/blocklycustomeditor.ts
@@ -35,6 +35,7 @@ namespace pxt.blocks {
         registerFieldEditor('toggleonoff', pxtblockly.FieldToggleOnOff);
         registerFieldEditor('toggleyesno', pxtblockly.FieldToggleYesNo);
         registerFieldEditor('toggleupdown', pxtblockly.FieldToggleUpDown);
+        registerFieldEditor('toggledownup', pxtblockly.FieldToggleDownUp);
         registerFieldEditor('togglehighlow', pxtblockly.FieldToggleHighLow);
         registerFieldEditor('colornumber', pxtblockly.FieldColorNumber);
     }
