656d7c9967126e54dbee0249c661e5744ebdbfdb
Michał Moskal <michal@moskal.me>
2018-01-31 09:06:35 -0800

    Allow for not checking in shims.d.ts and _locales (#3795)

--- pxtlib/main.ts
+++ pxtlib/main.ts
@@ -169,7 +169,7 @@ namespace pxt {
     }
 
     export interface Host {
-        readFile(pkg: Package, filename: string): string;
+        readFile(pkg: Package, filename: string, skipAdditionalFiles?: boolean): string;
         writeFile(pkg: Package, filename: string, contents: string, force?: boolean): void;
         downloadPackageAsync(pkg: Package): Promise<void>;
         getHexInfoAsync(extInfo: pxtc.ExtensionInfo): Promise<pxtc.HexInfo>;
