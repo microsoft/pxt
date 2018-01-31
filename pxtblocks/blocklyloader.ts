042f49c1884910ad93c2d34c7398701b9cebee3f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-30 19:29:06 -0800

    more linting

--- pxtblocks/blocklyloader.ts
+++ pxtblocks/blocklyloader.ts
@@ -185,7 +185,7 @@ namespace pxt.blocks {
             block.appendChild(createShadowValue(t, t.shadowBlockId || "variables_get", t.defaultValue || t.definitionName));
         }
         if (fn.parameters) {
-            comp.parameters.filter(pr => !pr.isOptional && 
+            comp.parameters.filter(pr => !pr.isOptional &&
                 (/^(string|number|boolean)$/.test(pr.type) || pr.shadowBlockId || pr.defaultValue))
                 .forEach(pr => {
                     let shadowValue: Element;
@@ -730,7 +730,7 @@ namespace pxt.blocks {
                 })
             }
         });
-        
+
         if (fn.attributes.imageLiteral) {
             for (let r = 0; r < 5; ++r) {
                 let ri = block.appendDummyInput();
@@ -805,13 +805,13 @@ namespace pxt.blocks {
 
                     const defName = pr.definitionName;
                     const actName = pr.actualName;
-    
+
                     let isEnum = typeInfo && typeInfo.kind == pxtc.SymbolKind.Enum
                     let isFixed = typeInfo && !!typeInfo.attributes.fixedInstances && !pr.shadowBlockId;
                     let customField = pr.fieldEditor;
                     let fieldLabel = defName.charAt(0).toUpperCase() + defName.slice(1);
                     let fieldType = pr.type;
-    
+
                     if (isEnum || isFixed) {
                         const syms = Util.values(info.apis.byQName)
                             .filter(e =>
@@ -851,7 +851,7 @@ namespace pxt.blocks {
                                 dd.unshift(shadowValue);
                             }
                         }
-    
+
                         if (customField) {
                             let defl = fn.attributes.paramDefl[actName] || "";
                             const options = {
@@ -865,7 +865,7 @@ namespace pxt.blocks {
                         }
                         else
                             current.appendField(new Blockly.FieldDropdown(dd), defName);
-    
+
                     } else if (customField) {
                         current = addParamInput();
                         const defl = fn.attributes.paramDefl[pr.actualName] || "";
@@ -907,9 +907,9 @@ namespace pxt.blocks {
             function addDummyInput(notLabel = false) {
                 if (expanded) {
                     if (notLabel) {
-                        return block.appendDummyInput(optionalInputWithFieldPrefix + (anonIndex ++))
+                        return block.appendDummyInput(optionalInputWithFieldPrefix + (anonIndex++))
                     }
-                    return block.appendDummyInput(optionalDummyInputPrefix + (anonIndex ++))
+                    return block.appendDummyInput(optionalDummyInputPrefix + (anonIndex++))
                 }
                 return block.appendDummyInput();
             }
@@ -2339,7 +2339,7 @@ namespace pxt.blocks {
         };
 
         if (pxt.appTarget.runtime && pxt.appTarget.runtime.pauseUntilBlock) {
-            const blockOptions =  pxt.appTarget.runtime.pauseUntilBlock;
+            const blockOptions = pxt.appTarget.runtime.pauseUntilBlock;
             const blockDef = pxt.blocks.getBlockDefinition(ts.pxtc.PAUSE_UNTIL_TYPE);
             Blockly.Blocks[pxtc.PAUSE_UNTIL_TYPE] = {
                 init: function () {
@@ -2349,9 +2349,9 @@ namespace pxt.blocks {
                         "message0": blockDef.block["message0"],
                         "args0": [
                             {
-                              "type": "input_value",
-                              "name": "PREDICATE",
-                              "check": "Boolean"
+                                "type": "input_value",
+                                "name": "PREDICATE",
+                                "check": "Boolean"
                             }
                         ],
                         "inputsInline": true,
