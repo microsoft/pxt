25ce409ddb9389d6bb850d2adf5f1c41432aa255
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-02-08 14:36:15 -0800

    updated description

--- webapp/src/cmds.ts
+++ webapp/src/cmds.ts
@@ -121,7 +121,7 @@ function askWebUSBPairAsync(resp: pxtc.CompileResult): Promise<void> {
         htmlBody: `
 <p><strong>${lf("Do you want to pair your {0} to the editor?", boardName)}</strong>
 ${lf("You will have access to instant download and data logging.")}</p>
-<p class="small">The pairing experience is a one-time process and available for the Chrome browser version 61 and up.</p>
+<p class="ui small">The pairing experience is a one-time process and available for the Chrome browser version 61 and up.</p>
         `,
     }).then(r => r ? showFirmwareUpdateInstructionsAsync(resp) : browserDownloadDeployCoreAsync(resp));
 }
