afcb51d454d8421b93ee72177fe30827f0ad7a89
Richard Knoll <riknoll@microsoft.com>
2018-02-14 17:46:46 -0800

    Adding warning comment

--- webapp/src/blocks.tsx
+++ webapp/src/blocks.tsx
@@ -409,6 +409,11 @@ export class Editor extends srceditor.Editor {
                 if (ev.element == 'category') {
                     let toolboxVisible = !!ev.newValue;
                     if (toolboxVisible) {
+                        // WARNING! Because we use the category open/close event to dismiss
+                        // the cookie banner, be careful when manipulating the toolbox to make
+                        // sure that this event only fires as the result of user action. Use
+                        // Blockly.Events.disable() and Blockly.Events.enable() to prevent
+                        // UI events from firing.
                         pxt.analytics.setConsent();
                     }
                     this.parent.setState({ hideEditorFloats: toolboxVisible });
