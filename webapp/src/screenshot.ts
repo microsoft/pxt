00e9ffaaa40592b2cd8a8e0f22f2250baf5b9e4c
Michal Moskal <michal@moskal.me>
2018-02-27 12:22:58 -0800

    Starting on saving in PNG files

--- webapp/src/screenshot.ts
+++ webapp/src/screenshot.ts
@@ -42,3 +42,64 @@ export function saveAsync(header: Header, screenshot: string): Promise<void> {
                 });
         });
 }
+
+export function loadImageAsync(url: string) {
+    return new Promise<HTMLCanvasElement>((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+            const canvas = document.createElement("canvas")
+            canvas.width = img.width
+            canvas.height = img.height
+            const ctx = canvas.getContext("2d")
+            ctx.drawImage(img, 0, 0);
+            resolve(canvas)
+        };
+    })
+}
+
+// randomly selected
+const imageHeaderPref = [75, 15, 39, 177]
+
+export function encodeBlobAsync(dataURL: string, blob: Uint8Array) {
+    return loadImageAsync(dataURL)
+        .then(canvas => {
+            const neededBits = blob.length * 8
+            // we take 6 bytes for the magic - 2 bits per channel, 6 bits per pixel - 8 pixels
+            const magicPixels = 8
+            const usableBytes = (canvas.width * canvas.height - magicPixels) * 3
+            let bpp = 1
+            while (bpp < 4) {
+                if (usableBytes * bpp >= neededBits)
+                    break
+            }
+            let addedLines = 0
+            const addedLimit = canvas.width * canvas.height * 4
+            if (usableBytes * bpp < neededBits) {
+                const missing = neededBits - usableBytes * bpp
+                const bitsPerLine = canvas.width * 3 * 8
+                addedLines = Math.ceil(missing / bitsPerLine)
+                const c2 = document.createElement("canvas")
+                c2.width = canvas.width
+                c2.height = canvas.height + addedLines
+                const ctx = c2.getContext("2d")
+                ctx.drawImage(canvas, 0, 0)
+                canvas = c2
+            }
+
+            function encode(ptr: number, bpp: number, data: ArrayLike<number>) {
+                for (let i = 0; i < data.length; ++i) {
+                    
+                }
+            }
+
+            const imageHeader = imageHeaderPref.concat([(addedLines >> 8) | (bpp << 6), addedLines & 0xff])
+
+            const ctx = canvas.getContext("2d")
+            const imgdat = ctx.getImageData(0, 0, canvas.width, canvas.height)
+            let ptr = 0
+
+            for ()
+
+
+          })
+}
