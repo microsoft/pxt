13b312984ad3d7e9e506475f1ba9ac1aa3ab090f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-02-27 17:10:57 -0800

    Mobile fixes v0 (#3928)
    * merge master
    
    * hide docs in mobile mode
    
    * fixing link
    
    * fixing position of popout button
    
    * increase timeout
    
    * for another PR

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -745,13 +745,13 @@ export class ProjectView
             core.showLoading(lf("loading project..."))
             importer.importAsync(this, data)
                 .done(
-                () => core.hideLoading(),
-                e => {
-                    pxt.reportException(e, { importer: importer.id });
-                    core.hideLoading();
-                    core.errorNotification(lf("Oops, something went wrong when importing your project"));
-                    if (createNewIfFailed) this.newProject();
-                });
+                    () => core.hideLoading(),
+                    e => {
+                        pxt.reportException(e, { importer: importer.id });
+                        core.hideLoading();
+                        core.errorNotification(lf("Oops, something went wrong when importing your project"));
+                        if (createNewIfFailed) this.newProject();
+                    });
         }
         else {
             core.warningNotification(lf("Sorry, we could not import this project."))
@@ -945,8 +945,8 @@ export class ProjectView
         this.reload = true;
         workspace.resetAsync()
             .done(
-            () => window.location.reload(),
-            () => window.location.reload()
+                () => window.location.reload(),
+                () => window.location.reload()
             );
     }
 
@@ -969,7 +969,7 @@ export class ProjectView
                     return this.resetWorkspace();
                 });
         })
-        .done();
+            .done();
     }
 
     promptRenameProjectAsync(): Promise<boolean> {
@@ -1699,11 +1699,13 @@ ${compileService ? `<p>${lf("{0} version:", "C++ runtime")} <a href="${Util.html
         const useSerialEditor = pxt.appTarget.serial && !!pxt.appTarget.serial.useEditor;
 
         const showSideDoc = sideDocs && this.state.sideDocsLoadUrl && !this.state.sideDocsCollapsed;
+        const isApp = pxt.BrowserUtils.hasWebKitHost() || pxt.winrt.isWinRT();
 
         // update window title
         document.title = this.state.header ? `${this.state.header.name} - ${pxt.appTarget.name}` : pxt.appTarget.name;
 
         let rootClassList = [
+            "ui",
             (this.state.hideEditorFloats || this.state.collapseEditorTools) && !inTutorial ? " hideEditorFloats" : '',
             this.state.collapseEditorTools && !inTutorial ? " collapsedEditorTools" : '',
             this.state.fullscreen ? 'fullscreensim' : '',
@@ -1717,6 +1719,7 @@ ${compileService ? `<p>${lf("{0} version:", "C++ runtime")} <a href="${Util.html
             !showEditorToolbar ? 'hideEditorToolbar' : '',
             this.state.bannerVisible ? "notificationBannerVisible" : "",
             sandbox && simActive ? 'simView' : '',
+            isApp ? "app" : "",
             'full-abs'
         ];
         let jQueryClasses = ["dimmable", "dimmed"];
@@ -1775,6 +1778,8 @@ ${compileService ? `<p>${lf("{0} version:", "C++ runtime")} <a href="${Util.html
                                             // we always need a way to clear local storage, regardless if signed in or not
                                         }
                                         <sui.Item role="menuitem" icon='sign out' text={lf("Reset")} onClick={uiHandler(this.reset)} tabIndex={-1} />
+                                        {docMenu ? <div className="ui divider mobile only"></div> : undefined}
+                                        {docMenu ? container.renderDocItems(this, "mobile only") : undefined}
                                         <div className="ui divider"></div>
                                         {targetTheme.privacyUrl ? <a className="ui item" href={targetTheme.privacyUrl} role="menuitem" title={lf("Privacy & Cookies")} target="_blank" tabIndex={-1}>{lf("Privacy & Cookies")}</a> : undefined}
                                         {targetTheme.termsOfUseUrl ? <a className="ui item" href={targetTheme.termsOfUseUrl} role="menuitem" title={lf("Terms Of Use")} target="_blank" tabIndex={-1}>{lf("Terms Of Use")}</a> : undefined}
