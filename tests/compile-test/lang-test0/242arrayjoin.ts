2a1fc6c80171f07796c0ae6bc976114c5a8d80c8
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-03-12 10:18:55 -0700

    fixing assserts

--- tests/compile-test/lang-test0/242arrayjoin.ts
+++ tests/compile-test/lang-test0/242arrayjoin.ts
@@ -3,13 +3,13 @@ function testArrayJoin() {
         msg("testArraySort")
         let strs = [1, 2, 3];
         let r = "A" + strs.join("X");
-        assert(r == "AX1X2X3", "join");
+        assert(r == "A1X2X3", "join");
     }
 
     {
         let strs = ["a", "b", "c"];
         let r = "B" + strs.join("Y");
-        assert(r == "BYaYbYc", "joinstr");
+        assert(r == "BaYbYc", "joinstr");
     }
 }
 
