280bbedec9b5acb7772994fb94ed8ae7fb005b5f
Sam El-Husseini <samelh@microsoft.com>
2018-03-28 13:25:57 -0700

    Move light to its own less file.

--- pxtsim/simdriver.ts
+++ pxtsim/simdriver.ts
@@ -131,7 +131,7 @@ namespace pxsim {
             frame.setAttribute('sandbox', 'allow-same-origin allow-scripts');
             frame.sandbox.value = "allow-scripts allow-same-origin"
             let simUrl = this.options.simUrl || ((window as any).pxtConfig || {}).simUrl || "/sim/simulator.html"
-            if (light) simUrl+= '?light=1';
+            frame.className = 'no-select'
             if (this.runOptions.aspectRatio)
                 wrapper.style.paddingBottom = (100 / this.runOptions.aspectRatio) + "%";
             frame.src = simUrl + '#' + frame.id;
