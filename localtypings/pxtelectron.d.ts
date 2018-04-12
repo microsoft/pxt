ba8e7696dace7f50bd83b38c1ce234c75389919e
Guillaume Jenkins <gujenkin@microsoft.com>
2018-04-12 11:12:37 -0400

    Remove non-squirrel stuff

--- localtypings/pxtelectron.d.ts
+++ localtypings/pxtelectron.d.ts
@@ -1,20 +1,8 @@
 /// <reference path="pxtpackage.d.ts" />
 
 declare namespace pxt.electron {
-    export interface VersionRange {
-        from: string; // Semver version
-        to: string; // Semver version
-    }
-
-    export interface MajorRelease {
-        latest: string; // Semver version
-        url?: string; // Alternatively, we can direct users to a web page to update instead of one-click installer
-        promptVersion?: string; // If app is older than or equal to this version (semver), user is modal prompted to update; otherwise a simple notification of "update available" is shown
-        bannedVersions?: VersionRange[]; // If app falls within one of these version ranges, user will be forced to update or app will quit
-    }
-
     export interface ElectronManifest {
-        majorReleases: { [majorVersion: number]: MajorRelease };
+        latest: string;
         timeStamp?: string;
     }
 
@@ -27,8 +15,6 @@ declare namespace pxt.electron {
 
     // The object that gets injected into the window
     export interface PxtElectron {
-        updateApp: (version: string, errorHandler: () => void) => void;
-        quitApp: () => void;
         initTelemetry: (telemetryHandler: TelemetryHandler) => void;
     }
 }
