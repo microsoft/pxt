6543f054b219665a4876755be9fbe4d04cc0fa80
Abhijith <abchatra@microsoft.com>
2018-04-13 17:28:15 -0700

    Simplify the about dialog

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1672,12 +1672,11 @@ export class ProjectView
         const description = pxt.appTarget.description || pxt.appTarget.title;
         const githubUrl = pxt.appTarget.appTheme.githubUrl;
         core.confirmAsync({
-            header: lf("About {0}", pxt.appTarget.name),
+            header: lf("About"),
             hideCancel: true,
             agreeLbl: lf("Ok"),
             agreeClass: "positive focused",
             htmlBody: `
-${description ? `<p>${Util.htmlEscape(description)}</p>` : ``}
 ${githubUrl ? `<p>${lf("{0} version:", Util.htmlEscape(pxt.appTarget.name))} <a class="focused" href="${Util.htmlEscape(githubUrl)}/releases/tag/v${Util.htmlEscape(pxt.appTarget.versions.target)}" aria-label="${lf("{0} version : {1}", Util.htmlEscape(pxt.appTarget.name), Util.htmlEscape(pxt.appTarget.versions.target))}" target="_blank">${Util.htmlEscape(pxt.appTarget.versions.target)}</a></p>` : ``}
 <p>${lf("{0} version:", "Microsoft MakeCode")} <a href="https://github.com/Microsoft/pxt/releases/tag/v${Util.htmlEscape(pxt.appTarget.versions.pxt)}" aria-label="${lf("{0} version: {1}", "Microsoft MakeCode", Util.htmlEscape(pxt.appTarget.versions.pxt))}" target="_blank">${Util.htmlEscape(pxt.appTarget.versions.pxt)}</a></p>
 ${compileService && compileService.githubCorePackage && compileService.gittag ? `<p>${lf("{0} version:", "C++ runtime")} <a href="${Util.htmlEscape("https://github.com/" + compileService.githubCorePackage + '/releases/tag/' + compileService.gittag)}" aria-label="${lf("{0} version: {1}", "C++ runtime", Util.htmlEscape(compileService.gittag))}" target="_blank">${Util.htmlEscape(compileService.gittag)}</a></p>` : ""}
