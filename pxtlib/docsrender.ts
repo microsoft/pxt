b600a3718babad2ef6805e9d97545a25d76e562c
Galen Nickel (Aquent LLC) <v-ganick@microsoft.com>
2018-04-16 13:40:26 -0700

    Change 'boardCommonName' to 'boardNickname'

--- pxtlib/docsrender.ts
+++ pxtlib/docsrender.ts
@@ -260,8 +260,8 @@ namespace pxt.docs {
 
         if (theme.boardName)
             params["boardname"] = html2Quote(theme.boardName);
-        if (theme.boardCommonName)
-            params["nickname"] = html2Quote(theme.boardCommonName);
+        if (theme.boardNickname)
+            params["nickname"] = html2Quote(theme.boardNickname);
         if (theme.driveDisplayName)
             params["drivename"] = html2Quote(theme.driveDisplayName);
         if (theme.homeUrl)
