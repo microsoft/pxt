a44e6d41c293b6c48c23393ad70506569e4d9131
Sam El-Husseini <samelh@microsoft.com>
2018-04-27 12:49:32 -0400

    Fix IE issue with querySelector :empty

--- webapp/src/blocks.tsx
+++ webapp/src/blocks.tsx
@@ -1260,7 +1260,9 @@ export class Editor extends toolboxeditor.ToolboxEditor {
             blockXml = Blockly.Xml.textToDom(block.blockXml);
         }
         if (blockXml) {
-            pxt.Util.toArray(blockXml.querySelectorAll('shadow:empty')).forEach((shadow, i) => {
+            pxt.Util.toArray(blockXml.querySelectorAll('shadow'))
+                .filter(shadow => shadow.innerHTML === "")
+                .forEach((shadow, i) => {
                 let type = shadow.getAttribute('type');
                 const builtin = snippets.allBuiltinBlocks()[type];
                 let b = this.getBlockXml(builtin ? builtin : { name: type, attributes: { blockId: type } }, true);
