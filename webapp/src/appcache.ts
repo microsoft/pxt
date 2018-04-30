2daab04c62d585632ac673b828fc2beb4815ff0b
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-30 15:53:27 -0700

    check status before reloading appcache

--- webapp/src/appcache.ts
+++ webapp/src/appcache.ts
@@ -2,13 +2,16 @@ import * as core from "./core";
 
 export function init(updated: () => void) {
     const appCache = window.applicationCache;
-    if (!(pxt.appTarget.appTheme && pxt.appTarget.appTheme.noReloadOnUpdate)) {
-        appCache.addEventListener('updateready', () => {
+    if (pxt.appTarget.appTheme && pxt.appTarget.appTheme.noReloadOnUpdate)
+        return;
+
+    appCache.addEventListener('updateready', () => {
+        if (appCache.status === window.applicationCache.UPDATEREADY) {
             core.infoNotification(lf("Update download complete. Reloading... "));
             setTimeout(() => {
                 pxt.tickEvent('appcache.updated')
                 updated();
             }, 3000);
-        }, false);
-    }
+        }
+    }, false);
 }
