c1d2b1e853e89c655ffdaaddc48eef3ee11de80a
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-30 16:02:30 -0700

    handle already updated

--- webapp/src/appcache.ts
+++ webapp/src/appcache.ts
@@ -2,18 +2,30 @@ import * as core from "./core";
 
 export function init(updated: () => void) {
     const appCache = window.applicationCache;
-    if (pxt.appTarget.appTheme && pxt.appTarget.appTheme.noReloadOnUpdate)
-        return;
 
-    appCache.addEventListener('updateready', () => {
+    function scheduleUpdate() {
         console.log(`app cache update ready (${appCache.status})`)
         if (appCache.status !== window.applicationCache.UPDATEREADY)
             return;
-
         core.infoNotification(lf("Update download complete. Reloading... "));
         setTimeout(() => {
             pxt.tickEvent('appcache.updated')
             updated();
         }, 3000);
-    }, false);
+    }
+
+    // disable in options
+    if (pxt.appTarget.appTheme && pxt.appTarget.appTheme.noReloadOnUpdate)
+        return;
+
+    // already dowloaded
+    if (appCache.status === window.applicationCache.UPDATEREADY) {
+        scheduleUpdate();
+    }
+    else {
+        // waiting for event
+        appCache.addEventListener('updateready', () => {
+            scheduleUpdate();
+        }, false);
+    }
 }
