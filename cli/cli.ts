1900aedb3882c4ae1d396bb3bbfa55fc6b0f1d72
Sam El-Husseini <16690124+samelhusseini@users.noreply.github.com>
2018-05-02 13:43:11 -0700

    Fix build issue by configuring the lessc path (#4240)

--- cli/cli.ts
+++ cli/cli.ts
@@ -1615,9 +1615,10 @@ function buildSemanticUIAsync(parsed?: commandParser.ParsedCommand) {
 
     nodeutil.mkdirP(path.join("built", "web"));
     const lessPath = require.resolve('less');
+    const lessCPath = path.join(path.dirname(lessPath), '/bin/lessc');
     return nodeutil.spawnAsync({
         cmd: "node",
-        args: [lessPath, "theme/style.less", "built/web/semantic.css", "--include-path=node_modules/semantic-ui-less:node_modules/pxt-core/theme:theme/foo/bar", "--no-ie-compat"]
+        args: [lessCPath, "theme/style.less", "built/web/semantic.css", "--include-path=node_modules/semantic-ui-less:node_modules/pxt-core/theme:theme/foo/bar", "--no-ie-compat"]
     }).then(() => {
         const fontFile = fs.readFileSync("node_modules/semantic-ui-less/themes/default/assets/fonts/icons.woff")
         const url = "url(data:application/font-woff;charset=utf-8;base64,"
@@ -1638,7 +1639,7 @@ function buildSemanticUIAsync(parsed?: commandParser.ParsedCommand) {
             return Promise.resolve();
         return nodeutil.spawnAsync({
             cmd: "node",
-            args: [lessPath, "theme/blockly.less", "built/web/blockly.css", "--include-path=node_modules/semantic-ui-less:node_modules/pxt-core/theme:theme/foo/bar", "--no-ie-compat"]
+            args: [lessCPath, "theme/blockly.less", "built/web/blockly.css", "--include-path=node_modules/semantic-ui-less:node_modules/pxt-core/theme:theme/foo/bar", "--no-ie-compat"]
         })
     }).then(() => {
         // run postcss with autoprefixer and rtlcss
