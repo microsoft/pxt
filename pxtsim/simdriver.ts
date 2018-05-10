6fdcddbe37d1638818fd0ba1d96463027a57a5e8
Sam El-Husseini <16690124+samelhusseini@users.noreply.github.com>
2018-05-09 22:22:39 -0700

    Updates for Chrome Autoplay policy
    Chrome autoplay policy affects how we play the sim. Since the simulator is under a different domain, we need to explicitly allow autoplay on the sim iframes such that if autoplay is allowed for the parent frame, it would share that with the simulator frame.

--- pxtsim/simdriver.ts
+++ pxtsim/simdriver.ts
@@ -121,6 +121,7 @@ namespace pxsim {
             const frame = document.createElement('iframe') as HTMLIFrameElement;
             frame.id = 'sim-frame-' + this.nextId()
             frame.allowFullscreen = true;
+            frame.setAttribute('allow', 'autoplay');
             frame.setAttribute('sandbox', 'allow-same-origin allow-scripts');
             frame.sandbox.value = "allow-scripts allow-same-origin"
             let simUrl = this.options.simUrl || ((window as any).pxtConfig || {}).simUrl || "/sim/simulator.html"
