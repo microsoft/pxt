d351097d83582bc23653bcd3267f71b29d5d460d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-11 15:10:47 -0700

    clearing tutorial state upon going to home

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1054,7 +1054,7 @@ export class ProjectView
         if (this.editor) this.editor.unloadFileAsync();
         // clear the hash
         pxt.BrowserUtils.changeHash("", true);
-        this.setState({ home: true, tracing: undefined, fullscreen: undefined });
+        this.setState({ home: true, tracing: undefined, fullscreen: undefined, tutorialOptions: undefined, editorState: undefined });
         this.allEditors.forEach(e => e.setVisible(false));
         this.homeLoaded();
     }
