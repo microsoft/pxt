20e137563718aa15677348a960375f47cbaa234f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-11 23:46:55 -0700

    added webcam selection

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -35,6 +35,7 @@ import * as sounds from "./sounds";
 import * as make from "./make";
 import * as blocklyToolbox from "./blocksSnippets";
 import * as monacoToolbox from "./monacoSnippets";
+import * as webcam from "./webcam";
 
 import * as monaco from "./monaco"
 import * as pxtjson from "./pxtjson"
@@ -2017,7 +2018,7 @@ export class ProjectView
         }
         return (
             <div id='root' className={rootClasses}>
-                {this.state.greenScreen ? <sui.WebCam /> : undefined}
+                {this.state.greenScreen ? <webcam.WebCam /> : undefined}
                 {hideMenuBar ? undefined :
                     <header className="menubar" role="banner">
                         {inEditor ? <accessibility.EditorAccessibilityMenu parent={this} highContrast={this.state.highContrast} /> : undefined}
---
+++ webapp/src/webcam.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as sui from "./sui";
+import * as core from "./core";
+
+export interface WebCamProps {
+
+}
+
+export class WebCam extends sui.UIElement<WebCamProps, {}> {
+    private stream: MediaStream;
+    private v: HTMLVideoElement;
+
+    componentDidMount() {
+        navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+                core.dialogAsync({
+                    header: lf("Choose a camera"),
+                    hideCancel: devices.length > 0,
+                    buttons: devices.filter(device => device.kind == "videoinput")
+                        .map(device => {
+                            return {
+                                label: device.label,
+                                onclick: () => {
+                                    navigator.mediaDevices.getUserMedia({
+                                        video: { deviceId: { exact: device.deviceId } },
+                                        audio: false
+                                    }).then(stream => {
+                                        this.stream = stream;
+                                        this.v.srcObject = this.stream;
+                                        this.v.play();
+                                    }, err => {
+                                        this.stop();
+                                    })
+                                }
+                            } as ModalButton;
+                        })
+                })
+            })
+    }
+
+    componentWillUnmount() {
+    }
+
+    private stop() {
+        if (this.stream) {
+            if (this.stream.stop)
+                this.stream.stop();
+            const tracks = this.stream.getTracks();
+            if (tracks)
+                tracks.forEach(track => track.stop());
+            this.stream = undefined;
+            this.v.srcObject = undefined;
+        }
+    }
+
+    private handleVideoRef = (ref: any) => {
+        this.v = ref;
+    }
+
+    render() {
+        return <video ref={this.handleVideoRef} />
+    }
+}
