713364de8838e60cca28f6e3309f9b0779cf4e18
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-23 13:52:34 -0700

    more logging

--- cli/pyconv.ts
+++ cli/pyconv.ts
@@ -1912,12 +1912,13 @@ export function convertAsync(fns: string[]) {
     let primFiles =
         U.toDictionary(mainFiles.length ? mainFiles : nodeutil.allFiles(fns[0]),
             s => s.replace(/\\/g, "/"))
-    let files = U.concat(fns.map(f => nodeutil.allFiles(f))).map(f => f.replace(/\\/g, "/"))
-    let dirs: Map<number> = {}
+    let files = U.concat(fns.map(f => nodeutil.allFiles(f))).map(f => f.replace(/\\/g, "/"));
+    let dirs: Map<number> = {};
     for (let f of files) {
-        for (let suff of ["/docs/conf.py", "/conf.py", "/setup.py", "/README.md", "/README.rst"]) {
+        for (let suff of ["/docs/conf.py", "/conf.py", "/setup.py", "/README.md", "/README.rst", "/__init__.py"]) {
             if (U.endsWith(f, suff)) {
-                dirs[f.slice(0, f.length - suff.length)] = 1
+                const dirName = f.slice(0, f.length - suff.length);
+                dirs[dirName] = 1;
             }
         }
     }
@@ -1943,7 +1944,7 @@ export function convertAsync(fns: string[]) {
     }
 
     const pkgFilesKeys = Object.keys(pkgFiles);
-    pxt.debug(`files (${pkgFilesKeys.length}):\n   ${pkgFilesKeys.join('\n   ')}`);
+    pxt.log(`files (${pkgFilesKeys.length}):\n   ${pkgFilesKeys.join('\n   ')}`);
 
     return nodeutil.spawnWithPipeAsync({
         cmd: /^win/i.test(process.platform) ? "py" : "python3",
