ffea195bf4b528e698a29baa9685c012fedb7994
Sébastien NEDJAR <nedseb@goldaire.com>
2018-06-15 09:01:55 +0200

    Add the local installation of openocd as first choice

--- cli/gdb.ts
+++ cli/gdb.ts
@@ -37,22 +37,36 @@ function getOpenOcdPath() {
     ]
 
     let pkgDir = ""
+    let openocdPath = ""
+    let openocdBin = ""
+    let gccPath = ""
+    let gdbBin = ""
 
-    for (let ardV = 15; ardV < 50; ++ardV) {
-        for (let d of dirs) {
-            pkgDir = d + ardV + "/packages/arduino/"
-            if (fs.existsSync(pkgDir)) break
-            pkgDir = ""
-        }
-        if (pkgDir) break
+    if(fs.existsSync("/usr/bin/openocd")){
+        openocdPath = "/usr/"
+        gccPath = "/usr/"
     }
+    else {
+        for (let ardV = 15; ardV < 50; ++ardV) {
+            for (let d of dirs) {
+                pkgDir = d + ardV + "/packages/arduino/"
+                if (fs.existsSync(pkgDir)) 
+                    break
+                pkgDir = ""
+            }
 
-    if (!pkgDir) fatal("cannot find Arduino packages directory")
+            if (pkgDir) 
+                break
+        }
 
+        if (!pkgDir) 
+            fatal("cannot find Arduino packages directory")
 
-
-    let openocdPath = latest("openocd")
-    let openocdBin = openocdPath + "bin/openocd"
+        openocdPath = latest("openocd")
+        gccPath = latest("arm-none-eabi-gcc")
+    }
+    
+    openocdBin = openocdPath + "bin/openocd"
 
     if (process.platform == "win32")
         openocdBin += ".exe"
@@ -63,11 +77,11 @@ function getOpenOcdPath() {
     let cmd = `log_output built/openocd.log; ${script}; init; halt;`
 
     let args = [openocdBin, "-d2",
-        "-s", openocdPath + "/share/openocd/scripts/",
+        "-s", openocdPath + "share/openocd/scripts/",
         "-c", cmd]
 
-    let gccPath = latest("arm-none-eabi-gcc")
-    let gdbBin = gccPath + "bin/arm-none-eabi-gdb"
+    
+    gdbBin = gccPath + "bin/arm-none-eabi-gdb"
 
     if (process.platform == "win32")
         gdbBin += ".exe"
