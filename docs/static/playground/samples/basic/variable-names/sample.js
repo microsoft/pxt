0141d4f74f9baaaf38357a3e7826f1bb99023150
Richard Knoll <riknoll@microsoft.com>
2018-06-27 17:58:18 -0700

    Adding playground sample

---
+++ docs/static/playground/samples/basic/variable-names/sample.js
@@ -0,0 +1,15 @@
+//
+// Set the default name given to a variable argument in the block string by
+// putting it in parentheses after the shadow block ID
+//
+
+//% color="#AA278D"
+namespace basic {
+    /**
+     * This API will have a variable shadow block with the name "someName" pre-filled
+     */
+    //% block="%x=variables_get(someName)"
+    export function foo(x: number) {
+
+    }
+}
