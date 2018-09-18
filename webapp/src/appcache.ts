bab9d1009864226de86cead3cbe68fd703ba85c8
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-09-18 15:07:14 -0700

    avoid reload cycle in FF (v0)

--- webapp/src/appcache.ts
+++ webapp/src/appcache.ts
@@ -2,7 +2,11 @@ import * as core from "./core";
 
 export function init(hash: { cmd: string, arg: string }) {
     let appCache = window.applicationCache;
+    if (!appCache) return; // might eventually go away
+
     appCache.addEventListener('updateready', () => {
+        if (appCache.status != appCache.UPDATEREADY)
+            return; // FF incorrectly fires this event
         core.infoNotification(lf("Update download complete. Reloading... "));
         setTimeout(() => {
             pxt.tickEvent('appcache.updated')
