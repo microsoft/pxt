fedc1486b18398b93321753aa73dc350c175c535
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-09-19 09:34:36 -0700

    added note

--- webapp/src/greenscreen.tsx
+++ webapp/src/greenscreen.tsx
@@ -102,6 +102,7 @@ export class WebCam extends data.Component<WebCamProps, WebCamState> {
     }
 
     render() {
+        // playsInline required for iOS
         const { hasPrompt, devices } = this.state;
         return <div className="videoContainer">
             <video playsInline ref={this.handleVideoRef} />
