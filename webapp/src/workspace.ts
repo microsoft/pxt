5f3d09c63f6c39a2d8734e7d8708282e5777123f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-09-20 13:28:29 -0700

    when computing workspace path, allow unicode characters

--- webapp/src/workspace.ts
+++ webapp/src/workspace.ts
@@ -227,7 +227,9 @@ export function saveAsync(h: Header, text?: ScriptText, isCloud?: boolean): Prom
 }
 
 function computePath(h: Header) {
-    let path = h.name.replace(/[^a-zA-Z0-9]+/g, " ").trim().replace(/ /g, "-")
+    let path = h.name.replace(/[^a-zA-Z0-9\u0000-\u007F]+/g, " ").trim().replace(/ /g, "-")
+    if (!path)
+        path = "Untitled"; // do not translate
     if (lookup(path)) {
         let n = 2
         while (lookup(path + "-" + n))
