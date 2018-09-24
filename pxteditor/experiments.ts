7bd2445ef088bbed6fed2b54643cd5183b01e46b
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-09-24 16:58:06 -0700

    add more options

--- pxteditor/experiments.ts
+++ pxteditor/experiments.ts
@@ -21,11 +21,26 @@ namespace pxt.editor.experiments {
     export function all(): Experiment[] {
         const ids = pxt.appTarget.appTheme.experiments;
         if (!ids) return [];
-        return [
-            <Experiment>{
+        return <Experiment[]>[
+            {
+                id: "print",
+                name: lf("Print Code"),
+                description: lf("Print the code from the current project")
+            },            
+            {
                 id: "greenScreen",
                 name: lf("Green screen"),
-                description: lf("Display a webcam video stream or a green background behind the code")
+                description: lf("Display a webcam video stream or a green background behind the code.")
+            },
+            {
+                id: "highContrast",
+                name: lf("High Contrast"),
+                description: lf("Color theme with higher color contrast.")
+            },
+            {
+                id: "allowPackageExtensions",
+                name: lf("Editor Extensions"),
+                description: lf("Allow Extensions to add buttons in the editor.")
             }
         ].filter(experiment => ids.indexOf(experiment.id) > -1);
     }
