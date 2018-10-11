ab1f66b384c380a7669c23616a0574e55fb30b6e
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-11 00:12:03 -0700

    starting write protocol

--- pxtlib/webble.ts
+++ pxtlib/webble.ts
@@ -25,13 +25,15 @@ namespace pxt.webBluetooth {
     const PF_CHARACTERISTIC_UUID = 'e97d3b10-251d-470a-a062-fa1922dfa9a8';
     const PF_REGION_INFO = 0x00;
     const PF_FLASH_DATA = 0x01;
+    const PF_PACKET_OUT_OF_ORDER = 0xAA;
+    const PF_PACKET_WRITTEN = 0xFF;
     const PF_END_OF_TRANSMISSION = 0x02;
     const PF_MICROBIT_STATUS = 0xEE;
     const PF_MICROBIT_RESET = 0xFF;
+    const PF_MICROBIT_MODE_PAIRING = 0x01;
     const PF_REGION_SOFTDEVICE = 0x00;
     const PF_REGION_DAL = 0x01;
     const PF_REGION_MAKECODE = 0x02;
-    const PF_REGIONNOTIFICATION = 0;
 
     class MicrobitDevice {
         device: BluetoothDevice = undefined;
@@ -100,6 +102,40 @@ namespace pxt.webBluetooth {
                 }]
             }
 
+            let pfCharacteristic: BluetoothRemoteGATTCharacteristic;
+            let flashOffset = 0;
+
+            function flashData(offset: number, data: Uint8Array) {
+                const chunk = new Uint8Array(16);
+                chunk[0] = PF_FLASH_DATA;
+
+                chunk[3] = 0; // packet number
+                chunk[1] = offset >> 8;
+                chunk[2] = offset;
+                for (let i = 0; i < 16; i++)
+                    chunk[4 + i] = data[i];
+                pfCharacteristic.writeValue(chunk);
+
+                chunk[3] = 1; // packet number
+                chunk[1] = offset >> 24;
+                chunk[2] = offset >> 16;
+                for (let i = 0; i < 16; i++)
+                    chunk[4 + i] = data[16 + i];
+                pfCharacteristic.writeValue(chunk);
+
+                chunk[3] = 2; // packet number
+                chunk[1] = 0;
+                chunk[2] = 0;
+                for (let i = 0; i < 16; i++)
+                    chunk[4 + i] = data[32 + i];
+                pfCharacteristic.writeValue(chunk);
+
+                chunk[3] = 3; // packet number
+                for (let i = 0; i < 16; i++)
+                    chunk[4 + i] = data[48 + i];
+                pfCharacteristic.writeValue(chunk);
+            }
+
             return new Promise((resolve, reject) => {
                 pxt.debug(`ble: connecting to partial flash service of ${this.device.name || "?"}`);
                 this.gattServer.getPrimaryService(PF_SERVICE_UUID)
@@ -108,8 +144,9 @@ namespace pxt.webBluetooth {
                         return service.getCharacteristic(PF_CHARACTERISTIC_UUID);
                     }).then(characteristic => {
                         pxt.debug(`ble: starting notifications of partial flash`)
-                        characteristic.startNotifications();
-                        characteristic.addEventListener('characteristicvaluechanged', (ev) => {
+                        pfCharacteristic = characteristic;
+                        pfCharacteristic.startNotifications();
+                        pfCharacteristic.addEventListener('characteristicvaluechanged', (ev) => {
                             const dataView: DataView = (<any>event.target).value;
                             const packet = new Uint8Array(dataView.buffer);
                             const cmd = packet[0];
@@ -118,9 +155,15 @@ namespace pxt.webBluetooth {
                                     state.version = packet[1];
                                     state.mode = packet[2];
                                     pxt.debug(`ble: flash service version ${state.version} mode ${state.mode}`)
+                                    if (state.mode != PF_MICROBIT_MODE_PAIRING) {
+                                        pxt.debug(`ble: reset into pairing mode`)
+                                        characteristic.writeValue(new Uint8Array([PF_MICROBIT_RESET, PF_MICROBIT_MODE_PAIRING]));
+                                        // user needs to try again TODO
+                                        return;
+                                    }
 
                                     pxt.debug(`ble: reading DAL region`)
-                                    characteristic.writeValue(new Uint8Array([PF_REGION_INFO, PF_REGION_DAL]));
+                                    pfCharacteristic.writeValue(new Uint8Array([PF_REGION_INFO, PF_REGION_DAL]));
                                     break;
                                 case PF_REGION_INFO:
                                     const region = state.regions[packet[1]] = {
@@ -129,11 +172,26 @@ namespace pxt.webBluetooth {
                                         hash: pxt.Util.toHex(packet.slice(10))
                                     };
                                     pxt.debug(`read region ${packet[1]} start ${region.start.toString(16)} end ${region.end.toString(16)} hash ${region.hash}`)
-                                    if (PF_REGION_DAL) {
+                                    if (packet[1] == PF_REGION_DAL) {
                                         pxt.debug(`ble: reading makecode region`)
-                                        characteristic.writeValue(new Uint8Array([PF_REGION_INFO, PF_REGION_MAKECODE]));
+                                        pfCharacteristic.writeValue(new Uint8Array([PF_REGION_INFO, PF_REGION_MAKECODE]));
+                                    } else if (packet[1] == PF_REGION_MAKECODE) {
+                                        // test hashes 
+                                        // ready to flash the data in 4 chunks
+                                        flashData(flashOffset, new Uint8Array(64));
                                     }
                                     break;
+                                case PF_FLASH_DATA:
+                                    switch (packet[1]) {
+                                        case PF_PACKET_OUT_OF_ORDER:
+                                            pxt.debug(`ble: packet out of order`);
+                                            break;
+                                        case PF_PACKET_WRITTEN:
+                                            pxt.debug(`ble: packet written`);
+                                            flashOffset += 64;
+                                            flashData(flashOffset, new Uint8Array(64));
+                                            break;
+                                    }
                                 default:
                                     pxt.debug(`ble: unknown message ${pxt.Util.toHex(packet)}`);
                                     break;
