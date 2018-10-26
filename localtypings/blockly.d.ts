6aadd64d3bd33881abc38839a3370f92d02a2948
Sam El-Husseini <samelh@microsoft.com>
2018-10-26 17:38:10 -0400

    Update centerOnBlock blockly dts (#4929)

--- localtypings/blockly.d.ts
+++ localtypings/blockly.d.ts
@@ -933,7 +933,7 @@ declare namespace Blockly {
         scrollCenter(): void;
         setScale(scale: number): void;
         highlightBlock(id: string): void;
-        centerOnBlock(id: string): void;
+        centerOnBlock(id: string, animate?: boolean): void;
         glowBlock(id: string, state: boolean): void;
         glowStack(id: string, state: boolean): void;
         undo(redo?: boolean): void;
