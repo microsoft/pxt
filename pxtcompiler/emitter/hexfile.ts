3beba8ea47739b7088d082f43c6d4305652e87de
Sam El-Husseini <samelh@microsoft.com>
2018-11-26 09:12:09 -0800

    Move polyfil into pxtlib so it’s part of the runner.

--- pxtcompiler/emitter/hexfile.ts
+++ pxtcompiler/emitter/hexfile.ts
@@ -1153,3 +1153,20 @@ __flash_checksums:
 
     }
 }
+
+// Inject Math imul polyfill
+if (!Math.imul) {
+    // for explanations see:
+    // http://stackoverflow.com/questions/3428136/javascript-integer-math-incorrect-results (second answer)
+    // (but the code below doesn't come from there; I wrote it myself)
+    // TODO use Math.imul if available
+    Math.imul = function (a: number, b: number): number {
+        const ah = (a >>> 16) & 0xffff;
+        const al = a & 0xffff;
+        const bh = (b >>> 16) & 0xffff;
+        const bl = b & 0xffff;
+        // the shift by 0 fixes the sign on the high part
+        // the final |0 converts the unsigned value into a signed value
+        return ((al * bl) + (((ah * bl + al * bh) << 16) >>> 0) | 0);
+    }
+}
