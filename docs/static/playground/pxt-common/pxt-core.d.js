af1110d315e08f2db065e0d02c1dda61935cc69a
Chase Mortensen <chasemor@uw.edu>
2018-12-07 13:31:50 -0800

    Removed extra whitespace

--- docs/static/playground/pxt-common/pxt-core.d.js
+++ docs/static/playground/pxt-common/pxt-core.d.js
@@ -17,7 +17,6 @@ interface Array<T> {
     //% blockId="array_push" block="%list| add value %value| to end" blockNamespace="arrays"
     push(item: T): void;
 
-
     /**
       * Concatenates the values with another array.
       * @param arr The other array that is being concatenated with
