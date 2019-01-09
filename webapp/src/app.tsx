df2b6bb7f44c0779666ac7233c78b7fa037338b3
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-09 13:10:59 -0800

    updated generatred name

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -1247,7 +1247,7 @@ export class ProjectView
                 resolve(this.exportProjectToFileAsync()
                     .then(blob => screenshot.encodeBlobAsync(img, blob))
                     .then(img => {
-                        const fn = pkg.genFileName(".png");
+                        const fn = pkg.genFileName("-cart.png");
                         pxt.BrowserUtils.browserDownloadDataUri(img, fn);
                     }))
             }
