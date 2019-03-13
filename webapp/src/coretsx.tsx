d7e330594b85d1fc8ce39cfa444784828df77a97
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-03-12 23:19:14 -0700

    move usb pairing dialogs to tsx (#5339)
    * move usb pairing dialogs to tsx
    
    * fix dark theming of script manager

--- webapp/src/coretsx.tsx
+++ webapp/src/coretsx.tsx
@@ -91,7 +91,7 @@ export class CoreDialog extends React.Component<core.PromptOptions, CoreDialogSt
     render() {
         const options = this.props;
         const { inputValue } = this.state;
-        const size: any = options.size || 'small';
+        const size: any = options.size === undefined ? 'small' : options.size;
 
         const buttons = options.buttons ? options.buttons.filter(b => !!b) : [];
         buttons.forEach(btn => {
