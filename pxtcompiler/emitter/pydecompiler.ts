d9e247369de69ff810141f87c1759347150d32fa
darzu <daryl.zuniga@gmail.com>
2019-03-21 12:38:28 +0100

    handle anonymous functions

--- pxtcompiler/emitter/pydecompiler.ts
+++ pxtcompiler/emitter/pydecompiler.ts
@@ -31,12 +31,18 @@ namespace ts.pxtc.decompiler {
         return result
     }
 
+    ///
+    /// SUPPORT
+    ///
     const INDENT = "  "
     function indent(lvl: number): (s: string) => string {
         return s => `${INDENT.repeat(lvl)}${s}`
     }
-    let indent1 = indent(1)
+    const indent1 = indent(1)
 
+    ///
+    /// NODES & CRUFT
+    ///
     function emitNode(s: ts.Node): string[] {
         switch (s.kind) {
             case ts.SyntaxKind.SyntaxList:
@@ -51,7 +57,6 @@ namespace ts.pxtc.decompiler {
                 return emitStmtWithNewlines(s as ts.Statement)
         }
     }
-
     function emitStmtWithNewlines(s: ts.Statement): string[] {
         let out: string[] = [];
 
@@ -67,6 +72,10 @@ namespace ts.pxtc.decompiler {
 
         return out;
     }
+
+    ///
+    /// STATEMENTS
+    ///
     function emitStmt(s: ts.Statement): string[] {
         // TODO(dz): why does the type system not recognize this as discriminated unions?
         switch (s.kind) {
@@ -75,7 +84,7 @@ namespace ts.pxtc.decompiler {
             case ts.SyntaxKind.ClassDeclaration:
                 return emitClassStmt(s as ts.ClassDeclaration)
             case ts.SyntaxKind.ExpressionStatement:
-                return [emitExpStmt(s as ts.ExpressionStatement)]
+                return emitExpStmt(s as ts.ExpressionStatement)
             case ts.SyntaxKind.FunctionDeclaration:
                 return emitFuncDecl(s as ts.FunctionDeclaration)
             case ts.SyntaxKind.IfStatement:
@@ -90,8 +99,8 @@ namespace ts.pxtc.decompiler {
         }
     }
     function emitIf(s: ts.IfStatement): string[] {
-        let cond = emitExp(s.expression)
-        let out = [`if ${cond}:`];
+        let [cond, condSup] = emitExp(s.expression)
+        let out = condSup.concat([`if ${cond}:`])
 
         let th = emitStmt(s.thenStatement)
             .map(indent1)
@@ -110,10 +119,10 @@ namespace ts.pxtc.decompiler {
         return out;
     }
     function emitVarStmt(s: ts.VariableStatement): string[] {
-        // console.log("s.declarationList")
-        // console.log(s.declarationList)
         let decls = s.declarationList.declarations;
-        return decls.map(emitVarDecl)
+        return decls
+            .map(emitVarDecl)
+            .reduce((p, c) => p.concat(c), [])
     }
     // TODO map names from camel case to snake case
     function emitClassStmt(s: ts.ClassDeclaration): string[] {
@@ -154,31 +163,38 @@ namespace ts.pxtc.decompiler {
     function emitClassMem(s: ts.ClassElement): string[] {
         switch (s.kind) {
             case ts.SyntaxKind.PropertyDeclaration:
-                return [emitPropDecl(s as ts.PropertyDeclaration)]
+                return emitPropDecl(s as ts.PropertyDeclaration)
             case ts.SyntaxKind.MethodDeclaration:
                 return emitFuncDecl(s as ts.MethodDeclaration)
             default:
                 return ["UNKNOWN ClassElement " + s.kind]
         }
     }
-    function emitPropDecl(s: ts.PropertyDeclaration): string {
+    function emitPropDecl(s: ts.PropertyDeclaration): string[] {
         // TODO(dz)
         let nm = s.name.getText()
         if (s.initializer) {
-            let init = emitExp(s.initializer)
-            return `${nm} = ${init}`
+            let [init, initSup] = emitExp(s.initializer)
+            return initSup.concat([`${nm} = ${init}`])
         }
         else {
             // can't do declerations without initilization in python
-            return ""
+            return []
         }
     }
-    function emitExpStmt(s: ts.ExpressionStatement): string {
+    function emitExpStmt(s: ts.ExpressionStatement): string[] {
         // TODO
-        let exp = emitExp(s.expression)
-        return `${exp}`
+        let [exp, expSup] = emitExp(s.expression)
+        return expSup.concat([`${exp}`])
     }
-    function emitFuncDecl(s: ts.FunctionDeclaration | ts.MethodDeclaration): string[] {
+    function emitBlock(s: ts.Block): string[] {
+        let stmts = s.statements
+            .map(emitStmt)
+            .reduce((p, c) => p.concat(c), [])
+            .filter(s => s)
+        return stmts
+    }
+    function emitFuncDecl(s: ts.FunctionDeclaration | ts.MethodDeclaration | ts.FunctionExpression, name: string = null): string[] {
         // TODO
         let paramList: string[] = []
 
@@ -193,14 +209,14 @@ namespace ts.pxtc.decompiler {
 
         let out = []
 
-        out.push(`def ${s.name.getText()}(${params}):`)
+        let fnName = name || s.name.getText()
+        out.push(`def ${fnName}(${params}):`)
 
-        let stmts = s.body.statements
-            .map(emitStmt)
-            .reduce((p, c) => p.concat(c), [])
-            .filter(s => s)
+        let stmts = emitBlock(s.body)
         if (stmts.length) {
             out = out.concat(stmts.map(indent1))
+        } else {
+            out.push(indent1("pass")) // cannot have an empty body
         }
 
         return out
@@ -211,51 +227,86 @@ namespace ts.pxtc.decompiler {
         let typ = s.type.getText()
         return `${nm}:${typ}`
     }
-    function emitVarDecl(s: ts.VariableDeclaration): string {
+    function emitVarDecl(s: ts.VariableDeclaration): string[] {
         let decl = s.name.getText();
         if (s.initializer) {
-            let exp = emitExp(s.initializer);
-            return `${decl} = ${exp}`
+            let [exp, expSup] = emitExp(s.initializer);
+            let declStmt = `${decl} = ${exp}`
+            return expSup.concat(declStmt)
         } else {
             // can't do declerations without initilization in python
-            return ""
+            return []
         }
     }
-    function emitBinExp(s: ts.BinaryExpression): string {
-        let left = emitExp(s.left)
+
+    ///
+    /// EXPRESIONS
+    ///
+    type ExpRes = [/*expresion:*/string, /*supportingStatements:*/string[]]
+    function asExpRes(str: string): ExpRes {
+        return [str, []]
+    }
+    function emitBinExp(s: ts.BinaryExpression): ExpRes {
+        let [left, leftSup] = emitExp(s.left)
         let op = s.operatorToken.getText() // TODO translate operators
-        let right = emitExp(s.right)
-        return `${left} ${op} ${right}`;
+        let [right, rightSup] = emitExp(s.right)
+        let sup = leftSup.concat(rightSup)
+        return [`${left} ${op} ${right}`, sup];
     }
-    function emitDotExp(s: ts.PropertyAccessExpression): string {
-        let left = emitExp(s.expression)
+    function emitDotExp(s: ts.PropertyAccessExpression): ExpRes {
+        let [left, leftSup] = emitExp(s.expression)
         let right = s.name.getText()
-        return `${left}.${right}`;
+        return [`${left}.${right}`, leftSup];
     }
-    function emitCallExp(s: ts.CallExpression): string {
-        let fn = emitExp(s.expression)
-        let args = s.arguments
+    function emitCallExp(s: ts.CallExpression): ExpRes {
+        let [fn, fnSup] = emitExp(s.expression)
+        let argExps = s.arguments
             .map(emitExp)
+        let args = argExps
+            .map(([a, aSup]) => a)
             .join(", ")
-        return `${fn}(${args})`
+        let sup = argExps
+            .map(([a, aSup]) => aSup)
+            .reduce((p, c) => p.concat(c), fnSup)
+        return [`${fn}(${args})`, sup]
     }
-    function emitExp(s: ts.Expression): string {
+
+    let nextFnNum = 0
+    function nextFnName() {
+        // TODO add sync lock
+        return `function_${nextFnNum++}`
+    }
+    function emitFnExp(s: ts.FunctionExpression): ExpRes {
+        // TODO handle case if body is only 1 line
+        let fnName = nextFnName()
+        let fnDef = emitFuncDecl(s, fnName)
+
+        return [fnName, fnDef]
+    }
+    function emitExp(s: ts.Expression): ExpRes {
         switch (s.kind) {
-            case ts.SyntaxKind.TrueKeyword:
-                return "True"
-            case ts.SyntaxKind.FalseKeyword:
-                return "False"
             case ts.SyntaxKind.BinaryExpression:
                 return emitBinExp(s as ts.BinaryExpression)
             case ts.SyntaxKind.PropertyAccessExpression:
                 return emitDotExp(s as ts.PropertyAccessExpression)
-            case ts.SyntaxKind.ThisKeyword:
-                return "self"
             case ts.SyntaxKind.CallExpression:
                 return emitCallExp(s as ts.CallExpression)
+            case ts.SyntaxKind.FunctionExpression:
+                return emitFnExp(s as ts.FunctionExpression)
+            case ts.SyntaxKind.TrueKeyword:
+                return asExpRes("True")
+            case ts.SyntaxKind.FalseKeyword:
+                return asExpRes("False")
+            case ts.SyntaxKind.ThisKeyword:
+                return asExpRes("self")
+            case ts.SyntaxKind.Identifier:
+            case ts.SyntaxKind.NumericLiteral:
+            case ts.SyntaxKind.StringLiteral:
+            case ts.SyntaxKind.ArrayLiteralExpression:
             default:
                 // TODO handle more expressions
-                return s.getText()
+                return asExpRes(s.getText())
+            // throw Error("Unknown expression: " + s.kind)
         }
     }
 }
