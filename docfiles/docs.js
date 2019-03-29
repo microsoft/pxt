439c14fdaf68e47430c605f1e45d594d42421ff9
Alejandro Toledo <alex@ArturoMookMusic.lan>
2019-03-28 18:11:06 -0600

    Fix Header Color

--- docfiles/docs.js
+++ docfiles/docs.js
@@ -261,7 +261,7 @@ function renderSnippets() {
 
 function responsiveResize(){
     // Add Responsive Adjustments depending on screen size 
-    $( window ).resize(function() {
+    window.addEventListener('resize', evt => {
         // Size width 1144px
         if (window.innerWidth > 1143){
             $('.ui.sidebar').sidebar('hide');
