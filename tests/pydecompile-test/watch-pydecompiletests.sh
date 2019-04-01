427615d4222d93c7453b22e91218d2f2240eb8bd
darzu <daryl.zuniga@gmail.com>
2019-04-01 10:05:44 +0200

    rename

--- tests/pydecompile-test/watch-pydecompiletests.sh
+++ tests/pydecompile-test/watch-pydecompiletests.sh
@@ -8,16 +8,16 @@ PXT_BUILD="$PXT_DIR/built"
 RUNNER_IN="$PXT_DIR/node_modules/typescript/lib/typescript.js $PXT_BUILD/pxtlib.js $PXT_BUILD/pxtcompiler.js $PXT_BUILD/pxtpy.js $PXT_BUILD/pxtsim.js $PXT_BUILD/tests/pydecompile-test/pydecompilerunner.js"
 RUNNER="$PXT_BUILD/tests/pydecompile-test/runner.js"
 
-function update_results() {
+function test_pydecompiler() {
     ls $PXT_DIR/tests/pydecompile-test/**/*.local* | xargs rm
     cat $RUNNER_IN > $RUNNER
     $PXT_DIR/node_modules/.bin/mocha $RUNNER --reporter dot
 }
 
-update_results
+test_pydecompiler
 
 fswatch $RUNNER_IN --one-per-batch | while read filename; do
-    update_results
+    test_pydecompiler
 done
 
 
