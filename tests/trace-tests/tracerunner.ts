2c30fcdbfc0fe31da4faff4280d57f37914f9251
darzu <daryl.zuniga@gmail.com>
2019-04-01 18:18:44 +0200

    trying to compile ts

--- tests/trace-tests/tracerunner.ts
+++ tests/trace-tests/tracerunner.ts
@@ -122,6 +122,38 @@ function fail(msg: string) {
     chai.assert(false, msg);
 }
 
+
+function compileAndRunTsAsync(filename: string): Promise<string> {
+    let cOpts: ts.CompilerOptions = {
+        // TODO(dz): check these options
+        noEmitOnError: true,
+        noImplicitAny: true,
+        target: ts.ScriptTarget.ES5,
+        module: ts.ModuleKind.CommonJS
+    }
+    let program = ts.createProgram([filename], cOpts);
+    let emitResult = program.emit();
+
+    let allDiagnostics = ts
+        .getPreEmitDiagnostics(program)
+        .concat(emitResult.diagnostics);
+
+    let diagMsgs: string[] = []
+    allDiagnostics.forEach(diagnostic => {
+        if (diagnostic.file) {
+            let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
+            let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+            console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+        } else {
+            console.log(
+                `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`
+            );
+        }
+    });
+
+    let exitCode = emitResult.emitSkipped ? 1 : 0;
+}
+
 function compileAndRunStsAsync(filename: string): Promise<string> {
     const pkg = new pxt.MainPackage(new CompileHost(filename));
 
