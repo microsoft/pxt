3f9437a059eb6f8e02ccde33887019ac720499e6
Joseph Wunderlich <jwunderl@uw.edu>
2019-04-09 10:17:53 -0700

    add some compile / decompile tests for strngi comparisons

---
+++ tests/decompile-test/cases/logic_string_comparisons.ts
@@ -0,0 +1,13 @@
+let a = "hello";
+let b = "world";
+
+if ("" == "") { }
+else if (a != b) { }
+
+if (a < b) { }
+else if (a >= b) { }
+
+if (a <= b) { }
+else if (a > b) { }
+
+if (!(a == "" && !(b == "world" && true))) { }
