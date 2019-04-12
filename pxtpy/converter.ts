cfa8cadad16d185de661f1cfd2d0e8be4777319c
Michał Moskal <michal@moskal.me>
2019-04-12 09:36:15 -0700

    Fix enum errors in py2ts (#5465)

--- pxtpy/converter.ts
+++ pxtpy/converter.ts
@@ -1083,7 +1083,7 @@ namespace pxt.py {
                     return B.mkGroup(res)
                 } else 
                 */
-                if (currIteration < 2) {
+                if (currIteration == 0) {
                     return B.mkText("/* skip for now */")
                 }
                 unifyTypeOf(n.targets[0], fd.pyRetType)
