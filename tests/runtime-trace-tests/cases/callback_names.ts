dcf49f26da96ba7fe57f1c3f68cbb5fcf97b450e
darzu <daryl.zuniga@gmail.com>
2019-04-12 12:39:24 +0200

    more elaborate test

--- tests/runtime-trace-tests/cases/callback_names.ts
+++ tests/runtime-trace-tests/cases/callback_names.ts
@@ -1,9 +1,36 @@
-function onEvent(callback: (a: number, b: number) => boolean) {
-    console.log(callback(3, 5) ? "t" : "f")
+// e.g.: controller.anyButton.onEvent(ControllerButtonEvent.Pressed, handler)
+enum ControllerButtonEvent {
+    Pressed,
+    Released
 }
-onEvent((b: number) => {
+class Button {
+    public onEvent(eventType: ControllerButtonEvent, callback: (a: number, b: number) => boolean) {
+        console.log(callback(3, 5) ? "t" : "f")
+    }
+}
+class Controller {
+    anyButton: Button
+    constructor() {
+        this.anyButton = new Button()
+    }
+}
+
+let controller: Controller = new Controller()
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, (b: number) => {
     return true
 })
-onEvent((b: number) => {
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function some_callback(b: number) {
     return false
 })
+
+// e.g.: sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, handler)
+enum SpriteKind {
+    Player,
+    Projectile
+}
+class Sprites {
+    public onOverlap(kind1: SpriteKind, kind2: SpriteKind, handler: () => void) {
+    }
+}
+let sprites = new Sprites()
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, () => { });
