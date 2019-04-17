001d999ef694c7dd04507521e24df3a8a5999680
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-17 06:35:31 -0700

    fix monacto toolbox when no filters

--- webapp/src/monaco.tsx
+++ webapp/src/monaco.tsx
@@ -1601,18 +1601,20 @@ export class Editor extends toolboxeditor.ToolboxEditor {
             const w1 = (f1.attributes.weight || 50) + (f1.attributes.advanced ? 0 : 1000);
             return w2 > w1 ? 1 : -1;
         }).map(fn => {
-            let fnState = filters.defaultState;
-            if (filters && filters.fns && filters.fns[fn.name] !== undefined) {
-                fnState = filters.fns[fn.name];
-            } else if (filters && filters.blocks && fn.attributes.blockId && filters.blocks[fn.attributes.blockId] !== undefined) {
-                fnState = filters.blocks[fn.attributes.blockId];
-            } else if (categoryState !== undefined) {
-                fnState = categoryState;
-            }
-            if (fnState == pxt.editor.FilterState.Hidden) return undefined;
+            let monacoBlockDisabled = false;
+            if (filters) {
+                let fnState = filters.defaultState;
+                if (filters && filters.fns && filters.fns[fn.name] !== undefined) {
+                    fnState = filters.fns[fn.name];
+                } else if (filters && filters.blocks && fn.attributes.blockId && filters.blocks[fn.attributes.blockId] !== undefined) {
+                    fnState = filters.blocks[fn.attributes.blockId];
+                } else if (categoryState !== undefined) {
+                    fnState = categoryState;
+                }
+                if (fnState == pxt.editor.FilterState.Hidden) return undefined;
 
-            const monacoBlockDisabled = fnState == pxt.editor.FilterState.Disabled;
-            return monacoEditor.getMonacoBlock(fn, ns, color, monacoBlockDisabled);
+                monacoBlockDisabled = fnState == pxt.editor.FilterState.Disabled;
+            }
         })
         monacoEditor.attachMonacoBlockAccessibility(monacoBlocks);
     }
