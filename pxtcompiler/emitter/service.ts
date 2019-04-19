18d9f611b5e24fc55687c2a7b6f5b3e120147082
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-18 21:48:25 -0700

    fix tabs, snakification in toolbox

--- pxtcompiler/emitter/service.ts
+++ pxtcompiler/emitter/service.ts
@@ -1137,6 +1137,9 @@ namespace ts.pxtc.service {
             fnName = getSymbolName(n.symbol) || n.name.getText();
         }
 
+        if (python)
+            fnName = snakify(fnName).toLowerCase();
+
         const attrs = fn.attributes;
         const checker = service && service.getProgram().getTypeChecker();
         const args = n.parameters ? n.parameters.filter(param => !param.initializer && !param.questionToken).map(param => {
@@ -1304,7 +1307,7 @@ namespace ts.pxtc.service {
                 let n = fnName || "fn";
                 if (functionCount++ > 0) n += functionCount;
                 n = snakify(n).toLowerCase();
-                preStmt += `def ${n}${functionArgument}:\n    ${returnValue || "pass"}\n`;
+                preStmt += `def ${n}${functionArgument}:\n\t${returnValue || "pass"}\n`;
                 return n;
             } else {
                 let functionArgument = "()";
@@ -1320,7 +1323,7 @@ namespace ts.pxtc.service {
         function emitFn(n: string): string {
             if (python) {
                 if (n) n = snakify(n).toLowerCase();
-                preStmt += `def ${n}():\n    pass\n`;
+                preStmt += `def ${n}():\n\tpass\n`;
                 return n;
             } else return `function () {}`;
         }
