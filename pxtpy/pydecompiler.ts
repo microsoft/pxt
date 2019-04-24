f1258abd5e1dc5bcd038f6a851c1e122b231410e
darzu <daryl.zuniga@gmail.com>
2019-04-24 16:09:01 +0200

    reserve python3 built-ins

--- pxtpy/pydecompiler.ts
+++ pxtpy/pydecompiler.ts
@@ -56,8 +56,8 @@ namespace pxt.py {
         // let ls = ts.createLanguageService(lhost) // TODO
         let file = prog.getSourceFile(filename)
         // TODO(dz):
-        // let reservedWords = pxt.U.toSet(getReservedNmes(), s => s)
-        let [renameMap, globalNames] = ts.pxtc.decompiler.buildRenameMap(prog, file)
+        let reservedWords = pxt.U.toSet(getReservedNmes(), s => s)
+        let [renameMap, globalNames] = ts.pxtc.decompiler.buildRenameMap(prog, file, reservedWords)
         let allSymbols = pxtc.getApiInfo(prog)
         let symbols = pxt.U.mapMap(allSymbols.byQName,
             // filter out symbols from the .ts corrisponding to this file
@@ -81,9 +81,34 @@ namespace pxt.py {
             return env.shift()
         }
         function getReservedNmes(): string[] {
-            // TODO(dz) get full list
-            const reservedNames = "def list print"
-            return reservedNames.split(" ").filter(s => s)
+            const reservedNames = ['ArithmeticError', 'AssertionError', 'AttributeError',
+                'BaseException', 'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning',
+                'ChildProcessError', 'ConnectionAbortedError', 'ConnectionError',
+                'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning', 'EOFError',
+                'Ellipsis', 'EnvironmentError', 'Exception', 'False', 'FileExistsError',
+                'FileNotFoundError', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError',
+                'ImportError', 'ImportWarning', 'IndentationError', 'IndexError',
+                'InterruptedError', 'IsADirectoryError', 'KeyError', 'KeyboardInterrupt', 'LookupError',
+                'MemoryError', 'NameError', 'None', 'NotADirectoryError', 'NotImplemented',
+                'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning',
+                'PermissionError', 'ProcessLookupError', 'RecursionError', 'ReferenceError',
+                'ResourceWarning', 'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration',
+                'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit',
+                'TabError', 'TimeoutError', 'True', 'TypeError', 'UnboundLocalError',
+                'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError',
+                'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'ZeroDivisionError', '_',
+                '__build_class__', '__debug__', '__doc__', '__import__', '__loader__', '__name__',
+                '__package__', '__spec__', 'abs', 'all', 'any', 'ascii', 'bin', 'bool',
+                'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex',
+                'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval',
+                'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr',
+                'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int',
+                'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals', 'map',
+                'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow',
+                'print', 'property', 'quit', 'range', 'repr', 'reversed', 'round', 'set',
+                'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple',
+                'type', 'vars', 'zip']
+            return reservedNames;
         }
         function tryGetPyName(exp: ts.BindingPattern | ts.PropertyName | ts.EntityName | ts.PropertyAccessExpression): string {
             if (!exp.getSourceFile())
