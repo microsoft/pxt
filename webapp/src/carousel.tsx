8a175ce1d1cc8d822d93bbfb4b5042e08f392007
Chase Mortensen <C_Mortensen@live.com>
2019-05-29 17:09:17 -0700

    Switch icon for carousel if language is set to RTL

--- webapp/src/carousel.tsx
+++ webapp/src/carousel.tsx
@@ -87,7 +87,7 @@ export class Carousel extends data.Component<ICarouselProps, ICarouselState> {
         return <div className="ui carouselouter">
             <span role="button" className={"carouselarrow left aligned" + (leftDisabled ? " arrowdisabled" : "")} aria-label={lf("See previous")}
                 tabIndex={leftDisabled ? -1 : 0} onClick={this.onLeftArrowClick} onKeyDown={sui.fireClickOnEnter} ref={this.handleArrowRefs}>
-                <sui.Icon icon="circle angle left" />
+                <sui.Icon icon={"circle angle " + (!pxt.Util.isUserLanguageRtl() ? "left" : "right")} />
             </span>
             <div className="carouselcontainer" ref={this.handleContainerRef}>
                 <div className="carouselbody" ref={this.handleDragSurfaceRef}>
@@ -101,7 +101,7 @@ export class Carousel extends data.Component<ICarouselProps, ICarouselState> {
             </div>
             <span role="button" className={"carouselarrow right aligned" + (rightDisabled ? " arrowdisabled" : "")} aria-label={lf("See more")}
                 tabIndex={rightDisabled ? -1 : 0} onClick={this.onRightArrowClick} onKeyDown={sui.fireClickOnEnter} ref={this.handleArrowRefs}>
-                <sui.Icon icon="circle angle right" />
+                <sui.Icon icon={"circle angle " + (!pxt.Util.isUserLanguageRtl() ? "right" : "left")} />
             </span>
         </div>
     }
