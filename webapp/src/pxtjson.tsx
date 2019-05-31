5d1cb7d887cecf05008a0f7830c690e7eed3ff3b
Chase Mortensen <C_Mortensen@live.com>
2019-05-31 15:05:09 -0700

    Allow for config values to be null

--- webapp/src/pxtjson.tsx
+++ webapp/src/pxtjson.tsx
@@ -107,7 +107,6 @@ export class Editor extends srceditor.Editor {
         // update cfg
         if (Object.keys(cfg).length) {
             if (!this.config.yotta) this.config.yotta = {};
-            Object.keys(cfg).filter(k => cfg[k] === null).forEach(k => delete cfg[k]);
             this.config.yotta.config = Util.jsonUnFlatten(cfg);
         } else {
             if (this.config.yotta) {
