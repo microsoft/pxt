266c743bf9fc2714ff1b5b98c07afaf8a523e94d
Richard Knoll <riknoll@microsoft.com>
2019-06-03 11:29:13 -0700

    Fixing telemetry exception logging

--- pxtblocks/blocklycompiler.ts
+++ pxtblocks/blocklycompiler.ts
@@ -1485,7 +1485,11 @@ namespace pxt.blocks {
             blockInfo.blocks
                 .forEach(fn => {
                     if (e.stdCallTable[fn.attributes.blockId]) {
-                        pxt.reportError("blocks", "function already defined", { "details": fn.attributes.blockId });
+                        pxt.reportError("blocks", "function already defined", {
+                            "details": fn.attributes.blockId,
+                            "qualifiedName": fn.qName,
+                            "sourceFile": fn.fileName,
+                        });
                         return;
                     }
                     e.renames.takenNames[fn.namespace] = true;
