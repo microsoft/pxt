bf42f265809987604cb494493d6ff72f2acaccb7
Jacob Bovee <boveejacob@gmail.com>
2019-06-07 11:29:25 -0700

    Parses questions json and builds a wizard

--- webapp/src/snippetBuilder.tsx
+++ webapp/src/snippetBuilder.tsx
@@ -9,16 +9,14 @@ type ISettingsProps = pxt.editor.ISettingsProps;
 
 export interface CreateSnippetBuilderState {
     visible?: boolean;
-    markdownContent?: string;
+    output?: string;
     projectView?: pxt.editor.IProjectView;
     answers?: any;
+    currentQuestion: number;
+    defaults: any;
 }
 
-const defaults: any = {
-    $nameToken: 'mySprite',
-};
-
-const exampleBlock: string = `\`\`\`blocks
+const exampleBlock: string = `
 enum SpriteKind {
     Player,
     Projectile,
@@ -26,7 +24,7 @@ enum SpriteKind {
     Enemy
 }
 
-let $nameToken = sprites.create(img\`
+let $spriteName = sprites.create(img\`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -43,8 +41,7 @@ let $nameToken = sprites.create(img\`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-\`, SpriteKind.Player)
-\`\`\``;
+\`, SpriteKind.Player)`;
 
 export class CreateSnippetBuilder extends data.Component<ISettingsProps, CreateSnippetBuilderState> {
     static cachedFunctionTypes: pxt.FunctionEditorTypeInfo[] = null;
@@ -53,29 +50,53 @@ export class CreateSnippetBuilder extends data.Component<ISettingsProps, CreateS
         super(props);
         this.state = {
             visible: false,
-            markdownContent: exampleBlock,
+            output: exampleBlock,
             answers: {},
+            currentQuestion: 0,
+            defaults: {},
         };
         this.hide = this.hide.bind(this);
         this.cancel = this.cancel.bind(this);
         this.confirm = this.confirm.bind(this);
+        this.backPage = this.backPage.bind(this);
+        this.nextPage = this.nextPage.bind(this);
     }
 
-    replaceTokens(markdownContent: string) {
-        const { answers } = this.state;
-        let output = markdownContent;
+    buildDefaults() {
+        const defaults: any = {};
+
+        for (const question of questions) {
+            const { inputs } = question;
+            for (const input of inputs) {
+                const { defaultAnswer, answerToken } = input;
+                defaults[answerToken] = defaultAnswer;
+            }
+        }
+
+        this.setState({ defaults });
+    }
+
+    componentDidMount() {
+        this.buildDefaults();
+    }
+
+    replaceTokens(output: string) {
+        const { answers, defaults } = this.state;
+        let cleanOutput = output;
         const tokens = Object.keys(defaults);
 
         for (let token of tokens) {
             if (answers[token]) {
-                output = output.replace(token, answers[token]);
+                cleanOutput = cleanOutput.split(`$${token}`).join(answers[token]);
             }
             else {
-                output = output.replace(token, defaults[token]);
+                cleanOutput = cleanOutput.split(`$${token}`).join(defaults[token]);
             }
         }
 
-        return output;
+        return `\`\`\`blocks
+        ${cleanOutput}
+        \`\`\``;
     }
 
     hide() {
@@ -101,6 +122,24 @@ export class CreateSnippetBuilder extends data.Component<ISettingsProps, CreateS
         this.hide();
     }
 
+    changePage(increment: 1 | -1) {
+        const { currentQuestion } = this.state;
+        this.setState({ currentQuestion: currentQuestion + increment });
+    }
+
+    nextPage() {
+        const { currentQuestion } = this.state;
+        this.changePage(1);
+        console.log('current question =>', currentQuestion + 1);
+        if (questions[currentQuestion + 1].output) {
+            this.setState({ output: `${this.state.output}\n${questions[currentQuestion + 1].output}`});
+        }
+    }
+
+    backPage() {
+        this.changePage(-1);
+    }
+
     textInputOnChange = (answerKey: string) => (v: string) => {
         const answers = this.state.answers;
         answers[answerKey] = v;
@@ -110,8 +149,20 @@ export class CreateSnippetBuilder extends data.Component<ISettingsProps, CreateS
     }
 
     renderCore() {
-        const { visible, markdownContent, projectView, answers } = this.state;
+        const { visible, output, projectView, answers, currentQuestion } = this.state;
         const actions: sui.ModalButton[] = [
+            {
+                label: lf("Back"),
+                onclick: this.backPage,
+                icon: 'arrow left',
+                className: 'arrow left',
+            },
+            {
+                label: lf("Next"),
+                onclick: this.nextPage,
+                icon: 'arrow right',
+                className: 'arrow right',
+            },
             {
                 label: lf("Cancel"),
                 onclick: this.hide,
@@ -126,8 +177,9 @@ export class CreateSnippetBuilder extends data.Component<ISettingsProps, CreateS
             }
         ];
 
+        const currQ = questions[currentQuestion];
+
         // TODO: Workspace component
-        // TODO: Make markdown editable by input
         return (
             <sui.Modal isOpen={visible} className="snippetBuilder" size="large"
                 closeOnEscape={false} closeIcon={false} closeOnDimmerClick={false} closeOnDocumentClick={false}
@@ -135,21 +187,117 @@ export class CreateSnippetBuilder extends data.Component<ISettingsProps, CreateS
             >
                 <div>
                     <div className="list">
-                        <div>
+                        {currQ &&
                             <div>
-                                <sui.Input
-                                    onChange={this.textInputOnChange('$nameToken')}
-                                    label={'What is the name of your sprite?'}
-                                    value={answers['$nameToken'] || ''}
-                                />
+                                <div>{currQ.title}</div>
+                                <div className='horizontal list'>
+                                    {currQ.inputs.map((input: any) =>
+                                        <div>
+                                            <sui.Input
+                                                label={input.label && input.label}
+                                                value={answers[input.answerToken] || ''}
+                                                onChange={this.textInputOnChange(input.answerToken)}
+                                            />
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        </div>
+                        }
                     </div>
-                    <div id="functionEditorWorkspace">
-                        {projectView && <md.MarkedContent markdown={this.replaceTokens(markdownContent)} parent={projectView} />}
+                    <div id="snippetBuilderOutput">
+                        {projectView && <md.MarkedContent markdown={this.replaceTokens(output)} parent={projectView} />}
                     </div>
                 </div>
             </sui.Modal>
         )
     }
 }
+
+const questions: any = [
+    {
+        "title": "What should your sprite be called?",
+        "inputs": [{
+                "answerToken": "spriteName",
+                "defaultAnswer": "mySprite",
+                "type": "text"
+        }],
+        "output": "",
+        "goTo": 2
+    },
+    {
+        "title": "Where should your sprite be placed?",
+        "inputs": [
+            {
+                "label": "x:",
+                "defaultAnswer": 80,
+                "answerToken": "xLocation",
+                "type": "number"
+            },
+            {
+                "label": "y:",
+                "defaultAnswer": 60,
+                "answerToken": "yLocation",
+                "type": "number"
+            }
+        ],
+        "output": "$spriteName.setPosition($xLocation,$yLocation)",
+        "goTo": 3
+    }, 
+    // {
+    //     "title": "What should your sprite look like?",
+    //     "inputs": [
+    //         {
+    //             "answerToken": "spriteImage",
+    //             "type": "spriteEditor"
+    //         }
+    //     ],
+    //     "ouput": "${spriteName} = sprites.setImage(img```${spriteImage}```)",
+    //     "goTo": 1
+    // },
+    {
+        "title": "What kind of sprite should this be?",
+        "inputs": [
+            {
+                "answerToken": "spriteKind",
+                "defaultAnswer": "SpriteKind.Player",
+                "type": {
+                    "options": [
+                        {
+                            "value": 0,
+                            "label": "Player"
+                        },
+                        {
+                            "value": 1,
+                            "label": "Projectile"
+                        },
+                        {
+                            "value": 2,
+                            "label": "Food"   
+                        },
+                        {
+                            "value": 3,
+                            "label": "Enemy"
+                        }
+                    ]
+                }
+            }
+        ],
+        "output": "$spriteName.setType($spriteKind)",
+        "goTo": {
+            "4": { "spriteKind": 0 },
+            "default": null
+        }
+    },
+    {
+        "title": "How many lives should your player have?",
+        "inputs": [
+            {
+                "answerToken": "gameLives",
+                "defaultAnswer": 3,
+                "type": "number",
+                // "label": "img`livesImg` x ${gameLives}"
+            }
+        ],
+        "output": "info.setLife($gameLives)"
+    }
+]
