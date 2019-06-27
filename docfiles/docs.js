6ee3bbf8dba08d05f43aeeb8810c7ec5146baf7b
Alejandro Toledo <atole333@live.com>
2019-06-27 10:41:06 -0500

    Adding fix for Edge

--- docfiles/docs.js
+++ docfiles/docs.js
@@ -477,7 +477,7 @@ function setStickyColumn() {
                     scrollTop: $(heading).offset().top
                 }, 1000);
                 if (window.navigator.userAgent.indexOf('Edge') !== -1) {
-                    heading.nextElementSibling.scrollIntoView(false);
+                    heading.scrollIntoView();
                 }
                 // resets default style for mobile behavior
                 if (window.innerWidth < 970) {
