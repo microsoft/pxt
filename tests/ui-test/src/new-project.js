0fa2c22357a865bd70d8017851dcbf3f413288fe
Steven Huang (Beyondsoft) <v-jianch@microsoft.com>
2019-07-08 18:49:38 +0800

    update new-project.js

--- tests/ui-test/src/new-project.js
+++ tests/ui-test/src/new-project.js
@@ -1,5 +1,4 @@
 import { DomObject } from './lib/dom-object';
-import { By } from 'selenium-webdriver';
 import assert from "assert";
 
 class NewProjectPage extends DomObject {
@@ -8,10 +7,8 @@ class NewProjectPage extends DomObject {
 
         console.debug("Start testGetCodeSource()");
 
-        await this.click('.newprojectcard', '.openproject');
-        await this.click(By.className('icon close remove circle '));
+        await this.click('.newprojectcard', '.openproject', '.closeIcon');
 
-        console.debug(`Input`);
         await this.click('.openproject');
 
         await this.sendKeys('input#projectNameInput', 'Project1');
@@ -20,13 +17,12 @@ class NewProjectPage extends DomObject {
         await driver.sleep(2000);
 
         let text = await this.getText("[aria-label='My Projects'] .carouselitem:nth-child(2) .header");
-        console.debug(`The header text in the first DIV of 'My Projects' is "${text}"`);
+
+        console.debug(`The header text in the first DIV of 'My Projects' is: ${text}"`);
 
         assert.equal(text, "Project1");
 
-        await this.click('.content .header');
-        console.log("We find it!")
-
+        await this.click("[aria-label='My Projects'] .carouselitem:nth-child(2)");
 
         return true;
     }
