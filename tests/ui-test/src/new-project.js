172d7c6aae8eb9f09299bfbc2ae08e7d14ab2d3b
Liu Jie <jieliu2000@live.com>
2019-07-11 08:10:25 +0000

    Refactor and clean code

--- tests/ui-test/src/new-project.js
+++ tests/ui-test/src/new-project.js
@@ -8,21 +8,30 @@ class NewProjectPage extends DomObject {
 
         console.debug("Start testGetCodeSource()");
 
-        await this.click('.newprojectcard', '.openproject', '.closeIcon');
-        await this.takeScreenshot('editorPage1');
-        assert.equal(await this.getAttribute('.openproject'), 'Home');
+        //Create a new project
+        await this.click('.newprojectcard', '.openproject');
 
+        //Close the new project name input popup
+        await this.click('.closeIcon');
+
+        await this.takeScreenshot('editorPage1');
+
+        //Assert the title is correct
+        assert.equal(await this.getAttribute('.openproject', 'title'), 'Home');
+        
+        //Open the project name input popup again
         await this.click('.openproject');
+        //Name the new project to "Project1"
         await this.sendKeys('input#projectNameInput', 'Project1');
 
+        //Confirm the changes
         await this.click('.button.positive');
         
+        //Validate the project is correctly created
         let text = await this.getText("[aria-label='My Projects'] .carouselitem:nth-child(2) .header");
         console.debug(`The header text in the first DIV of 'My Projects' is "${text}"`);
 
         assert.equal(text, "Project1");
-
-        await this.click('.content .header');
 
         return true;
     }
