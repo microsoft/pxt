1045f25c2bd8acd35b0cdf51141804fd11cc9e91
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-08-26 11:04:53 -0700

    updated readme

--- pxtlib/initprj.ts
+++ pxtlib/initprj.ts
@@ -36,7 +36,7 @@ test:
 - [ ] Add a reference for your blocks here
 - [ ] Add "icon.png" image (300x200) in the root folder
 - [ ] Add "- beta" to the GitHub project description if you are still iterating it.
-- [ ] Turn on your automated build on https://travis-ci.org
+- [ ] Turn on your automated builds with GitHub Actions or Travis-CI https://travis-ci.org
 - [ ] Use "pxt bump" to create a tagged release on GitHub
 - [ ] On GitHub, create a new file named LICENSE. Select the MIT License template.
 - [ ] Get your package reviewed and approved @DOCS@extensions/approval
