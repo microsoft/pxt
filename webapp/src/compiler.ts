b62904c2100b434f85533464eee0a33c4e91cac3
Michał Moskal <michal@moskal.me>
2019-09-05 14:28:10 -0700

    Emit ldlit in Thumb in the correct order (#5869)
    * Emit ldlit in Thumb in the correct order
    
    * Log output.txt to console if there are errors

--- webapp/src/compiler.ts
+++ webapp/src/compiler.ts
@@ -23,8 +23,10 @@ function setDiagnostics(diagnostics: pxtc.KsDiagnostic[]) {
         output += `${category} TS${diagnostic.code}: ${ts.pxtc.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}\n`;
     }
 
-    if (output) // helpful for debugging
-        pxt.debug(output);
+    // people often ask about where to look for errors, and many will look in the console
+    // the output.txt has usability issues
+    if (output)
+        pxt.log(output);
 
     if (!output)
         output = U.lf("Everything seems fine!\n")
