cb090612339f56c237b0c968b2417363b2c4f4d2
Steven Huang (Beyondsoft) <v-jianch@microsoft.com>
2019-09-06 18:31:17 +0800

    Update search-box.js

--- tests/ui-test/src/search-box.js
+++ tests/ui-test/src/search-box.js
@@ -1,19 +1,39 @@
 import { DomObject } from './lib/dom-object';
 import assert from "assert";
-const actions = driver.actions();
 
-class SearchBox extends DomObject {
+class BlocklyToolBox extends DomObject {
 
     async searchBox() {
-        await this.sendKeys('#blocklySearchInputField', 'Basic');
+        await this.sendKeys('.blocklySearchInputField', 'Basic');
+        await driver.sleep(2000);
+        let searchText = await this.getText('[id="blocklySearchLabel"]');
+        console.debug(`This is the blockly search label: ${searchText}`);
+        assert.equal(searchText, "7 result matching 'basic'");
+
+    }
+    async removeBlocks() {
+        await this.dragAndDrop('g.blocklyDraggable:nth-child(4)', '.blocklyToolboxDiv');
+
+    }
+    async matchBlocks() {
+        await this.click('[role="treeitem"]:nth-child(1) .blocklyTreeRow');
+        await this.dragAndDrop('g.blocklyDraggable:nth-child(4)', 'g.blocklyDraggable:nth-child(1)');
+        await this.click('.fullscreen-button');
+        await this.catchScreenShot('LaunchInFullScreen');
 
     }
 
     test() {
         it('Get various blocks', async () => {
             return await this.searchBox();
-        })
+        });
+        it('Remove the block', async () => {
+            return await this.removeBlocks();
+        });
+        it('Match blocks', async () => {
+            return await this.matchBlocks();
+        });
     }
 }
 
-export let searchBox = new SearchBox();
+export let blocklyToolBox = new BlocklyToolBox();
