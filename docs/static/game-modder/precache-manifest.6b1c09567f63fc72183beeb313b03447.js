62698e82ca4660043be7ba9242d70b0fadc123e1
darzu <daryl.zuniga@gmail.com>
2019-09-09 15:56:01 -0700

    game-modder: finally fix ios scrolling

--- docs/static/game-modder/precache-manifest.6b1c09567f63fc72183beeb313b03447.js
+++ docs/static/game-modder/precache-manifest.6b1c09567f63fc72183beeb313b03447.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c23d76f550e6580e767fc32145de5b0",
+    "revision": "f9d573d78365c4da002e3aabba0bd03f",
     "url": "/static/game-modder/index.html"
   },
   {
-    "revision": "bdcc0f48cb55062be068",
+    "revision": "23ee6534bc5adb5b8696",
     "url": "/static/game-modder/static/css/main.c2a309ca.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/game-modder/static/js/2.32b899f4.chunk.js"
   },
   {
-    "revision": "bdcc0f48cb55062be068",
-    "url": "/static/game-modder/static/js/main.de1300b7.chunk.js"
+    "revision": "23ee6534bc5adb5b8696",
+    "url": "/static/game-modder/static/js/main.21197101.chunk.js"
   },
   {
     "revision": "21fc42b4348f8b2bfd43",
