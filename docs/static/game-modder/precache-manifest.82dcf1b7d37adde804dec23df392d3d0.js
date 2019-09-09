bddf05a822c10473375f1bb3ade711234ccf419a
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2019-09-09 14:33:28 -0700

    update game-modder static to use embedded bin.js strings (#5881)

---
+++ docs/static/game-modder/precache-manifest.82dcf1b7d37adde804dec23df392d3d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289c986874f88b993d9e6f19a39c0ab8",
+    "url": "/static/game-modder/index.html"
+  },
+  {
+    "revision": "813f3521e454422216c5",
+    "url": "/static/game-modder/static/css/main.d7cfe8e4.chunk.css"
+  },
+  {
+    "revision": "4e7533b267d4b864efaa",
+    "url": "/static/game-modder/static/js/2.32b899f4.chunk.js"
+  },
+  {
+    "revision": "813f3521e454422216c5",
+    "url": "/static/game-modder/static/js/main.7181645c.chunk.js"
+  },
+  {
+    "revision": "21fc42b4348f8b2bfd43",
+    "url": "/static/game-modder/static/js/runtime~main.ed37a638.js"
+  }
+]);
