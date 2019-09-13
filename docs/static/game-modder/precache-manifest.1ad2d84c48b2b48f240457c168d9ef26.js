396057a3926a034f92073a3859e0ae06db56e075
shakao <34112083+shakao@users.noreply.github.com>
2019-09-13 15:14:33 -0700

    game-modder: additional instrumentation, fixes (#5901)

--- docs/static/game-modder/precache-manifest.1ad2d84c48b2b48f240457c168d9ef26.js
+++ docs/static/game-modder/precache-manifest.1ad2d84c48b2b48f240457c168d9ef26.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d7b86caa1be605368e657ad90679ffb9",
+    "revision": "413755f87108ef607b86af86a2501349",
     "url": "/static/game-modder/index.html"
   },
   {
-    "revision": "7700c926c6bae90cd9c0",
-    "url": "/static/game-modder/static/css/main.c5ff4778.chunk.css"
+    "revision": "25a65abf88d276dddc24",
+    "url": "/static/game-modder/static/css/main.f3a6415b.chunk.css"
   },
   {
     "revision": "6ca46f0f803680255468",
     "url": "/static/game-modder/static/js/2.7b15abf0.chunk.js"
   },
   {
-    "revision": "7700c926c6bae90cd9c0",
-    "url": "/static/game-modder/static/js/main.b031c67c.chunk.js"
+    "revision": "25a65abf88d276dddc24",
+    "url": "/static/game-modder/static/js/main.6284ecda.chunk.js"
   },
   {
     "revision": "21fc42b4348f8b2bfd43",
