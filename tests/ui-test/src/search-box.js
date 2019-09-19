16540f504982328d16c785ee616c3d7f1c1597d9
Steven Huang (Beyondsoft) <v-jianch@microsoft.com>
2019-09-19 16:30:23 +0800

    Update search-box.js

--- tests/ui-test/src/search-box.js
+++ tests/ui-test/src/search-box.js
@@ -1,38 +1,51 @@
 import { DomObject } from './lib/dom-object';
 import assert from "assert";
-
+import By from 'selenium-webdriver';
 class BlocklyToolBox extends DomObject {
 
     async searchBox() {
         await this.sendKeys('.blocklySearchInputField', 'Basic');
+
         await driver.sleep(2000);
+
         let searchText = await this.getText('[id="blocklySearchLabel"]');
+
         console.debug(`This is the blockly search label: ${searchText}`);
+
         assert.equal(searchText, "7 result matching 'basic'");
 
     }
-    async removeBlocks() {
-        await this.dragAndDrop('g.blocklyDraggable:nth-child(4)', '.blocklyToolboxDiv');
-
-    }
-    async matchBlocks() {
-        await this.click('[role="treeitem"]:nth-child(1) .blocklyTreeRow');
+    async dragBlocks() {
         await this.dragAndDrop('g.blocklyDraggable:nth-child(4)', 'g.blocklyDraggable:nth-child(1)');
+
+        await this.click('[role="treeitem"]:nth-child(2) .blocklyTreeRow');
+
+        await this.dragAndDrop('g.blocklyDraggable:nth-child(8)', '.blocklyToolboxDiv');
+        
+        await this.click('#blocksEditorToolbox > div.blocklyTreeRoot > div > div:nth-child(2) > div.blocklyTreeRow');
+
+        await this.contextClick('#blocksEditor > div > svg:nth-child(7) > g > g.blocklyBlockCanvas > g:nth-child(2) > path.blocklyPath.blocklyBlockBackground');
+
+        await this.click('div.goog-menuitem');
+
+        await this.switchToWindow();
+        
         await this.click('.fullscreen-button');
+
         await this.catchScreenShot('LaunchInFullScreen');
 
     }
+    
 
     test() {
         it('Get various blocks', async () => {
             return await this.searchBox();
         });
-        it('Remove the block', async () => {
-            return await this.removeBlocks();
+
+        it('Drag and drop blocks', async () => {
+            return await this.dragBlocks();
         });
-        it('Match blocks', async () => {
-            return await this.matchBlocks();
-        });
+
     }
 }
 
