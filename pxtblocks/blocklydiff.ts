0fbd97ea65bd6233efd362b8b178b8a3d02ac9c0
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-09-20 11:44:24 -0700

    more logging

--- pxtblocks/blocklydiff.ts
+++ pxtblocks/blocklydiff.ts
@@ -19,6 +19,8 @@ namespace pxt.blocks {
         const oldTopBlocks = oldWs.getTopBlocks(false);
         const newBlocks = newWs.getAllBlocks();
         const newTopBlocks = newWs.getTopBlocks(false);
+        console.log(`blocks`, newBlocks.map(b => b.id));
+        console.log(newBlocks)
 
         // locate deleted and added blocks
         const deletedTopBlocks = oldTopBlocks.filter(b => !newWs.getBlockById(b.id));
@@ -29,25 +31,65 @@ namespace pxt.blocks {
         // clone new workspace into rendering workspace
         const ws = pxt.blocks.initRenderingWorkspace();
         pxt.blocks.domToWorkspaceNoEvents(Blockly.Xml.textToDom(newXml), ws);
+        const todoBlocks = Util.toDictionary(ws.getAllBlocks(), b => b.id);
+        log('start')
 
-        //const ws = pxt.blocks.loadWorkspaceXml(pxt.blocks.saveWorkspaceXml(newWs), true);
-
-        // all blocks are disabled
-        ws.getAllBlocks().forEach(b => disable(b, true));
-
-        // 1. topblocks
-        // add deleted top blocks and disable them
+        // 1. deleted top blocks
         deletedTopBlocks.forEach(b => {
+            console.log(`deleted top ${b.id}`)
+            done(b);
             const bdom = Blockly.Xml.blockToDom(b, false);
             const b2 = Blockly.Xml.domToBlock(bdom, ws);
-            disable(b2, false);
             col(b2, "#aa0000");
         });
-        // find added top blocks and mark them as added: TODO better marking
+        log('deleted top')
+
+        // 2. added top blocks
         addedTopBlocks.map(b => ws.getBlockById(b.id)).forEach(b => {
-            disable(b, false);
+            console.log(`added top ${b.id}`)
+            done(b);
             col(b, "#00aa00");
         });
+        log('added top')
+
+        // 3. moved blocks
+        Util.values(todoBlocks).filter(b => {
+            const oldb = oldWs.getBlockById(b.id); // extra block created in added step
+            if (!oldb)
+                return false;
+
+            const newPrevious = b.getPreviousBlock();
+            // connection already already processed
+            if (newPrevious && !todoBlocks[newPrevious.id])
+                return false;
+            const newNext = b.getNextBlock();
+            // already processed
+            if (newNext && !todoBlocks[newNext.id])
+                return false;
+
+            const oldPrevious = oldb.getPreviousBlock();
+            if (!oldPrevious && !newPrevious) return false; // no connection
+            if (!!oldPrevious != !!newPrevious // new connection
+                || oldPrevious.id != newPrevious.id) // new connected blocks
+                return true;
+            const oldNext = oldb.getNextBlock();
+            if (!oldNext && !newNext) return false; // no connection
+            if (!!oldNext != !!newNext // new connection
+                || oldNext.id != newNext.id) // new connected blocks
+                return true;
+            return false;
+        }).forEach(b => {
+            console.log(`moved ${b.id}`)
+            b.setColour("#0000aa");
+            delete todoBlocks[b.id]
+        })
+        log('moved')
+
+        // all unmodifed blocks are greyed out
+        Util.values(todoBlocks).forEach(b => b.setColour("#c0c0c0"));
+
+        // make sure everything is rendered
+        ws.getAllBlocks().forEach(forceRender);
 
         // final render
         const svg = pxt.blocks.renderWorkspace(options);
@@ -61,16 +103,22 @@ namespace pxt.blocks {
             modified: 0
         }
 
+        function forceRender(b: Blockly.Block) {
+            const a = <any>b;
+            a.rendered = false;
+            b.inputList.forEach(i => i.fieldRow.forEach(f => {
+                delete f.fieldGroup_; // force field rendering
+                delete (<any>f).backgroundColour_;
+                delete (<any>f).borderColour_;
+            }));
+        }
+
         function col(b: Blockly.Block, c: string) {
             vis(b, t => t.setColour(c));
         }
 
-        function disable(b: Blockly.Block, v: boolean) {
-            vis(b, t => {
-                t.setDeletable(false);
-                t.setEditable(false);
-                t.setDisabled(v);
-            });
+        function done(b: Blockly.Block) {
+            vis(b, t => { delete todoBlocks[t.id]; });
         }
 
         function vis(b: Blockly.Block, f: (b: Blockly.Block) => void) {
@@ -79,6 +127,10 @@ namespace pxt.blocks {
                 f(t);
                 t = t.getNextBlock();
             }
+        }
+
+        function log(msg: string) {
+            console.log(`${msg}:`, Object.keys(todoBlocks))
         }
     }
 }
