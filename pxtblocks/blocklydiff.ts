d5a524d84bf4cee78cd5b0285916a183180d3050
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-09-21 07:26:58 -0700

    don't splitsvg

--- pxtblocks/blocklydiff.ts
+++ pxtblocks/blocklydiff.ts
@@ -216,7 +216,7 @@ namespace pxt.blocks {
         Blockly.svgResize(ws);
 
         // final render
-        const svg = pxt.blocks.renderWorkspace(options.renderOptions || { emPixels: 18, splitSvg: true});
+        const svg = pxt.blocks.renderWorkspace(options.renderOptions);
 
         // and we're done
         const r: DiffResult = {
