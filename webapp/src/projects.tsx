0e778ea0abb93edb92e66ec53ade9a98061f90dc
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-09-24 14:44:48 -0700

    precise ticks

--- webapp/src/projects.tsx
+++ webapp/src/projects.tsx
@@ -714,7 +714,7 @@ export class ImportDialog extends data.Component<ISettingsProps, ImportDialogSta
     }
 
     private cloneGithub() {
-        pxt.tickEvent("projects.clonegithub", undefined, { interactiveConsent: true });
+        pxt.tickEvent("github.projects.clone", undefined, { interactiveConsent: true });
         this.hide();
         this.props.parent.showImportGithubDialog();
     }
