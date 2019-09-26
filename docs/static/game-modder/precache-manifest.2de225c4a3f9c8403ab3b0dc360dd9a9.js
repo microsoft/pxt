3b073b3b396000c03b14521ba27aec95c2de3323
shakao <34112083+shakao@users.noreply.github.com>
2019-09-26 13:56:46 -0700

    Play button CSS for iOS (#5952)

--- docs/static/game-modder/precache-manifest.2de225c4a3f9c8403ab3b0dc360dd9a9.js
+++ docs/static/game-modder/precache-manifest.2de225c4a3f9c8403ab3b0dc360dd9a9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0088d2bd63720ef66f2cff0d0b150052",
+    "revision": "fef8c14fbe1b861deff2743aa01e5c89",
     "url": "/static/game-modder/index.html"
   },
   {
-    "revision": "d957842d181ae43e1b82",
-    "url": "/static/game-modder/static/css/main.23ee8760.chunk.css"
+    "revision": "993d2d9fc032d34aca46",
+    "url": "/static/game-modder/static/css/main.bfb22948.chunk.css"
   },
   {
     "revision": "6ca46f0f803680255468",
     "url": "/static/game-modder/static/js/2.7b15abf0.chunk.js"
   },
   {
-    "revision": "d957842d181ae43e1b82",
-    "url": "/static/game-modder/static/js/main.17c7c15a.chunk.js"
+    "revision": "993d2d9fc032d34aca46",
+    "url": "/static/game-modder/static/js/main.dfbd27f3.chunk.js"
   },
   {
     "revision": "21fc42b4348f8b2bfd43",
