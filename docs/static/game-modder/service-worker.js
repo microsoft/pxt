d21d9f8237c9e4d300556bb8dbdb1abb23cdb147
Shannon Kao <shakao@microsoft.com>
2019-09-26 10:58:42 -0700

    Play button CSS for iOS

--- docs/static/game-modder/service-worker.js
+++ docs/static/game-modder/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/static/game-modder/precache-manifest.64cd40ca99b68d381b1ad2905785a7e8.js"
+  "/static/game-modder/precache-manifest.2de225c4a3f9c8403ab3b0dc360dd9a9.js"
 );
 
 self.addEventListener('message', (event) => {
