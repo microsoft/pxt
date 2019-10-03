4780d9da2683a555398bb0f79d3064badc2bc3e2
Franklin Tse <FranklinWhale@users.noreply.github.com>
2019-10-03 14:40:13 +0800

    Better order for short circuit

--- pxtcompiler/emitter/decompiler.ts
+++ pxtcompiler/emitter/decompiler.ts
@@ -2864,7 +2864,7 @@ ${output}</xml>`;
 
     function isAutoDeclaration(decl: VariableDeclaration) {
         if (decl.initializer) {
-            if (isTopLevelNode(decl) && decl.initializer.kind === SyntaxKind.NullKeyword) {
+            if (decl.initializer.kind === SyntaxKind.NullKeyword && isTopLevelNode(decl)) {
                 return true
             }
             const callInfo: pxtc.CallInfo = pxtc.pxtInfo(decl.initializer).callInfo
