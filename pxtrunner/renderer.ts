7aa1572607e548a4b3ff2a5e0a4d2a2649248df8
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-10-09 09:47:04 -0700

    insert localizatin string

--- pxtrunner/renderer.ts
+++ pxtrunner/renderer.ts
@@ -324,16 +324,15 @@ namespace pxt.runner {
             // TODO python
             const py: JQuery = undefined;// $('<code class="lang-python highlight"/>').text(sig);
             if (options.snippetReplaceParent) c = c.parent();
-            fillWithWidget(options, c, js, py, s, r, { showJs: true, showPy: true, hideGutter: true });
-
             // add an html widge that allows to translate the block
             if (pxt.Util.isTranslationMode()) {
-                const trs = $('div class="ui segment" />');
+                const trs = $('<div class="ui segment" />');
                 trs.append($('<div class="ui label">translate this block</div>'));
-                trs.append($('<div class="ui">').text(symbolInfo.attributes.block));
-                c.append(trs);
+                if (symbolInfo.attributes.block) trs.append($('<div class="ui message">').text(symbolInfo.attributes.block));
+                if (symbolInfo.attributes.jsDoc) trs.append($('<div class="ui message">').text(symbolInfo.attributes.jsDoc));
+                trs.insertAfter(c);
             }
-
+            fillWithWidget(options, c, js, py, s, r, { showJs: true, showPy: true, hideGutter: true });
         }, { package: options.package, snippetMode: true, aspectRatio: options.blocksAspectRatio });
     }
 
