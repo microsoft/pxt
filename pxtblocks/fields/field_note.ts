93ce62911a835b11e9b092b931dfbfe60e6c2918
Joseph Wunderlich <jwunderl@uw.edu>
2019-11-15 16:47:01 -0800

    remove border from black keys; move from rlf with lf comments to just lf; general clean up

--- pxtblocks/fields/field_note.ts
+++ pxtblocks/fields/field_note.ts
@@ -74,57 +74,57 @@ namespace pxtblockly {
     }
 
     const Notes: {[key: number]: NoteData} = {
-        28: {name: "C", prefixedName: "Low C", freq: 131},
-        29: {name: "C#", prefixedName: "Low C#", freq: 139},
-        30: {name: "D", prefixedName: "Low D", freq: 147},
-        31: {name: "D#", prefixedName: "Low D#", freq: 156},
-        32: {name: "E", prefixedName: "Low E", freq: 165},
-        33: {name: "F", prefixedName: "Low F", freq: 175},
-        34: {name: "F#", prefixedName: "Low F#", freq: 185},
-        35: {name: "G", prefixedName: "Low G", freq: 196},
-        36: {name: "G#", prefixedName: "Low G#", freq: 208},
-        37: {name: "A", prefixedName: "Low A", freq: 220},
-        38: {name: "A#", prefixedName: "Low A#", freq: 233},
-        39: {name: "B", prefixedName: "Low B", freq: 247},
+        28: {name: lf("C"), prefixedName: lf("Low C"), freq: 131},
+        29: {name: lf("C#"), prefixedName: lf("Low C#"), freq: 139},
+        30: {name: lf("D"), prefixedName: lf("Low D"), freq: 147},
+        31: {name: lf("D#"), prefixedName: lf("Low D#"), freq: 156},
+        32: {name: lf("E"), prefixedName: lf("Low E"), freq: 165},
+        33: {name: lf("F"), prefixedName: lf("Low F"), freq: 175},
+        34: {name: lf("F#"), prefixedName: lf("Low F#"), freq: 185},
+        35: {name: lf("G"), prefixedName: lf("Low G"), freq: 196},
+        36: {name: lf("G#"), prefixedName: lf("Low G#"), freq: 208},
+        37: {name: lf("A"), prefixedName: lf("Low A"), freq: 220},
+        38: {name: lf("A#"), prefixedName: lf("Low A#"), freq: 233},
+        39: {name: lf("B"), prefixedName: lf("Low B"), freq: 247},
 
-        40: {name: "C", prefixedName: "Middle C", freq: 262},
-        41: {name: "C#", prefixedName: "Middle C#", freq: 277},
-        42: {name: "D", prefixedName: "Middle D", freq: 294},
-        43: {name: "D#", prefixedName: "Middle D#", freq: 311},
-        44: {name: "E", prefixedName: "Middle E", freq: 330},
-        45: {name: "F", prefixedName: "Middle F", freq: 349},
-        46: {name: "F#", prefixedName: "Middle F#", freq: 370},
-        47: {name: "G", prefixedName: "Middle G", freq: 392},
-        48: {name: "G#", prefixedName: "Middle G#", freq: 415},
-        49: {name: "A", prefixedName: "Middle A", freq: 440},
-        50: {name: "A#", prefixedName: "Middle A#", freq: 466},
-        51: {name: "B", prefixedName: "Middle B", freq: 494},
+        40: {name: lf("C"), prefixedName: lf("Middle C"), freq: 262},
+        41: {name: lf("C#"), prefixedName: lf("Middle C#"), freq: 277},
+        42: {name: lf("D"), prefixedName: lf("Middle D"), freq: 294},
+        43: {name: lf("D#"), prefixedName: lf("Middle D#"), freq: 311},
+        44: {name: lf("E"), prefixedName: lf("Middle E"), freq: 330},
+        45: {name: lf("F"), prefixedName: lf("Middle F"), freq: 349},
+        46: {name: lf("F#"), prefixedName: lf("Middle F#"), freq: 370},
+        47: {name: lf("G"), prefixedName: lf("Middle G"), freq: 392},
+        48: {name: lf("G#"), prefixedName: lf("Middle G#"), freq: 415},
+        49: {name: lf("A"), prefixedName: lf("Middle A"), freq: 440},
+        50: {name: lf("A#"), prefixedName: lf("Middle A#"), freq: 466},
+        51: {name: lf("B"), prefixedName: lf("Middle B"), freq: 494},
 
-        52: {name: "C", prefixedName: "Tenor C", altPrefixedName: "High C", freq: 523},
-        53: {name: "C#", prefixedName: "Tenor C#", altPrefixedName: "High C#", freq: 554},
-        54: {name: "D", prefixedName: "Tenor D", altPrefixedName: "High D", freq: 587},
-        55: {name: "D#", prefixedName: "Tenor D#", altPrefixedName: "High D#", freq: 622},
-        56: {name: "E", prefixedName: "Tenor E", altPrefixedName: "High E", freq: 659},
-        57: {name: "F", prefixedName: "Tenor F", altPrefixedName: "High F", freq: 698},
-        58: {name: "F#", prefixedName: "Tenor F#", altPrefixedName: "High F#", freq: 740},
-        59: {name: "G", prefixedName: "Tenor G", altPrefixedName: "High G", freq: 784},
-        60: {name: "G#", prefixedName: "Tenor G#", altPrefixedName: "High G#", freq: 831},
-        61: {name: "A", prefixedName: "Tenor A", altPrefixedName: "High A", freq: 880},
-        62: {name: "A#", prefixedName: "Tenor A#", altPrefixedName: "High A#", freq: 932},
-        63: {name: "B", prefixedName: "Tenor B", altPrefixedName: "High B", freq: 988},
+        52: {name: lf("C"), prefixedName: lf("Tenor C"), altPrefixedName: lf("High C"), freq: 523},
+        53: {name: lf("C#"), prefixedName: lf("Tenor C#"), altPrefixedName: lf("High C#"), freq: 554},
+        54: {name: lf("D"), prefixedName: lf("Tenor D"), altPrefixedName: lf("High D"), freq: 587},
+        55: {name: lf("D#"), prefixedName: lf("Tenor D#"), altPrefixedName: lf("High D#"), freq: 622},
+        56: {name: lf("E"), prefixedName: lf("Tenor E"), altPrefixedName: lf("High E"), freq: 659},
+        57: {name: lf("F"), prefixedName: lf("Tenor F"), altPrefixedName: lf("High F"), freq: 698},
+        58: {name: lf("F#"), prefixedName: lf("Tenor F#"), altPrefixedName: lf("High F#"), freq: 740},
+        59: {name: lf("G"), prefixedName: lf("Tenor G"), altPrefixedName: lf("High G"), freq: 784},
+        60: {name: lf("G#"), prefixedName: lf("Tenor G#"), altPrefixedName: lf("High G#"), freq: 831},
+        61: {name: lf("A"), prefixedName: lf("Tenor A"), altPrefixedName: lf("High A"), freq: 880},
+        62: {name: lf("A#"), prefixedName: lf("Tenor A#"), altPrefixedName: lf("High A#"), freq: 932},
+        63: {name: lf("B"), prefixedName: lf("Tenor B"), altPrefixedName: lf("High B"), freq: 988},
 
-        64: {name: "C", prefixedName: "High C", freq: 1046},
-        65: {name: "C#", prefixedName: "High C#", freq: 1109},
-        66: {name: "D", prefixedName: "High D", freq: 1175},
-        67: {name: "D#", prefixedName: "High D#", freq: 1245},
-        68: {name: "E", prefixedName: "High E", freq: 1319},
-        69: {name: "F", prefixedName: "High F", freq: 1397},
-        70: {name: "F#", prefixedName: "High F#", freq: 1478},
-        71: {name: "G", prefixedName: "High G", freq: 1568},
-        72: {name: "G#", prefixedName: "High G#", freq: 1661},
-        73: {name: "A", prefixedName: "High A", freq: 1760},
-        74: {name: "A#", prefixedName: "High A#", freq: 1865},
-        75: {name: "B", prefixedName: "High B", freq: 1976}
+        64: {name: lf("C"), prefixedName: lf("High C"), freq: 1046},
+        65: {name: lf("C#"), prefixedName: lf("High C#"), freq: 1109},
+        66: {name: lf("D"), prefixedName: lf("High D"), freq: 1175},
+        67: {name: lf("D#"), prefixedName: lf("High D#"), freq: 1245},
+        68: {name: lf("E"), prefixedName: lf("High E"), freq: 1319},
+        69: {name: lf("F"), prefixedName: lf("High F"), freq: 1397},
+        70: {name: lf("F#"), prefixedName: lf("High F#"), freq: 1478},
+        71: {name: lf("G"), prefixedName: lf("High G"), freq: 1568},
+        72: {name: lf("G#"), prefixedName: lf("High G#"), freq: 1661},
+        73: {name: lf("A"), prefixedName: lf("High A"), freq: 1760},
+        74: {name: lf("A#"), prefixedName: lf("High A#"), freq: 1865},
+        75: {name: lf("B"), prefixedName: lf("High B"), freq: 1976}
     }
 
     const noteEnumRegex: RegExp = /^Note\.(.+)$/;
@@ -249,83 +249,9 @@ namespace pxtblockly {
                     else if (thisField.minNote_ >= 28 && thisField.maxNote_ <= 63) {
                         name = Notes[i].altPrefixedName || name;
                     }
-                    thisField.noteName_.push(ts.pxtc.Util.rlf(name));
+                    thisField.noteName_.push(name);
                     thisField.noteFreq_.push(Notes[i].freq);
                 }
-
-                // Do not remove this comment.
-                // lf("C")
-                // lf("C#")
-                // lf("D")
-                // lf("D#")
-                // lf("E")
-                // lf("F")
-                // lf("F#")
-                // lf("G")
-                // lf("G#")
-                // lf("A")
-                // lf("A#")
-                // lf("B")
-                // lf("Deep C")
-                // lf("Deep C#")
-                // lf("Deep D")
-                // lf("Deep D#")
-                // lf("Deep E")
-                // lf("Deep F")
-                // lf("Deep F#")
-                // lf("Deep G")
-                // lf("Deep G#")
-                // lf("Deep A")
-                // lf("Deep A#")
-                // lf("Deep B")
-                // lf("Low C")
-                // lf("Low C#")
-                // lf("Low D")
-                // lf("Low D#")
-                // lf("Low E")
-                // lf("Low F")
-                // lf("Low F#")
-                // lf("Low G")
-                // lf("Low G#")
-                // lf("Low A")
-                // lf("Low A#")
-                // lf("Low B")
-                // lf("Middle C")
-                // lf("Middle C#")
-                // lf("Middle D")
-                // lf("Middle D#")
-                // lf("Middle E")
-                // lf("Middle F")
-                // lf("Middle F#")
-                // lf("Middle G")
-                // lf("Middle G#")
-                // lf("Middle A")
-                // lf("Middle A#")
-                // lf("Middle B")
-                // lf("Tenor C")
-                // lf("Tenor C#")
-                // lf("Tenor D")
-                // lf("Tenor D#")
-                // lf("Tenor E")
-                // lf("Tenor F")
-                // lf("Tenor F#")
-                // lf("Tenor G")
-                // lf("Tenor G#")
-                // lf("Tenor A")
-                // lf("Tenor A#")
-                // lf("Tenor B")
-                // lf("High C")
-                // lf("High C#")
-                // lf("High D")
-                // lf("High D#")
-                // lf("High E")
-                // lf("High F")
-                // lf("High F#")
-                // lf("High G")
-                // lf("High G#")
-                // lf("High A")
-                // lf("High A#")
-                // lf("High B")
             }
         }
         /**
@@ -415,26 +341,24 @@ namespace pxtblockly {
             const readOnly = quietInput;
             (FieldNote as any).superClass_.showEditor_.call(this, e, false, readOnly);
 
-            let pianoWidth: number;
-            let pianoHeight: number;
-            let keyWidth: number = 22;
-            let keyHeight: number = 90;
-            let labelHeight: number = 24;
-            let prevNextHeight: number = 20;
-            let whiteKeyCounter: number = 0;
-            let soundingKeys: number = 0;
-            const selectedKeyColor: string = "yellowgreen";
+            let keyWidth = 22;
+            let keyHeight = 90;
+            let labelHeight = 24;
+            let prevNextHeight = 20;
+            let whiteKeyCounter = 0;
+            let soundingKeys = 0;
+            const selectedKeyColor = "yellowgreen";
             const thisField = this;
             //  Record windowSize and scrollOffset before adding the piano.
             const windowSize = goog.dom.getViewportSize();
             const editorWidth = windowSize.width;
             const piano: Array<goog.ui.CustomButton> = [];
 
-            let pagination: boolean = false;
-            let mobile: boolean = false;
+            let pagination = false;
+            let mobile = false;
             //  initializate
-            pianoWidth = keyWidth * (this.nKeys_ - (this.nKeys_ / 12 * 5));
-            pianoHeight = keyHeight + labelHeight;
+            let pianoWidth = keyWidth * (this.nKeys_ - (this.nKeys_ / 12 * 5));
+            let pianoHeight = keyHeight + labelHeight;
 
             //  Create the piano using Closure (CustomButton).
             for (let i = 0; i < this.nKeys_; i++) {
@@ -478,7 +402,7 @@ namespace pxtblockly {
                     octaveCounter++;
                 const key = piano[i];
                 //  What color is i key
-                const bgColor = (isWhite(i)) ? "white" : "black";
+                const bgColor = isWhite(i) ? "white" : "black";
                 const width = getKeyWidth(i);
                 const height = getKeyHeight(i);
                 let position = getPosition(i);
@@ -486,7 +410,7 @@ namespace pxtblockly {
                 //  modify original position in pagination
                 if (pagination && i >= 12)
                     position -= 7 * octaveCounter * keyWidth;
-                const style = getKeyStyle(bgColor, width, height, position + leftPosition, topPosition, isWhite(i) ? 1000 : 1001, isWhite(i) ? this.colour_ : "black", mobile);
+                const style = getKeyStyle(bgColor, width, height, position + leftPosition, topPosition, isWhite(i) ? 1000 : 1001, this.colour_, mobile);
                 key.setContent(style);
                 key.setId(this.noteName_[i]);
                 key.render(pianoDiv);
@@ -561,9 +485,9 @@ namespace pxtblockly {
                 //  right arrow - next button
                 script.textContent = ">";
 
-                const Npages = this.nKeys_ / 12;
+                const pageCount = this.nKeys_ / 12;
                 const changePage = (next: boolean) => {
-                    if (currentPage == (next ? Npages - 1 : 0)) {
+                    if (currentPage == (next ? pageCount - 1 : 0)) {
                         scriptLabel.textContent = "Octave #" + (currentPage + 1);
                         return;
                     }
