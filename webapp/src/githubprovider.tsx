8109373b57510fa8f12c8d13fb2a190d9de76e3b
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-11-19 12:47:33 -0800

    show dialog when using github oauth

--- webapp/src/githubprovider.tsx
+++ webapp/src/githubprovider.tsx
@@ -33,18 +33,70 @@ export class GithubProvider extends cloudsync.ProviderBase {
         this.loginCheck()
         let p = Promise.resolve();
         if (!this.token()) {
-            // auth flow
-            const cl = pxt.appTarget && pxt.appTarget.cloud && pxt.appTarget.cloud.cloudProviders && pxt.appTarget.cloud.cloudProviders[this.name];
-            if (cl)
-                p = p.then(() => this.oauthLoginAsync());
-            else
-                p = p.then(() => this.showGithubLoginAsync());
+            // auth flow if github provider is prsent
+            const useOAuth = pxt.appTarget
+                && pxt.appTarget.cloud
+                && pxt.appTarget.cloud.cloudProviders
+                && !!pxt.appTarget.cloud.cloudProviders[this.name];
+            p = p.then(() => this.showLoginAsync(useOAuth));
         }
         return p.then(() => { return { accessToken: this.token() } as cloudsync.ProviderLoginResponse; });
 
     }
 
-    private oauthLoginAsync(): Promise<void> {
+    private showLoginAsync(useOAuth: boolean): Promise<void> {
+        pxt.tickEvent("github.login.dialog");
+        const useToken = !useOAuth;
+        let input: HTMLInputElement;
+        return core.confirmAsync({
+            header: lf("Sign in with GitHub"),
+            hideCancel: true,
+            hasCloseIcon: true,
+            helpUrl: "/github",
+            agreeLbl: lf("Sign in"),
+            onLoaded: (el) => {
+                input = el.querySelectorAll('input')[0] as HTMLInputElement;
+            },
+            jsx: <div className="ui form">
+                <p>{lf("Host your code on GitHub and work together with friends on projects.")}</p>
+                {useToken && <ol>
+                    <li>
+                        {lf("Navigate to: ")}
+                        <a href="https://github.com/settings/tokens/new" target="_blank" rel="noopener noreferrer">
+                            {lf("GitHub token generation page")}
+                        </a>
+                    </li>
+                    <li>
+                        {lf("Put something like 'MakeCode {0}' in description", pxt.appTarget.name)}
+                    </li>
+                    <li>
+                        {lf("Select either '{0}' or '{1}' scope, depending which repos you want to edit from here", "repo", "public_repo")}
+                    </li>
+                    <li>
+                        {lf("Click generate token, copy it, and paste it below.")}
+                    </li>
+                </ol>}
+                {useToken && <div className="ui field">
+                    <label id="selectUrlToOpenLabel">{lf("Paste GitHub token here:")}</label>
+                    <input type="url" tabIndex={0} autoFocus aria-labelledby="selectUrlToOpenLabel" placeholder="0123abcd..." className="ui blue fluid"></input>
+                </div>}
+            </div>,
+        }).then(res => {
+            if (!res) {
+                pxt.tickEvent("github.login.cancel");
+                return Promise.resolve()
+            } else {
+                if (useOAuth)
+                    return this.oauthRedirectAsync();
+                else {
+                    const hextoken = input.value.trim();
+                    return this.saveAndValidateTokenAsync(hextoken);
+                }
+            }
+        })
+    }
+
+    private oauthRedirectAsync(): Promise<void> {
         core.showLoading("ghlogin", lf("Logging you in to GitHub..."))
         const self = window.location.href.replace(/#.*/, "")
         const state = ts.pxtc.Util.guidGen();
@@ -82,54 +134,6 @@ export class GithubProvider extends cloudsync.ProviderBase {
     setNewToken(token: string) {
         super.setNewToken(token);
         pxt.github.token = token;
-    }
-
-    private showGithubLoginAsync() {
-        pxt.tickEvent("github.token.dialog");
-        let input: HTMLInputElement;
-        return core.confirmAsync({
-            header: lf("Sign in to GitHub"),
-            hideCancel: true,
-            hasCloseIcon: true,
-            helpUrl: "/github/token",
-            onLoaded: (el) => {
-                input = el.querySelectorAll('input')[0] as HTMLInputElement;
-            },
-            jsx: <div className="ui form">
-                <p>{lf("Host your code on GitHub and work together with friends on projects.")}
-                    {sui.helpIconLink("/github", lf("Learn more about GitHub"))}</p>
-                <p>{lf("You will need a GitHub token:")}</p>
-                <ol>
-                    <li>
-                        {lf("Navigate to: ")}
-                        <a href="https://github.com/settings/tokens/new" target="_blank" rel="noopener noreferrer">
-                            {lf("GitHub token generation page")}
-                        </a>
-                    </li>
-                    <li>
-                        {lf("Put something like 'MakeCode {0}' in description", pxt.appTarget.name)}
-                    </li>
-                    <li>
-                        {lf("Select either '{0}' or '{1}' scope, depending which repos you want to edit from here", "repo", "public_repo")}
-                    </li>
-                    <li>
-                        {lf("Click generate token, copy it, and paste it below.")}
-                    </li>
-                </ol>
-                <div className="ui field">
-                    <label id="selectUrlToOpenLabel">{lf("Paste GitHub token here:")}</label>
-                    <input type="url" tabIndex={0} autoFocus aria-labelledby="selectUrlToOpenLabel" placeholder="0123abcd..." className="ui blue fluid"></input>
-                </div>
-            </div>,
-        }).then(res => {
-            if (!res) {
-                pxt.tickEvent("github.token.cancel");
-                return Promise.resolve()
-            } else {
-                const hextoken = input.value.trim();
-                return this.saveAndValidateTokenAsync(hextoken);
-            }
-        })
     }
 
     private saveAndValidateTokenAsync(hextoken: string): Promise<void> {
