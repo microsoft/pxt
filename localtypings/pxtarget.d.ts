113ddb0a526361fabe63cc4837b109060c7a3757
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-11-19 06:02:53 -0800

    add flag to disable translation upload (#6242)
    * add flag to disable translation upload
    
    * not a theme flag

--- localtypings/pxtarget.d.ts
+++ localtypings/pxtarget.d.ts
@@ -47,6 +47,7 @@ declare namespace pxt {
         appTheme: AppTheme;
         compileService?: TargetCompileService;
         ignoreDocsErrors?: boolean;
+        disableDocsUpload?: boolean;
         variants?: Map<AppTarget>; // patches on top of the current AppTarget for different chip variants
         queryVariants?: Map<AppTarget>; // patches on top of the current AppTarget using query url regex
         unsupportedBrowsers?: BrowserOptions[]; // list of unsupported browsers for a specific target (eg IE11 in arcade). check browserutils.js browser() function for strings
