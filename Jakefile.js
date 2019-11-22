78af244fca8bd7b1536e6f94af1b75c4451c1dbf
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-11-22 04:07:00 -0800

    updated test mode

--- Jakefile.js
+++ Jakefile.js
@@ -72,7 +72,7 @@ function runKarma(that, flags) {
     else {
         command = "./karma start ../../karma.conf.js " + flags;
         if (process.env.GITHUB_ACTION)
-            command = "xvfb-run " + command;
+            command = "xvfb-run --auto-servernum " + command;
     }
     cmdIn(that, "node_modules/.bin", command);
 }
