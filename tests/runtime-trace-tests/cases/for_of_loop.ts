19ed7411107149835642e591870562721bab91f0
Joseph Wunderlich <jwunderl@uw.edu>
2020-01-07 10:46:21 -0800

    trace tests

---
+++ tests/runtime-trace-tests/cases/for_of_loop.ts
@@ -0,0 +1,17 @@
+const word = "def";
+
+for (const letter of word) {
+    console.log(letter);
+}
+for (const letter of "abc") {
+    console.log(letter);
+}
+
+const strList = ["Hi", "bye"];
+for (const text of strList) {
+    console.log(text)
+}
+
+for (const num of [1, 2, 3]) {
+    console.log(num)
+}
