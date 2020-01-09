dd2590e761cafb95e54c8b4552627fff08d53866
Joseph Wunderlich <jwunderl@uw.edu>
2020-01-09 14:45:56 -0800

    add inconsistent indentation test

--- tests/errors-test/cases/bad-indentation.py
+++ tests/errors-test/cases/bad-indentation.py
@@ -1,10 +1,15 @@
 if True:
-def on_chat(num1, num2, num3): # TS9554
-print("a") # TS9554
-    pass # TS9573
+def on_chat(num1, num2, num3): # TS9554 missing
+print("a") # TS9554 missing
+    pass # TS9573 unexpected
 on_chat(1, 2, 3)
-    pass # TS9573
+    pass # TS9573 unexpected
 
 if True:
-if True: # TS9554
+if True: # TS9554 missing
     pass
+
+a = 5
+if True:
+    b = 3
+   c = 4 # TS9552 inconsistent
