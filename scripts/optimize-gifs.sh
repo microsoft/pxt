8d8466724d97da12721dc4d8cf8237cd0ecb8a47
Peli <jhalleux@microsoft.com>
2020-01-25 00:16:48 -0800

    updated script

--- scripts/optimize-gifs.sh
+++ scripts/optimize-gifs.sh
@@ -13,15 +13,14 @@ do
     if [ $old_kb -lt $new_kb ]
     then # delete new file and restore old
         cp -f "$file.original" "$file"
-    else
-        # regenerate mp4
-        png="${file%.gif}.png";
-        if [ ! test -f "$png" ]
-        then
-            magick convert "$file[0]" "${file%.gif}.png";
-        fi
-        ffmpeg -y -f gif -i "$file" -pix_fmt yuv420p -movflags faststart -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${file%.gif}.mp4";
     fi
     # delete old copy
     rm -f "$file.original"
+    # regenerate mp4
+    png="${file%.gif}.png";
+    if [ ! test -f "$png" ]
+    then
+        magick convert "$file[0]" "${file%.gif}.png";
+    fi
+    ffmpeg -y -f gif -i "$file" -pix_fmt yuv420p -movflags faststart -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "${file%.gif}.mp4";
 done
