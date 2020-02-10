f7959e8fce8beccf6dd90d09f4f9f497eb311268
Peli <jhalleux@microsoft.com>
2020-02-09 22:27:41 -0800

    typo

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -2758,7 +2758,7 @@ export class ProjectView
             return;
         let url = window.location.href.replace(/#.*$/, '');
         url = url + (url.indexOf('?') > -1 ? '&' : '?') + "nestededitorsim=1";
-        url += "pub:" + hd.id;
+        url += "#pub:" + hd.id;
         const w = window.open(url, pxt.appTarget.id + hd.id);
         if (w) {
             pxt.log(`dependent editor window registered`)
