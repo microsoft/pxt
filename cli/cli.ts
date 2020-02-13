70303c4e025bce3b4a7786ea068db8207e833c38
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-02-12 21:56:20 -0800

    Update cli/cli.ts
    Co-Authored-By: Joey Wunderlich <jwunderl@users.noreply.github.com>

--- cli/cli.ts
+++ cli/cli.ts
@@ -3158,7 +3158,7 @@ export function downloadDiscourseTagAsync(parsed: commandParser.ParsedCommand): 
                     return extractAsyncInternal(id, out, false)
                         .then(() => {
                             // does the current card have an image?
-                            let card = cards.filter(c => c.url == topic.url)[0];
+                            let card = cards.find(c => c.url == topic.url);
                             if (card && card.imageUrl) {
                                 pxt.log(`${card.name} already in markdown`)
                                 return Promise.resolve(); // already handled
