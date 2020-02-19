bb9c3fa93f873f6943db50873f30d90f026df355
Joseph Wunderlich <jwunderl@uw.edu>
2020-02-19 15:50:08 -0800

    add tick for show / hide code options

--- webapp/src/projects.tsx
+++ webapp/src/projects.tsx
@@ -1170,11 +1170,16 @@ export class NewProjectDialog extends data.Component<ISettingsProps, NewProjectD
     }
 
     onExpandedMenuHide = () => {
+        pxt.tickEvent('newprojectdialog.codeoptions.hide');
         // reset language restrictions when user closes the options menu;
         // it's an 'advanced' feature that we want an easy escape hatch for.
         this.setState({
             languageRestriction: pxt.editor.LanguageRestriction.Standard
         });
+    }
+
+    onExpandedMenuShow = () => {
+        pxt.tickEvent('newprojectdialog.codeoptions.show');
     }
 
     renderCore() {
@@ -1223,7 +1228,7 @@ export class NewProjectDialog extends data.Component<ISettingsProps, NewProjectD
             </div>
             {chooseLanguageRestrictionOnNewProject && <div>
                 <br />
-                <sui.ExpandableMenu title={lf("Code options")} onHide={this.onExpandedMenuHide}>
+                <sui.ExpandableMenu title={lf("Code options")} onShow={this.onExpandedMenuShow} onHide={this.onExpandedMenuHide}>
                     <sui.Select options={langOpts} onChange={this.handleLanguageChange} aria-label={lf("Select Language")} />
                 </sui.ExpandableMenu>
             </div>}
