8c6fec199d1cc95370b77cac6641a8402d874fce
Peli <jhalleux@microsoft.com>
2020-03-04 10:13:57 -0800

    remove extra file

--- tests/pydecompile-test/baselines/calls.py
+++
@@ -1,6 +0,0 @@
-testNamespace.defaultArguments(1);
-testNamespace.defaultArguments(1, 2);
-
-testNamespace.multipleDefaultArguments();
-testNamespace.multipleDefaultArguments(1);
-testNamespace.multipleDefaultArguments(1,2);
