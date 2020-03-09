b966cfaaf25af159daff6a06eca42f3597e96d41
Richard Knoll <riknoll@users.noreply.github.com>
2020-03-09 14:58:53 -0700

    Updating dependencies (#6679)
    * Updating dependencies
    
    * test code

--- pxtlib/docsrender.ts
+++ pxtlib/docsrender.ts
@@ -1,4 +1,5 @@
 /// <reference path='../localtypings/pxtarget.d.ts' />
+/// <reference path='../localtypings/dompurify.d.ts' />
 /// <reference path="commonutil.ts"/>
 
 namespace pxt.docs {
@@ -78,6 +79,12 @@ namespace pxt.docs {
         if (typeof marked !== "undefined") return marked;
         if (typeof require === "undefined") return undefined;
         return require("marked") as typeof marked;
+    }
+
+    export let requireDOMSanitizer = () => {
+        if (typeof DOMPurify !== "undefined") return DOMPurify.sanitize;
+        if (typeof require === "undefined") return undefined;
+        return (require("DOMPurify") as typeof DOMPurify).sanitize;
     }
 
     export interface RenderData {
@@ -468,6 +475,8 @@ namespace pxt.docs {
             const html = linkRenderer.call(renderer, href, title, text);
             return html.replace(/^<a /, `<a ${target ? `target="${target}"` : ''} rel="nofollow noopener" `);
         };
+
+        let sanitizer = requireDOMSanitizer();
         markedInstance.setOptions({
             renderer: renderer,
             gfm: true,
@@ -475,6 +484,7 @@ namespace pxt.docs {
             breaks: false,
             pedantic: false,
             sanitize: true,
+            sanitizer: sanitizer,
             smartLists: true,
             smartypants: true
         });
@@ -794,13 +804,15 @@ ${opts.repo.name.replace(/^pxt-/, '')}=github:${opts.repo.fullName}#${opts.repo.
             return null
 
         const markedInstance = pxt.docs.requireMarked();
+        const sanitizer = requireDOMSanitizer();
         const options = {
             renderer: new markedInstance.Renderer(),
             gfm: true,
             tables: false,
             breaks: false,
             pedantic: false,
-            sanitize: false,
+            sanitize: true,
+            sanitizer: sanitizer,
             smartLists: false,
             smartypants: false
         };
