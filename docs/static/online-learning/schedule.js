ed70f887fda479653108faf9fefe8b127ada28c7
Galen Nickel <v-gani@microsoft.com>
2020-04-02 15:04:32 -0700

    Add Digital All-Stars to stream list on OLP (#6804)
    * Add 'Digital All-Stars' stream to OLP
    
    * add all-stars image
    
    * fix schedule stuff
    
    * change all star image
    
    * delete 'football' image
    
    * correct the description
    
    * Re-order chrono

--- docs/static/online-learning/schedule.js
+++ docs/static/online-learning/schedule.js
@@ -36,6 +36,15 @@ var lessons = [
         "days": [Day.All]
     },
     {
+        "title": "Digital All-Stars",
+        "description": "Join your favorite all-star athletes as they use MakeCode Arcade to code games with you at home!",
+        "url": "https://aka.ms/makecodeDASstream",
+        "img": "/static/online-learning/img/all-stars.png",
+        "time": 12,
+        "startDay": 2,
+        "days": [Day.Thursday]
+    },
+    {
         "title": "Advanced Arcade",
         "description": "Create new games (or recreate old ones) with a rotating cast of developers from the MakeCode team, including Richard, Shannon, Daryl, and Joey",
         "url": "https://aka.ms/makecodearcadestream",
