e39fae0522fbecf914482c574313e0df922d15e2
Richard Knoll <riknoll@users.noreply.github.com>
2020-04-07 09:58:10 -0700

    Use close icon instead of cancel in create function dialog (#6821)

--- webapp/src/createFunction.tsx
+++ webapp/src/createFunction.tsx
@@ -144,12 +144,6 @@ export class CreateFunctionDialog extends data.Component<ISettingsProps, CreateF
         const { visible } = this.state;
         const actions: sui.ModalButton[] = [
             {
-                label: lf("Cancel"),
-                onclick: this.hide,
-                icon: "cancel",
-                className: "cancel lightgrey"
-            },
-            {
                 label: lf("Done"),
                 onclick: this.confirm,
                 icon: "check",
@@ -160,9 +154,10 @@ export class CreateFunctionDialog extends data.Component<ISettingsProps, CreateF
 
         return (
             <sui.Modal isOpen={visible} className="createfunction" size="large"
-                closeOnEscape={false} closeIcon={false} closeOnDimmerClick={false} closeOnDocumentClick={false}
+                closeOnEscape={false} closeIcon={true} closeOnDimmerClick={false} closeOnDocumentClick={false}
                 dimmer={true} buttons={actions} header={lf("Edit Function")}
                 modalDidOpen={this.modalDidOpen}
+                onClose={this.hide}
             >
                 <div>
                     <span className="ui text mobile only paramlabel">{lf("Add a parameter")}</span>
