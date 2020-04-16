2f8592b7203f795c40573da73633a2882843ab91
Hristo Ramirez-Diaz <hristoramirezdiaz@gmail.com>
2020-04-16 14:22:44 -0700

    added error message with line and column

--- webapp/src/errorList.tsx
+++ webapp/src/errorList.tsx
@@ -44,7 +44,7 @@ export class ErrorList extends React.Component<ErrorListProps, ErrorListState> {
             <div className="errorListInner" hidden={this.state.isCollapsed}>
                 {
                     (this.state.errors || []).map(e =>
-                        <div key={errorKey(e)}>{e.messageText}</div>)
+                        <div key={errorKey(e)}>{`${e.messageText}-(${e.line + 1}:${e.column})`}</div>)
                 }
             </div>
         </div>
