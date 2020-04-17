743b8b483454b9bf6cb89a6819957510068a79d7
Richard Knoll <riknoll@microsoft.com>
2020-04-17 09:12:07 -0700

    Get rid of client-side search alltogether

--- pxtlib/github.ts
+++ pxtlib/github.ts
@@ -87,10 +87,10 @@ namespace pxt.github {
         return true
     }
 
-    function useProxy(ignoreToken = false) {
+    function useProxy() {
         if (forceProxy)
             return true;
-        if (token && !ignoreToken)
+        if (token)
             return false
         return hasProxy();
     }
@@ -844,11 +844,8 @@ namespace pxt.github {
             return Promise.all(repos.map(id => repoAsync(id.path, config)))
                 .then(rs => rs.filter(r => r && r.status != GitRepoStatus.Banned)); // allow deep links to github repos
 
-        let fetch = () => useProxy(true)
-            ? U.httpGetJsonAsync(`${pxt.Cloud.apiRoot}ghsearch/${appTarget.id}/${appTarget.platformid || appTarget.id}?q=`
-                + encodeURIComponent(query))
-            : ghGetJsonAsync("https://api.github.com/search/repositories?q="
-                + encodeURIComponent(query + ` in:name,description,readme "for PXT/${appTarget.platformid || appTarget.id}"`))
+        let fetch = () => U.httpGetJsonAsync(`${pxt.Cloud.apiRoot}ghsearch/${appTarget.id}/${appTarget.platformid || appTarget.id}?q=`
+            + encodeURIComponent(query))
 
         return fetch()
             .then((rs: SearchResults) =>
