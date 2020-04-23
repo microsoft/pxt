5b015d7f49b39eba44108b192529b50e5476bb08
Peli <jhalleux@microsoft.com>
2020-04-23 14:54:32 -0700

    center gif

--- webapp/src/webusb.tsx
+++ webapp/src/webusb.tsx
@@ -120,10 +120,11 @@ export function webUsbPairDialogAsync(pairAsync: () => Promise<boolean>, confirm
 export function renderUnpairDialog() {
     const boardName = pxt.appTarget.appTheme.boardName || lf("device");
     const header = lf("How to unpair your {0}", boardName);
-    const jsx = <p>
+    const jsx = <div><p>
         {lf("You can unpair your {0} if the WebUSB download is malfunctioning. Click on the lock icon and uncheck your device.", boardName)}
-        <img className="ui image medium" src={"./static/webusb/unpair.gif"} alt={lf("A gif showing how to unpair the {0}", boardName)} />
-    </p>;
+    </p>
+        <img className="ui image centered medium" src={"./static/webusb/unpair.gif"} alt={lf("A gif showing how to unpair the {0}", boardName)} />
+    </div>
     const helpUrl = pxt.appTarget.appTheme.usbDocs
         && (pxt.appTarget.appTheme.usbDocs + "/webusb#unpair");
     return { header, jsx, helpUrl };
