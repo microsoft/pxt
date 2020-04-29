9d4f40fe88901dfe785cfdbbe827c001ea91e3c5
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-29 10:29:17 -0700

    Write error file in testghpkgs (#6935)
    * write error file when testing packages
    
    * update text

--- cli/cli.ts
+++ cli/cli.ts
@@ -5519,7 +5519,9 @@ function testGithubPackagesAsync(parsed: commandParser.ParsedCommand): Promise<v
     }
     const pkgsroot = path.join("temp", "ghpkgs");
     const logfile = path.join(pkgsroot, "log.txt");
+    const errorfile = path.join(pkgsroot, "error.txt");
     nodeutil.writeFileSync(logfile, ""); // reset file
+    nodeutil.writeFileSync(errorfile, ""); // reset file
 
     function pxtAsync(dir: string, args: string[]) {
         return nodeutil.spawnAsync({
@@ -5530,19 +5532,20 @@ function testGithubPackagesAsync(parsed: commandParser.ParsedCommand): Promise<v
     }
 
     let errorCount = 0;
+    let warningCount = 0;
     function reportWarning(er: { repo: string; title: string; body: string; }) {
-        const msg = `- [ ]  [${er.repo}](https://github.com/${er.repo}) ${er.title} / [new issue](https://github.com/${er.repo}/issues/new?title=${encodeURIComponent(er.title)}&body=${encodeURIComponent(er.body)})`;
-        console.log(`warning: https://github.com/${er.repo} ${er.title}`)
-        fs.appendFileSync(logfile, msg + "\n");
+        reportLog(`warning: https://github.com/${er.repo} ${er.title}`)
+        const msg = `- [ ] warning: [${er.repo}](https://github.com/${er.repo}) ${er.title} / [new issue](https://github.com/${er.repo}/issues/new?title=${encodeURIComponent(er.title)}&body=${encodeURIComponent(er.body)})`;
+        fs.appendFileSync(errorfile, msg + "\n");
+        warningCount++;
     }
 
     function reportError(er: { repo: string; title: string; body: string; }) {
-        const msg = `- [ ]  [${er.repo}](https://github.com/${er.repo}) ${er.title} / [new issue](https://github.com/${er.repo}/issues/new?title=${encodeURIComponent(er.title)}&body=${encodeURIComponent(er.body)})`;
-        console.error(`error: https://github.com/${er.repo} ${er.title}`)
-        fs.appendFileSync(logfile, msg + "\n");
+        reportLog(`error: https://github.com/${er.repo} ${er.title}`)
+        const msg = `- [ ] error:  [${er.repo}](https://github.com/${er.repo}) ${er.title} / [new issue](https://github.com/${er.repo}/issues/new?title=${encodeURIComponent(er.title)}&body=${encodeURIComponent(er.body)})`;
+        fs.appendFileSync(errorfile, msg + "\n");
         errorCount++;
     }
-
     function reportLog(msg: any) {
         pxt.log(msg);
         fs.appendFileSync(logfile, msg + "\n");
@@ -5630,7 +5633,7 @@ function testGithubPackagesAsync(parsed: commandParser.ParsedCommand): Promise<v
         })
         .then(() => {
             if (errorCount > 0) {
-                reportLog(`${errorCount} errors found`);
+                reportLog(`${errorCount} errors, ${warningCount} warnings`);
                 process.exit(1);
             }
         })
