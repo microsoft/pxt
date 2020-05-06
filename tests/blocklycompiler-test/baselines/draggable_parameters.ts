1748f3c62119927af2ae0bdf21507eec70fd4beb
Richard Knoll <riknoll@microsoft.com>
2020-05-05 17:29:51 -0700

    Fix test cases again

--- tests/blocklycompiler-test/baselines/draggable_parameters.ts
+++ tests/blocklycompiler-test/baselines/draggable_parameters.ts
@@ -1,12 +1,12 @@
-testNamespace.callbackWithDraggableParams(function (okay, d) {
-
-})
 testNamespace.callbackWithDraggableParams(function (hello, goodbye) {
 
 })
-testNamespace.callbackWithDraggableParams(function (c, d) {
+testNamespace.callbackWithDraggableParams(function (okay, d) {
 
 })
 testNamespace.callbackWithDraggableParams(function (sure, whatever) {
+
+})
+testNamespace.callbackWithDraggableParams(function (c, d) {
 
 })
