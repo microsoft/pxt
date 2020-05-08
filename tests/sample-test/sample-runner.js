ca5bf082e2b19228b168fbe09d5a0dd7ee64c62c
darzu <daryl.zuniga@gmail.com>
2020-05-08 08:57:56 -0700

    work in progress

--- tests/sample-test/sample-runner.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello, world!");
-function returnFive() {
-    return 5;
-}
-exports.returnFive = returnFive;
-//# sourceMappingURL=sample-runner.js.map
