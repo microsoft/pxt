b52e13c0b9acbc2e1122171b37fe3cdb0441eaeb
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-05-14 14:37:07 -0700

    Handling invalid github token (#7033)
    * force use github proxy when loading tutorials
    
    * handle bogus token when querying user
    
    * handling error in user display
    
    * log errors
    
    * handle github errors centrally
    
    * better error handling of github queries
    
    * handle invalid token automitclly

--- webapp/src/githubprovider.tsx
+++ webapp/src/githubprovider.tsx
@@ -157,6 +157,10 @@ export class GithubProvider extends cloudsync.ProviderBase {
             return Promise.resolve(undefined);
         return pxt.github.authenticatedUserAsync()
             .then(ghuser => {
+                if (!ghuser) {
+                    pxt.log(`unkown github user`)
+                    return undefined;
+                }
                 return {
                     id: ghuser.login,
                     userName: ghuser.login,
@@ -164,12 +168,6 @@ export class GithubProvider extends cloudsync.ProviderBase {
                     photo: ghuser.avatar_url,
                     profile: `https://github.com/${ghuser.login}`
                 }
-            }).catch(e => {
-                // the token expired or got deleted by the user
-                if (e.statusCode == 401) {
-                    this.setNewToken(undefined);
-                }
-                throw e;
             })
     }
 
