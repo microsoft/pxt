03dc51ec4dad6ffc28bc200073a849736babedda
darzu <daryl.zuniga@gmail.com>
2020-05-14 15:15:24 -0700

    fix completion snippet name prefix

--- webapp/src/monaco.tsx
+++ webapp/src/monaco.tsx
@@ -103,7 +103,7 @@ class CompletionProvider implements monaco.languages.CompletionItemProvider {
                         // remove what precedes the "." in the full snippet.
                         // E.g. if the user is typing "mobs.", we want to complete with "spawn" (name) not "mobs.spawn" (qName)
                         if (completions.isMemberCompletion && completionSnippet) {
-                            const nameStart = completionSnippet.indexOf(name);
+                            const nameStart = completionSnippet.lastIndexOf(name);
                             if (nameStart !== -1) {
                                 completionSnippet = completionSnippet.substr(nameStart)
                             }
