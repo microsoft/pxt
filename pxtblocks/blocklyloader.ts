18d1159f68e1939e6f47a9121a2b51e2596c007a
peli <pelikhan@users.noreply.github.com>
2020-05-18 06:34:32 -0700

    change defaults to 0,1,2

--- pxtblocks/blocklyloader.ts
+++ pxtblocks/blocklyloader.ts
@@ -164,7 +164,7 @@ namespace pxt.blocks {
 
                 switch (isArray) {
                     case "number":
-                        fieldValues = ["1", "2", "3"];
+                        fieldValues = ["0", "1", "2"];
                         break;
                     case "string":
                         fieldValues = ["a", "b", "c"];
