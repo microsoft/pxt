60451e0dd5fa76ba7f2a51c24206105d8fe12ae7
Hristo Ramirez-Diaz <46362603+hristoramirez@users.noreply.github.com>
2020-05-27 10:23:58 -0700

    pointer and 2 second error debouncing (#7071)

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -745,7 +745,7 @@ export class ProjectView
             this.typecheck();
         }
         this.markdownChangeHandler();
-    }, 500, false);
+    }, 2000, false);
     private initEditors() {
         this.textEditor = new monaco.Editor(this);
         this.pxtJsonEditor = new pxtjson.Editor(this);
