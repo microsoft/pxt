23e7e0fdd8a43bb2ab2e5351b49cd3935ee350b6
Vivian Li <vivl@microsoft.com>
2020-05-28 14:26:23 -0700

    Enable pinch zoom in editor (#7101)
    * Add pinch to zoom options
    
    * Pull latest pxt-blockly
    
    Co-authored-by: Vivian Li <livcheerful>

--- webapp/src/blocks.tsx
+++ webapp/src/blocks.tsx
@@ -1016,7 +1016,8 @@ export class Editor extends toolboxeditor.ToolboxEditor {
                 maxScale: 2.5,
                 minScale: .2,
                 scaleSpeed: 1.5,
-                startScale: pxt.BrowserUtils.isMobile() ? 0.7 : 0.9
+                startScale: pxt.BrowserUtils.isMobile() ? 0.7 : 0.9,
+                pinch: true
             },
             rtl: Util.isUserLanguageRtl()
         };
