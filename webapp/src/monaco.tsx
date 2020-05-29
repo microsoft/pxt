cf305a7aabf29febf1de95f7e5a7df0d751fd48a
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-05-28 18:34:59 -0700

    Android monaco fixes (#7099)
    * disable autocomplete on browsers claiming to be android
    
    * add all the settings, offset the text area to account for left margin
    
    * disable accessibility support on android; that's the thing doubling lines exponentially after periods

--- webapp/src/monaco.tsx
+++ webapp/src/monaco.tsx
@@ -598,9 +598,10 @@ export class Editor extends toolboxeditor.ToolboxEditor {
     }
 
     display(): JSX.Element {
-        let showErrorList = pxt.appTarget.appTheme.errorList;
+        const showErrorList = pxt.appTarget.appTheme.errorList;
+        const isAndroid = pxt.BrowserUtils.isAndroid();
         return (
-            <div id="monacoEditorArea" className="monacoEditorArea" style={{ direction: 'ltr' }}>
+            <div id="monacoEditorArea" className={`monacoEditorArea ${isAndroid ? "android" : ""}`} style={{ direction: 'ltr' }}>
                 {this.isVisible && <div className={`monacoToolboxDiv ${(this.toolbox && !this.toolbox.state.visible && !this.isDebugging()) ? 'invisible' : ''}`}>
                     <toolbox.Toolbox ref={this.handleToolboxRef} editorname="monaco" parent={this} />
                     <div id="monacoDebuggerToolbox"></div>
