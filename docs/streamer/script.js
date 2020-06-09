8e5ae599595cde2684070bc4fa5c282ae9f00905
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-06-09 08:32:33 -0700

    Streamer: fix reducing countdown (#7156)
    * add playsinline to video
    
    * fix countdown

--- docs/streamer/script.js
+++ docs/streamer/script.js
@@ -136,7 +136,7 @@
 
         if (currentScene == "countdownscene") {
             addButton("Add", "Add 1 minute to countdown", () => updateCountdown(60))
-            addButton("Remove", "Remove 1 minute from countdonw", () => updateCountdown(-60))
+            addButton("Remove", "Remove 1 minute from countdown", () => updateCountdown(-60))
             addSep()
         }
 
@@ -240,7 +240,7 @@
         // add seconds
         let remaining = Math.max(0, timerEnd - Date.now()) + seconds * 1000;
         // round to a multiple 30 seconds
-        remaining = ((Math.ceil(remaining / 60000) * 60000) + 1000) | 0;
+        remaining = ((Math.round(remaining / 60000) * 60000) + 1000) | 0;
         state.timerEnd = Date.now() + remaining;
         renderCountdown();
         startCountdown();
