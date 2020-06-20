f6e7c4cd9d8cf717da3a14b52bae9966d7e20cf4
peli <pelikhan@users.noreply.github.com>
2020-06-20 11:07:03 -0700

    streamer: better contrast on toolbox

--- docs/static/streamer/script.js
+++ docs/static/streamer/script.js
@@ -662,10 +662,6 @@ color: white;
 #title {
 background: ${primary};
 }
-.icons:hover {
-    background: ${primary};
-    border: solid 3px ${menu};
-}
 `
         const faceCamFilter = camFilter(config.faceCamFilter)
         if (faceCamFilter)
