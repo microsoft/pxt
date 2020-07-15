9a2d3f6fda07dd90d5dc31d4e0e6b84a8f5d4e96
peli <pelikhan@users.noreply.github.com>
2020-07-14 22:29:29 -0700

    move to 16/9 ratio

--- docs/static/streamer/script.js
+++ docs/static/streamer/script.js
@@ -1095,9 +1095,9 @@ background-image: url(${config.backgroundImage});
             const constraints = {
                 audio: false,
                 video: {
-                    aspectRatio: 4 / 3,
-                    width: { ideal: 1080 },
-                    height: { ideal: 720 }
+                    aspectRatio: 16 / 9,
+                    width: { ideal: 1920 },
+                    height: { ideal: 1080 }
                 }
             };
             if (deviceId)
