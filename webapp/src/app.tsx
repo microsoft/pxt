fae9152fd8e969284d36375e5f045b1b3cbd7376
peli <pelikhan@users.noreply.github.com>
2020-07-15 11:44:48 -0700

    pr feedback

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -3963,8 +3963,10 @@ function enableAnalytics() {
         stats["screen.innerHeight"] = window.innerHeight;
         stats["screen.devicepixelratio"] = pxt.BrowserUtils.devicePixelRatio();
         const body = document.firstElementChild; // body
-        stats["screen.clientWidth"] = body.clientWidth;
-        stats["screen.clientHeight"] = body.clientHeight;
+        if (body) {
+            stats["screen.clientWidth"] = body.clientWidth;
+            stats["screen.clientHeight"] = body.clientHeight;
+        }
     }
     pxt.tickEvent("editor.loaded", stats);
 }
