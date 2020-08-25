caf859241b9b2023f15d4c1ceb4d8c134b56ec8d
Richard Knoll <riknoll@microsoft.com>
2020-08-25 14:30:15 -0700

    Monaco field editor ranges should just cover the first line

--- webapp/src/monacoFieldEditorHost.ts
+++ webapp/src/monacoFieldEditorHost.ts
@@ -305,6 +305,7 @@ export class FieldEditorManager implements monaco.languages.FoldingRangeProvider
 
         const oldDecorations = this.decorations[owner];
         const ranges = this.liveRanges.filter(r => r.owner === owner);
+        const model = this.editor.getModel();
 
         const newDecorations: monaco.editor.IModelDeltaDecoration[] = [];
         for (const r of ranges) {
@@ -315,7 +316,7 @@ export class FieldEditorManager implements monaco.languages.FoldingRangeProvider
             }
 
             newDecorations.push({
-                range: r.range,
+                range: new monaco.Range(r.line, model.getLineMinColumn(r.line), r.line, model.getLineMaxColumn(r.line)),
                 options: {
                     glyphMarginClassName: glyph
                 }
