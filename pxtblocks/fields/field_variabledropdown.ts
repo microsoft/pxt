43f28309e27570f11e2f1eb620a33c0e66bb1835
Vivian Li <vivl@microsoft.com>
2020-08-26 16:49:29 -0700

    Update name

--- pxtblocks/fields/field_variabledropdown.ts
+++ pxtblocks/fields/field_variabledropdown.ts
@@ -2,9 +2,9 @@ namespace pxtblockly {
     export class FieldVariableDropdown extends Blockly.FieldVariable {
         constructor(varName: string) {
             super(varName);
-            this.menuGenerator_ = this.customDropdownCreate;
+            this.menuGenerator_ = this.dropdownCreate;
         }
-        customDropdownCreate(): any {
+        dropdownCreate(): any {
             const options = Blockly.FieldVariable.dropdownCreate.call(this);
             return options.filter((opt: any) => opt[1] != Blockly.DELETE_VARIABLE_ID);
         }
