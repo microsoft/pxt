8b0bd5159fb5c6d6da319346b176e097406baea4
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-08-27 11:40:03 -0700

    also cache if api info matches pkg id (#7427)

--- webapp/src/compiler.ts
+++ webapp/src/compiler.ts
@@ -618,6 +618,7 @@ async function cacheApiInfoAsync(project: pkg.EditorPackage, info: pxtc.ApisInfo
 
         for (const dep of externalPackages) {
             const name = dep.getKsPkg().config.name;
+            const pkgId = dep.getPkgId();
             const entry: pxt.PackageApiInfo = {
                 sha: null,
                 apis: { byQName: {} }
@@ -625,8 +626,9 @@ async function cacheApiInfoAsync(project: pkg.EditorPackage, info: pxtc.ApisInfo
 
 
             for (const api of apiList) {
-                if (info.byQName[api].pkg === name) {
-                    entry.apis.byQName[api] = info.byQName[api];
+                const apiInfo = info.byQName[api];
+                if (apiInfo.pkg === name || apiInfo.pkg === pkgId) {
+                    entry.apis.byQName[api] = apiInfo;
                 }
             }
 
