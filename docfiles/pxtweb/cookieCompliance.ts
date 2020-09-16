f296f907a4d6e7b83234c4ca9a0170d7807b280f
Richard Knoll <riknoll@microsoft.com>
2020-09-15 17:28:25 -0700

    Update to use new cookie banner

--- docfiles/pxtweb/cookieCompliance.ts
+++ docfiles/pxtweb/cookieCompliance.ts
@@ -6,37 +6,7 @@ declare var process: any;
 namespace pxt {
     type Map<T> = { [index: string]: T };
 
-    interface CookieBannerInfo {
-        /* Does the banner need to be shown? */
-        IsConsentRequired: boolean;
-
-        /* Name of the cookie, usually MSCC */
-        CookieName: string;
-
-        /* HTML for the banner to be embedded into the page */
-        Markup: string;
-
-        /* Scripts to be loaded in the page for the banner */
-        Js: string[];
-
-        /* CSS files to be loaded in the page for the banner*/
-        Css: string[];
-
-        /* The minimum date for which consent is considered valid (any consent given before this date does not count) */
-        MinimumConsentDate: string;
-
-        /* Error message from the server, if present */
-        Error?: string;
-    }
-
-    interface HttpResponse {
-        status: number;
-        body: string;
-    }
-
-    interface Callback<T> {
-        (err?: any, res?: T): void;
-    }
+    declare const WcpConsent: any;
 
     const eventBufferSizeLimit = 20;
     const queues: TelemetryQueue<any, any, any>[] = [];
@@ -175,52 +145,15 @@ namespace pxt {
             return;
         }
 
-        if (isSandboxMode() || (window as any).pxtSkipAnalyticsCookie) {
+        if (isNotHosted() || isSandboxMode() || (window as any).pxtSkipAnalyticsCookie) {
             initializeAppInsightsInternal(false);
             return;
         }
 
-        getCookieBannerAsync(document.domain, detectLocale(), (bannerErr, info) => {
-            if (bannerErr || info.Error) {
-                // Start app insights, just don't drop any cookies
-                initializeAppInsightsInternal(false);
-                return;
-            }
-
-            // Clear the cookies if the consent is too old, mscc won't do it automatically
-            if (isConsentExpired(info.CookieName, info.MinimumConsentDate)) {
-                const definitelyThePast = new Date(0).toUTCString();
-                document.cookie = `ai_user=; expires=${definitelyThePast}`;
-                document.cookie = `ai_session=; expires=${definitelyThePast}`;
-                document.cookie = `${info.CookieName}=0; expires=${definitelyThePast}`;
-            }
-
-            let bannerDiv = document.getElementById("cookiebanner");
-            if (!bannerDiv) {
-                bannerDiv = document.createElement("div");
-                document.body.insertBefore(bannerDiv, document.body.firstChild);
-            }
-
-            // The markup is trusted because it's from our backend, so it shouldn't need to be scrubbed
-            /* tslint:disable:no-inner-html */
-            bannerDiv.innerHTML = info.Markup;
-            /* tslint:enable:no-inner-html */
-
-            if (info.Css && info.Css.length) {
-                info.Css.forEach(injectStylesheet)
-            }
-
-            all(info.Js || [], injectScriptAsync, msccError => {
-                if (!msccError && typeof mscc !== "undefined") {
-                    if (mscc.hasConsent()) {
-                        initializeAppInsightsInternal(true)
-                    }
-                    else {
-                        mscc.on("consent", () => initializeAppInsightsInternal(true));
-                    }
-                }
-            });
-        });
+        WcpConsent.init(detectLocale(), "cookiebanner", function() {
+            // MakeCode only uses "required" cookies, so we don't actually need to check for consent
+            initializeAppInsightsInternal(true);
+        })
     }
 
     export function aiTrackEvent(id: string, data?: any, measures?: any) {
@@ -245,51 +178,6 @@ namespace pxt {
         return (mlang ? mlang[2] : ((navigator as any).userLanguage || navigator.language)) || "en";
     }
 
-    function getCookieBannerAsync(domain: string, locale: string, cb: Callback<CookieBannerInfo>) {
-        httpGetAsync(`https://makecode.com/api/mscc/${domain}/${locale}`, function (err, resp) {
-            if (err) {
-                cb(err);
-                return;
-            }
-
-            if (resp.status === 200) {
-                try {
-                    const info = JSON.parse(resp.body);
-                    cb(undefined, info as CookieBannerInfo);
-                    return;
-                }
-                catch (e) {
-                    cb(new Error("Bad response from server: " + resp.body))
-                    return;
-                }
-            }
-            cb(new Error("didn't get 200 response: " + resp.status + " " + resp.body));
-        });
-    }
-
-    function isConsentExpired(cookieName: string, minimumConsentDate: string) {
-        const minDate = Date.parse(minimumConsentDate);
-
-        if (!isNaN(minDate)) {
-            if (document && document.cookie) {
-                const cookies = document.cookie.split(";");
-                for (let cookie of cookies) {
-                    cookie = cookie.trim();
-                    if (cookie.indexOf("=") == cookieName.length && cookie.substr(0, cookieName.length) == cookieName) {
-                        const value = parseInt(cookie.substr(cookieName.length + 1));
-                        if (!isNaN(value)) {
-                            // The cookie value is the consent date in seconds since the epoch
-                            return value < Math.floor(minDate / 1e3);
-                        }
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
     export function initializeAppInsightsInternal(includeCookie = false) {
         // loadAppInsights is defined in docfiles/tracking.html
         const loadAI = (window as any).loadAppInsights;
@@ -297,67 +185,6 @@ namespace pxt {
             loadAI(includeCookie);
             analyticsLoaded = true;
             queues.forEach(a => a.flush());
-        }
-    }
-
-    function httpGetAsync(url: string, cb: Callback<HttpResponse>) {
-        try {
-            let client: XMLHttpRequest;
-            let resolved = false
-            client = new XMLHttpRequest();
-            client.onreadystatechange = () => {
-                if (resolved) return // Safari/iOS likes to call this thing more than once
-
-                if (client.readyState == 4) {
-                    resolved = true
-                    let res: HttpResponse = {
-                        status: client.status,
-                        body: client.responseText
-                    }
-                    cb(undefined, res);
-                }
-            }
-
-            client.open("GET", url);
-            client.send();
-        }
-        catch (e) {
-            cb(e);
-        }
-    }
-
-    function injectStylesheet(href: string) {
-        if (document.head) {
-            const link = document.createElement("link");
-            link.setAttribute("rel", "stylesheet");
-            link.setAttribute("href", href);
-            link.setAttribute("type", "text/css");
-            document.head.appendChild(link);
-        }
-    }
-
-    function injectScriptAsync(src: string, cb: Callback<void>) {
-        let resolved = false;
-        if (document.body) {
-            const script = document.createElement("script");
-            script.setAttribute("type", "text/javascript");
-            script.onload = function (ev) {
-                if (!resolved) {
-                    cb();
-                    resolved = true;
-                }
-            };
-            script.onerror = function (err) {
-                if (!resolved) {
-                    cb(err);
-                    resolved = true;
-                }
-            }
-            document.body.appendChild(script);
-            script.setAttribute("src", src);
-        }
-        else {
-            throw new Error("Bad call to injectScriptAsync")
         }
     }
 
@@ -370,6 +197,10 @@ namespace pxt {
         const isPxtElectron = hasWindow && !!(window as any).pxtElectron;
         const isCC = hasWindow && !!(window as any).ipcRenderer || /ipc=1/.test(location.hash) || /ipc=1/.test(location.search); // In WKWebview, ipcRenderer is injected later, so use the URL query
         return isUwp || isPxtElectron || isCC;
+    }
+    function isNotHosted(): boolean {
+        const config = (window as any).pxtConfig;
+        return !config || config.isStatic
     }
     /**
      * Checks whether we should hide the cookie banner
@@ -394,30 +225,5 @@ namespace pxt {
         //We don't want cookie notification in the share page
         const sandbox = /sandbox=1|#sandbox|#sandboxproject/i.test(window.location.href)
         return sandbox;
-    }
-
-    // No promises, so here we are
-    function all<T, U>(values: T[], func: (value: T, innerCb: Callback<U>) => void, cb: Callback<U[]>) {
-        let index = 0;
-        let res: U[] = [];
-
-        let doNext = () => {
-            if (index >= values.length) {
-                cb(undefined, res);
-            }
-            else {
-                func(values[index++], (err, val) => {
-                    if (err) {
-                        cb(err);
-                    }
-                    else {
-                        res.push(val);
-                        doNext();
-                    }
-                });
-            }
-        };
-
-        doNext();
     }
 }
