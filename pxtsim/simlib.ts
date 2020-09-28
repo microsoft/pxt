f536396051c5090c6a87f1aaacfff82a8270e2fc
Michal Moskal <michal@moskal.me>
2020-09-28 13:44:15 +0200

    Fix sound frequency sweeps

--- pxtsim/simlib.ts
+++ pxtsim/simlib.ts
@@ -423,7 +423,7 @@ namespace pxsim {
                 if (!ctx || prevStop != instrStopId)
                     return Promise.delay(duration)
 
-                if (currWave != soundWaveIdx || currFreq != freq) {
+                if (currWave != soundWaveIdx || currFreq != freq || freq != endFreq) {
                     if (ch.generator) {
                         return Promise.delay(timeOff)
                             .then(() => {
