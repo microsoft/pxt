988053e257c2459e1608051f1296e8affcfcbaf9
darzu <daryl.zuniga@gmail.com>
2020-09-29 09:38:26 -0700

    change to backends provider notion

--- webapp/src/identity.tsx
+++ webapp/src/identity.tsx
@@ -26,11 +26,11 @@ export function showLoginDialogAsync(projectView: pxt.editor.IProjectView) {
                 jsx: <div>
                     Login with:
                     <br></br>
-                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("microsoft")}>Microsoft</a>
+                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("ms")}>Microsoft</a>
                     <br></br>
                     <a target="_blank" rel="noopener noreferrer" href={loginUrl("github")}>GitHub</a>
                     <br></br>
-                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("google")}>Google</a>
+                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("goog")}>Google</a>
                     <br></br>
                 </div>
             })
