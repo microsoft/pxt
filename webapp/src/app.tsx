b6f6beb17cc78fab9852d60a08c490492b925cce
shakao <shakao@microsoft.com>
2020-10-05 18:03:26 -0700

    Tick event for 'exiting' a recipe

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -3484,6 +3484,12 @@ export class ProjectView
     }
 
     private startTutorialAsync(tutorialId: string, tutorialTitle?: string, recipe?: boolean, editorProjectName?: string): Promise<void> {
+        // custom tick for recipe "completion". recipes use links in the markdown to
+        // progress, so we track when a user "exits" a recipe by loading a new one
+        if (this.state.header?.tutorial?.tutorialRecipe) {
+            pxt.tickEvent("recipe.exit", { id: this.state.header?.tutorial?.tutorial, goto: tutorialId });
+        }
+
         core.hideDialog();
         core.showLoading("tutorial", lf("starting tutorial..."));
         sounds.initTutorial(); // pre load sounds
