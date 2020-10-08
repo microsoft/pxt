eaea5ac55a396ce65699308b138303ce46776f11
darzu <daryl.zuniga@gmail.com>
2020-10-08 10:37:04 -0700

    add random "remember me?" flag

--- webapp/src/identity.tsx
+++ webapp/src/identity.tsx
@@ -11,10 +11,14 @@ import Util = pxt.Util;
 export function showLoginDialogAsync(projectView: pxt.editor.IProjectView) {
     const targetTheme = pxt.appTarget.appTheme;
 
+    // TODO: implement remember me correctly. This form should be done with a real react component.
+    let rememberMe: boolean = Math.random() > 0.5;
+
     const buttons: sui.ModalButton[] = [];
-    let loginUrl = (provider: string) => `${pxt.Cloud.apiRoot}login?provider=${provider}`;
+    let loginUrl = (provider: string) => `${pxt.Cloud.apiRoot}signin?provider=${provider}&rememberMe=${rememberMe}`;
 
     // TODO: merge with githubprovider.tsx
+
     pxt.targetConfigAsync()
         .then(config => {
             return core.confirmAsync({
@@ -26,12 +30,14 @@ export function showLoginDialogAsync(projectView: pxt.editor.IProjectView) {
                 jsx: <div>
                     Login with:
                     <br></br>
-                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("ms")}>Microsoft</a>
+                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("microsoft")}>Microsoft</a>
                     <br></br>
                     <a target="_blank" rel="noopener noreferrer" href={loginUrl("github")}>GitHub</a>
                     <br></br>
-                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("goog")}>Google</a>
+                    <a target="_blank" rel="noopener noreferrer" href={loginUrl("google")}>Google</a>
                     <br></br>
+                    <input type="checkbox" id="rememberMe" name="rememberMe" checked={rememberMe} disabled />
+                    <label htmlFor="rememberMe">Remember me?</label>
                 </div>
             })
         }).done();
