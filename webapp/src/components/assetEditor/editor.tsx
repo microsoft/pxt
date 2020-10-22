3cf743f543e4024c85e258295dbd15fe60c7f752
shakao <shakao@microsoft.com>
2020-10-22 15:40:05 -0700

    Only update main.ts for temporary assets, null checks

--- webapp/src/components/assetEditor/editor.tsx
+++ webapp/src/components/assetEditor/editor.tsx
@@ -161,9 +161,12 @@ export class AssetEditor extends Editor {
             if (asset.type == pxt.AssetType.Tilemap) result.data = this.updateTilemapTiles(result.data);
 
             Promise.resolve(cb(result)).then(() => {
-                this.parent.saveBlocksToTypeScriptAsync().then((src) => {
-                    pkg.mainEditorPkg().setContentAsync("main.ts", src)
-                })
+                // for temporary (unnamed) assets, update the underlying typescript image literal
+                if (!result.meta?.displayName) {
+                    this.parent.saveBlocksToTypeScriptAsync().then((src) => {
+                        if (src) pkg.mainEditorPkg().setContentAsync("main.ts", src)
+                    })
+                }
             });
         });
         fieldView.show();
