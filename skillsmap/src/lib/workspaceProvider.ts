f2977f85e08fedd64dc44922304f0cc9b6ad23b7
Richard Knoll <riknoll@microsoft.com>
2020-10-28 11:04:23 -0700

    Removing accidental file

--- skillsmap/src/lib/workspaceProvider.ts
+++
@@ -1,8 +0,0 @@
-export interface WorkspaceProvider {
-    getUserStateAsync(userId: string): Promise<UserState>;
-    setUserStateAsync(id: string, state: UserState): Promise<UserState>;
-
-    getProjectAsync(headerId: string): Promise<any>;
-    setProjectAsync(headerId: string, state: any): Promise<void>;
-    deleteProjectAsync(headerId: string): Promise<void>;
-}
