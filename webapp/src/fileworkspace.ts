79861321a4e4c7f25adbfb8da0611d3cc6f85f0d
Eric Anderson <eanders@microsoft.com>
2020-10-29 16:39:37 -0700

    Fix for local file workspace

--- webapp/src/fileworkspace.ts
+++ webapp/src/fileworkspace.ts
@@ -14,7 +14,8 @@ let apiAsync = (path: string, data?: any) => {
         headers: { "Authorization": Cloud.localToken },
         method: data ? "POST" : "GET",
         data: data || undefined,
-        allowHttpErrors: true
+        allowHttpErrors: true,
+        allowRelativeUrl: true
     }).then(r => r.json).catch(core.handleNetworkError);
 }
 
