922b0121eabb891ad00f58fdc1715e53eb7a65fe
Shannon Kao <shakao@microsoft.com>
2020-12-09 17:28:50 -0800

    Add tutorial.finish tick for combining recipe and tutorial exit events

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -3563,10 +3563,11 @@ export class ProjectView
     }
 
     private startTutorialAsync(tutorialId: string, tutorialTitle?: string, recipe?: boolean, editorProjectName?: string): Promise<void> {
-        // custom tick for recipe "completion". recipes use links in the markdown to
+        // custom tick for recipe or tutorial "completion". recipes use links in the markdown to
         // progress, so we track when a user "exits" a recipe by loading a new one
-        if (this.state.header?.tutorial?.tutorialRecipe) {
+        if (this.state.header?.tutorial?.tutorial) {
             pxt.tickEvent("recipe.exit", { tutorial: this.state.header?.tutorial?.tutorial, goto: tutorialId });
+            pxt.tickEvent("tutorial.finish", { tutorial: this.state.header?.tutorial?.tutorial });
         }
 
         core.hideDialog();
@@ -3632,6 +3633,7 @@ export class ProjectView
     }
 
     completeTutorialAsync(): Promise<void> {
+        pxt.tickEvent("tutorial.finish", { tutorial: this.state.header?.tutorial?.tutorial });
         pxt.tickEvent("tutorial.complete", { tutorial: this.state.header?.tutorial?.tutorial });
         core.showLoading("leavingtutorial", lf("leaving tutorial..."));
 
