4a89da52350cae17ae558bc693cd3af02e7060d7
shakao <34112083+shakao@users.noreply.github.com>
2021-01-11 14:11:54 -0800

    Run expandHtml on skillmap before uploading file (#7760)

--- cli/cli.ts
+++ cli/cli.ts
@@ -1095,6 +1095,13 @@ function uploadCoreAsync(opts: UploadOptions) {
         "skillmap.html"
     ]
 
+    // expandHtml is manually called on these files before upload
+    // runs <!-- @include --> substitutions, fills in locale, etc
+    let expandFiles = [
+        "index.html",
+        "skillmap.html"
+    ]
+
     nodeutil.mkdirP("built/uploadrepl")
 
     function encodeURLs(urls: string[]) {
@@ -1131,7 +1138,7 @@ function uploadCoreAsync(opts: UploadOptions) {
             data = rdata;
             if (isText) {
                 content = data.toString("utf8")
-                if (fileName == "index.html") {
+                if (expandFiles.indexOf(fileName) >= 0) {
                     if (!opts.localDir) {
                         let m = pxt.appTarget.appTheme as Map<string>
                         for (let k of Object.keys(m)) {
