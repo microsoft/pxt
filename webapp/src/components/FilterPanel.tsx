e72cad806809672a50a466288b4727a66ad808df
Vivian Li <vivl@microsoft.com>
2021-02-16 10:53:31 -0800

    Add Filter option to Gallery images

---
+++ webapp/src/components/FilterPanel.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+
+interface FilterTagProps {
+    tag: string;
+    selected: boolean;
+    onClick: (selected: string) => void;
+}
+
+export class FilterTag extends React.Component<FilterTagProps> {
+
+    render() {
+        return <div className="filter-tag">
+            <div className="filter-tag-box" role="button" onClick={()=>{this.props.onClick(this.props.tag)}}>
+                <i className={this.props.selected ? `icon check square outline` : `icon square outline`}></i>
+            </div>
+            <div className="filter-tag-name" onClick={()=>{this.props.onClick(this.props.tag)}}>{lf(this.props.tag)}</div>
+        </div>
+    }
+}
+
+
+interface FilterPanelSubheadingProps {
+    subheading: string;
+    buttonText?: string;
+    buttonAction?: () => void;
+}
+
+export class FilterPanelSubheading extends React.Component<FilterPanelSubheadingProps> {
+
+    render() {
+        return <div className="filter-subheading-row">
+            <div className="filter-subheading-title">{`${this.props.subheading}:`}</div>
+            {this.props.buttonText && <div className="filter-subheading-button" role="button" onClick={this.props.buttonAction}>{this.props.buttonText}</div>}
+        </div>
+    }
+}
+
+interface FilterPanelProps {
+    enabledTags: string[];
+    tagClickHandler: (tag: string) => void;
+    clearTags: () => void;
+    tagOptions: string[];
+}
+
+export class FilterPanel extends React.Component<FilterPanelProps> {
+    protected isTagSelected(tag: string) {
+        return this.props.enabledTags.indexOf(tag.toLowerCase()) < 0 ? false : true;
+    }
+
+    render() {
+        const tags = this.props.tagOptions;
+        return <div className="filter-panel">
+            <div className="filter-title">{lf("Filter")}</div>
+            <FilterPanelSubheading subheading={lf("Categories")} buttonText="Clear" buttonAction={this.props.clearTags}/>
+            <div className="filter-tag-list">
+                {tags.map(tag => <FilterTag key={tag} tag={tag} selected={this.isTagSelected(tag)} onClick={this.props.tagClickHandler}/>)}
+            </div>
+        </div>
+    }
+}
--- webapp/src/components/assetEditor/assetTopbar.tsx
+++ webapp/src/components/assetEditor/assetTopbar.tsx
@@ -3,28 +3,12 @@ import { connect } from 'react-redux';
 
 import { GalleryView } from './store/assetEditorReducer';
 import { AssetGalleryTab } from './assetGalleryTab';
-
-import {dispatchFilterButtonClicked} from './actions/dispatch';
-
-interface AssetTopbarProps {
-    dispatchFilterButtonClicked: () => void;
-}
-
-export class AssetTopbarImpl extends React.Component<AssetTopbarProps> {
+export class AssetTopbar extends React.Component<{}> {
 
     render() {
         return <div className="asset-editor-topbar">
             <AssetGalleryTab title={lf("My Assets")} view={GalleryView.User} />
             <AssetGalleryTab title={lf("Gallery")} view={GalleryView.Gallery} />
-            <div className="asset-editor-filter-button" role="button" onClick={this.props.dispatchFilterButtonClicked}>
-                <i className="icon filter" />
-            </div>
         </div>
     }
 }
-
-const mapDispatchToProps = {
-    dispatchFilterButtonClicked
-};
-
-export const AssetTopbar = connect(undefined, mapDispatchToProps)(AssetTopbarImpl)
