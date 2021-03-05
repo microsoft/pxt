556a99696074e3a7caa26d9882423842a476102d
Eric Anderson <eanders@microsoft.com>
2021-03-05 13:31:09 -0800

    lint

--- webapp/src/cloud.ts
+++ webapp/src/cloud.ts
@@ -32,8 +32,8 @@ const cloudMetadataFields: (keyof Header)[] = [
     'cloudVersion', 'cloudCurrent', 'cloudLastSyncTime', 'cloudUserId'
 ]
 
-function excludeMetadataFields(h: Header, fields:(keyof Header)[]): Header {
-    const clone = {...h}
+function excludeMetadataFields(h: Header, fields: (keyof Header)[]): Header {
+    const clone = { ...h }
     for (let k of fields)
         delete clone[k]
     return clone
@@ -200,7 +200,7 @@ async function transferToCloud(local: Header, cloudVersion: string): Promise<Hea
 }
 
 async function transferFromCloud(local: Header | null, remoteFile: File): Promise<Header> {
-    const newHeader = {...local || {}, ...remoteFile.header} // make sure we keep local-only metadata like _rev
+    const newHeader = { ...local || {}, ...remoteFile.header } // make sure we keep local-only metadata like _rev
     if (local) {
         // we've decided to overwrite what we have locally with what is in the
         // the cloud, so acquire the header session
@@ -258,7 +258,7 @@ async function resolveConflict(local: Header, remoteFile: File) {
     } catch (e) {
         // we want to swallow this and keep going since step 3. is the essentail one to resolve the conflcit.
         pxt.reportException(e);
-        pxt.tickEvent(`identity.sync.conflict.reloadEditorFailed`, {exception: e});
+        pxt.tickEvent(`identity.sync.conflict.reloadEditorFailed`, { exception: e });
     }
 
     // 3. overwrite local changes in the original project with cloud changes
@@ -268,7 +268,7 @@ async function resolveConflict(local: Header, remoteFile: File) {
         // let exceptions propegate since there's nothing localy we can do to recover, but log something
         //  since this is a bad case (may lead to repeat duplication).
         pxt.reportException(e);
-        pxt.tickEvent(`identity.sync.conflict.overwriteLocalFailed`, {exception: e});
+        pxt.tickEvent(`identity.sync.conflict.overwriteLocalFailed`, { exception: e });
         throw e;
     }
 
@@ -281,7 +281,7 @@ async function resolveConflict(local: Header, remoteFile: File) {
             header: lf("Project '{0}' had a conflict", local.name),
             body:
                 lf("Project '{0}' was edited in two places and the changes conflict. The changes from this computer (from {1}) have instead been saved to '{2}'. The changes made elsewhere (from {3}) remain in '{4}'.",
-                local.name, U.timeSince(local.modificationTime), newCopyHdr.name, U.timeSince(remoteFile.header.modificationTime), remoteFile.header.name),
+                    local.name, U.timeSince(local.modificationTime), newCopyHdr.name, U.timeSince(remoteFile.header.modificationTime), remoteFile.header.name),
             disagreeLbl: lf("Got it!"),
             disagreeClass: "green",
             hasCloseIcon: false,
@@ -289,7 +289,7 @@ async function resolveConflict(local: Header, remoteFile: File) {
     } catch (e) {
         // we want to swallow this and keep going since it's non-essential
         pxt.reportException(e);
-        pxt.tickEvent(`identity.sync.conflict.dialogNotificationFailed`, {exception: e});
+        pxt.tickEvent(`identity.sync.conflict.dialogNotificationFailed`, { exception: e });
     }
 }
 
@@ -310,7 +310,7 @@ async function syncAsyncInternal(hdrs?: Header[]): Promise<Header[]> {
         const syncStart = U.nowSeconds()
         pxt.tickEvent(`identity.sync.start`)
         const agoStr = (t: number) => `${syncStart - t} seconds ago`
-        const remoteFiles: {[id: string]: File} = {}
+        const remoteFiles: { [id: string]: File } = {}
         const getWithCacheAsync = async (h: Header): Promise<File> => {
             if (!remoteFiles[h.id]) {
                 remoteFiles[h.id] = await getAsync(h)
@@ -492,7 +492,7 @@ export async function convertCloudToLocal(userId: string) {
 
 const CLOUDSAVE_DEBOUNCE_MS = 3000;
 const CLOUDSAVE_MAX_MS = 15000;
-let headerWorklist: {[headerId: string]: boolean} = {};
+let headerWorklist: { [headerId: string]: boolean } = {};
 let onHeaderChangeTimeout: number = 0;
 let onHeaderChangeStarted: number = 0;
 const onHeaderChangeSubscriber: data.DataSubscriber = {
