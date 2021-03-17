8e9d7b0edce6e5a78fa5e0f307dbcc12621ff58f
Daryl Zuniga <Daryl.Zuniga@gmail.com>
2021-03-16 18:44:38 -0600

    Fixes compilation of py `int()` function (#7967)
    * Add failing test case for Math.trunc
    
    * hard-code a fix for py int() function
    
    * clarify

--- pxtpy/converter.ts
+++ pxtpy/converter.ts
@@ -401,9 +401,9 @@ namespace pxt.py {
     }
 
     function canonicalize(t: Type): Type {
-        if (t.union) {
-            t.union = canonicalize(t.union)
-            return t.union
+        if (t.unifyWith) {
+            t.unifyWith = canonicalize(t.unifyWith)
+            return t.unifyWith
         }
         return t
     }
@@ -575,7 +575,7 @@ namespace pxt.py {
             return
 
         if (t0.primType === "any") {
-            t0.union = t1
+            t0.unifyWith = t1
             return
         }
 
@@ -584,12 +584,12 @@ namespace pxt.py {
 
         if (c0 && c1) {
             if (c0 === c1) {
-                t0.union = t1 // no type-state change here - actual change would be in arguments only
+                t0.unifyWith = t1 // no type-state change here - actual change would be in arguments only
                 if (t0.typeArgs && t1.typeArgs) {
                     for (let i = 0; i < Math.min(t0.typeArgs.length, t1.typeArgs.length); ++i)
                         unify(a, t0.typeArgs[i], t1.typeArgs[i])
                 }
-                t0.union = t1
+                t0.unifyWith = t1
             } else {
                 typeError(a, t0, t1)
             }
@@ -598,7 +598,7 @@ namespace pxt.py {
         } else {
             // the type state actually changes here
             numUnifies++
-            t0.union = t1
+            t0.unifyWith = t1
             // detect late unifications
             // if (currIteration > 2) error(a, `unify ${t2s(t0)} ${t2s(t1)}`)
         }
@@ -2075,6 +2075,30 @@ namespace pxt.py {
                     unifyClass(n, n.tsType!, ctx.currClass.baseClass.symInfo)
                 }
                 return B.mkText("super")
+            }
+
+            if (isCallTo(n, "int") && orderedArgs.length === 1 && orderedArgs[0]) {
+                // int() compiles to either Math.trunc or parseInt depending on how it's used. Our builtin
+                // function mapping doesn't handle this well so we special case that here.
+                // TODO: consider generalizing this approach.
+                const arg = orderedArgs[0]
+                const argN = expr(arg)
+                const argT = typeOf(arg)
+                if (argT.primType === "string") {
+                    return B.mkGroup([
+                        B.mkText(`parseInt`),
+                        B.mkText("("),
+                        argN,
+                        B.mkText(")")
+                    ]);
+                } else if (argT.primType === "number") {
+                    return B.mkGroup([
+                        B.mkInfix(B.mkText(`Math`), ".", B.mkText(`trunc`)),
+                        B.mkText("("),
+                        argN,
+                        B.mkText(")")
+                    ]);
+                }
             }
 
             if (!fun) {
