2fcbe3c22dba4ee25ec5b7a513fee3137caf8b75
Richard Knoll <riknoll@users.noreply.github.com>
2021-03-18 17:07:52 -0700

    Adding stars instead of circle icons (#7980)
    * Adding stars instead of circle icons
    
    * Fixing accidental revert

--- skillmap/src/components/GraphNode.tsx
+++ skillmap/src/components/GraphNode.tsx
@@ -65,22 +65,19 @@ export class GraphNode extends React.Component<GraphNodeProps, GraphNodeState> {
         }
     }
 
-    protected getNodeMarker(status: string, width: number, theme: SkillGraphTheme): JSX.Element {
+    protected getNodeMarker(status: string, width: number, foreground: string, background: string): JSX.Element {
         // Used for positioning the corner circle on completed activities so that it isn't
         // perfectly aligned with the node
         const nudgeUnit = width / 50;
+        const starUnit = width / 576;
 
         switch (status) {
             case "notstarted":
-                return <g transform={`translate(${(width / 2) - (3 * nudgeUnit)} ${(-width / 2) + (3 * nudgeUnit)})`}>
-                    <circle cx={0} cy={0} r={(width / 4) - nudgeUnit} stroke={theme.strokeColor} strokeWidth="2" fill={theme.selectedStrokeColor}/>
-                    <text dy="2"
-                        textAnchor="middle"
-                        alignmentBaseline="middle"
-                        fill={theme.strokeColor}
-                        className="graph-status-icon">
-                            {"\uf12a"}
-                        </text>
+                return <g transform={`translate(${(width / 2) - (12 * nudgeUnit)} ${(-width / 2) - (9 * nudgeUnit)})`}>
+                    <title>{lf("Not Started")}</title>
+                    <g transform={`scale(${starUnit / 2})`}>
+                        {fontAwesomeStar(foreground, background)}
+                    </g>
                 </g>
             default:
                 return <g />
@@ -114,7 +111,7 @@ export class GraphNode extends React.Component<GraphNodeProps, GraphNodeState> {
                 <circle cx={0} cy={0} r={width / 2} fill={background} stroke={foreground} strokeWidth="2" /> :
                 <rect x={-width / 2} y={-width / 2} width={width} height={width} rx={width / 10} fill={background} stroke={foreground} strokeWidth="2" />
             }
-            { kind === "activity" && this.getNodeMarker(status, width, theme) }
+            { kind === "activity" && this.getNodeMarker(status, width, theme.selectedStrokeColor, theme.strokeColor) }
             <text dy="4"
                 textAnchor="middle"
                 alignmentBaseline="middle"
@@ -135,4 +132,13 @@ export class GraphNode extends React.Component<GraphNodeProps, GraphNodeState> {
             });
         }
     }
+}
+
+function fontAwesomeStar(fill: string, stroke: string) {
+    // Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+    return <path
+            fill={fill}
+            stroke={stroke}
+            strokeWidth="50"
+            d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" />
 }
