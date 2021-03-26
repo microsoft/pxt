b71267c97c819b88ec3bb025a927e16a600b4835
Vivian Li <vivl@microsoft.com>
2021-03-25 17:29:00 -0700

    More tutorial cleaning for Immersive Reader (#8002)
    * More tutorial cleaning for Immersive Reader
    
    * lint
    
    * Remove debug logs
    
    * Bigger and Better regexs
    
    * lint
    
    * lint friendly regex, put metadata regex together
    
    * metadata regex too

--- webapp/src/immersivereader.tsx
+++ webapp/src/immersivereader.tsx
@@ -15,15 +15,22 @@ export type ImmersiveReaderToken = {
 }
 
 function beautifyText(content: string): string {
+    // The order of these functions matter
     const cleaningFuncs = [
         cleanImages,
+        cleanAltText,
         cleanBlockAnnotation,
+        cleanMetadata,
         cleanBold,
-        cleanHorizontalRule,
+        cleanItalics,
         cleanNewLines,
+        cleanHorizontalRule,
+        cleanBlockquotes,
+        cleanEmojis,
+        cleanUnicodeEmojis,
+        convertToggles
     ];
     let contentWIP = content;
-
     cleaningFuncs.forEach(clean => {
         contentWIP = clean(contentWIP);
     })
@@ -38,6 +45,14 @@ function beautifyText(content: string): string {
         );
     }
 
+    // Keep the displayed word in the alt text, discard the rest
+    function cleanAltText(content: string): string {
+        return content.replace(
+            /\w*\[([^\]]*)\]\w*\([^)]*\)/gu,
+            (matched, word, offset, s) => lf("{0}", word)
+        );
+    }
+
     // Don't show any images
     function cleanImages(content: string): string {
         return content.replace(
@@ -46,19 +61,37 @@ function beautifyText(content: string): string {
         );
     }
 
-    // Remove ** around bold text, replace with "
-    function cleanBold(content: string): string {
+    // Remove any triple tick annotations ```
+    function cleanMetadata(content: string): string {
         return content.replace(
-            /\*\*([^*]+)\*\*/gu,
-            (matched, word, offset, s) => lf("\"{0}\"", word)
+            pxt.tutorial.getMetadataRegex(),
+            ""
+        );
+    }
+
+    // Remove ** and __ around bold text, replace with " if it's only one word
+    function cleanBold(content: string): string {
+
+        let singleWordBold = content.replace(
+            /\*\*([^*^\s]+)\*\*|__([^\s^_]+)__/gu,
+            (matched, boldStar, boldUnder, offset, s) => {
+                return "\"" + lf("{0}", (boldStar ? boldStar : boldUnder)) + "\""
+            }
+        )
+
+        return singleWordBold.replace(
+            /\*\*([^*]+)\*\*|__([^_]+)__/gu,
+            (matched, boldStar, boldUnder, offset, s) => {
+                return boldStar ? lf("{0}", boldStar) : lf ("{0}", boldUnder);
+            }
         );
     }
 
     // replace horizontal rules with new lines
     function cleanHorizontalRule(content: string): string {
         return content.replace(
-            /^\* \* \*$/gum,
-            "- - -"
+            /^\* \* \*$|^- - -$|^<hr\/>$|---/gum,
+            "<br />"
         );
     }
 
@@ -66,8 +99,73 @@ function beautifyText(content: string): string {
     function cleanNewLines(content: string): string {
         return content.replace(
             /[\r\n]{2,}/gum,
-            `<br />`
+            `<br />\n`
         );
+    }
+
+    // Remove >> for blockquotes
+    function cleanBlockquotes(content: string): string {
+        return content.replace(
+            /^>>/gu,
+            ""
+        )
+    }
+
+    // Replace unicode emojis with text that can be read aloud
+    function cleanUnicodeEmojis(content: string): string {
+        const replacedA = content.replace(
+            /[Ⓐ|🅐]/gu,
+            lf("{0}", "A")
+        );
+
+        return replacedA.replace(
+            /[Ⓑ|🅑]/gu,
+            lf("{0}", "B")
+        );
+    }
+
+    // Remove all emojis because they get read out. Characters that aren't
+    // emojis don't get hit by this RegEx, but they're silent
+    function cleanEmojis(content: string): string {
+        let PICTOGRAPHIC_REGEX: RegExp;
+        try { // Some browsers do not support unicode property escape
+            PICTOGRAPHIC_REGEX = new RegExp("\\p{Extended_Pictographic}", "ug")
+        } catch {}
+        const specialEmojis: string[] = [];
+        if (PICTOGRAPHIC_REGEX) {
+            return content.replace(
+                PICTOGRAPHIC_REGEX,
+                (emoji, capture, offset, s) => {
+                    if (emoji == "🔲") {
+                        return "•"
+                    } else if (specialEmojis.indexOf(emoji) < 0) {
+                        return ""
+                    } else {
+                        return emoji
+                    }
+                }
+            )
+        }
+        return content;
+    }
+
+    // Remove * and _ italic annotations
+    function cleanItalics(content: string): string {
+        return content.replace(
+            /\*([^*]+)\*|_([^_]+)_/gu,
+            (matched, starItalics, underlineItalics, offset, s) =>
+                lf("{0}", starItalics ? starItalics : underlineItalics)
+        );
+    }
+
+    // Change toggles to be surrounded with ` instead of <>
+    function convertToggles(content: string): string {
+        return content.replace(
+            /<(true|false|down|up|high|low|on|off|yes|no|win|lose)>/gui,
+            (matched, word, offset, s) => {
+                return "`" + lf("{0}", word) + "`";
+            }
+        )
     }
 }
 
