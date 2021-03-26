578bcd43c4c21a3a59f5d931a370a2a0227e243c
Vivian Li <vivl@microsoft.com>
2021-03-25 21:18:33 -0700

    [stable6.12] Immersive Reader ports (#8012)
    * more immersive reader beautification #7986
    
    * More tutorial cleaning for Immersive Reader #8002
    
    * Make it more obvious when Immersive Reader is focused/hovered on #8011

--- webapp/src/immersivereader.tsx
+++ webapp/src/immersivereader.tsx
@@ -15,40 +15,157 @@ export type ImmersiveReaderToken = {
 }
 
 function beautifyText(content: string): string {
-    const cleaningFuncs = [cleanImages, cleanBlockAnnotation, cleanBold];
+    // The order of these functions matter
+    const cleaningFuncs = [
+        cleanImages,
+        cleanAltText,
+        cleanBlockAnnotation,
+        cleanMetadata,
+        cleanBold,
+        cleanItalics,
+        cleanNewLines,
+        cleanHorizontalRule,
+        cleanBlockquotes,
+        cleanEmojis,
+        cleanUnicodeEmojis,
+        convertToggles
+    ];
     let contentWIP = content;
-
     cleaningFuncs.forEach(clean => {
         contentWIP = clean(contentWIP);
     })
 
     return contentWIP;
 
-    // Change ``|| around blocks to {}
+    // Change ``|| around blocks to ""
     function cleanBlockAnnotation(content: string): string {
-        const blockLiteral = /``\|\|([\w|\s]+:[\w|\s]+)\|\|``/
-        const blockRegex = new RegExp(blockLiteral, "g");
-        const blocksRemoved = content.replace(blockRegex, (matched, word, offset, s) => {
-            return "{" + word + "}";
-        })
-        return blocksRemoved;
+        return content.replace(
+            /`?`\|\|[\w|\s]+:(.+?)\|\|``?/gu,
+            (matched, word, offset, s) => lf("\"{0}\"", word)
+        );
+    }
+
+    // Keep the displayed word in the alt text, discard the rest
+    function cleanAltText(content: string): string {
+        return content.replace(
+            /\w*\[([^\]]*)\]\w*\([^)]*\)/gu,
+            (matched, word, offset, s) => lf("{0}", word)
+        );
     }
 
     // Don't show any images
     function cleanImages(content: string): string {
-        const imageRegEx = /(!\[.*\]\(.*\))/g;
-        const imagesRemoved = content.replace(imageRegEx, "");
-        return imagesRemoved;
+        return content.replace(
+            /(!\w*\[[^\]]*\]\w*\([^)]*\))/gu,
+            ""
+        );
     }
 
-    // Remove ** around bold text, replace with "
+    // Remove any triple tick annotations ```
+    function cleanMetadata(content: string): string {
+        return content.replace(
+            pxt.tutorial.getMetadataRegex(),
+            ""
+        );
+    }
+
+    // Remove ** and __ around bold text, replace with " if it's only one word
     function cleanBold(content: string): string {
-        const boldLiteral = /\*\*([\w|\s]+)\*\*/
-        const boldRegex = new RegExp(boldLiteral, "g");
-        const boldRemoved = content.replace(boldRegex, (matched, word, offset, s) => {
-            return "\"" + word + "\""
-        })
-        return boldRemoved;
+
+        let singleWordBold = content.replace(
+            /\*\*([^*^\s]+)\*\*|__([^\s^_]+)__/gu,
+            (matched, boldStar, boldUnder, offset, s) => {
+                return "\"" + lf("{0}", (boldStar ? boldStar : boldUnder)) + "\""
+            }
+        )
+
+        return singleWordBold.replace(
+            /\*\*([^*]+)\*\*|__([^_]+)__/gu,
+            (matched, boldStar, boldUnder, offset, s) => {
+                return boldStar ? lf("{0}", boldStar) : lf ("{0}", boldUnder);
+            }
+        );
+    }
+
+    // replace horizontal rules with new lines
+    function cleanHorizontalRule(content: string): string {
+        return content.replace(
+            /^\* \* \*$|^- - -$|^<hr\/>$|---/gum,
+            "<br />"
+        );
+    }
+
+    // replace consecutive new lines with break tags so that they don't get smooshed
+    function cleanNewLines(content: string): string {
+        return content.replace(
+            /[\r\n]{2,}/gum,
+            `<br />\n`
+        );
+    }
+
+    // Remove >> for blockquotes
+    function cleanBlockquotes(content: string): string {
+        return content.replace(
+            /^>>/gu,
+            ""
+        )
+    }
+
+    // Replace unicode emojis with text that can be read aloud
+    function cleanUnicodeEmojis(content: string): string {
+        const replacedA = content.replace(
+            /[Ⓐ|🅐]/gu,
+            lf("{0}", "A")
+        );
+
+        return replacedA.replace(
+            /[Ⓑ|🅑]/gu,
+            lf("{0}", "B")
+        );
+    }
+
+    // Remove all emojis because they get read out. Characters that aren't
+    // emojis don't get hit by this RegEx, but they're silent
+    function cleanEmojis(content: string): string {
+        let PICTOGRAPHIC_REGEX: RegExp;
+        try { // Some browsers do not support unicode property escape
+            PICTOGRAPHIC_REGEX = new RegExp("\\p{Extended_Pictographic}", "ug")
+        } catch {}
+        const specialEmojis: string[] = [];
+        if (PICTOGRAPHIC_REGEX) {
+            return content.replace(
+                PICTOGRAPHIC_REGEX,
+                (emoji, capture, offset, s) => {
+                    if (emoji == "🔲") {
+                        return "•"
+                    } else if (specialEmojis.indexOf(emoji) < 0) {
+                        return ""
+                    } else {
+                        return emoji
+                    }
+                }
+            )
+        }
+        return content;
+    }
+
+    // Remove * and _ italic annotations
+    function cleanItalics(content: string): string {
+        return content.replace(
+            /\*([^*]+)\*|_([^_]+)_/gu,
+            (matched, starItalics, underlineItalics, offset, s) =>
+                lf("{0}", starItalics ? starItalics : underlineItalics)
+        );
+    }
+
+    // Change toggles to be surrounded with ` instead of <>
+    function convertToggles(content: string): string {
+        return content.replace(
+            /<(true|false|down|up|high|low|on|off|yes|no|win|lose)>/gui,
+            (matched, word, offset, s) => {
+                return "`" + lf("{0}", word) + "`";
+            }
+        )
     }
 }
 
