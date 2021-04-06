7ed01f6f338f7a604cd34846029109f5c7015e0d
shakao <shakao@microsoft.com>
2021-04-06 15:51:29 -0700

    Apply high contrast styles when OS high contrast is enabled

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -162,7 +162,7 @@ export class ProjectView
         this.reload = false; //set to true in case of reset of the project where we are going to reload the page.
         this.settings = JSON.parse(pxt.storage.getLocal("editorSettings") || "{}")
         const shouldShowHomeScreen = this.shouldShowHomeScreen();
-        const isHighContrast = /hc=(\w+)/.test(window.location.href);
+        const isHighContrast = /hc=(\w+)/.test(window.location.href) || (window.matchMedia?.('(forced-colors: active)')?.matches);
         if (isHighContrast) core.setHighContrast(true);
 
         const simcfg = pxt.appTarget.simulator;
