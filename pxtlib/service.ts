a30971ffd55e54de7235b111f5f569aa16ed27b6
Joey Wunderlich <joey.wunderlich@gmail.com>
2021-05-13 11:58:13 -0700

    fix block.loc. where lang is fully clarified (e.g. .zh-CN)

--- pxtlib/service.ts
+++ pxtlib/service.ts
@@ -805,7 +805,7 @@ namespace ts.pxtc {
         let didSomething = true
         while (didSomething) {
             didSomething = false
-            cmt = cmt.replace(/\/\/%[ \t]*([\w\.]+)(=(("[^"\n]*")|'([^'\n]*)'|([^\s]*)))?/,
+            cmt = cmt.replace(/\/\/%[ \t]*([\w\.-]+)(=(("[^"\n]*")|'([^'\n]*)'|([^\s]*)))?/,
                 (f: string, n: string, d0: string, d1: string,
                     v0: string, v1: string, v2: string) => {
                     let v = v0 ? JSON.parse(v0) : (d0 ? (v0 || v1 || v2) : "true");
