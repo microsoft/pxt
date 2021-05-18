fb2c7acc7d861edf5c5a959d87d8c85ed81a80d6
shakao <34112083+shakao@users.noreply.github.com>
2021-05-18 13:31:41 -0700

    Fix skill graph position relative to background image (#8160)
    * Fix skill graph position relative to background image
    
    * Move SVG computation into skillGraphUtils

--- skillmap/src/lib/skillGraphUtils.ts
+++ skillmap/src/lib/skillGraphUtils.ts
@@ -1,7 +1,88 @@
+////////////////////////////////////////////
+////////                            ////////
+////////        SVG RENDERING       ////////
+////////                            ////////
+////////////////////////////////////////////
+
 export interface SvgCoord {
     x: number;
     y: number;
 }
+
+export interface SvgGraphItem {
+    activity: MapActivity;
+    position: SvgCoord;
+}
+
+export interface SvgGraphPath {
+    points: SvgCoord[];
+}
+
+export interface SvgGraph {
+    map: SkillMap;
+    items: SvgGraphItem[];
+    paths: SvgGraphPath[];
+    width: number;
+    height: number
+}
+
+export const UNIT = 10;
+export const PADDING = 4;
+export const MIN_HEIGHT = 40 * UNIT;
+export const MIN_WIDTH = 60 * UNIT;
+
+export function getGraph(map: SkillMap): SvgGraph {
+    const nodes = orthogonalGraph(map.root);
+    let maxDepth = 0, maxOffset = 0;
+
+    // Convert into renderable items
+    const items: SvgGraphItem[] = [];
+    const paths: SvgGraphPath[] = [];
+    for (let current of nodes) {
+        const { depth, offset } = current;
+        items.push({
+            activity: current,
+            position: getPosition(depth, offset)
+        } as any);
+
+        if (current.edges) {
+            current.edges.forEach(edge => {
+                const points: SvgCoord[] = [];
+                edge.forEach(n => points.push(getPosition(n.depth, n.offset)));
+                paths.push({ points });
+            });
+        }
+
+        maxDepth = Math.max(maxDepth, current.depth);
+        maxOffset = Math.max(maxOffset, current.offset);
+    }
+
+    const width = getX(maxDepth) + UNIT * PADDING;
+    const height = getY(maxOffset) + UNIT * PADDING;
+
+    return { map, items, paths, width, height };
+}
+
+// This function converts graph position (no units) to x/y (SVG units)
+function getPosition(depth: number, offset: number): SvgCoord {
+    return { x: getX(depth), y: getY(offset) }
+}
+
+function getX(position: number) {
+    return ((position * 12) + PADDING) * UNIT;
+}
+
+function getY(position: number) {
+    return ((position * 9) + PADDING) * UNIT;
+}
+
+
+
+////////////////////////////////////////////
+////////                            ////////
+////////        GRAPH LAYOUT        ////////
+////////                            ////////
+////////////////////////////////////////////
 
 interface GraphCoord {
     depth: number; // The depth of this node (distance from root)
