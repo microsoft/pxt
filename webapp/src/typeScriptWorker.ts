49bbff25d39647b02665be7ea0304c1fa570cc75
Richard Knoll <riknoll@microsoft.com>
2021-05-26 17:56:52 -0700

    get outta here tabs

--- webapp/src/typeScriptWorker.ts
+++ webapp/src/typeScriptWorker.ts
@@ -1,19 +1,19 @@
 /// <reference path="../../localtypings/monacoTypeScript.d.ts"/>
 
 const worker: monaco.languages.typescript.CustomTSWebWorkerFactory = (TSWorkerClass, tsc, libs) => {
-	return class PxtWorker extends TSWorkerClass {
-		async getCompletionsAtPosition(
-			fileName: string,
-			position: number
-		) {
-			const res: ts.CompletionInfo = await super.getCompletionsAtPosition(fileName, position)
-			if (res) {
-				res.entries = res.entries.filter(ent => !ent.name.startsWith("_"));
-			}
+    return class PxtWorker extends TSWorkerClass {
+        async getCompletionsAtPosition(
+            fileName: string,
+            position: number
+        ) {
+            const res: ts.CompletionInfo = await super.getCompletionsAtPosition(fileName, position)
+            if (res) {
+                res.entries = res.entries.filter(ent => !ent.name.startsWith("_"));
+            }
 
-			return res;
-		}
-	}
+            return res;
+        }
+    }
 }
 
 (self as any).customTSWorkerFactory = worker;
