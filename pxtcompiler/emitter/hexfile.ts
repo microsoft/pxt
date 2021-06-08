244fd9878b7a2ead29c14bf0b2d9a7a9d1916c4b
Michal Moskal <michal@moskal.me>
2021-06-07 17:40:03 -0700

    Avoid the need for bin-patching strings with C++ magic

--- pxtcompiler/emitter/hexfile.ts
+++ pxtcompiler/emitter/hexfile.ts
@@ -159,6 +159,7 @@ namespace ts.pxtc {
             }
         }
 
+        // see PXT_EXPORTData
         const pointerListMarker = "0108010842424242010801083ed8e98d"
 
         export function setupFor(opts: CompileTarget, extInfo: ExtensionInfo) {
@@ -228,6 +229,7 @@ namespace ts.pxtc {
                                 }
                                 off += 4
                             }
+                            // store the start of code address at PXT_EXPORTData[4]
                             pxt.HF2.write32(drom.data, ptroff, ctx.codeStartAddrPadded)
                             break
                         }
@@ -457,133 +459,6 @@ namespace ts.pxtc {
             return r.toUpperCase();
         }
 
-        // constant strings in the binary are 4-byte aligned, and marked
-        // with "@PXT@:" at the beginning - this 6 byte string needs to be
-        // replaced with proper reference count (0xfffe to indicate read-only
-        // flash location), string virtual table, and the length of the string
-        function patchString(bytes: ArrayLike<number>) {
-            let stringVT = [0xfe, 0xff, 0x01, 0x00]
-            assert(stringVT.length == 4)
-            // @PXT
-            if (!(bytes[0] == 0x40 && bytes[1] == 0x50 && bytes[2] == 0x58 && bytes[3] == 0x54))
-                oops();
-            // @:
-            if (bytes[5] == 0x3a) {
-                let isString = false
-                let isBuffer = false
-                if (bytes[4] == 0x40) isString = true
-                else if (bytes[4] == 0x23) isBuffer = true
-                else return null
-
-                let vt = lookupFunctionAddr(isString ? "pxt::string_inline_ascii_vt" : "pxt::buffer_vt")
-                let headerBytes = new Uint8Array(6)
-
-                if (!vt) oops("missing vt: " + isString)
-                vt ^= 1
-                if (vt & 3) oops("Unaligned vt: " + vt)
-
-                pxt.HF2.write32(headerBytes, 0, vt)
-
-                let len = 0
-                if (isString)
-                    while (6 + len < bytes.length) {
-                        if (bytes[6 + len] == 0)
-                            break
-                        len++
-                    }
-                if (6 + len >= bytes.length)
-                    U.oops("constant string too long!")
-                pxt.HF2.write16(headerBytes, 4, len)
-                return headerBytes
-                //console.log("patch file: @" + addr + ": " + U.toHex(patchV))
-            }
-            return null
-        }
-
-        function applyPatches(f: UF2.BlockFile, binfile: Uint8Array = null) {
-            let patchAt = (b: Uint8Array, i: number,
-                readMore: () => Uint8Array) => {
-                // @PXT
-                if (b[i] == 0x40 && b[i + 1] == 0x50 && b[i + 2] == 0x58 && b[i + 3] == 0x54) {
-                    return patchString(readMore())
-                }
-                return null
-            }
-
-            if (binfile) {
-                for (let i = 0; i < binfile.length - 8; i += 4) {
-                    let patchV = patchAt(binfile, i, () => binfile.slice(i, i + 200))
-                    if (patchV)
-                        U.memcpy(binfile, i, patchV)
-                }
-            } else {
-                for (let bidx = 0; bidx < f.blocks.length; ++bidx) {
-                    let b = f.blocks[bidx]
-                    let upper = f.ptrs[bidx] << 8
-                    for (let i = 32; i < 32 + 256; i += 4) {
-                        let addr = upper + i - 32
-                        let patchV = patchAt(b, i, () => UF2.readBytesFromFile(f, addr, 200))
-                        if (patchV)
-                            UF2.writeBytes(f, addr, patchV)
-                    }
-                }
-            }
-        }
-
-        function applyHexPatches(myhex: string[]) {
-            const marker = "40505854" // @PXT
-            for (let i = 0; i < myhex.length; ++i) {
-                // There could be a few such hex strings per line
-                for (let k = 0; k < 4; ++k) {
-                    let idx = myhex[i].indexOf(marker)
-                    if (idx > 0) {
-                        let off = (idx - 9) >> 1
-                        let bytes = readHex(myhex, i, off, 200)
-                        let patch = patchString(bytes)
-                        if (patch)
-                            writeHex(myhex, i, off, patch)
-                    } else {
-                        break
-                    }
-                }
-            }
-        }
-
-        function writeHex(myhex: string[], lineNo: number, offsetInLine: number, patch: ArrayLike<number>) {
-            let src = 0
-            while (src < patch.length) {
-                let parsedLine = parseHexBytes(myhex[lineNo])
-                parsedLine.pop() // pop the checksum
-                if (parsedLine[3] == 0x00) {
-                    // if data
-                    let pos = 4 + offsetInLine
-                    let len = parsedLine.length - pos
-                    for (let i = 0; i < len; ++i)
-                        if (src < patch.length)
-                            parsedLine[pos++] = patch[src++]
-                    myhex[lineNo] = hexBytes(parsedLine)
-                }
-                lineNo++
-                offsetInLine = 0
-            }
-        }
-
-        function readHex(myhex: string[], lineNo: number, offsetInLine: number, len: number) {
-            let outp: number[] = []
-            while (outp.length < len) {
-                let b = parseHexBytes(myhex[lineNo])
-                b.pop() // pop the checksum
-                if (b[3] == 0x00) {
-                    // if data
-                    let data = b.slice(4 + offsetInLine)
-                    U.pushRange(outp, data)
-                }
-                lineNo++
-                offsetInLine = 0
-            }
-            return outp
-        }
-
         export function patchHex(bin: Binary, buf: number[], shortForm: boolean, useuf2: boolean) {
             let myhex = ctx.hexlines.slice(0, ctx.codeStartIdx)
 
@@ -631,7 +506,6 @@ namespace ts.pxtc {
                 let resbuf = pxt.elf.patch(ctx.elfInfo, prog)
                 for (let i = 0; i < hd.length; ++i)
                     pxt.HF2.write16(resbuf, i * 2 + ctx.jmpStartAddr, hd[i])
-                applyPatches(null, resbuf)
                 if (uf2 && !bin.target.switches.rawELF) {
                     let bn = bin.options.name || "pxt"
                     bn = bn.replace(/[^a-zA-Z0-9\-\.]+/g, "_")
@@ -659,7 +533,6 @@ namespace ts.pxtc {
 
             if (uf2) {
                 UF2.writeHex(uf2, myhex)
-                applyPatches(uf2)
                 UF2.writeBytes(uf2, ctx.jmpStartAddr, nextLine(hd, ctx.jmpStartIdx).slice(4))
                 if (bin.checksumBlock) {
                     let bytes: number[] = []
@@ -668,7 +541,6 @@ namespace ts.pxtc {
                     UF2.writeBytes(uf2, bin.target.flashChecksumAddr, bytes)
                 }
             } else {
-                applyHexPatches(myhex)
                 myhex[ctx.jmpStartIdx] = hexBytes(nextLine(hd, ctx.jmpStartAddr))
                 if (bin.checksumBlock) {
                     U.oops("checksum block in HEX not implemented yet")
