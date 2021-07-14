7ccc0752e67d497db9a5f3d24f42d9eb476b26cd
shakao <34112083+shakao@users.noreply.github.com>
2021-07-14 09:38:45 -0700

    Send 'backtap' event for touchend events < longpress threshold (#8292)

--- webapp/src/sui.tsx
+++ webapp/src/sui.tsx
@@ -487,9 +487,9 @@ export interface ItemProps extends UiProps {
     active?: boolean;
     value?: string;
     onClick?: () => void;
-    onMouseDown?: () => void;
-    onMouseUp?: () => void;
-    onMouseLeave?: () => void;
+    onMouseDown?: (e: any) => void;
+    onMouseUp?: (e: any) => void;
+    onMouseLeave?: (e: any) => void;
     onKeyDown?: (e: React.KeyboardEvent<HTMLElement>) => void;
 }
 
