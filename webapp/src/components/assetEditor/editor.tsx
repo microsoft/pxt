9876853e3815a90291a698859bd9a224c0503c44
shakao <34112083+shakao@users.noreply.github.com>
2021-08-04 09:46:43 -0700

    Disable 'Create Asset' button in tutorials (#8349)

--- webapp/src/components/assetEditor/editor.tsx
+++ webapp/src/components/assetEditor/editor.tsx
@@ -83,10 +83,12 @@ export class AssetEditor extends Editor {
     }
 
     display(): JSX.Element {
+        // TODO: re-enable the create asset button in tutorials when we add
+        // the ability to switch editors inside a tutorial
         return <Provider store={store}>
             <div className="asset-editor-outer">
                 <AssetSidebar showAssetFieldView={this.showAssetFieldView} />
-                <AssetGallery showAssetFieldView={this.showAssetFieldView} />
+                <AssetGallery showAssetFieldView={this.showAssetFieldView} disableCreateButton={this.parent.isTutorial()} />
             </div>
         </Provider>
     }
