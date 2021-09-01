85992b8d700266df276b66a19e8b4d2c6a9f6323
shakao <shakao@microsoft.com>
2021-09-01 10:40:27 -0700

    Add markdown format for hiding step titles

--- pxtlib/tutorial.ts
+++ pxtlib/tutorial.ts
@@ -244,8 +244,14 @@ ${code}
         markdown.replace(stepRegex, function (match, flags, step) {
             step = step.trim();
             let { header, hint, requiredBlocks } = parseTutorialHint(step, metadata && metadata.explicitHints, metadata.tutorialCodeValidation);
+
+            // if title is not hidden ("{TITLE HERE}"), strip flags
+            const title = !flags.match(/^\{.*\}$/)
+                ? flags.replace(/@(fullscreen|unplugged|showdialog|showhint|tutorialCompleted|resetDiff)/gi, "")
+                : undefined;
+
             let info: TutorialStepInfo = {
-                title: flags, // TODO (shakao) determine format for title, strip flags
+                title,
                 contentMd: step,
                 headerContentMd: header
             }
