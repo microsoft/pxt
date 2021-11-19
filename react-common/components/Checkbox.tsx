56df87a2b2095d643d710c195e04c93d429a80f5
livcheerful <vivl@microsoft.com>
2021-11-19 09:08:20 -0800

    Remove mixed from checkbox

--- react-common/components/Checkbox.tsx
+++ react-common/components/Checkbox.tsx
@@ -16,7 +16,7 @@ export const Checkbox = (props: CheckboxProps) => {
     const { isChecked, label } = props;
     return (
         <div className="checkbox">
-            {isChecked == CheckboxStatus.Waiting ? <div className={`ui inline mini loader active`} aria-checked="mixed"/> :
+            {isChecked == CheckboxStatus.Waiting ? <div className={`ui inline mini loader active`}/> :
             <i className={`icon square outline ${isChecked == CheckboxStatus.Selected ? "check":""}`} onClick={() => props.onClick(!(isChecked == CheckboxStatus.Selected))}
                 role="checkbox" aria-checked={isChecked == CheckboxStatus.Selected} aria-labelledby={label}/>}
         </div>
