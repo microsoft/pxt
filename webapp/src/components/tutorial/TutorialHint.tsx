b59b9173cfd391aac58b9a4cd4bf5942175c9f61
shakao <shakao@microsoft.com>
2022-02-01 11:31:25 -0800

    Generalize tutorial hint bubble to a callout component

--- webapp/src/components/tutorial/TutorialHint.tsx
+++ webapp/src/components/tutorial/TutorialHint.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Button } from "../../sui";
 import { MarkedContent } from "../../marked";
+import { TutorialCallout } from "./TutorialCallout";
 
 interface TutorialHintProps {
     parent: pxt.editor.IProjectView;
@@ -15,36 +16,17 @@ interface TutorialHintProps {
 
 export function TutorialHint(props: TutorialHintProps) {
     const { parent, markdown, showLabel, tutorialId, currentStep } = props;
-    const [ visible, setVisible ] = React.useState(false);
 
-    const captureEvent = (e: any) => {
-        e.preventDefault();
-        e.stopPropagation();
-        e.nativeEvent?.stopImmediatePropagation();
-    }
-
-    const closeHint = (e: any) => {
-        document.removeEventListener("click", closeHint);
-        setVisible(false);
-    }
-
-    const toggleHint = () => {
+    const onHintClick = (visible: boolean) => {
         if (!visible) {
             pxt.tickEvent(`tutorial.showhint`, { tutorial: tutorialId, step: currentStep });
-            document.addEventListener("click", closeHint);
-        } else {
-            document.removeEventListener("click", closeHint);
         }
-        setVisible(!visible);
     }
 
-    return <div className="tutorial-hint-container">
-        <Button icon="lightbulb" text={showLabel ? lf("Hint") : undefined} className="tutorial-hint"
-            disabled={!markdown} onClick={markdown ? toggleHint : undefined} />
-        {visible && <div className={`tutorialhint no-select`} onClick={captureEvent}>
-            <MarkedContent markdown={markdown} unboxSnippets={true} parent={parent} />
-        </div>}
-        {visible && <div className="tutorial-hint-mask" onClick={closeHint} />}
-    </div>
-
+    return <TutorialCallout className="tutorial-hint"
+            buttonLabel={showLabel ? lf("Hint") : undefined}
+            buttonIcon="lightbulb"
+            onClick={onHintClick}>
+                {markdown && <MarkedContent markdown={markdown} unboxSnippets={true} parent={parent} />}
+        </TutorialCallout>
 }
