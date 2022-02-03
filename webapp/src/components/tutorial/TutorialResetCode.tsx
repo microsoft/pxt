6cf5e83765200913689dd43089803ac7fbd195eb
shakao <shakao@microsoft.com>
2022-02-03 11:48:51 -0800

    Add period after last sentence in reset callout

--- webapp/src/components/tutorial/TutorialResetCode.tsx
+++ webapp/src/components/tutorial/TutorialResetCode.tsx
@@ -23,7 +23,7 @@ export function TutorialResetCode(props: TutorialResetCodeProps) {
     return <TutorialCallout buttonLabel={lf("Replace my code")} className="tutorial-replace-code">
         <p>{lf("Did the code you're working with get off track? It happens.")}</p>
         <p>{lf("Click below to replace your code with updated blocks.")}</p>
-        <p>{lf("This will delete all your current code blocks. Any custom images and tiles can still be found in the gallery under \"My Assets\"")}</p>
+        <p>{lf("This will delete all your current code blocks. Any custom images and tiles can still be found in the gallery under \"My Assets\".")}</p>
         <div className="tutorial-replace-code-actions">
             <Button className="primary" text={lf("Replace my code")} onClick={onResetClick} />
         </div>
