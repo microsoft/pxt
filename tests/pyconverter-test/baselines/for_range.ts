f773f494c0963d0b5a0857bbd223177dcd407e9e
Richard Knoll <riknoll@users.noreply.github.com>
2022-05-04 16:03:15 -0700

    Fix py for-loop conversion when using three range arguments (#8813)

---
+++ tests/pyconverter-test/baselines/for_range.ts
@@ -0,0 +1,28 @@
+for (let a = 0; a < 5; a++) {
+
+}
+for (let b = 5; b < -1; b++) {
+
+}
+for (let c = 5; c < 5; c++) {
+
+}
+for (let d = 0; d < 5; d++) {
+
+}
+for (let e = 2; e < 5; e++) {
+
+}
+for (let f = 0; f < 5; f += 1) {
+
+}
+for (let g = 0; g > 5; g += -1) {
+
+}
+for (let h = 0; h > 5; h += -5) {
+
+}
+let x = 5
+for (let i of _py.range(5, -1, -x)) {
+
+}
