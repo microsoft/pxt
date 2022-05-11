adc0abcd99df05614acc2dfd92952fa1b9fbed7a
Abhijith Chatra <abchatra@microsoft.com>
2022-05-11 13:58:26 -0700

    Chaning a name

--- localtypings/pxtarget.d.ts
+++ localtypings/pxtarget.d.ts
@@ -49,7 +49,7 @@ declare namespace pxt {
         // list of trusted custom editor extension urls
         // that can bypass consent and send/receive messages
         approvedEditorExtensionUrls?: string[];
-        extensionsToolboxNoDelete?: string[]; // List of extensions to ignore when allowing for deletion in toolbox
+        extensionsToolboxDisallowDelete?: string[]; // List of extensions to ignore when allowing for deletion in toolbox
     }
 
     interface RepoData {
