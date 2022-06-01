ba13d523a04c20fc27d68c6ae2de7b274d11091e
Richard Knoll <riknoll@microsoft.com>
2022-06-01 14:19:53 -0700

    Add tests for extending a typescript class

---
+++ tests/pyconverter-test/baselines/class_extend_ts.ts
@@ -0,0 +1,11 @@
+class MySprite extends sprites.ExtendableSprite {
+    constructor() {
+        super(5)
+    }
+    
+    public draw(drawLeft: number, drawTop: number) {
+        super.draw(drawLeft, drawTop)
+    }
+    
+}
+
