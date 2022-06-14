5e84de7b005cba811654aaf3d796815f68f50cb1
Joey Wunderlich <jwunderl@users.noreply.github.com>
2022-06-14 15:54:05 -0700

    add a bit of padding for serial/csv toggle (#8910)
    * add a bit of padding for serial/csv toggle
    
    * lint

--- webapp/src/serial.tsx
+++ webapp/src/serial.tsx
@@ -672,7 +672,7 @@ export class Editor extends srceditor.Editor {
                         <span className="ui small header">{this.isSim ? lf("Simulator") : lf("Device")}</span>
                     </div>
                 </div>
-                {this.charts?.length == 0 &&<div id="serialPlaceholder" className="ui segment">
+                {this.charts?.length == 0 && <div id="serialPlaceholder" className="ui segment">
                     <div className="ui bottom left attached no-select label seriallabel">{lf("Values will be logged when the {0} sends data", this.isSim ? lf("simulator") : lf("device"))}</div>
                 </div>}
                 <div id="serialCharts" ref={this.handleChartRootRef}></div>
