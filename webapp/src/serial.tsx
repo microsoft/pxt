d16cdbb46a56467be3f9abf2e1a74d8678cc14ff
Joey Wunderlich <jwunderl@users.noreply.github.com>
2022-06-16 15:27:58 -0700

    Show data and dont show csv on hw (#8916)
    * change serial indicator to 'show data {dev/sim}', dont show csv opt on hw
    
    * on clear make sure not in csv view if in hw mode as well

--- webapp/src/serial.tsx
+++ webapp/src/serial.tsx
@@ -66,11 +66,11 @@ export class Editor extends srceditor.Editor {
         }
         this.isVisible = b;
 
-        if (this.isCsvView === undefined || !this.receivedLog) {
+        if (this.isSim && (this.isCsvView === undefined || !this.receivedLog)) {
             // If only csv received, default to csv.
             this.setCsv(!this.receivedLog);
             this.parent.forceUpdate();
-        } else if (this.isCsvView && !this.receivedCsv) {
+        } else if (this.isCsvView && (!this.receivedCsv || !this.isSim)) {
             this.setCsv(false);
             this.parent.forceUpdate();
         }
@@ -223,7 +223,7 @@ export class Editor extends srceditor.Editor {
 
         const niceSource = this.mapSource(source);
 
-        if (this.receivedCsv && this.receivedLog) {
+        if (this.shouldShowToggle()) {
             pxt.BrowserUtils.removeClass(this.serialRoot, "no-toggle")
         }
         if (isCsv) {
@@ -470,6 +470,10 @@ export class Editor extends srceditor.Editor {
             pxt.BrowserUtils.addClass(this.consoleRoot, "nochart");
         }
 
+        if (!this.isSim) {
+            this.setCsv(false);
+        }
+
         this.charts = []
         this.serialInputDataBuffer = ""
         this.rawDataBuffer = ""
@@ -643,9 +647,13 @@ export class Editor extends srceditor.Editor {
         />
     }
 
+    shouldShowToggle = () => {
+        return this.receivedCsv && this.receivedLog && this.isSim;
+    }
+
     display() {
         const rootClasses = classList(
-            !(this.receivedCsv && this.receivedLog) && "no-toggle",
+            !this.shouldShowToggle() && "no-toggle",
             this.isCsvView && "csv-view"
         );
 
