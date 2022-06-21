42fd11ba45512de6c8679aefa22c8e3cae099796
Richard Knoll <riknoll@users.noreply.github.com>
2022-06-21 13:08:35 -0700

    Adding an autocomplete field for datalogger (#8861) (#8863)
    * Adding an autocomplete field
    
    * Add comment and fall back to block type for key
    
    * Mark the text as valid initially

--- pxtblocks/blocklycustomeditor.ts
+++ pxtblocks/blocklycustomeditor.ts
@@ -34,6 +34,7 @@ namespace pxt.blocks {
         registerFieldEditor('protractor', pxtblockly.FieldProtractor);
         registerFieldEditor('position', pxtblockly.FieldPosition);
         registerFieldEditor('melody', pxtblockly.FieldCustomMelody);
+        registerFieldEditor('autocomplete', pxtblockly.FieldAutoComplete);
     }
 
     export function registerFieldEditor(selector: string, field: Blockly.FieldCustomConstructor, validator?: any) {
