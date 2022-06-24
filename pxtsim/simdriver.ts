f5c95d7c2aa6017ece12d5902f23da3d57ebbb8c
Richard Knoll <riknoll@users.noreply.github.com>
2022-06-24 15:26:45 -0700

    Add autofocus param to run page (#8943)

--- pxtsim/simdriver.ts
+++ pxtsim/simdriver.ts
@@ -70,6 +70,7 @@ namespace pxsim {
         dependencies?: Map<string>;
         // single iframe, no message simulators
         single?: boolean;
+        autofocus?: boolean;
     }
 
     export interface HwDebugger {
@@ -425,6 +426,7 @@ namespace pxsim {
             frame.frameBorder = "0";
             frame.dataset['runid'] = this.runId;
             frame.dataset['origin'] = new URL(furl).origin || "*";
+            if (this._runOptions.autofocus) frame.setAttribute("autofocus", "true");
 
             wrapper.appendChild(frame);
 
