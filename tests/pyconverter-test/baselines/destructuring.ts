3c6a5098fb5b923aae35d4587d1464285fa8c5da
Richard Knoll <riknoll@microsoft.com>
2022-08-19 15:53:56 -0700

    Add tests

---
+++ tests/pyconverter-test/baselines/destructuring.ts
@@ -0,0 +1,12 @@
+function initValue() {
+
+    let __tempvar2 = [1, 0, 3]
+    g_value = __tempvar2[0]
+    let _ = __tempvar2[1]
+    _ = __tempvar2[2]
+    let [a, b, c] = ["A", "B", "C"]
+    let [d, e, f] = ["D", "E", "F"]
+}
+
+let g_value = 0
+initValue()
