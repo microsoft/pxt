14b11d7d5b189ea0683940ffdda9ca70372ab8c2
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2022-09-09 14:54:12 -0700

    InfoPanel thumbnail alt text updated to be more descriptive.

--- skillmap/src/components/InfoPanel.tsx
+++ skillmap/src/components/InfoPanel.tsx
@@ -83,12 +83,13 @@ export class InfoPanelImpl extends React.Component<InfoPanelProps> {
         const isMap = !node;
         const isActivity = node && !isRewardNode(node);
         const tags = isActivity && (node as MapActivity).tags || undefined;
+        const thumbnailAltText: string = `Thumbnail for ${!subtitle ? "starting" : title} activity in skillmap ${!subtitle ? title : subtitle}`;
 
         const hasCloudSync = pxt.auth.hasIdentity();
         return <div className="info-panel" ref={this.handleRef}>
             <div className="info-panel-image">
                 {imageUrl
-                ? <img src={imageUrl} alt={lf("Preview of activity content")} />
+                ? <img src={imageUrl} alt={lf(thumbnailAltText)} />
                 : <i className={`fas fa-image`} />}
             </div>
             <div className="info-panel-content">
