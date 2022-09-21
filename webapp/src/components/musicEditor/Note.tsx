4f09a17f0e2b8410f216cd9a33b50fefd1ae5c0c
Richard Knoll <riknoll@microsoft.com>
2022-09-21 14:17:37 -0700

    cute play animations

--- webapp/src/components/musicEditor/Note.tsx
+++ webapp/src/components/musicEditor/Note.tsx
@@ -12,6 +12,15 @@ export const Note = (props: NoteProps) => {
     const { row, iconURI, length, opacity } = props;
 
     return <g className="music-staff-note" transform={`translate(${-(NOTE_ICON_WIDTH / 2)}, ${rowY(row) - (NOTE_ICON_WIDTH / 2)})`}>
+        { row === 0 &&
+            <line
+                className="music-staff-row"
+                x1={-NOTE_ICON_WIDTH / 2}
+                y1={NOTE_ICON_WIDTH / 2}
+                x2={NOTE_ICON_WIDTH * 3 / 2}
+                y2={NOTE_ICON_WIDTH / 2}
+                opacity={opacity} />
+        }
         { !!length &&
             <rect
                 x={NOTE_ICON_WIDTH / 2}
@@ -19,6 +28,13 @@ export const Note = (props: NoteProps) => {
                 width={length}
                 height={NOTE_DURATION_HEIGHT} />
         }
-        <image width={NOTE_ICON_WIDTH} height={NOTE_ICON_WIDTH} href={iconURI} opacity={opacity} />
+        <image x={0} y={0} width={NOTE_ICON_WIDTH} height={NOTE_ICON_WIDTH} href={iconURI} opacity={opacity}>
+            <animate
+                attributeName="y"
+                values="0;-7;-10;-7;0"
+                dur="0.25s"
+                repeatCount="1"
+                begin="indefinite" />
+        </image>
     </g>
 }
--- webapp/src/components/musicEditor/NoteGroup.tsx
+++ webapp/src/components/musicEditor/NoteGroup.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Note } from "./Note";
+import { addPlaybackStopListener, addTickListener, removePlaybackStopListener, removeTickListener } from "./playback";
 import { tickToX } from "./svgConstants";
 import { noteToRow } from "./utils";
 
@@ -13,10 +14,57 @@ export interface NoteGroupProps {
 export const NoteGroup = (props: NoteGroupProps) => {
     const { song, noteEvent, octave, iconURI } = props;
 
+    let noteGroupRef: SVGGElement;
+
+    const playingClass = "music-note-playing";
+
+    React.useEffect(() => {
+        let isPlaying = false;
+
+        const onTick = (tick: number) => {
+            if (tick >= noteEvent.startTick && tick < noteEvent.endTick) {
+                if (!isPlaying) {
+                    isPlaying = true;
+                    noteGroupRef.classList.add(playingClass);
+                    const anims = noteGroupRef.querySelectorAll("animate");
+                    for (let i = 0; i < anims.length; i++) {
+                        anims.item(i).beginElement();
+                    }
+                }
+            }
+            else if (isPlaying) {
+                isPlaying = false;
+                noteGroupRef.classList.remove(playingClass);
+            }
+        }
+
+        const onStop = () => {
+            if (isPlaying) {
+                isPlaying = false;
+                noteGroupRef.classList.remove(playingClass);
+            }
+        }
+
+
+        addTickListener(onTick);
+        addPlaybackStopListener(onStop);
+
+        return () => {
+            removeTickListener(onTick);
+            removePlaybackStopListener(onStop);
+            noteGroupRef.classList.remove(playingClass);
+        }
+    }, [noteEvent])
+
+
+    const handleNoteGroupRef = (ref: SVGGElement) => {
+        if (ref) noteGroupRef = ref;
+    }
+
     const xOffset = tickToX(song, noteEvent.startTick)
     const noteLength = tickToX(song, noteEvent.endTick) - xOffset;
 
-    return <g className="music-staff-note-group" transform={`translate(${xOffset}, 0)`}>
+    return <g className="music-staff-note-group" transform={`translate(${xOffset}, 0)`} ref={handleNoteGroupRef}>
         {noteEvent.notes.map((note, index) =>
             <Note key={index} row={noteToRow(octave, note)} iconURI={iconURI} length={noteLength} />
         )}
