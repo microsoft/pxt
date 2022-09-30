7a183a9eef2edcc5c0b0579a83b2ed98d75af8d4
Joey Wunderlich <joey.wunderlich@gmail.com>
2022-09-30 16:43:53 -0700

    hack together desktop resizable sim pane

--- webapp/src/components/core/TabPane.tsx
+++ webapp/src/components/core/TabPane.tsx
@@ -16,6 +16,8 @@ export function TabPane(props: TabPaneProps) {
     const childArray = (Array.isArray(children) ? children : [children]).filter((el: any) => !!el);
     if (!childArray || childArray.length == 0) return <div />;
 
+    const tabPaneRef: React.MutableRefObject<HTMLDivElement> = React.useRef(undefined);
+
     const tabsToShow = childArray.filter(el => !(el.props as TabContentProps).disabled);
 
     const [ activeTab, setActiveTab ] = React.useState(activeTabName);
@@ -37,7 +39,43 @@ export function TabPane(props: TabPaneProps) {
         selectTab(tab.props);
     }, [activeTabName])
 
-    return <div id={id} className={`tab-container ${className || ""}`} style={style}>
+    let lastMouseX: number;
+    const resize = (e: React.MouseEvent | MouseEvent) => {
+        const dx = lastMouseX - e.pageX;
+        lastMouseX = e.pageX;
+
+        const editorContent: HTMLDivElement = document.querySelector("#editorcontent");
+        const currentWidth = getComputedStyle(editorContent).getPropertyValue("--simulator-panel-width");
+        editorContent.style.setProperty(
+            "--simulator-panel-width",
+            // `max(min(40rem, ${currentWidth} + ${dx}px), 17rem)`
+            `max(min(40rem, ${e.pageX}px), 19rem)`
+
+        );
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    const cleanEvents = () => {
+        document.removeEventListener("mousemove", resize, false);
+        document.removeEventListener("mouseup", cleanEvents, false);
+        document.querySelector("body")?.classList.remove("cursor-resize");
+        // trigger blocks workspace resize?
+    }
+
+    const RESIZABLE_BORDER_SIZE = 4;
+    const onMouseDown = (e: React.MouseEvent) => {
+        const computedStyle = getComputedStyle(tabPaneRef?.current);
+        const paneWidth = parseInt(computedStyle.width) - parseInt(computedStyle.borderWidth);
+        if (e.nativeEvent.offsetX > paneWidth - RESIZABLE_BORDER_SIZE - 2) {
+            document.querySelector("body")?.classList.add("cursor-resize");
+            lastMouseX = e.pageX;
+            document.addEventListener("mousemove", resize, false);
+            document.addEventListener("mouseup", cleanEvents, false);
+        }
+    }
+
+    return <div id={id} ref={tabPaneRef} onMouseDown={onMouseDown} className={`tab-container ${className || ""}`} style={style}>
         {tabsToShow.length > 1 && <div className="tab-navigation">
             {tabsToShow.map(el => {
                 const { name, icon, title, ariaLabel, showBadge } = el.props as TabContentProps;
