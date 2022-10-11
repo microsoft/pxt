ba353db7eeedc202728287faf26440b82d2dd29c
thsparks <thsparks@microsoft.com>
2022-10-11 16:33:06 -0700

    Import css colors into tailwind config
    This allows us to refer to them more easily using taiwind syntax (bg-primary-color) instead of needing to refrence a css var every time (bg-[var(--primary-color)]).

--- multiplayer/tailwind.config.js
+++ multiplayer/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    /* TODO thsparks : How does high contrast work? */
+    colors: {
+      'primary-color': 'var(--primary-color)',
+      'secondary-color': 'var(--secondary-color)',
+      'tertiary-color': 'var(--tertiary-color)',
+      'inactive-color': 'var(--inactive-color)',
+      'body-background-color': 'var(--body-background-color);',
+      'white': 'var(--white)'
+    },
+    fontFamily: {
+      segoueUI: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana']
+    },
     extend: {},
   },
   plugins: [],
