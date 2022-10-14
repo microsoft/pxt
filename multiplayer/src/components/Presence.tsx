d4f42353a3c06c23ef3c26c1f5f93c0b1e834285
thsparks <thsparks@microsoft.com>
2022-10-14 14:27:50 -0700

    Remove borders on presence to align more closely with design images

--- multiplayer/src/components/Presence.tsx
+++ multiplayer/src/components/Presence.tsx
@@ -20,27 +20,27 @@ export default function Render() {
     const getPlayerColors = (slot: number | undefined) => {
         switch(slot) {
             case 1:
-                return "tw-border-red-800 tw-bg-red-200";
+                return "tw-border-red-800 tw-bg-red-300";
             case 2:
-                return "tw-border-blue-800 tw-bg-blue-200";
+                return "tw-border-blue-800 tw-bg-blue-300";
             case 3:
-                return "tw-border-yellow-600 tw-bg-yellow-200";
+                return "tw-border-yellow-600 tw-bg-yellow-300";
             case 4:
-                return "tw-border-green-800 tw-bg-green-200";
+                return "tw-border-green-800 tw-bg-green-300";
             default:
-                return "tw-border-neutral-600 tw-bg-neutral-50";
+                return "tw-border-neutral-600 tw-bg-neutral-300";
         }
     }
 
     return (
-        <div className="tw-flex tw-flex-row tw-items-center tw-justify-center tw-gap-2 tw-mt-2">
+        <div className="tw-flex tw-flex-row tw-items-center tw-justify-center tw-gap-2">
             {[1, 2, 3, 4].map(slot => {
                 const user = players.find(u => u.slot === slot);
                 return (
                     <div
                         key={slot}
                         ref={ref => (slotRef.current[slot] = ref)}
-                        className={`tw-flex tw-border-2 tw-text-black tw-font-bold tw-rounded-full tw-h-11 tw-w-11 tw-justify-center tw-items-center ${getPlayerColors(user?.slot)}`}>
+                        className={`tw-flex tw-border-0 tw-text-black tw-font-bold tw-rounded-full tw-h-11 tw-w-11 tw-justify-center tw-items-center ${getPlayerColors(user?.slot)}`}>
                         {user?.slot}
                         {user && (
                             <ReactionEmitter
