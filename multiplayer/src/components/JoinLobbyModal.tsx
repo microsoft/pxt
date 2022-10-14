2845689ae1cf8d8af22ac8baa99994ed7bb62972
thsparks <thsparks@microsoft.com>
2022-10-14 11:18:06 -0700

    Unbold text in join lobby

--- multiplayer/src/components/JoinLobbyModal.tsx
+++ multiplayer/src/components/JoinLobbyModal.tsx
@@ -7,7 +7,7 @@ export default function Render() {
 
     return (
         <Modal title={lf("Lobby")}>
-            <div className="tw-flex tw-flex-col tw-items-center tw-space-y-1 tw-text-lg tw-font-bold">
+            <div className="tw-flex tw-flex-col tw-items-center tw-space-y-1 tw-text-lg">
                 <div>{lf("Waiting for players.")}</div>
                 <div>{lf("Your host will start the game soon.")}</div>
             </div>
