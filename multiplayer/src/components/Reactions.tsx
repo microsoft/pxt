6514079a0952ff8152d68fc231b3f6b5af2e7757
thsparks <thsparks@microsoft.com>
2022-10-14 15:55:46 -0700

    Move reactions

--- multiplayer/src/components/Reactions.tsx
+++ multiplayer/src/components/Reactions.tsx
@@ -14,25 +14,26 @@ export default function Render() {
     // const smileIcon = {...faFaceSmile, prefix: "fal"};
     return (
         <div>
-            <div className={`tw-flex tw-border-0 tw-text-black tw-rounded-full tw-h-11 tw-w-11 tw-justify-center tw-items-center tw-text-[2rem] tw-bg-neutral-300 tw-cursor-pointer`}
+            {/* Separate component for reaction picker? */}
+            { showReactionPicker &&
+                <div className="tw-flex tw-flex-row tw-gap-1 tw-absolute">
+                    {ReactionDb.map((def, i) => {
+                        return (
+                            <div
+                                className="tw-cursor-pointer tw-select-none tw-border tw-rounded-full tw-border-slate-300 tw-bg-neutral-50 hover:tw-scale-150 tw-ease-linear tw-duration-[50ms] active:tw-bg-neutral-200"
+                                key={i}
+                                onClick={() => onReactionClick(i)}>
+                                {def.emoji}
+                            </div>
+                        );
+                    })}
+                </div>
+            }
+            <div className={`tw-flex tw-border-0 tw-text-black tw-rounded-full tw-h-11 tw-w-11 tw-items-center tw-justify-center tw-text-[2rem] tw-bg-neutral-300 tw-cursor-pointer`}
                 onClick={() => setShowReactionPicker(!showReactionPicker)}>
                 {/* {<FontAwesomeIcon icon={faFaceSmile} className="tw-w-8 tw-h-8"/>} */}
                 🙂
             </div>
-            {/* Separate component for reaction picker? */}
-            { showReactionPicker && <div className="tw-flex tw-flex-row tw-gap-1">
-                {ReactionDb.map((def, i) => {
-                    return (
-                        <div
-                            className="tw-cursor-pointer tw-select-none tw-border tw-rounded-full tw-border-slate-300 tw-bg-neutral-50 hover:tw-scale-150 tw-ease-linear tw-duration-[50ms] active:tw-bg-neutral-200"
-                            key={i}
-                            onClick={() => onReactionClick(i)}
-                        >
-                            {def.emoji}
-                        </div>
-                    );
-                })}
-            </div> }
         </div>
     );
 }
