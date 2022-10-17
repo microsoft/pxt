eb2577f33f0efdb44690760350c3853f15b2ea9e
thsparks <thsparks@microsoft.com>
2022-10-17 15:43:42 -0700

    Minor adjustments to reaction panel

--- multiplayer/src/components/Reactions.tsx
+++ multiplayer/src/components/Reactions.tsx
@@ -16,11 +16,11 @@ export default function Render() {
         <div>
             {/* Separate component for reaction picker? */}
             { showReactionPicker &&
-                <div className="tw-flex tw-flex-row tw-gap-1 tw-p-1 tw-absolute tw-translate-x-[-4rem] tw-translate-y-[-120%] tw-bg-white tw-shadow-md">
+                <div className="tw-flex tw-flex-row tw-gap-1 tw-p-2 tw-absolute tw-translate-x-[-5.8rem] tw-translate-y-[-120%] tw-bg-white tw-shadow-md">
                     {ReactionDb.map((def, i) => {
                         return (
                             <div
-                                className="tw-cursor-pointer tw-select-none tw-border tw-rounded-full tw-border-slate-300 tw-bg-neutral-50 hover:tw-scale-150 tw-ease-linear tw-duration-[50ms] active:tw-bg-neutral-200"
+                                className="tw-flex tw-items-center tw-justify-center tw-cursor-pointer tw-select-none tw-border tw-rounded-full tw-border-slate-300 tw-bg-neutral-50 hover:tw-scale-125 tw-ease-linear tw-duration-[50ms] active:tw-bg-neutral-200 tw-h-8 tw-w-8 tw-text-2xl"
                                 key={i}
                                 onClick={() => onReactionClick(i)}>
                                 {def.emoji}
@@ -29,7 +29,7 @@ export default function Render() {
                     })}
                 </div>
             }
-            <div className={`tw-flex tw-border-0 tw-text-black tw-rounded-full tw-h-11 tw-w-11 tw-items-center tw-justify-center tw-text-[2rem] tw-bg-neutral-300 tw-cursor-pointer`}
+            <div className={`tw-flex tw-border-0 tw-rounded-full tw-h-11 tw-w-11 tw-items-center tw-justify-center tw-text-[2rem] tw-cursor-pointer hover:tw-scale-110 tw-ease-linear tw-duration-[50ms]`}
                 onClick={() => setShowReactionPicker(!showReactionPicker)}>
                 {/* {<FontAwesomeIcon icon={faFaceSmile} className="tw-w-8 tw-h-8"/>} */}
                 🙂
