0ab684eec517bf420fe9793bd081bde58bece45b
thsparks <thsparks@microsoft.com>
2022-10-18 16:25:24 -0700

    Add a join code copy button on the game page

--- multiplayer/src/components/GamePage.tsx
+++ multiplayer/src/components/GamePage.tsx
@@ -1,4 +1,7 @@
-import { useContext } from "react";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext, useState } from "react";
 import { Button } from "react-common/components/controls/Button";
 import { leaveGameAsync } from "../epics";
 import { AppStateContext, dispatch } from "../state/AppStateContext";
@@ -12,12 +15,20 @@ export interface GamePageProps {
 export default function Render(props: GamePageProps) {
     const { state } = useContext(AppStateContext);
     const { appMode } = state;
-    const { netMode, uiMode } = appMode;
+    const { netMode } = appMode;
+    const [copySuccessful, setCopySuccessful] = useState(false);
 
     const onLeaveGameClick = async () => {
         pxt.tickEvent("mp.leavegame");
         await leaveGameAsync();
     };
+
+    const copyJoinCode = async() => {
+        if(state.gameState?.joinCode) {
+            navigator.clipboard.writeText(state.gameState?.joinCode);
+            setCopySuccessful(true)
+        }
+    }
 
     return (
         <div>
@@ -30,7 +41,17 @@ export default function Render(props: GamePageProps) {
                 <div className="tw-flex tw-flex-col tw-items-center">
                     <ArcadeSimulator />
                     <div className="tw-flex tw-flex-row tw-space-x-2 tw-w-full">
-                        <div>{state.gameState?.joinCode && `${lf("Join Code")}: ${state.gameState?.joinCode}`}</div>
+                        {state.gameState?.joinCode &&
+                            <div>
+                                {state.gameState?.joinCode && `${lf("Join Code")}: ${state.gameState?.joinCode}`}
+                                <button onClick={copyJoinCode} title={lf("Copy Join Code")} onBlur={() => setCopySuccessful(false)}>
+                                    <div className="tw-text-sm tw-ml-1 tw-mb-[0.1rem] ">
+                                    {!copySuccessful && <FontAwesomeIcon icon={faCopy} className="hover:tw-scale-105"/>}
+                                    {copySuccessful && <FontAwesomeIcon icon={faCheck} className="tw-text-green-600"/>}
+                                    </div>
+                                </button>
+                            </div>
+                        }
                         <div className="tw-flex-grow"/>
                         <div>{lf("Keyboard Controls")}</div>
                     </div>
