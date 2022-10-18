47627bcf7985ce913f368a356624da7978e53861
thsparks <thsparks@microsoft.com>
2022-10-17 17:12:52 -0700

    Start the game if the host lobby modal is closed. Currently, there's no way to re-open it and start the game, so if it doesn't start then the player just gets stuck in an eternal lobby.
    Long term we probably either: 1) don't want a modal for this, 2) want to hide the "X" button that closes the modal,  or 3) provide a way to open it back up and some clear indication outside of the modal that the game is still in a lobby state.

--- multiplayer/src/components/HostLobbyModal.tsx
+++ multiplayer/src/components/HostLobbyModal.tsx
@@ -12,7 +12,7 @@ export default function Render() {
     let inputRef: HTMLInputElement;
 
     const onStartGameClick = async () => {
-        pxt.tickEvent("mp.startgame");
+        pxt.tickEvent("mp.hostlobby.startgame");
         dispatch(clearModal());
         await startGameAsync();
     };
@@ -35,9 +35,15 @@ export default function Render() {
         if (ref) inputRef = ref;
     }
 
+    const handleOnClose = async () => {
+        // TODO multiplayer : This is probably not the right behavior long-term if this modal is closed.
+        pxt.tickEvent("mp.hostlobby.closed");
+        await onStartGameClick();
+    }
+
     const joinLink = `${state.gameState?.joinCode}`; // TODO multiplayer : create full link
     return (
-        <Modal title={lf("Invite Players")} onClose={() => dispatch(clearModal())}>
+        <Modal title={lf("Invite Players")} onClose={handleOnClose}>
             <div className="tw-flex tw-flex-col tw-gap-1 tw-items-center">
                 <div className="tw-flex tw-flex-col tw-items-center">
                     <div>{lf("Invite anyone to join your game instantly.")}</div>
