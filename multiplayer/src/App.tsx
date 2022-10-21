0120cef7355cbdebc169c7f518fbebca14961414
thsparks <thsparks@microsoft.com>
2022-10-21 16:03:51 -0700

    Add footer with privacy and terms of use.
    The code also supports a copyright section, but we don't have one in the target theme so it doesn't appear here.
    
    This is lifted pretty directly from https://github.com/microsoft/pxt/blob/master/webapp/src/projects.tsx#L209-L216 with some tailwind-ification. (I also removed the language section. We can add it back later if needed, but I suspect that may go in the gear menu instead.)

--- multiplayer/src/App.tsx
+++ multiplayer/src/App.tsx
@@ -15,6 +15,7 @@ function App() {
     const { state } = useContext(AppStateContext);
     const { signedIn, appMode } = state;
     const { uiMode } = appMode;
+    const targetTheme = pxt?.appTarget?.appTheme;
 
     const loading = useMemo(() => uiMode === "init", [uiMode]);
 
@@ -27,13 +28,22 @@ function App() {
     }, []);
 
     return (
-        <div className={`${pxt.appTarget.id}`}>
+        <div className={`${pxt.appTarget.id} tw-flex tw-flex-col`}>
             {loading && <Loading />}
             {!loading && <HeaderBar />}
             {!loading && !signedIn && <SignInPage />}
             {!loading && signedIn && <SignedInPage />}
             <AppModal />
             <Toast />
+            <div className="tw-flex-grow"/>
+
+            {/* Footer */}
+            {targetTheme?.organizationUrl || targetTheme?.organizationUrl || targetTheme?.privacyUrl || targetTheme?.copyrightText ? <div className="ui horizontal small divided link list tw-text-center" role="contentinfo">
+                {targetTheme.organizationUrl && targetTheme.organization ? <a className="item !tw-text-black" target="_blank" rel="noopener noreferrer" href={targetTheme.organizationUrl}>{targetTheme.organization}</a> : undefined}
+                {targetTheme.termsOfUseUrl ? <a target="_blank" className="item !tw-text-black" href={targetTheme.termsOfUseUrl} rel="noopener noreferrer">{lf("Terms of Use")}</a> : undefined}
+                {targetTheme.privacyUrl ? <a target="_blank" className="item !tw-text-black" href={targetTheme.privacyUrl} rel="noopener noreferrer">{lf("Privacy")}</a> : undefined}
+                {targetTheme.copyrightText ? <div className="ui item copyright !tw-text-black">{targetTheme.copyrightText}</div> : undefined}
+            </div> : undefined}
         </div>
     );
 }
