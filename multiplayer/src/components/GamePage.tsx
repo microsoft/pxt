dcb5512c67a8480d90d168f7d0e608968f6bf443
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2022-10-24 14:51:50 -0600

    Add Multiplayer Join Code Copy Button on Game Page (#9125)
    Add a copy button next to the Join Code in the multiplayer Game Page

--- multiplayer/src/components/GamePage.tsx
+++ multiplayer/src/components/GamePage.tsx
@@ -1,4 +1,7 @@
-import { useContext } from "react";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext, useEffect, useState } from "react";
 import { Button } from "react-common/components/controls/Button";
 import { leaveGameAsync } from "../epics";
 import { AppStateContext, dispatch } from "../state/AppStateContext";
@@ -11,12 +14,33 @@ export interface GamePageProps {}
 export default function Render(props: GamePageProps) {
     const { state } = useContext(AppStateContext);
     const { appMode } = state;
-    const { netMode, uiMode } = appMode;
+    const { netMode } = appMode;
+    const [copySuccessful, setCopySuccessful] = useState(false);
+    const copyTimeoutMs = 1000;
 
     const onLeaveGameClick = async () => {
         pxt.tickEvent("mp.leavegame");
         await leaveGameAsync();
     };
+
+    const copyJoinCode = async () => {
+        pxt.tickEvent("mp.copyjoincode");
+        if (state.gameState?.joinCode) {
+            navigator.clipboard.writeText(state.gameState?.joinCode);
+            setCopySuccessful(true);
+        }
+    };
+
+    useEffect(() => {
+        if (copySuccessful) {
+            let resetCopyTimer = setTimeout(() => {
+                setCopySuccessful(false);
+            }, copyTimeoutMs);
+            return () => {
+                clearTimeout(resetCopyTimer);
+            };
+        }
+    }, [copySuccessful]);
 
     return (
         <div>
@@ -29,12 +53,33 @@ export default function Render(props: GamePageProps) {
                 <div className="tw-flex tw-flex-col tw-items-center">
                     {state.playerSlot && <ArcadeSimulator />}
                     <div className="tw-flex tw-flex-row tw-space-x-2 tw-w-full">
-                        <div>
-                            {state.gameState?.joinCode &&
-                                `${lf("Join Code")}: ${
-                                    state.gameState?.joinCode
-                                }`}
-                        </div>
+                        {state.gameState?.joinCode && (
+                            <div>
+                                {state.gameState?.joinCode &&
+                                    `${lf("Join Code")}: ${
+                                        state.gameState?.joinCode
+                                    }`}
+                                <button
+                                    onClick={copyJoinCode}
+                                    title={lf("Copy Join Code")}
+                                >
+                                    <div className="tw-text-sm tw-ml-1">
+                                        {!copySuccessful && (
+                                            <FontAwesomeIcon
+                                                icon={faCopy}
+                                                className="hover:tw-scale-105 tw-mb-[0.1rem]"
+                                            />
+                                        )}
+                                        {copySuccessful && (
+                                            <FontAwesomeIcon
+                                                icon={faCheck}
+                                                className="tw-text-green-600 tw-mb-[0.1rem]"
+                                            />
+                                        )}
+                                    </div>
+                                </button>
+                            </div>
+                        )}
                         <div className="tw-flex-grow" />
                         <div>{lf("Keyboard Controls")}</div>
                     </div>
