09b0b333abb82411a294ea34c17c785aa57ef8a4
thsparks <thsparks@microsoft.com>
2022-10-26 13:27:00 -0700

    Reformat text in join lobby

--- multiplayer/src/components/JoinLobby.tsx
+++ multiplayer/src/components/JoinLobby.tsx
@@ -3,11 +3,9 @@ import PresenceBar from "./PresenceBar";
 export default function Render() {
     return (
         <div>
-            <div className="tw-flex tw-flex-col tw-items-center tw-text-lg tw-bg-white tw-py-[3rem] tw-px-[7rem] tw-shadow-lg tw-rounded-lg">
-                <div className="tw-flex tw-flex-col tw-items-center tw-space-y-3">
-                    <div>{lf("Waiting for players.")}</div>
-                    <div>{lf("Your host will start the game soon.")}</div>
-                </div>
+            <div className="tw-flex tw-flex-col tw-items-center tw-bg-white tw-py-[2rem] tw-px-[7rem] tw-shadow-lg tw-rounded-lg">
+                <div className="tw-font-segoueUI tw-font-bold tw-text-2xl tw-text-neutral-800">{lf("Waiting for players")}</div>
+                <div className="tw-font-segoueUI tw-mt-3 tw-text-center tw-text-neutral-700">{lf('Your host will start the game soon!')}</div>
                 <div className="tw-mt-5">
                     <PresenceBar />
                 </div>
