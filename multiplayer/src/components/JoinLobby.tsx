5b0d0224ceb5217541f8884b3185143b7330ca9e
thsparks <thsparks@microsoft.com>
2022-10-26 14:02:16 -0700

    Prettier

--- multiplayer/src/components/JoinLobby.tsx
+++ multiplayer/src/components/JoinLobby.tsx
@@ -4,8 +4,12 @@ export default function Render() {
     return (
         <div>
             <div className="tw-flex tw-flex-col tw-items-center tw-bg-white tw-py-[2rem] tw-px-[7rem] tw-shadow-lg tw-rounded-lg">
-                <div className="tw-font-segoueUI tw-font-bold tw-text-2xl tw-text-neutral-800">{lf("Waiting for players")}</div>
-                <div className="tw-font-segoueUI tw-mt-3 tw-text-center tw-text-neutral-700">{lf('Your host will start the game soon!')}</div>
+                <div className="tw-font-segoueUI tw-font-bold tw-text-2xl tw-text-neutral-800">
+                    {lf("Waiting for players")}
+                </div>
+                <div className="tw-font-segoueUI tw-mt-3 tw-text-center tw-text-neutral-700">
+                    {lf("Your host will start the game soon!")}
+                </div>
                 <div className="tw-mt-5">
                     <PresenceBar />
                 </div>
