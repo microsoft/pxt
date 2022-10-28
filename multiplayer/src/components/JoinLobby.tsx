68c0e7dcdf9f399f544f7da592afa8a23c5cba28
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2022-10-28 17:34:11 -0600

    Add Beta Indicator to Multiplayer Pages (#9157)
    This adds a little "beta" tag to the home page, lobbies, and game page. Margins on the sim were a bit weird, so I just put it in the corner of the page in that scenario for now. If needed, we can revisit this later, but it may require css updates on the sim-side of things.

--- multiplayer/src/components/JoinLobby.tsx
+++ multiplayer/src/components/JoinLobby.tsx
@@ -1,9 +1,13 @@
+import BetaTag from "./BetaTag";
 import PresenceBar from "./PresenceBar";
 
 export default function Render() {
     return (
-        <div>
-            <div className="tw-flex tw-flex-col tw-items-center tw-bg-white tw-py-[2rem] tw-px-[7rem] tw-shadow-lg tw-rounded-lg">
+        <div className="tw-bg-white tw-shadow-lg tw-rounded-lg">
+            <div className="tw-absolute tw-translate-y-[-130%]">
+                <BetaTag />
+            </div>
+            <div className="tw-flex tw-flex-col tw-items-center tw-py-[2rem] tw-px-[6rem]">
                 <div className="tw-font-bold tw-text-2xl tw-text-neutral-800">
                     {lf("Waiting for players")}
                 </div>
--- multiplayer/src/components/JoinOrHost.tsx
+++ multiplayer/src/components/JoinOrHost.tsx
@@ -5,6 +5,7 @@ import { Input } from "react-common/components/controls/Input";
 import { Link } from "react-common/components/controls/Link";
 import { hostGameAsync, joinGameAsync } from "../epics";
 import TabButton from "./TabButton";
+import BetaTag from "./BetaTag";
 
 export default function Render() {
     const [currTab, setCurrTab] = useState<"join" | "host">("join");
@@ -31,85 +32,90 @@ export default function Render() {
 
     return (
         <div className="tw-flex tw-w-screen tw-h-screen tw-justify-center tw-items-center">
-            <div className="tw-bg-white tw-rounded-lg tw-drop-shadow-xl tw-py-5 tw-px-10 tw-min-h-[17rem] tw-min-w-[25rem]">
-                <div className="tw-pt-2">
-                    <div className="tw-flex tw-justify-center">
-                        <TabButton
-                            title={lf("Join Game")}
-                            selected={currTab === "join"}
-                            onClick={() => setCurrTab("join")}
-                        />
-                        <TabButton
-                            title={lf("Host Game")}
-                            selected={currTab === "host"}
-                            onClick={() => setCurrTab("host")}
-                        />
-                    </div>
+            <div className="tw-bg-white tw-rounded-lg tw-drop-shadow-xl tw-min-h-[17rem] tw-min-w-[25rem]">
+                <div className="tw-absolute tw-translate-y-[-130%]">
+                    <BetaTag />
+                </div>
+                <div className="tw-py-5 tw-px-10 ">
                     <div className="tw-pt-2">
-                        {currTab === "join" && (
-                            <div className="tw-w-full tw-flex tw-flex-col tw-gap-4 tw-mt-2 tw-justify-center tw-items-center">
-                                <div className="tw-w-full">
-                                    <Input
-                                        groupClassName="tw-w-full tw-border-1 tw-h-full tw-rounded"
-                                        inputClassName="tw-text-center tw-p-2"
-                                        title={lf("Join Code")}
-                                        autoComplete={false}
-                                        handleInputRef={setJoinCodeRef}
-                                        preserveValueOnBlur={true}
-                                        onEnterKey={onJoinGameClick}
-                                        placeholder={lf("Enter game code")}
-                                    />
+                        <div className="tw-flex tw-justify-center">
+                            <TabButton
+                                title={lf("Join Game")}
+                                selected={currTab === "join"}
+                                onClick={() => setCurrTab("join")}
+                            />
+                            <TabButton
+                                title={lf("Host Game")}
+                                selected={currTab === "host"}
+                                onClick={() => setCurrTab("host")}
+                            />
+                        </div>
+                        <div className="tw-pt-2">
+                            {currTab === "join" && (
+                                <div className="tw-w-full tw-flex tw-flex-col tw-gap-4 tw-mt-2 tw-justify-center tw-items-center">
+                                    <div className="tw-w-full">
+                                        <Input
+                                            groupClassName="tw-w-full tw-border-1 tw-h-full tw-rounded"
+                                            inputClassName="tw-text-center tw-p-2"
+                                            title={lf("Join Code")}
+                                            autoComplete={false}
+                                            handleInputRef={setJoinCodeRef}
+                                            preserveValueOnBlur={true}
+                                            onEnterKey={onJoinGameClick}
+                                            placeholder={lf("Enter game code")}
+                                        />
+                                    </div>
+                                    <div>
+                                        <Button
+                                            className={"primary tw-px-10"}
+                                            label={lf("Enter")}
+                                            title={lf("Join Game")}
+                                            onClick={onJoinGameClick}
+                                        />
+                                    </div>
+                                    <div className="tw-text-sm">
+                                        <Link
+                                            href="/docs/multiplayer#join-game"
+                                            target="_blank"
+                                        >
+                                            {lf("How do I get a game code?")}
+                                        </Link>
+                                    </div>
                                 </div>
-                                <div>
-                                    <Button
-                                        className={"primary tw-px-10"}
-                                        label={lf("Enter")}
-                                        title={lf("Join Game")}
-                                        onClick={onJoinGameClick}
-                                    />
+                            )}
+                            {currTab === "host" && (
+                                <div className="tw-w-full tw-flex tw-flex-col tw-gap-4 tw-mt-2 tw-justify-center tw-items-center">
+                                    <div className="tw-w-full">
+                                        <Input
+                                            groupClassName="tw-w-full tw-border-1 tw-h-full tw-rounded"
+                                            inputClassName="tw-p-2"
+                                            title={lf("Share Code")}
+                                            autoComplete={false}
+                                            handleInputRef={setShareCodeRef}
+                                            preserveValueOnBlur={true}
+                                            onEnterKey={onHostGameClick}
+                                            placeholder={lf("Enter share code or link")}
+                                        />
+                                    </div>
+                                    <div>
+                                        <Button
+                                            className={"primary tw-px-10"}
+                                            label={lf("Launch")}
+                                            title={lf("Host Game")}
+                                            onClick={onHostGameClick}
+                                        />
+                                    </div>
+                                    <div className="tw-text-sm">
+                                        <Link
+                                            href="/docs/multiplayer#host-game"
+                                            target="_blank"
+                                        >
+                                            {lf("How do I get a share code or link?")}
+                                        </Link>
+                                    </div>
                                 </div>
-                                <div className="tw-text-sm">
-                                    <Link
-                                        href="/docs/multiplayer#join-game"
-                                        target="_blank"
-                                    >
-                                        {lf("How do I get a game code?")}
-                                    </Link>
-                                </div>
-                            </div>
-                        )}
-                        {currTab === "host" && (
-                            <div className="tw-w-full tw-flex tw-flex-col tw-gap-4 tw-mt-2 tw-justify-center tw-items-center">
-                                <div className="tw-w-full">
-                                    <Input
-                                        groupClassName="tw-w-full tw-border-1 tw-h-full tw-rounded"
-                                        inputClassName="tw-p-2"
-                                        title={lf("Share Code")}
-                                        autoComplete={false}
-                                        handleInputRef={setShareCodeRef}
-                                        preserveValueOnBlur={true}
-                                        onEnterKey={onHostGameClick}
-                                        placeholder={lf("Enter share code or link")}
-                                    />
-                                </div>
-                                <div>
-                                    <Button
-                                        className={"primary tw-px-10"}
-                                        label={lf("Launch")}
-                                        title={lf("Host Game")}
-                                        onClick={onHostGameClick}
-                                    />
-                                </div>
-                                <div className="tw-text-sm">
-                                    <Link
-                                        href="/docs/multiplayer#host-game"
-                                        target="_blank"
-                                    >
-                                        {lf("How do I get a share code or link?")}
-                                    </Link>
-                                </div>
-                            </div>
-                        )}
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
