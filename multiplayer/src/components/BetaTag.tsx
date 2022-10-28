b2e354353d99cd48cf38048b3349da34be0d9d5e
thsparks <thsparks@microsoft.com>
2022-10-28 16:20:33 -0700

    Uppercase "BETA" using css

--- multiplayer/src/components/BetaTag.tsx
+++ multiplayer/src/components/BetaTag.tsx
@@ -1,8 +1,8 @@
 export default function Render() {
     return (
         <div className="tw-text-sm">
-            <span className="tw-bg-white tw-shadow-sm tw-text-primary-color tw-rounded-md tw-font-bold tw-px-1 tw-pt-[0.05rem] tw-pb-[0.15rem] tw-mr-1">
-                {lf("BETA")}
+            <span className="tw-bg-white tw-shadow-sm tw-uppercase tw-text-primary-color tw-rounded-md tw-font-bold tw-px-1 tw-pt-[0.05rem] tw-pb-[0.15rem] tw-mr-1">
+                {lf("Beta")}
             </span>
             <span className="tw-text-white">{lf("Multiplayer")}</span>
         </div>
