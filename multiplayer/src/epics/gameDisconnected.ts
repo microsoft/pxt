e5024ff3dcd15be183032f67c2851f51e5945a70
Joey Wunderlich <joey.wunderlich@gmail.com>
2022-11-04 16:08:36 -0700

    clear game info when disconnected to fix playing state for sim

--- multiplayer/src/epics/gameDisconnected.ts
+++ multiplayer/src/epics/gameDisconnected.ts
@@ -1,11 +1,13 @@
 import { dispatch } from "../state";
-import { showToast, setClientRole, setNetMode } from "../state/actions";
+import { showToast, setClientRole, setNetMode, clearGameInfo, clearGameMetadata } from "../state/actions";
 import { GameOverReason } from "../types";
 
 export function gameDisconnected(reason: GameOverReason | undefined) {
     try {
         dispatch(setClientRole(undefined));
         dispatch(setNetMode("init"));
+        dispatch(clearGameInfo());
+        dispatch(clearGameMetadata());
         switch (reason) {
             case "kicked":
                 pxt.tickEvent("mp.gotkicked");
