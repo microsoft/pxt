91159d5a6e2bd040c4081892ffa15a801f08f4a2
thsparks <thsparks@microsoft.com>
2022-11-04 13:27:19 -0700

    Make Join Lobby more responsive to mobile sizes

--- multiplayer/src/components/JoinLobby.tsx
+++ multiplayer/src/components/JoinLobby.tsx
@@ -3,11 +3,11 @@ import PresenceBar from "./PresenceBar";
 
 export default function Render() {
     return (
-        <div className="tw-bg-white tw-shadow-lg tw-rounded-lg">
+        <div className="tw-bg-white tw-shadow-lg tw-rounded-lg tw-m-1">
             <div className="tw-absolute tw-translate-y-[-130%]">
                 <BetaTag />
             </div>
-            <div className="tw-flex tw-flex-col tw-items-center tw-py-[2rem] tw-px-[6rem]">
+            <div className="tw-flex tw-flex-col tw-items-center tw-py-[2rem] tw-px-[1.5rem] sm:tw-px-[3.5rem] md:tw-px-[6rem]">
                 <div className="tw-font-bold tw-text-2xl tw-text-neutral-800">
                     {lf("Waiting for players")}
                 </div>
