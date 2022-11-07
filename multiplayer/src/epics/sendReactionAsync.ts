2ec256969a4e989ba7f7d71f7f54941abdff3a83
Joey Wunderlich <joey.wunderlich@gmail.com>
2022-11-07 09:42:53 -0800

    prettier an bump up timeout to 250

--- multiplayer/src/epics/sendReactionAsync.ts
+++ multiplayer/src/epics/sendReactionAsync.ts
@@ -1,11 +1,14 @@
 import * as gameClient from "../services/gameClient";
 import { Reactions } from "../types/reactions";
 
-const TIMEOUT_PER_REACTION = 200;
+const TIMEOUT_PER_REACTION = 250;
 const lastSentTime: number[] = [];
 export async function sendReactionAsync(index: number) {
     try {
-        if (!lastSentTime[index] || lastSentTime[index] + TIMEOUT_PER_REACTION < Date.now()) {
+        if (
+            !lastSentTime[index] ||
+            lastSentTime[index] + TIMEOUT_PER_REACTION < Date.now()
+        ) {
             lastSentTime[index] = Date.now();
             const reaction = Reactions[index];
             pxt.tickEvent("mp.sendreaction", { reaction: reaction.name });
