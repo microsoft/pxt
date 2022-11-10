30584c252778e39b3507b22fd57fc32481981b21
Joey Wunderlich <joey.wunderlich@gmail.com>
2022-11-10 14:00:49 -0800

    prettier

--- multiplayer/src/types/index.ts
+++ multiplayer/src/types/index.ts
@@ -13,7 +13,14 @@ export type ModalType =
 
 export type ClientRole = "host" | "guest" | "none";
 export type GameMode = "lobby" | "playing";
-export type GameOverReason = "kicked" | "ended" | "left" | "full" | "rejected" | "not-found" | "compile-failed";
+export type GameOverReason =
+    | "kicked"
+    | "ended"
+    | "left"
+    | "full"
+    | "rejected"
+    | "not-found"
+    | "compile-failed";
 
 export type GameInfo = {
     joinCode?: string;
