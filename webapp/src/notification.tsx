89230fe2ec37f7f51daabfdef7a84533a0b22963
thsparks <thsparks@microsoft.com>
2022-11-15 16:36:46 -0800

    Remove trailing whitespace

--- webapp/src/notification.tsx
+++ webapp/src/notification.tsx
@@ -167,18 +167,18 @@ export class NotificationBanner extends data.Component<ISettingsProps, {}> {
     // Based on https://github.com/microsoft/pxt/blob/master/react-common/components/util.tsx#L15.
     jsxLF(loc: string, ...rest: JSX.Element[]) {
         const indices: number[] = [];
-    
+
         loc.replace(/\{\d\}/g, match => {
             indices.push(parseInt(match.substr(1, 1)));
             return match;
         });
-    
+
         const out: JSX.Element[] = [];
-    
+
         let parts: string[];
-    
+
         let i = 0;
-    
+
         for (const index of indices) {
             parts = loc.split(`{${index}}`);
             pxt.U.assert(parts.length === 2);
@@ -187,7 +187,7 @@ export class NotificationBanner extends data.Component<ISettingsProps, {}> {
             loc = parts[1];
         }
         out.push(<span key={i++}>{loc}</span>);
-    
+
         return out;
     }
 }
