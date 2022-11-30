15c69236bf7034cb993e38f6ecb1a6282ed616c2
Kim Ying <15070078+kimprice@users.noreply.github.com>
2022-11-29 17:59:20 -0800

    initial move palette picker to asset editor

---
+++ webapp/src/components/assetEditor/assetPalette.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { Button } from "../../../../react-common/components/controls/Button";
+import { PalettePicker } from "../../../../react-common/components/palette/PalettePicker";
+import { AllPalettes, Palette } from "../../../../react-common/components/palette/Palettes";
+
+export const AssetPalette = () => {
+    const [openPalette, setOpenPalette] = React.useState<boolean>(false);
+
+    const [currentColors, setCurrentColors] = React.useState<string[] | undefined>(pxt.appTarget.runtime.palette);
+
+    const openPaletteModal = () => {
+        setOpenPalette(!openPalette);
+    }
+
+    const onPaletteSelected = (selected: Palette) => {
+        //this.config.palette = selected.colors;
+        setCurrentColors(selected.colors);
+        // save pxt.json
+    }
+
+    const renderPalettePicker = () => {
+        const currentPalette = getCurrentPalette();
+        let paletteOptions = AllPalettes.slice();
+
+        if (!paletteOptions.some(p => p.id === currentPalette.id)) {
+            paletteOptions.unshift(currentPalette)
+        }
+
+        return <>
+            <PalettePicker
+                palettes={paletteOptions}
+                selectedId={currentPalette.id}
+                onPaletteSelected={onPaletteSelected}
+            />
+        </>
+    }
+
+    const getCurrentPalette = () => {
+        // const configPalette = this.config.palette || pxt.appTarget.runtime.palette;
+        // const configPalette = pxt.appTarget.runtime.palette;
+        const configPalette = currentColors || pxt.appTarget.runtime.palette;
+        
+        if (configPalette) {
+            for (const palette of AllPalettes) {
+                let isEqual = true;
+                for (let i = 0; i < palette.colors.length; i++) {
+                    if (configPalette[i].toLowerCase() !== palette.colors[i].toLowerCase()) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if (isEqual) return palette;
+            }
+        }
+
+        return {
+            id: "custom",
+            name: lf("Custom Palette"),
+            colors: configPalette
+        };
+    }
+
+
+    return <div className="asset-palette">
+        <Button className="teal" title={lf("Color Palette")} label={lf("Color Palette")} leftIcon="fas fa-palette" onClick={openPaletteModal} />
+        {openPalette && renderPalettePicker()}
+    </div>
+}
--- webapp/src/components/assetEditor/assetTopbar.tsx
+++ webapp/src/components/assetEditor/assetTopbar.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 
 import { GalleryView } from './store/assetEditorReducerState';
 import { AssetGalleryTab } from './assetGalleryTab'
+import { AssetPalette } from "./assetPalette";
 
 export class AssetTopbar extends React.Component<{}> {
     render() {
         return <div className="asset-editor-topbar">
             <AssetGalleryTab title={lf("My Assets")} view={GalleryView.User} />
             <AssetGalleryTab title={lf("Gallery")} view={GalleryView.Gallery} />
+            <AssetPalette />
         </div>
     }
 }
