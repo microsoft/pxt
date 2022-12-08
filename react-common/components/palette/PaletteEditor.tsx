61df8c940e34278214b460a297053296416c4ed0
Kim Ying <15070078+kimprice@users.noreply.github.com>
2022-12-07 17:03:52 -0800

    update editor when palette selected from dropdown

--- react-common/components/palette/PaletteEditor.tsx
+++ react-common/components/palette/PaletteEditor.tsx
@@ -11,14 +11,18 @@ export interface PaletteEditorProps {
 export const PaletteEditor = (props: PaletteEditorProps) => {
     const { palette, onPaletteChanged } = props;
 
-    const [currentPalette, setCurrentPalette] = React.useState<Palette | undefined>(undefined);
+    const [currentPalette, setCurrentPalette] = React.useState<Palette | undefined>(palette);
 
     useEffect(() => {
-        onPaletteChanged(currentPalette || palette);
+        onPaletteChanged(currentPalette);
     }, [currentPalette]);
 
+    useEffect(() => {
+        setCurrentPalette(palette);
+    }, [palette]);
+
     const updateColor = (index: number, newColor: string) => {
-        const toUpdate = currentPalette || palette;
+        const toUpdate = currentPalette;
         setCurrentPalette({
             ...toUpdate,
             colors: toUpdate.colors.map((c, i) =>
@@ -28,7 +32,7 @@ export const PaletteEditor = (props: PaletteEditorProps) => {
     }
 
     const moveColor = (index: number, up: boolean) => {
-        const toUpdate = currentPalette || palette;
+        const toUpdate = currentPalette;
         const res = {
             ...toUpdate,
             colors: toUpdate.colors.slice()
@@ -50,7 +54,7 @@ export const PaletteEditor = (props: PaletteEditorProps) => {
     }
 
     return <div className="common-palette-editor">
-        {(currentPalette || palette).colors.slice(1).map((c, i) =>
+        {(currentPalette).colors.slice(1).map((c, i) =>
             <ColorPickerField
                 key={i}
                 index={i}
