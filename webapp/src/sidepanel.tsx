0b62a1fe5cedca4c8da9f4df165de9b39b2e0e63
Eric Anderson <eanders@microsoft.com>
2023-01-10 16:33:29 -0800

    updates

--- webapp/src/sidepanel.tsx
+++ webapp/src/sidepanel.tsx
@@ -162,7 +162,7 @@ export class Sidepanel extends data.Component<SidepanelProps, SidepanelState> {
                         <div className="ui item portrait hide hidefullscreen">
                             {pxt.options.debug && <Button key="hwdebugbtn" className="teal" icon="xicon chip" text={"Dev Debug"} onClick={handleHardwareDebugClick} />}
                         </div>
-                        {showHostMultiplayerGameButton && <Button className={"teal portrait hide hidefullscreen"} icon={"xicon multiplayer"} text={lf("Host multiplayer game")} ariaLabel={lf("Host multiplayer game")} onClick={this.onHostMultiplayerGameClick} />}
+                        {showHostMultiplayerGameButton && <Button className={"teal hostmultiplayergame-button portrait hide hidefullscreen"} icon={"xicon multiplayer"} text={lf("Host multiplayer game")} ariaLabel={lf("Host multiplayer game")} onClick={this.onHostMultiplayerGameClick} />}
                         {showSerialButtons && <div id="serialPreview" className="ui editorFloat portrait hide hidefullscreen">
                             <serialindicator.SerialIndicator ref="simIndicator" isSim={true} onClick={this.handleSimSerialClick} parent={parent} />
                             <serialindicator.SerialIndicator ref="devIndicator" isSim={false} onClick={this.handleDeviceSerialClick} parent={parent} />
