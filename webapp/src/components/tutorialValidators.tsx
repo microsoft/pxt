d588f1a398cdf48143f860e1d34dd75ecb72a694
thsparks <thsparks@microsoft.com>
2023-01-18 09:26:39 -0800

    Remove commented-out code for now

--- webapp/src/components/tutorialValidators.tsx
+++ webapp/src/components/tutorialValidators.tsx
@@ -96,17 +96,6 @@ export class BlocksExistValidator extends CodeValidatorBase {
         }
 
         const isValid = missingBlocks.length == 0;
-
-        // TODO thsparks : Revisit showing individual blocks if time permits.
-        /*
-        const blockImageUris = isValid ? [] : await getBlockImageUris(missingBlocks);
-        const blockImages = (
-            <div>
-                <p>{lf("Double check your workspace to make sure these blocks are inside and connected to the rest of your code:")}</p>
-                {blockImageUris.filter(b => !!b).map((blockUri, index) => <img key={index + blockUri} src={blockUri} alt={lf("missing block")} />)}
-            </div>);
-        */
-
         const blockImages = stepInfo?.hintContentMd ? (<div>
             <strong>{lf("Looks like you're missing some blocks.")}</strong>
             <p>{lf("Make sure you see blocks that look like this and that they're connected to the rest of your code.")}</p>
@@ -149,36 +138,3 @@ function getTutorialStepHash(tutorial: TutorialOptions): string {
 
     return pxt.BrowserUtils.getTutorialCodeHash([hintCode]);
 }
-
-// TODO thsparks : Revisit if time permits.
-// TODO thsparks : Reduce duplication from tutorialCodeValidation.
-/*
-async function getBlockImageUris(blockIds: string[]): Promise<string[]> {
-    // Get all APIs from compiler
-    return compiler.getBlocksAsync().then(blocksInfo => {
-        return Promise.all(blockIds.map(id => {
-            // TODO thsparks - cache? - if (this.renderedBlockCache[id]) return Promise.resolve(this.renderedBlockCache[id]);
-
-            const symbol = blocksInfo.blocksById[id];
-            if (!symbol) return Promise.resolve(undefined);
-
-            // Render toolbox block from symbol info
-            const fn = pxt.blocks.blockSymbol(id);
-            if (fn) {
-                const comp = pxt.blocks.compileInfo(fn);
-                const blockXml = pxt.blocks.createToolboxBlock(blocksInfo, fn, comp);
-                const svg = pxt.blocks.render(`<xml xmlns="https://developers.google.com/blockly/xml">${blockXml.outerHTML}</xml>`,
-                    { snippetMode: true, splitSvg: false }) as SVGSVGElement;
-                const viewBox = svg.getAttribute("viewBox").split(/\s+/).map(d => parseInt(d));
-                return pxt.blocks.layout.blocklyToSvgAsync(svg, viewBox[0], viewBox[1], viewBox[2], viewBox[3])
-                    .then(sg => {
-                        if (!sg) return Promise.resolve<string>(undefined);
-                        return pxt.BrowserUtils.encodeToPngAsync(sg.xml, { width: sg.width, height: sg.height, pixelDensity: 1 });
-                    })
-            }
-
-            return Promise.resolve(undefined)
-        }));
-    })
-}
-*/
