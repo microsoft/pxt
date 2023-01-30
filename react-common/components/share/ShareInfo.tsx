0912f9d5424e755a240549590756e0afde1a7de2
Sarah Rietkerk <49178322+srietkerk@users.noreply.github.com>
2023-01-30 09:17:31 -0800

    Kiosk share: input strings converted to upper case (#9341)

--- react-common/components/share/ShareInfo.tsx
+++ react-common/components/share/ShareInfo.tsx
@@ -112,8 +112,9 @@ export const ShareInfo = (props: ShareInfoProps) => {
     const handleKioskSubmitClick = async () => {
         const gameId = pxt.Cloud.parseScriptId(shareData.url);
         if (kioskInputRef?.value) {
-            const validKioskId = /^[a-zA-Z0-9]{6}$/.exec(kioskInputRef.value)?.[0];
+            let validKioskId = /^[a-zA-Z0-9]{6}$/.exec(kioskInputRef.value)?.[0];
             if (validKioskId) {
+                validKioskId = validKioskId.toUpperCase();
                 setKioskSubmitSuccessful(true);
                 try {
                     await addGameToKioskAsync(validKioskId, gameId);
