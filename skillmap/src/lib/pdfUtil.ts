1185fa4d8a7fa6af0dd9636a201b05c3f420be36
Joey Wunderlich <joey.wunderlich@gmail.com>
2023-02-03 09:56:05 -0800

    bit of linting

--- skillmap/src/lib/pdfUtil.ts
+++ skillmap/src/lib/pdfUtil.ts
@@ -23,12 +23,15 @@ export async function pdfRenderNameField(pdfBuf: ArrayBuffer, name?: string) {
         TextAlignment,
         rgb,
     } = PDFLib;
+
     const pdfDoc = await PDFDocument.load(pdfBuf);
 
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
     const pages = pdfDoc.getPages();
     const firstPage = pages[0];
+    if (!firstPage)
+        return pdfBuf;
 
     const { width: pageWidth, height: pageHeight } = firstPage.getSize();
 
@@ -45,31 +48,23 @@ export async function pdfRenderNameField(pdfBuf: ArrayBuffer, name?: string) {
         nameEntryField.enableReadOnly();
         nameEntryField.setAlignment(TextAlignment.Center);
         nameEntryField.disableScrolling();
-        const [widget] = nameEntryField.acroField.getWidgets();
         nameEntryField.updateAppearances(helveticaFont);
+        const [widget] = nameEntryField.acroField.getWidgets();
         const { width, height } = widget.getRectangle();
         const widthAtSize10 = helveticaFont.widthOfTextAtSize(name, 10);
         const fontSize = Math.min(
             /** Account for font descender (y, j) size below line */
             helveticaFont.sizeAtHeight(height * .8),
-            (width / widthAtSize10) * 10) | 0;
+            (width / widthAtSize10) * 10
+        ) | 0;
         nameEntryField.setFontSize(fontSize)
     } else {
         const maxPercentWidth = .5;
         const maxPxWidth = maxPercentWidth * pageWidth | 0;
         const widthAtSize10 = helveticaFont.widthOfTextAtSize(name, 10);
         const scalePerc = maxPxWidth / widthAtSize10;
-        // todo configurable? or just remove htis whole thing & require nameEntryField;
+        // todo configurable? or just remove this whole thing & require nameEntryField;
         let offset = 15;
-        // switch (chosen) {
-        //     case "shark":
-        //         offset = -75; break;
-        //     case "racer":
-        //         offset = 20; break;
-        //     default:
-        //         offset = 15; break;
-        // }
-        const sLen = name.length;
         const fontSize = Math.min(70, (scalePerc * 10) | 0);
         firstPage.drawText(name, {
             x: pageWidth / 2 - (helveticaFont.widthOfTextAtSize(name, fontSize)) / 2,
