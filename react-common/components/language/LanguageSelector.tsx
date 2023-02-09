0a754c023bfd5e5a684c6fd197ca3651419a6ffd
thsparks <thsparks@microsoft.com>
2023-02-09 13:21:52 -0800

    Simplify UI

--- react-common/components/language/LanguageSelector.tsx
+++ react-common/components/language/LanguageSelector.tsx
@@ -3,7 +3,7 @@ import { Link } from "../controls/Link";
 import { Modal } from "../controls/Modal";
 import { LanguageCard } from "./LanguageCard";
 
-// TODO thsparks : reduce duplication with lang.tsx. 
+// TODO thsparks : reduce duplication with lang.tsx.
 
 const defaultLanguages = ["en"];
 let initialLang: string;
@@ -46,50 +46,49 @@ export class LanguageSelector extends React.Component<LanguageSelectorProps> {
     }
 
     render() {
-        if(!this.props.visible) {
+        if (!this.props.visible) {
             return null;
         }
-        
+
         const targetTheme = pxt.appTarget.appTheme;
         const languageList = this.languageList();
         // TODO thsparks : Remove or figure out : const modalSize = languageList.length > 4 ? "large" : "small";
-        const translateTheEditor = !pxt.BrowserUtils.isIE()
-            && !pxt.shell.isReadOnly()
-            && !pxt.BrowserUtils.isPxtElectron()
-            && pxt.appTarget.appTheme.crowdinProject;
+        const translateTheEditor =
+            !pxt.BrowserUtils.isIE() &&
+            !pxt.shell.isReadOnly() &&
+            !pxt.BrowserUtils.isPxtElectron() &&
+            pxt.appTarget.appTheme.crowdinProject;
 
         return (
             <Modal
                 onClose={this.props.onClose}
                 title={lf("Select Language")}
                 className="language-selector-modal">
-                <div id="langmodal">
-                    <div
-                        id="availablelocales"
-                        className="ui cards centered language-selector"
-                        role="list"
-                        aria-label={lf("List of available languages")}>
-                        {languageList.map((langId) => {
-                            const lang = pxt.Util.allLanguages[langId];
-                            return (
-                                <LanguageCard
-                                    key={langId}
-                                    langId={langId}
-                                    name={lang.localizedName}
-                                    ariaLabel={lang.englishName}
-                                    description={lang.englishName}
-                                    onClick={this.changeLanguage}/>
-                            );
-                        })}
-                    </div>
-                    {targetTheme.crowdinProject ? (
-                        <div className="ui" id="langmodalfooter">
-                            <Link aria-label={lf("How do I add a new language?")} href="/translate" target="_blank">
-                                {lf("How do I add a new language?")}
-                            </Link>
-                        </div>
-                    ) : undefined}
+                <div
+                    className="ui cards centered language-selector"
+                    role="list"
+                    aria-label={lf("List of available languages")}>
+                    {languageList.map((langId) => {
+                        const lang = pxt.Util.allLanguages[langId];
+                        return (
+                            <LanguageCard
+                                key={langId}
+                                langId={langId}
+                                name={lang.localizedName}
+                                ariaLabel={lang.englishName}
+                                description={lang.englishName}
+                                onClick={this.changeLanguage}/>
+                        );
+                    })}
                 </div>
+                {targetTheme.crowdinProject ? (
+                    <Link
+                        aria-label={lf("How do I add a new language?")}
+                        href="/translate"
+                        target="_blank">
+                        {lf("How do I add a new language?")}
+                    </Link>
+                ) : undefined}
             </Modal>
         );
     }
