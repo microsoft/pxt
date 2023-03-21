acaee2ff75f8203855c3bf46892ab8dedc859976
Kim Ying <15070078+kimprice@users.noreply.github.com>
2023-03-21 13:19:43 -0700

    reposition bubble if it doesn't fit

--- react-common/components/controls/TeachingBubble.tsx
+++ react-common/components/controls/TeachingBubble.tsx
@@ -59,6 +59,12 @@ export const TeachingBubble = (props: TeachingBubbleProps) => {
     } = props;
 
     const margin = 10;
+    const tryFit = {
+        above: false,
+        below: false,
+        left: false,
+        right: false
+    }
 
     useEffect(() => {
         positionBubbleAndCutout();
@@ -76,8 +82,7 @@ export const TeachingBubble = (props: TeachingBubbleProps) => {
         // To Do: check that targetContent.targetQuery is a valid selector
         const targetElement = document.querySelector(targetContent.targetQuery) as HTMLElement;
         if (!targetElement) {
-            // display bubble in center of screen
-            updatePosition(bubble, (window.innerHeight - bubbleBounds.height) / 2, (window.innerWidth - bubbleBounds.width) / 2);
+            positionCenterScreen(bubble, bubbleBounds);
             clearCutout();
             return;
         }
@@ -147,58 +152,112 @@ export const TeachingBubble = (props: TeachingBubbleProps) => {
         cutout.style.height = "0px";
     }
 
+    const resetTryFit = () => {
+        tryFit.above = false;
+        tryFit.below = false;
+        tryFit.left = false;
+        tryFit.right = false;
+    }
+
     const setPosition = (cutoutBounds: CutoutBounds, bubble: HTMLElement, bubbleBounds: DOMRect, bubbleArrow: HTMLElement) => {
+        resetTryFit();
         const transparentBorder = `${margin}px solid transparent`;
         const opaqueBorder = `${margin}px solid`;
 
         const positionAbove = () => {
             const top = cutoutBounds.top - bubbleBounds.height - margin;
             const left = cutoutBounds.left - (bubbleBounds.width - cutoutBounds.width) / 2;
+            tryFit.above = true;
+            if (bubbleFits(bubbleBounds, top, left)) {
+                updatePosition(bubble, top, left);
+            } else {
+                reposition();
+                return;
+            }
             const arrowTop = top + bubbleBounds.height;
             const arrowLeft = cutoutBounds.left + (cutoutBounds.width - margin) / 2;
             bubbleArrow.style.borderLeft = transparentBorder;
             bubbleArrow.style.borderRight = transparentBorder;
             bubbleArrow.style.borderTop = opaqueBorder;
-            checkAndUpdatePosition(bubble, top, left, bubbleArrow, arrowTop, arrowLeft);
+            updatePosition(bubbleArrow, arrowTop, arrowLeft);
         }
 
         const positionBelow = () => {
             const top = cutoutBounds.bottom + margin;
             const left = cutoutBounds.left - (bubbleBounds.width - cutoutBounds.width) / 2;
+            tryFit.below = true;
+            if (bubbleFits(bubbleBounds, top, left)) {
+                updatePosition(bubble, top, left);
+            } else {
+                reposition();
+                return;
+            }
             const arrowTop = top - margin;
             const arrowLeft = cutoutBounds.left + (cutoutBounds.width - margin) / 2;
             bubbleArrow.style.borderLeft = transparentBorder;
             bubbleArrow.style.borderRight = transparentBorder;
             bubbleArrow.style.borderBottom = opaqueBorder;
-            checkAndUpdatePosition(bubble, top, left, bubbleArrow, arrowTop, arrowLeft);
+            updatePosition(bubbleArrow, arrowTop, arrowLeft);
         }
 
         const positionLeft = () => {
             const top = cutoutBounds.top - (bubbleBounds.height - cutoutBounds.height) / 2;
-            const left = cutoutBounds.left - margin;
+            const left = cutoutBounds.left - bubbleBounds.width - margin;
+            tryFit.left = true;
+            if (bubbleFits(bubbleBounds, top, left)) {
+                updatePosition(bubble, top, left);
+            } else {
+                reposition();
+                return;
+            }
             const arrowTop = top + (bubbleBounds.height - margin) / 2;
             const arrowLeft = cutoutBounds.left - margin;
             bubbleArrow.style.borderLeft = opaqueBorder;
             bubbleArrow.style.borderTop = transparentBorder;
             bubbleArrow.style.borderBottom = transparentBorder;
-            checkAndUpdatePosition(bubble, top, left, bubbleArrow, arrowTop, arrowLeft);
+            updatePosition(bubbleArrow, arrowTop, arrowLeft);
         }
 
         const positionRight = () => {
             const top = cutoutBounds.top - (bubbleBounds.height - cutoutBounds.height) / 2;
             const left = cutoutBounds.right + margin;
+            tryFit.right = true;
+            if (bubbleFits(bubbleBounds, top, left)) {
+                updatePosition(bubble, top, left);
+            } else {
+                reposition();
+                return;
+            }
             const arrowTop = top + (bubbleBounds.height - margin) / 2;
             const arrowLeft = cutoutBounds.right;
             bubbleArrow.style.borderRight = opaqueBorder;
             bubbleArrow.style.borderTop = transparentBorder;
             bubbleArrow.style.borderBottom = transparentBorder;
-            checkAndUpdatePosition(bubble, top, left, bubbleArrow, arrowTop, arrowLeft);
+            updatePosition(bubbleArrow, arrowTop, arrowLeft);
         }
 
         const positionCenter = () => {
             const top = (cutoutBounds.height - bubbleBounds.height) / 2 + cutoutBounds.top;
             const left = (cutoutBounds.width - bubbleBounds.width) / 2 + cutoutBounds.left;
-            checkAndUpdatePosition(bubble, top, left);
+            if (bubbleFits(bubbleBounds, top, left)) {
+                updatePosition(bubble, top, left);
+            } else {
+                reposition();
+            }
+        }
+
+        const reposition = () => {
+            if (!tryFit.above) {
+                positionAbove();
+            } else if (!tryFit.below) {
+                positionBelow();
+            } else if (!tryFit.left) {
+                positionLeft();
+            } else if (!tryFit.right) {
+                positionRight();
+            } else {
+                positionCenterScreen(bubble, bubbleBounds);
+            }
         }
 
         switch (targetContent.location) {
@@ -219,15 +278,21 @@ export const TeachingBubble = (props: TeachingBubbleProps) => {
         }
     }
 
-    const checkAndUpdatePosition = (bubble: HTMLElement, top: number, left: number, bubbleArrow?: HTMLElement, arrowTop?: number, arrowLeft?: number) => {
-        if (top < 10) top = 10;
-        if (left < 10) left = 10;
-        updatePosition(bubble, top, left);
-        if (!bubbleArrow || !arrowTop || !arrowLeft) return;
-        updatePosition(bubbleArrow, arrowTop, arrowLeft);
+    const bubbleFits = (bubbleBounds: DOMRect, top: number, left: number): boolean => {
+        if (top < margin) top = margin;
+        if (left < margin) left = margin;
+        const right = left + bubbleBounds.width;
+        const bottom = top + bubbleBounds.height;
+        return right < window.innerWidth && bottom < window.innerHeight;
+    }
+
+    const positionCenterScreen = (bubble: HTMLElement, bubbleBounds: DOMRect) => {
+        updatePosition(bubble, (window.innerHeight - bubbleBounds.height) / 2, (window.innerWidth - bubbleBounds.width) / 2);
     }
 
     const updatePosition = (element: HTMLElement, top: number, left: number) => {
+        if (top < margin) top = margin;
+        if (left < margin) left = margin;
         element.style.top = top + "px";
         element.style.left = left + "px";
     }
