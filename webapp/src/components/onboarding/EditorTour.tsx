3d0a0c71f710d910f751c41f3b53d15493c60a57
Kim Ying <15070078+kimprice@users.noreply.github.com>
2023-03-27 13:54:23 -0700

    update tour content

--- webapp/src/components/onboarding/EditorTour.tsx
+++ webapp/src/components/onboarding/EditorTour.tsx
@@ -4,21 +4,21 @@ import { Location, TeachingBubble, TargetContent } from "../../../../react-commo
 
 const Simulator: TargetContent = {
     title: lf("Micro:bit Simulator"),
-    description: lf("The simulator shows what your program will look like running on a micro:bit."),
+    description: lf("See what your code looks like running on a micro:bit!"),
     targetQuery: "#boardview",
     location: Location.Right,
 };
 
 const Toolbox: TargetContent = {
     title: lf("Toolbox"),
-    description: lf("The toolbox is organized into different categories with blocks of code you can drag out and use in your program. You can search for specific blocks, open the extensions gallery for more blocks, or click Advanced to expand the toolbox categories shown."),
+    description: lf("Drag out blocks of code from the Toolbox categories into the Workspace."),
     targetQuery: "#blocksEditorToolbox",
     location: Location.Right,
 };
 
 const Workspace: TargetContent = {
     title: lf("Workspace"),
-    description: lf("The workspace is where you will build your micro:bit program by dragging blocks from the toolbox and snapping them together."),
+    description: lf("Snap blocks of code together to build your program."),
     targetQuery: "#blocksEditor", // includes the toolbox
     sansQuery: ".blocklyToolboxDiv",
     location: Location.Center,
@@ -33,7 +33,7 @@ const Share: TargetContent = {
 
 const Download: TargetContent = {
     title: lf("Download"),
-    description: lf("Download your program to your micro:bit."),
+    description: lf("Download your program to the micro:bit."),
     targetQuery: "#downloadArea",
     location: Location.Above,
 };
