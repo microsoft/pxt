a4ce62cc1a904a73093b49f3020603f426d482e3
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2023-04-18 13:56:47 -0700

    Undo localhost debugging

--- pxtlib/emitter/cloud.ts
+++ pxtlib/emitter/cloud.ts
@@ -2,7 +2,7 @@
 namespace pxt.Cloud {
     import Util = pxtc.Util;
 
-    export let apiRoot = (pxt.BrowserUtils.isLocalHost() || Util.isNodeJS) ? "http://localhost:8080/api/" : "/api/";
+    export let apiRoot = (pxt.BrowserUtils.isLocalHost() || Util.isNodeJS) ? "https://www.makecode.com/api/" : "/api/";
 
     export let accessToken = "";
     export let localToken = "";
