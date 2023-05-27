33a3a64b8a86b79c64e7d60907b8797fe98061b3
Joey Wunderlich <joey.wunderlich@gmail.com>
2023-05-26 18:26:17 -0700

    add progressbar for main loader, use in webusb

---
+++ react-common/components/controls/ProgressBar.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { classList, ControlProps } from "../util";
+
+export interface ProgressBarProps extends ControlProps {
+    value: number;
+    max?: number; // default: 1
+    title?: string;
+    label?: string;
+
+    ariaValueText?: string;
+    // showPercentage?: boolean
+}
+
+export const ProgressBar = (props: ProgressBarProps) => {
+    const {
+        value,
+        max,
+        id,
+        className,
+        title,
+        label,
+        role,
+        ariaHidden,
+        ariaLabel,
+        ariaDescribedBy,
+        ariaValueText,
+    } = props;
+
+    return (
+        <div className={classList("common-progressbar-wrapper", className)}>
+            {label && <label className="common-textarea-label">
+                {label}
+            </label>}
+            <progress
+                className="common-progressbar"
+                value={value}
+                aria-valuetext={ariaValueText}
+                max={max || 1.0}
+                id={id}
+                role={role || "progressbar"}
+                title={title}
+                aria-label={ariaLabel}
+                aria-describedby={ariaDescribedBy}
+                aria-hidden={ariaHidden}
+            />
+        </div>
+    );
+}
