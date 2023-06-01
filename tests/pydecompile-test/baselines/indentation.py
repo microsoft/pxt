8c4b723c69f0feea93e718afc8a5955d340d1b5e
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2023-05-31 17:38:15 -0700

    Add test case for indentation fix

---
+++ tests/pydecompile-test/baselines/indentation.py
@@ -0,0 +1,19 @@
+mySprite = sprites.create(img("""    
+        . . . . . . . e c 7 . . . . . .
+        . . . . e e e c 7 7 e e . . . .
+        . . c e e e e c 7 e 2 2 e e . .
+        . c e e e e e c 6 e e 2 2 2 e .
+        . c e e e 2 e c c 2 4 5 4 2 e .
+        c e e e 2 2 2 2 2 2 4 5 5 2 2 e
+        c e e 2 2 2 2 2 2 2 2 4 4 2 2 e
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+        c e e 2 2 2 2 2 2 2 2 2 2 4 2 e
+        . e e e 2 2 2 2 2 2 2 2 2 4 e .
+        . 2 e e 2 2 2 2 2 2 2 2 4 2 e .
+        . . 2 e e 2 2 2 2 2 4 4 2 e . .
+        . . . 2 2 e e 4 4 4 2 e e . . .
+        . . . . . 2 2 e e e e . . . . .
+        """),
+    SpriteKind.player)
