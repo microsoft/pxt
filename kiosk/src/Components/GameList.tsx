b233fb61d514b39f8ad8e40443c3eac5176a1850
Eric Anderson <eanders@microsoft.com>
2023-08-21 11:28:26 -0700

    Sync kiosk changes (#9645)

--- kiosk/src/Components/GameList.tsx
+++ kiosk/src/Components/GameList.tsx
@@ -158,7 +158,7 @@ const GameList: React.FC<IProps> = ({ kiosk, addButtonSelected, deleteButtonSele
                     return (
                         <SwiperSlide key={game.id}>
                             <GameSlide highScores={gameHighScores} addButtonSelected={addButtonSelected}
-                                deleteButtonSelected={deleteButtonSelected} game={game} />
+                                deleteButtonSelected={deleteButtonSelected} game={game} locked={kiosk.locked}/>
                         </SwiperSlide>
                     )
                 })}
