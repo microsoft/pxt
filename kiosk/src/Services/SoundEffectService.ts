853964bd2b555b073c2e1dc7f563d526de856b56
Eric Anderson <eanders@microsoft.com>
2023-09-05 15:48:16 -0700

    New UI effects for Kiosk, authored in Arcade (#9671)
    * New UI effects authored in Arcade
    
    * Update readme
    
    * Clarify readme

--- kiosk/src/Services/SoundEffectService.ts
+++ kiosk/src/Services/SoundEffectService.ts
@@ -1,7 +1,7 @@
 import { Howl } from "howler";
 import configData from "../config.json";
 
-export type SoundEffect = "swipe" | "switch" | "back" | "select";
+export type SoundEffect = "swipe" | "switch" | "select" | "notification";
 
 class SoundEffectService {
     private sounds: { [key: string]: Howl };
@@ -10,25 +10,25 @@ class SoundEffectService {
     constructor() {
         this.sounds = {
             swipe: new Howl({
-                src: ["/kiosk-data/sfx/click1.ogg"],
+                src: ["/kiosk-data/sfx/swipe.ogg"],
                 autoplay: false,
                 preload: true,
                 loop: false,
             }),
             switch: new Howl({
-                src: ["/kiosk-data/sfx/rollover2.ogg"],
-                autoplay: false,
-                preload: true,
-                loop: false,
-            }),
-            back: new Howl({
-                src: ["/kiosk-data/sfx/click2.ogg"],
+                src: ["/kiosk-data/sfx/switch.ogg"],
                 autoplay: false,
                 preload: true,
                 loop: false,
             }),
             select: new Howl({
-                src: ["/kiosk-data/sfx/switch4.ogg"],
+                src: ["/kiosk-data/sfx/select.ogg"],
+                autoplay: false,
+                preload: true,
+                loop: false,
+            }),
+            notification: new Howl({
+                src: ["/kiosk-data/sfx/notification.ogg"],
                 autoplay: false,
                 preload: true,
                 loop: false,
