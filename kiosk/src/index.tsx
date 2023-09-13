3798d97e6cd853fc01b292aa5a6726921eefd589
Eric Anderson <eanders@microsoft.com>
2023-09-13 13:35:23 -0700

    Remove unused components from kiosk (#9678)

--- kiosk/src/index.tsx
+++ kiosk/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Kiosk.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { devicePixelRatio, isLocal, tickEvent } from "./browserUtils";
 
 interface Map<T> {
@@ -41,8 +40,3 @@ window.addEventListener("DOMContentLoaded", () => {
         </React.StrictMode>
     );
 });
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
