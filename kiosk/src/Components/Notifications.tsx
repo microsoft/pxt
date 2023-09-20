c5eac6fefb5627704a19d6edae296576b41752f9
Eric Anderson <eanders@microsoft.com>
2023-09-19 18:30:39 -0700

    kiosk: refactor add game polling (#9691)
    * kiosk: refactor add game polling
    
    * show toast on game delete
    
    * poll for games even while playing a game

---
+++ kiosk/src/Components/Notifications.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState, useContext } from "react";
+import { AppStateContext } from "../State/AppStateContext";
+import "../Kiosk.css";
+
+interface IProps {}
+
+const Notifications: React.FC<IProps> = ({}) => {
+    const { state: kiosk, dispatch } = useContext(AppStateContext);
+
+    return (
+        <div className="notification-container">
+            {kiosk.notifications.map((n, i) => (
+                <div key={i} className="notification-contents">
+                    <p>{n.message}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Notifications;
