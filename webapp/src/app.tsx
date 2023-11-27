cf01a8928276296f660539845884b349a993192c
Eric Anderson <eanders@microsoft.com>
2023-11-27 14:45:50 -0800

    filter jacdac-specific messages (#9787)
    * filter jacdac-specific messages
    
    * whitespace
    
    * whitespace
    
    * whitespace

--- webapp/src/app.tsx
+++ webapp/src/app.tsx
@@ -5230,7 +5230,8 @@ function initPacketIO() {
         const msg = ev.data
         if (msg.type === 'messagepacket'
             && msg.sender !== "packetio"
-            && pxt.appTarget.simulator?.messageSimulators?.[msg.channel])
+            && pxt.appTarget.simulator?.messageSimulators?.[msg.channel]
+            && msg.channel === pxt.HF2.CUSTOM_EV_JACDAC)
             pxt.packetio.sendCustomEventAsync(msg.channel, msg.data)
                 .then(() => { }, err => {
                     core.errorNotification(lf("{0}: {1}", msg.channel, err.message));
