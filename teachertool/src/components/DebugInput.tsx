ac7012213743d9253f326764b83da53b2c80ff8a
Kim Ying <15070078+kimprice@users.noreply.github.com>
2023-12-21 09:49:26 -0800

    log project blocksXML and meta data

--- teachertool/src/components/DebugInput.tsx
+++ teachertool/src/components/DebugInput.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "react-common/components/controls/Button";
 import { Input } from "react-common/components/controls/Input";
 import { Textarea } from "react-common/components/controls/Textarea";
+import { getProjectMetaAsync, getProjectTextAsync } from "../services/BackendRequests";
 
 interface IProps {}
 
@@ -9,8 +10,18 @@ const DebugInput: React.FC<IProps> = ({}) => {
     const [shareLink, setShareLink] = useState("https://arcade.makecode.com/S50644-45891-08403-36583");
     const [rubric, setRubric] = useState("");
 
-    const runEvaluate = () => {
+    const runEvaluate = async () => {
         console.log(`Evaluate ${shareLink} with ${rubric}!`);
+        const scriptId = pxt.Cloud.parseScriptId(shareLink);
+        if (!scriptId) {
+            console.log("Invalid share link!");
+            return;
+        }
+        const projText = await getProjectTextAsync(scriptId);
+        if (projText) console.log(projText["main.blocks"] || "Failed to get blocks xml!");
+
+        const projMeta = await getProjectMetaAsync(scriptId);
+        if (projMeta) console.log(projMeta);
     }
 
     return <>
