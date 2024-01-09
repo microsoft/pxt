a54bd62abf7adaba89d5a1c08e6d048b716206a0
Thomas Sparks <thsparks@microsoft.com>
2024-01-09 13:40:01 -0800

    Push state into the MakeCodeFrame to be more consistent with other components

--- teachertool/src/components/MakecodeFrame.tsx
+++ teachertool/src/components/MakecodeFrame.tsx
@@ -1,21 +1,28 @@
 /// <reference path="../../../built/pxteditor.d.ts" />
-import * as React from "react";
+import { useContext } from "react";
 import { setEditorRef } from "../services/makecodeEditorService";
-interface MakeCodeFrameProps {
-    pageSourceUrl: string;
-    tutorialEventHandler?: (event: pxt.editor.EditorMessageTutorialEventRequest) => void;
-}
+import { AppStateContext } from "../state/appStateContext";
+import { createIFrameUrl } from "../utils";
 
-export const MakeCodeFrame: React.FC<MakeCodeFrameProps> =
-    ( { pageSourceUrl} ) => {
+interface MakeCodeFrameProps {}
+export const MakeCodeFrame: React.FC<MakeCodeFrameProps> = () => {
+    const { state: teacherTool } = useContext(AppStateContext)
 
     const handleIFrameRef = (el: HTMLIFrameElement | null) => {
         setEditorRef(el ?? undefined);
     }
 
     /* eslint-disable @microsoft/sdl/react-iframe-missing-sandbox */
-    return <div className="makecode-frame-outer" style={{ display: "block" }}>
-        <iframe className="makecode-frame" src={pageSourceUrl} title={"title"} ref={handleIFrameRef}></iframe>
-    </div>
+    return (
+        <div className="makecode-frame-outer" style={{ display: "block" }}>
+            {teacherTool.projectMetadata && (
+                <iframe
+                    className="makecode-frame"
+                    src={createIFrameUrl(teacherTool.projectMetadata.id, teacherTool.projectMetadata?.target)}
+                    title={"title"}
+                    ref={handleIFrameRef} />
+            )}
+        </div>
+    );
     /* eslint-enable @microsoft/sdl/react-iframe-missing-sandbox */
 }
