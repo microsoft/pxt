dff315fdec1c29432a403bf0ad5fe10b39b716f9
Thomas Sparks <thsparks@microsoft.com>
2024-01-22 11:35:19 -0800

    Add telemetry for adding a criteria

--- teachertool/src/components/CatalogDisplay.tsx
+++ teachertool/src/components/CatalogDisplay.tsx
@@ -10,16 +10,19 @@ interface IProps {}
 const CatalogDisplay: React.FC<IProps> = ({}) => {
     const { state: teacherTool } = useContext(AppStateContext);
 
-    function handleClick (id: string) {
+    function handleClick (criteria: pxt.blocks.CatalogCriteria) {
         // Todo call into addCriteriaInstance
-        logDebug(`Add criteria with id: ${id}`);
+        logDebug(`Add criteria with id: ${criteria.id}`);
+
+        // Template prop is technically redundant but added for easy readability when querying
+        pxt.tickEvent("teachertool.addcriteria", { id: criteria.id, template: criteria.template });
     }
 
     return (
         <div className="catalog-container">
             {
                 teacherTool.catalog?.map(criteria => {
-                    return criteria?.template && <Button id={`btn${criteria.id}`} className="catalog-item" label={criteria.template} onClick={() => handleClick(criteria.id)} title={criteria.id} />
+                    return criteria?.template && <Button id={`btn${criteria.id}`} className="catalog-item" label={criteria.template} onClick={() => handleClick(criteria)} title={criteria.id} />
                 })
             }
         </div>
