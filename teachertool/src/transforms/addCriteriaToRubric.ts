2b6462c6b8f10f5f149b7d3293f62d0a19d756b4
Thomas Sparks <thsparks@microsoft.com>
2024-01-23 12:39:06 -0800

    Use helper for looking up catalog criteria from id

--- teachertool/src/transforms/addCriteriaToRubric.ts
+++ teachertool/src/transforms/addCriteriaToRubric.ts
@@ -2,14 +2,15 @@ import { stateAndDispatch } from "../state";
 import { v4 as uuidV4 } from 'uuid';
 import * as Actions from "../state/actions";
 import { logDebug, logError } from "../services/loggingService";
+import { getCatalogCriteriaWithId } from "../utils";
 
 export async function addCriteriaToRubric(catalogCriteriaIds: string[]) {
-    const { state: teacherTool, dispatch } = stateAndDispatch();
+    const { dispatch } = stateAndDispatch();
 
     // Create instances for each of the catalog criteria.
     const instances = catalogCriteriaIds.reduce((accumulator, catalogCriteriaId) => {
         logDebug(`Adding criteria with ID ${catalogCriteriaId}`);
-        const catalogCriteria = teacherTool.catalog?.find(cat => cat.id === catalogCriteriaId);
+        const catalogCriteria = getCatalogCriteriaWithId(catalogCriteriaId);
         if (!catalogCriteria) {
             logError("adding_missing_criteria", "Attempting to add criteria with unrecognized id", { id: catalogCriteriaId });
             return accumulator;
