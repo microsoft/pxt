367e5d5aab1a3b0f2393abbbf22e80626920d182
Thomas Sparks <thsparks@microsoft.com>
2024-02-02 16:43:40 -0800

    Add an action menu with import and export

--- teachertool/src/components/DebugInput.tsx
+++ teachertool/src/components/DebugInput.tsx
@@ -5,20 +5,14 @@ import { Button } from "react-common/components/controls/Button";
 import { runEvaluateAsync } from "../transforms/runEvaluateAsync";
 import { writeRubricToFile } from "../services/fileSystemService";
 import { AppStateContext } from "../state/appStateContext";
-import { showModal } from "../transforms/showModal";
 
 interface IProps {}
 
 export const DebugInput: React.FC<IProps> = ({}) => {
-    const {state: teacherTool } = useContext(AppStateContext);
-
     const evaluate = async () => {
         await runEvaluateAsync();
     };
 
-    const runExport = () => {
-        const success = writeRubricToFile(teacherTool.rubric);
-    }
 
     return (
         <div className="debug-container">
@@ -28,23 +22,6 @@ export const DebugInput: React.FC<IProps> = ({}) => {
                 onClick={evaluate}
                 title={"Evaluate"}
                 label={lf("Evaluate")}
-            />
-
-            {/* TODO thsparks : move out of debug input */}
-            <Button
-                id="exportButton"
-                className="primary"
-                onClick={runExport}
-                title={"Export"}
-                label={lf("Export")}
-            />
-
-            <Button
-                id="importButton"
-                className="primary"
-                onClick={() => showModal("import-rubric")}
-                title={"Import"}
-                label={lf("Import")}
             />
         </div>
     );
