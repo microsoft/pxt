ce2a214d232132df00b19c14a9d7fa33842c9c0b
Thomas Sparks <thsparks@microsoft.com>
2024-02-05 11:14:36 -0800

    Remove notifications from getRubricFromFileAsync

--- teachertool/src/transforms/getRubricFromFileAsync.ts
+++ teachertool/src/transforms/getRubricFromFileAsync.ts
@@ -1,6 +1,4 @@
 import { logDebug, logError } from "../services/loggingService";
-import { postNotification } from "./postNotification";
-import { makeNotification } from "../utils";
 import { loadRubricFromFileAsync } from "../services/fileSystemService";
 import { verifyRubricIntegrity } from "../state/helpers";
 import { Rubric } from "../types/rubric";
@@ -14,11 +12,8 @@ export async function getRubricFromFileAsync(file: File, allowPartial: boolean):
         const rubricVerificationResult = verifyRubricIntegrity(rubric);
 
         if (!rubricVerificationResult.valid) {
-            postNotification(makeNotification(lf("Rubric contains invalid criteria."), 2000));
             rubric = allowPartial ? { ...rubric, criteria: rubricVerificationResult.validCriteria } : undefined;
         }
-    } else {
-        postNotification(makeNotification(lf("Unable to read rubric file."), 2000));
     }
 
     return rubric;
