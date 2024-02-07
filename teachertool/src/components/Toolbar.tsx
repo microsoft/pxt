360c7bfc628fe4a971d2459966a0b9804b27ce96
Eric Anderson <eanders@microsoft.com>
2024-02-07 11:30:02 -0800

    teacher tool: add auto-run toggle (#9851)
    * teacher tool: add auto-run
    
    * min exports
    
    * no-var
    
    * autorun on rubric change
    
    * use aria-pressed for toggle button
    
    * remove localstorage json apis

--- teachertool/src/components/Toolbar.tsx
+++ teachertool/src/components/Toolbar.tsx
@@ -3,14 +3,72 @@ import * as React from "react";
 import css from "./styling/Toolbar.module.scss";
 
 import { classList } from "react-common/components/util";
+import { Button, ButtonProps } from "react-common/components/controls/Button";
+import { MenuDropdown, MenuDropdownProps } from "react-common/components/controls/MenuDropdown";
 
-interface IProps {
+interface ToolbarButtonProps extends ButtonProps {
+    icon: string;
+}
+
+const ToolbarButton: React.FC<ToolbarButtonProps> = props => {
+    return (
+        <Button {...props} className={classList(css["toolbar-button"], "tt-toolbar-button")} rightIcon={props.icon} />
+    );
+};
+
+interface ToolbarControlGroupProps extends React.PropsWithChildren<{}> {}
+
+const ToolbarControlGroup: React.FC<ToolbarControlGroupProps> = ({ children }) => {
+    return <div className={classList(css["toolbar-control-group"], "tt-toolbar-control-group")}>{children}</div>;
+};
+
+interface ToolbarMenuDropdownProps extends MenuDropdownProps {}
+
+const ToolbarMenuDropdown: React.FC<ToolbarMenuDropdownProps> = props => {
+    const dropdownLabel = <i className={"fas fa-ellipsis-v"} />;
+    return (
+        <MenuDropdown
+            {...props}
+            className={classList(css["toolbar-menu-dropdown"], "tt-toolbar-menu-dropdown")}
+            label={dropdownLabel}
+        />
+    );
+};
+
+interface ToolbarToggleProps {
+    label: string;
+    isChecked: boolean;
+    onChange: (checked: boolean) => void;
+}
+
+const ToolbarToggle: React.FC<ToolbarToggleProps> = ({ label, isChecked, onChange }) => {
+    const [checked, setChecked] = React.useState(isChecked);
+
+    const onClick = () => {
+        setChecked(!checked);
+        onChange(!checked);
+    };
+
+    return (
+        <Button
+            className={css["toggle-button"]}
+            title={label}
+            label={label}
+            ariaLabel={label}
+            onClick={onClick}
+            rightIcon={checked ? "fas fa-toggle-on" : "fas fa-toggle-off"}
+            ariaPressed={checked}
+        />
+    );
+};
+
+interface ToolbarControlProps {
     left?: React.ReactNode;
     center?: React.ReactNode;
     right?: React.ReactNode;
 }
 
-export const Toolbar: React.FC<IProps> = ({ left, center, right }) => {
+const ToolbarControl: React.FC<ToolbarControlProps> = ({ left, center, right }) => {
     return (
         <div className={classList(css["toolbar"], "tt-toolbar")}>
             <div className={classList(css["left"], "tt-toolbar-left")}>{left}</div>
@@ -19,3 +77,10 @@ export const Toolbar: React.FC<IProps> = ({ left, center, right }) => {
         </div>
     );
 };
+
+export const Toolbar = Object.assign(ToolbarControl, {
+    Button: ToolbarButton,
+    ControlGroup: ToolbarControlGroup,
+    MenuDropdown: ToolbarMenuDropdown,
+    Toggle: ToolbarToggle,
+});
