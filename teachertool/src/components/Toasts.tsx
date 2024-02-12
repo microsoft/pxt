f9b8688af504eece9c68f416a5cce01ecc9b35ea
Eric Anderson <eanders@microsoft.com>
2024-02-12 08:29:56 -0800

    restore css

--- teachertool/src/components/Toasts.tsx
+++ teachertool/src/components/Toasts.tsx
@@ -52,7 +52,7 @@ const ToastNotification: React.FC<IToastNotificationProps> = ({ toast }) => {
                     </div>
                 )}
                 <div className={css["text-container"]}>
-                    {toast.text && <div className={css["text"]}>{toast.text}</div>}
+                    {toast.text && <div className={classList(css["text"], "tt-toast-text")}>{toast.text}</div>}
                     {toast.detail && <div className={css["detail"]}>{toast.detail}</div>}
                     {toast.jsx && <div>{toast.jsx}</div>}
                 </div>
