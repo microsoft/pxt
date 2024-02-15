71f4dadfb5bd8366e87b858c181b1acd43d11707
thsparks <thsparks@microsoft.com>
2024-02-15 12:51:13 -0800

    Display question in result

--- teachertool/src/components/EvalResultDisplay.tsx
+++ teachertool/src/components/EvalResultDisplay.tsx
@@ -4,18 +4,31 @@ import { useContext } from "react";
 import { AppStateContext } from "../state/appStateContext";
 import { getCatalogCriteriaWithId } from "../state/helpers";
 import { CriteriaEvaluationResult } from "../types/criteria";
+import { get } from "jquery";
 
 interface IProps {}
 
 export const EvalResultDisplay: React.FC<IProps> = ({}) => {
     const { state: teacherTool } = useContext(AppStateContext);
 
-    function getTemplateStringFromCriteriaInstanceId(instanceId: string): string {
-        const catalogCriteriaId = teacherTool.rubric.criteria?.find(
+    function getCriteriaInstanceDisplayString(instanceId: string): string {
+        const criteriaInstance = teacherTool.rubric.criteria?.find(
             criteria => criteria.instanceId === instanceId
-        )?.catalogCriteriaId;
+        );
+        if (!criteriaInstance) return "";
+
+        const catalogCriteriaId = criteriaInstance.catalogCriteriaId;
         if (!catalogCriteriaId) return "";
-        return getCatalogCriteriaWithId(catalogCriteriaId)?.template ?? "";
+
+        const templateString = getCatalogCriteriaWithId(catalogCriteriaId)?.template;
+        if (!templateString) return "";
+
+        let displayString = templateString;
+        for (const param of criteriaInstance.params ?? []) {
+            displayString = displayString.replace(`\${${param.name}}`, param.value);
+        }
+
+        return displayString;
     }
 
     return (
@@ -26,28 +39,26 @@ export const EvalResultDisplay: React.FC<IProps> = ({}) => {
 
                     {Object.keys(teacherTool.evalResults ?? {}).map(criteriaInstanceId => {
                         const result = teacherTool.evalResults[criteriaInstanceId];
-                        const label = getTemplateStringFromCriteriaInstanceId(criteriaInstanceId);
+                        const label = getCriteriaInstanceDisplayString(criteriaInstanceId);
                         return (
                             label && (
                                 <div key={criteriaInstanceId}>
-                                    <div className="result-block-id">
-                                        <p className="block-id-label">
-                                            {getTemplateStringFromCriteriaInstanceId(criteriaInstanceId)}:
-                                        </p>
-                                        {result.result === CriteriaEvaluationResult.InProgress && (
-                                            <div className="common-spinner" />
-                                        )}
-                                        {result.result === CriteriaEvaluationResult.CompleteWithNoResult && (
-                                            <p>{lf("N/A")}</p>
-                                        )}
-                                        {result.result === CriteriaEvaluationResult.Fail && (
-                                            <p className="negative-text">{lf("Needs Work")}</p>
-                                        )}
-                                        {result.result === CriteriaEvaluationResult.Pass && (
-                                            <p className="positive-text">{lf("Looks Good!")}</p>
-                                        )}
-                                    </div>
-                                    {result.notes && <p className="notes">{result.notes}</p>}
+                                <div className="result-block-id">
+                                    <p className="block-id-label">
+                                        {label}:
+                                    </p>
+                                    {result.result === CriteriaEvaluationResult.InProgress && (
+                                        <div className="common-spinner" />
+                                    )}
+                                    {result.result === CriteriaEvaluationResult.CompleteWithNoResult && <p>{lf("N/A")}</p>}
+                                    {result.result === CriteriaEvaluationResult.Fail && (
+                                        <p className="negative-text">{lf("Needs Work")}</p>
+                                    )}
+                                    {result.result === CriteriaEvaluationResult.Pass && (
+                                        <p className="positive-text">{lf("Looks Good!")}</p>
+                                    )}
+                                </div>
+                                {result.notes && <p className="notes">{result.notes}</p>}
                                 </div>
                             )
                         );
