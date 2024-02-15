66eb078e33d3401227a09b86f0c5d8077deb0b9c
thsparks <thsparks@microsoft.com>
2024-02-15 12:51:31 -0800

    Remove unused import

--- teachertool/src/components/EvalResultDisplay.tsx
+++ teachertool/src/components/EvalResultDisplay.tsx
@@ -4,7 +4,6 @@ import { useContext } from "react";
 import { AppStateContext } from "../state/appStateContext";
 import { getCatalogCriteriaWithId } from "../state/helpers";
 import { CriteriaEvaluationResult } from "../types/criteria";
-import { get } from "jquery";
 
 interface IProps {}
 
