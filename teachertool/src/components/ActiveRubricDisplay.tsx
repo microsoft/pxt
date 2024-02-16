9100031500121f1dd66ee6cad4897c674206a87d
thsparks <thsparks@microsoft.com>
2024-02-15 16:57:22 -0800

    Draw rubric with table, not really working...

--- teachertool/src/components/ActiveRubricDisplay.tsx
+++ teachertool/src/components/ActiveRubricDisplay.tsx
@@ -8,40 +8,70 @@ import { removeCriteriaFromRubric } from "../transforms/removeCriteriaFromRubric
 import { setRubricName } from "../transforms/setRubricName";
 import { DebouncedInput } from "./DebouncedInput";
 import { AddCriteriaButton } from "./AddCriteriaButton";
+import css from "./styling/ActiveRubricDisplay.module.scss";
+import { CriteriaInstance } from "../types/criteria";
+
+// TODO thsparks - move to different file or keep here?
+interface CriteriaInstanceDisplayProps {
+    criteriaInstance: CriteriaInstance;
+}
+const CriteriaInstanceDisplay: React.FC<CriteriaInstanceDisplayProps> = ({ criteriaInstance }) => {
+    const catalogCriteria = getCatalogCriteriaWithId(criteriaInstance.catalogCriteriaId);
+    if (!catalogCriteria) {
+        return null;
+    }
+
+    return catalogCriteria ? (
+        <tr className={css["criteria-instance-display"]}>
+            <td className={css["criteria-display-text"]}>{catalogCriteria.template}</td>
+            <td>
+                <Button
+                    className={css["criteria-btn-remove"]}
+                    label={lf("X")}
+                    onClick={() => removeCriteriaFromRubric(criteriaInstance)}
+                    title={lf("Remove")}
+                />
+            </td>
+        </tr>
+    ) : null;
+};
 
 interface IProps {}
-
 export const ActiveRubricDisplay: React.FC<IProps> = ({}) => {
     const { state: teacherTool } = useContext(AppStateContext);
 
     return (
-        <div className="rubric-display">
+        <div className={css["rubric-display"]}>
             <DebouncedInput
-                label={lf("Rubric Name")}
-                ariaLabel={lf("Rubric Name")}
+                label={lf("Name")}
+                ariaLabel={lf("Name")}
                 onChange={setRubricName}
                 placeholder={lf("Rubric Name")}
                 initialValue={teacherTool.rubric.name}
                 preserveValueOnBlur={true}
+                className={css["rubric-name-input"]}
             />
-            {teacherTool.rubric.criteria?.map(criteriaInstance => {
-                if (!criteriaInstance) return null;
-
-                const catalogCriteria = getCatalogCriteriaWithId(criteriaInstance.catalogCriteriaId);
-                return (
-                    criteriaInstance.catalogCriteriaId && (
-                        <div className="criteria-instance-display" key={criteriaInstance.instanceId}>
-                            {catalogCriteria?.template}
-                            <Button
-                                className="criteria-btn-remove"
-                                label={lf("X")}
-                                onClick={() => removeCriteriaFromRubric(criteriaInstance)}
-                                title={lf("Remove")}
+            <table className={css["criteria-table"]}>
+                <thead>
+                    <tr>
+                        <th>{lf("Criteria")}</th>
+                        <th>
+                            <i className={"fas fa-ellipsis-v"} />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {teacherTool.rubric.criteria?.map(criteriaInstance => {
+                        if (!criteriaInstance) return null;
+                        return (
+                            <CriteriaInstanceDisplay
+                                criteriaInstance={criteriaInstance}
+                                key={criteriaInstance.instanceId}
                             />
-                        </div>
-                    )
-                );
-            })}
+                        );
+                    })}
+                </tbody>
+            </table>
             <AddCriteriaButton />
         </div>
     );
