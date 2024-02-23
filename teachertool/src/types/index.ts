b8477ec0895083cf898d42fb9bb421e42dbea3d6
Eric Anderson <eanders@microsoft.com>
2024-02-22 17:45:03 -0800

    card type

--- teachertool/src/types/index.ts
+++ teachertool/src/types/index.ts
@@ -25,13 +25,15 @@ export type ModalType = "catalog-display" | "import-rubric";
 
 export type TabName = "home" | "rubric" | "results";
 
+export type CardType = "rubric-resource";
+
 // Rubric Card types that can be appear in the carousel
 export type CarouselCard = {
-    cardType: string;
+    cardType: CardType;
 };
 
 export type CarouselRubricResourceCard = CarouselCard & {
-    cardType: "rubric-card";
+    cardType: "rubric-resource";
     cardTitle: string;
     imageUrl: string;
     rubricUrl: string;
