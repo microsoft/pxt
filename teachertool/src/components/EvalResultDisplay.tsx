3d0798909030e0ea238de93ec7352ac14fa2c857
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2024-02-27 15:34:26 -0800

    moved results css to sass module, started react fc for results header

--- teachertool/src/components/EvalResultDisplay.tsx
+++ teachertool/src/components/EvalResultDisplay.tsx
@@ -2,8 +2,20 @@ import { useContext } from "react";
 import { AppStateContext } from "../state/appStateContext";
 import { getCatalogCriteriaWithId } from "../state/helpers";
 import { CriteriaEvaluationResult } from "../types/criteria";
+import css from "./styling/EvalResultDisplay.module.scss";
+import { classList } from "react-common/components/util";
+
 
 interface IProps {}
+
+const ResultsHeader: React.FC = () => {
+    const { state: teacherTool } = useContext(AppStateContext);
+    return (
+        <>
+            <h1>{teacherTool.rubric.name}</h1>
+        </>
+    );
+};
 
 export const EvalResultDisplay: React.FC<IProps> = ({}) => {
     const { state: teacherTool } = useContext(AppStateContext);
@@ -19,28 +31,28 @@ export const EvalResultDisplay: React.FC<IProps> = ({}) => {
     return (
         <>
             {teacherTool.projectMetadata && (
-                <div className="eval-results-container">
+                <div className={classList(css["eval-results-container"])}>
                     <h3>{lf("Project: {0}", teacherTool.projectMetadata.name)}</h3>
 
-                    {teacherTool.evalResults.length === 0 && <div className="common-spinner" />}
+                    {teacherTool.evalResults.length === 0 && <div className={css["common-spinner"]} />}
                     {Object.keys(teacherTool.evalResults ?? {}).map(criteriaInstanceId => {
                         const result = teacherTool.evalResults[criteriaInstanceId];
                         const label = getTemplateStringFromCriteriaInstanceId(criteriaInstanceId);
                         return (
                             label && (
-                                <div className="result-block-id" key={criteriaInstanceId}>
-                                    <p className="block-id-label">
-                                        {getTemplateStringFromCriteriaInstanceId(criteriaInstanceId)}:
+                                <div className={css["result-block-id"]} key={criteriaInstanceId}>
+                                    <p className={css["block-id-label"]}>
+                                        {label}:
                                     </p>
                                     {result === CriteriaEvaluationResult.InProgress && (
-                                        <div className="common-spinner" />
+                                        <div className={css["common-spinner"]} />
                                     )}
                                     {result === CriteriaEvaluationResult.CompleteWithNoResult && <p>{lf("N/A")}</p>}
                                     {result === CriteriaEvaluationResult.Fail && (
-                                        <p className="negative-text">{lf("Needs Work")}</p>
+                                        <p className={css["negative-text"]}>{lf("Needs Work")}</p>
                                     )}
                                     {result === CriteriaEvaluationResult.Pass && (
-                                        <p className="positive-text">{lf("Looks Good!")}</p>
+                                        <p className={css["positive-text"]}>{lf("Looks Good!")}</p>
                                     )}
                                 </div>
                             )
