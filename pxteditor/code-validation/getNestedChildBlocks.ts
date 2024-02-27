8c93ff61a0b180fd10eacaf43fd6264fe497561b
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2024-02-26 16:57:49 -0800

    add comment for getNestedChildBlocks

--- pxteditor/code-validation/getNestedChildBlocks.ts
+++ pxteditor/code-validation/getNestedChildBlocks.ts
@@ -2,9 +2,11 @@
 
 import * as Blockly from "blockly";
 
+// returns the directly nested children of a block
+// for blocks with a mouth, it returns the blocks inside the mouth and its inputs, if any
+// for something like pick random, it would return the two number blocks
 export function getNestedChildBlocks(parentBlock: Blockly.Block): Blockly.Block[] {
-    // want param to be false to filter out disabled blocks
-    const descendants = parentBlock.getDescendants(false);
+    const descendants = parentBlock.getDescendants(true);
     const nestedChildren = descendants.filter((block) => block.getSurroundParent() === parentBlock);
     return nestedChildren;
 }
