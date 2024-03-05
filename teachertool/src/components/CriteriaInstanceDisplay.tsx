39127a44a817932570a3823bb8a27e318642c0bb
thsparks <thsparks@microsoft.com>
2024-03-05 14:52:13 -0800

    Add error state for non-numeric input in numeric field

--- teachertool/src/components/CriteriaInstanceDisplay.tsx
+++ teachertool/src/components/CriteriaInstanceDisplay.tsx
@@ -1,11 +1,13 @@
 import { getCatalogCriteriaWithId } from "../state/helpers";
-import { CriteriaInstance, CriteriaParameter, CriteriaParameterValue } from "../types/criteria";
+import { CriteriaInstance, CriteriaParameterValue } from "../types/criteria";
 import { DebouncedInput } from "./DebouncedInput";
 import { logDebug } from "../services/loggingService";
 import { setParameterValue } from "../transforms/setParameterValue";
 import { classList } from "react-common/components/util";
+import { useState } from "react";
 // eslint-disable-next-line import/no-internal-modules
 import css from "./styling/CriteriaInstanceDisplay.module.scss";
+
 
 interface InlineInputSnippetProps {
     initialValue: string;
@@ -21,9 +23,14 @@ const InlineInputSnippet: React.FC<InlineInputSnippetProps> = ({
     shouldExpand,
     numeric,
 }) => {
+    const [error, setError] = useState("");
+
     function onChange(newValue: string) {
         if (!numeric || !isNaN(Number(newValue))) {
+            setError("");
             setParameterValue(instance.instanceId, param.name, newValue);
+        } else {
+            setError(lf("value must be numeric"));
         }
     }
 
@@ -32,12 +39,14 @@ const InlineInputSnippet: React.FC<InlineInputSnippetProps> = ({
             className={classList(
                 css["inline-input"],
                 numeric ? css["number-input"] : css["string-input"],
-                shouldExpand ? css["long"] : undefined
+                shouldExpand ? css["long"] : undefined,
+                error ? css["error"] : undefined
             )}
             initialValue={initialValue}
             onChange={onChange}
             preserveValueOnBlur={true}
-            placeholder={lf(param.name)}
+            placeholder={numeric ? "0" : lf(param.name)}
+            title={error ? lf("{0} - {1}", param.name, error) : param.name}
         />
     );
 };
