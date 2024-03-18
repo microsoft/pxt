5268f00aba4e1da1622a74ef86ce1c7dd42f39d8
thsparks <thsparks@microsoft.com>
2024-03-18 10:00:57 -0700

    Debug block picking & displaying

--- teachertool/src/components/ActiveRubricDisplay.tsx
+++ teachertool/src/components/ActiveRubricDisplay.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Strings } from "../constants";
 import { AppStateContext } from "../state/appStateContext";
 import { setRubricName } from "../transforms/setRubricName";
@@ -7,10 +7,32 @@ import { AddCriteriaButton } from "./AddCriteriaButton";
 import css from "./styling/ActiveRubricDisplay.module.scss";
 import React from "react";
 import { CriteriaTable } from "./CriteriaTable";
+import { getBlockImageUriAsync, getBlocksInfo } from "../services/makecodeEditorService";
 
 interface ActiveRubricDisplayProps {}
 export const ActiveRubricDisplay: React.FC<ActiveRubricDisplayProps> = ({}) => {
     const { state: teacherTool } = useContext(AppStateContext);
+
+    const [tempUri, setTempUri] = React.useState<string | undefined>(undefined);
+
+    const [blocksInfo, setBlocksInfo] = React.useState<pxtc.BlocksInfo | undefined>(undefined);
+
+    useEffect(() => {
+        Promise.resolve().then(async () => {
+            // TODO thsparks - move initializing this into a transform that runs on init.
+            const info = await getBlocksInfo();
+            setBlocksInfo(info);
+            if (!info) return;
+
+            const ids = Object.keys(info.blocksById);
+            const randomType = ids[Math.floor(Math.random() * (ids.length - 1))];
+
+            const blockUri = await getBlockImageUriAsync(randomType!);
+            setTempUri(blockUri);
+        });
+
+    }, [teacherTool.rubric.name]);
+
 
     return (
         <div className={css["rubric-display"]}>
@@ -25,6 +47,10 @@ export const ActiveRubricDisplay: React.FC<ActiveRubricDisplayProps> = ({}) => {
                     className={css["rubric-name-input"]}
                 />
             </div>
+            <div>{tempUri && <img className="ui image" src={tempUri} alt="sample block"/>}</div>
+            <div>{`Blocks Info: ${!!blocksInfo}`}</div>
+            {blocksInfo && <div>{`Block Count: ${blocksInfo.blocks.length}`}</div>}
+
             <CriteriaTable />
             <AddCriteriaButton />
         </div>
