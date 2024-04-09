52dccca7413d26e69ee3e45763f6dbb41f1628e5
thsparks <thsparks@microsoft.com>
2024-04-09 10:55:45 -0700

    Comment update

--- teachertool/src/validatorPlanOverrides/runValidatorPlanOverrideAsync.ts
+++ teachertool/src/validatorPlanOverrides/runValidatorPlanOverrideAsync.ts
@@ -1,6 +1,6 @@
 import { runAiQuestionValidationPlanAsync } from "./runAiQuestionValidationPlanAsync";
 
-// This function allows the teacher tool to override validation for any validator plans (instead of calling into the iframe).
+// This allows the teacher tool to override validation for any validator plans (instead of calling into the iframe).
 // It will return the result of the override if one is found.
 // If there is no override for the plan, it will return undefined.
 export async function runValidatorPlanOverrideAsync(plan: pxt.blocks.ValidatorPlan): Promise<pxt.blocks.EvaluationResult | undefined> {
