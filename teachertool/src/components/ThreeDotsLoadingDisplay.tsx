ffa278a097be4806555422b0ad1d0bd00f428078
thsparks <thsparks@microsoft.com>
2024-04-09 10:09:34 -0700

    Rings instead of circles for loading

--- teachertool/src/components/ThreeDotsLoadingDisplay.tsx
+++ teachertool/src/components/ThreeDotsLoadingDisplay.tsx
@@ -9,9 +9,9 @@ export interface ThreeDotsLoadingDisplayProps {
 export const ThreeDotsLoadingDisplay: React.FC<ThreeDotsLoadingDisplayProps> = ({ className }) => {
     return (
         <div className={classList(className, css["loading-ellipsis"])} aria-label={Strings.Loading}>
-            <i className={classList("fas fa-circle", css["circle"], css["circle-1"])} aria-hidden={true} />
-            <i className={classList("fas fa-circle", css["circle"], css["circle-2"])} aria-hidden={true} />
-            <i className={classList("fas fa-circle", css["circle"], css["circle-3"])} aria-hidden={true} />
+            <i className={classList("far fa-circle", css["circle"], css["circle-1"])} aria-hidden={true} />
+            <i className={classList("far fa-circle", css["circle"], css["circle-2"])} aria-hidden={true} />
+            <i className={classList("far fa-circle", css["circle"], css["circle-3"])} aria-hidden={true} />
         </div>
     );
 };
