d796dd86111d706b4a791330b08b27d41a616c1b
thsparks <thsparks@microsoft.com>
2024-04-23 10:18:48 -0700

    Prettier

--- teachertool/src/components/ScreenReaderAnnouncer.tsx
+++ teachertool/src/components/ScreenReaderAnnouncer.tsx
@@ -5,9 +5,13 @@ import css from "./styling/ActionAnnouncer.module.scss";
 export interface ScreenReaderAnnouncerProps {}
 export const ScreenReaderAnnouncer: React.FC<ScreenReaderAnnouncerProps> = () => {
     const { state: teacherTool } = useContext(AppStateContext);
-    return <>
-        {teacherTool.screenReaderAnnouncement && (
-            <div className={css["sr-only"]} aria-live="polite"> {teacherTool.screenReaderAnnouncement} </div>
-        )}
-    </>
-}
+    return (
+        <>
+            {teacherTool.screenReaderAnnouncement && (
+                <div className={css["sr-only"]} aria-live="polite">
+                    {teacherTool.screenReaderAnnouncement}
+                </div>
+            )}
+        </>
+    );
+};
