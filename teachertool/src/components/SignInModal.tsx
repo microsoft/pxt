0a826c0b32ac6aeb9df60bdf9b223585f8cf7f96
thsparks <thsparks@microsoft.com>
2024-07-30 11:22:42 -0700

    Remove localhost auth stuff, apparently this does actually work. Not sure what was causing the CORS errors before, but they're gone now.

--- teachertool/src/components/SignInModal.tsx
+++ teachertool/src/components/SignInModal.tsx
@@ -3,35 +3,10 @@ import { AppStateContext } from "../state/appStateContext";
 import { SignInModal as RcSignInModal } from "react-common/components/profile/SignInModal";
 import { hideModal } from "../transforms/hideModal";
 import * as authClient from "../services/authClient";
-import { Modal } from "react-common/components/controls/Modal";
-import { setUserProfile } from "../transforms/setUserProfile";
-import { Button } from "react-common/components/controls/Button";
 
 export interface IProps {}
 export const SignInModal: React.FC<IProps> = () => {
     const { state: teacherTool } = useContext(AppStateContext);
-    const [showDebugOption, setShowDebugOption] = useState(pxt.BrowserUtils.isLocalHost());
-
-    if (teacherTool.modalOptions?.modal === "sign-in" && showDebugOption) {
-        function debugLogin() {
-            setUserProfile({
-                id: "debug_login",
-                idp: {
-                    username: "Debug Login",
-                    displayName: "Debug Login",
-                },
-            });
-            hideModal();
-        }
-        return (
-            <Modal title={"Debug Login?"}>
-                <div>
-                    <Button onClick={debugLogin} title={"Debug Login"} label={"Debug Login"} className={"primary"} />
-                    <Button onClick={() => setShowDebugOption(false)} title={"Cancel"} label={"Cancel"} />
-                </div>
-            </Modal>
-        );
-    }
 
     return teacherTool.modalOptions?.modal === "sign-in" ? (
         <RcSignInModal
