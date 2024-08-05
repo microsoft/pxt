e9abbad5b5bf84ca144445aa3d6e18bb05c75953
Richard Knoll <riknoll@microsoft.com>
2024-08-05 16:12:31 -0700

    use a regular array for iterating through shadow blocks

--- pxtblocks/toolbox.ts
+++ pxtblocks/toolbox.ts
@@ -210,10 +210,8 @@ export function createShadowValue(info: pxtc.BlocksInfo, p: pxt.blocks.BlockPara
     }
 
     if (maxRecursion) {
-        const allShadows = value.getElementsByTagName("shadow");
-        for (let i = 0; i < allShadows.length; i++) {
-            const shadow = allShadows.item(i);
-
+        const allShadows = pxt.Util.toArray(value.getElementsByTagName("shadow"));
+        for (const shadow of allShadows) {
             if (!shadow.innerHTML) {
                 const shadowSymbol = info.blocks.find(s => s.attributes.blockId === shadow.getAttribute("type"));
                 if (shadowSymbol) {
