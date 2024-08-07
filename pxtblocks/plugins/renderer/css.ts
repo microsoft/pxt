97c244f0956eb86820e4bbe045b33979b35a3dc1
Richard Knoll <riknoll@microsoft.com>
2024-08-07 14:29:40 -0700

    make dropdown checkbox white

---
+++ pxtblocks/plugins/renderer/css.ts
@@ -0,0 +1,7 @@
+import * as Blockly from "blockly";
+
+Blockly.Css.register(`
+.blocklyDropdownMenu .blocklyMenuItemCheckbox.goog-menuitem-checkbox {
+    filter: contrast(0) brightness(100);
+}
+`)
