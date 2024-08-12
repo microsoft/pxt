5775fa2127f950ef454a8c985476106dfc596e73
Richard Knoll <riknoll@microsoft.com>
2024-08-12 10:57:21 -0700

    Give dropdown fields a border rect if they aren't the only editable field in a block

--- pxtblocks/fields/field_dropdown.ts
+++ pxtblocks/fields/field_dropdown.ts
@@ -3,6 +3,19 @@ import { showEditorMixin } from "../plugins/newVariableField/fieldDropdownMixin"
 
 export class FieldDropdown extends Blockly.FieldDropdown {
     protected shouldAddBorderRect_() {
+        // Returning false for this function will turn the entire block into
+        // a click target for this field. If there are other editable fields
+        // in this block, make sure we return true so that we don't make them
+        // inaccessible
+        for (const input of this.sourceBlock_.inputList) {
+            for (const field of input.fieldRow) {
+                if (field === this) continue;
+
+                if (field.EDITABLE) {
+                    return true;
+                }
+            }
+        }
         if (!this.sourceBlock_.getInputsInline()) {
             return true;
         }
