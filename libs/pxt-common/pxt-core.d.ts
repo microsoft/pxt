861e4d034694416d510473c112111f2cf7f32885
Abhijith Chatra <abchatra@microsoft.com>
2024-08-14 13:51:47 -0700

    Adding the weight to the block

--- libs/pxt-common/pxt-core.d.ts
+++ libs/pxt-common/pxt-core.d.ts
@@ -284,7 +284,7 @@ declare interface String {
      * Return the Unicode value of the character at the specified location.
      * @param index The zero-based index of the desired character. If there is no character at the specified index, NaN is returned.
      */
-    //% shim=String_::charCodeAt
+    //% shim=String_::charCodeAt weight=46
     //% help=text/char-code-at
     //% blockId="string_code_at" block="char code from %this=text|at %pos" blockNamespace="text"
     //% this.defl="this"
