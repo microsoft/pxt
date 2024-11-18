663187940b943c1550471c694f2ff0d56d9119fb
Sarah Rietkerk <49178322+srietkerk@users.noreply.github.com>
2024-11-18 10:03:47 -0800

    Inline blocks for tutorial content: agent icon now showing up (#10271)
    * fix the tabbing for has categories check
    
    * add support for image icons in inline blocks in tutorials

--- webapp/src/marked.tsx
+++ webapp/src/marked.tsx
@@ -406,7 +406,8 @@ export class MarkedContent extends data.Component<MarkedContentProps, MarkedCont
                     inlineBlock.appendChild(inlineBlockDiv);
                     inlineBlockDiv.className = lev;
                     if (hasCategories) {
-                        const inlineBlockIcon = document.createElement('i');
+                        const inlineBlockIcon = document.createElement('span');
+                        inlineBlockIcon.setAttribute("role", "presentation");
                         inlineBlockDiv.append(inlineBlockIcon);
                     }
                     inlineBlockDiv.append(pxt.U.rlf(txt));
@@ -448,28 +449,42 @@ export class MarkedContent extends data.Component<MarkedContentProps, MarkedCont
                     }
 
                     if (hasCategories) {
-                            const isAdvanced = bi?.attributes?.advanced || ns === "arrays";
-                            inlineBlock.classList.add("clickable");
-                            inlineBlock.tabIndex = 0;
-                            inlineBlock.ariaLabel = lf("Toggle the {0} category", ns);
-                            inlineBlock.title = inlineBlock.ariaLabel;
-                            inlineBlock.children[0].append(bi?.attributes?.icon || pxt.toolbox.getNamespaceIcon(ns) || "");
-                            inlineBlock.addEventListener("click", e => {
-                                // need to filter out editors that are currently hidden as we leave toolboxes in dom
-                                const editorSelector = `#maineditor > div:not([style*="display:none"]):not([style*="display: none"])`;
+                        const isAdvanced = bi?.attributes?.advanced || ns === "arrays";
+                        inlineBlock.classList.add("clickable");
+                        inlineBlock.tabIndex = 0;
+                        inlineBlock.ariaLabel = lf("Toggle the {0} category", ns);
+                        inlineBlock.title = inlineBlock.ariaLabel;
 
-                                if (isAdvanced) {
-                                    // toggle advanced open first if it is collapsed.
-                                    const advancedSelector = `${editorSelector} .blocklyTreeRow[data-ns="advancedcollapsed"]`;
-                                    const advancedRow = document.querySelector<HTMLDivElement>(advancedSelector);
-                                    advancedRow?.click();
-                                }
+                        // handle adding the icon
+                        const iconContainer = inlineBlock.children[0];
+                        const currentIcon = bi?.attributes?.icon || pxt.toolbox.getNamespaceIcon(ns) || "";
+                        const isImageIcon = currentIcon.length > 1;  // It's probably an image icon, and not an icon code
+                        if (isImageIcon) {
+                            iconContainer.classList.add('image-icon');
+                            iconContainer.classList.add(ns);
+                            iconContainer.setAttribute("style", `--image-icon-url: url("${pxt.Util.pathJoin(pxt.webConfig.commitCdnUrl, encodeURI(currentIcon))}")`);
+                        } else {
+                            const inlineBlockIcon = document.createElement('i');
+                            inlineBlockIcon.append(currentIcon)
+                            iconContainer.append(inlineBlockIcon);
+                        }
 
-                                const toolboxSelector = `${editorSelector} .blocklyTreeRow[data-ns="${ns}"]`;
-                                const toolboxRow = document.querySelector<HTMLDivElement>(toolboxSelector);
-                                toolboxRow?.click();
-                            });
-                            inlineBlock.addEventListener("keydown", e => fireClickOnEnter(e as any))
+                        inlineBlock.addEventListener("click", e => {
+                            // need to filter out editors that are currently hidden as we leave toolboxes in dom
+                            const editorSelector = `#maineditor > div:not([style*="display:none"]):not([style*="display: none"])`;
+
+                            if (isAdvanced) {
+                                // toggle advanced open first if it is collapsed.
+                                const advancedSelector = `${editorSelector} .blocklyTreeRow[data-ns="advancedcollapsed"]`;
+                                const advancedRow = document.querySelector<HTMLDivElement>(advancedSelector);
+                                advancedRow?.click();
+                            }
+
+                            const toolboxSelector = `${editorSelector} .blocklyTreeRow[data-ns="${ns}"]`;
+                            const toolboxRow = document.querySelector<HTMLDivElement>(toolboxSelector);
+                            toolboxRow?.click();
+                        });
+                        inlineBlock.addEventListener("keydown", e => fireClickOnEnter(e as any))
                     }
                 }
             });
