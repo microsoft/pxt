00f42a9a7cb016d4ccb7ae24339019d60d4720d4
thsparks <thsparks@microsoft.com>
2025-02-28 17:11:10 -0800

    Remove weight property from custom themes

--- themebuilder/src/transforms/setCurrentEditingTheme.ts
+++ themebuilder/src/transforms/setCurrentEditingTheme.ts
@@ -5,6 +5,9 @@ import { setCurrentFrameTheme } from "./setCurrentFrameTheme";
 export function setCurrentEditingTheme(theme: pxt.ColorThemeInfo) {
     const { dispatch } = stateAndDispatch();
 
+    // remove weight property so it's always at the end of theme lists
+    delete theme.weight;
+
     // Updates to editing theme implicitly update frame theme as well
     setCurrentFrameTheme(theme);
 
