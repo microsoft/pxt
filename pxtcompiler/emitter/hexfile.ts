d6fa0592ce120f50642b5868d006684aa12e0451
Tom Ball <tball@microsoft.com>
2025-03-12 11:31:15 -0700

    renaming

--- pxtcompiler/emitter/hexfile.ts
+++ pxtcompiler/emitter/hexfile.ts
@@ -51,7 +51,7 @@ namespace ts.pxtc {
             jmpStartAddr: number;
             jmpStartIdx: number;
 
-            bottomFlashAddr: number;
+            topFlashAddr: number;
 
             elfInfo: pxt.elf.Info;
             espInfo: pxt.esp.Image;
@@ -71,7 +71,7 @@ namespace ts.pxtc {
                 codeStartAddr: undefined,
                 elfInfo: undefined,
                 espInfo: undefined,
-                bottomFlashAddr: 0
+                topFlashAddr: 0
             }
         }
 
@@ -85,8 +85,8 @@ namespace ts.pxtc {
             return ctx.codeStartAddrPadded
         }
 
-        export function getBottomFlashAddress() {
-            return ctx.bottomFlashAddr
+        export function getTopFlashAddress() {
+            return ctx.topFlashAddr
         }
 
         // utility function
@@ -226,7 +226,7 @@ namespace ts.pxtc {
                         if (hasMarker(drom.data, off)) {
                             found = true
                             off += marker.length
-                            ctx.bottomFlashAddr = pxt.HF2.read32(drom.data, off)
+                            ctx.topFlashAddr = pxt.HF2.read32(drom.data, off)
                             off += 4
                             const ptroff = off
                             for (let ptr of extInfo.vmPointers || []) {
@@ -280,7 +280,7 @@ namespace ts.pxtc {
                 ctx.jmpStartIdx = -1
 
                 const hexb = hexlines[0].slice(jmpIdx + 32, jmpIdx + 40)
-                ctx.bottomFlashAddr = parseInt(swapBytes(hexb), 16)
+                ctx.topFlashAddr = parseInt(swapBytes(hexb), 16)
 
                 let ptrs = hexlines[0].slice(jmpIdx + 40, jmpIdx + 40 + funs.length * 8 + 16)
                 readPointers(ptrs)
@@ -380,8 +380,7 @@ namespace ts.pxtc {
                 if (i === ctx.jmpStartIdx + 1) {
                     let step = opts.shortPointers ? 4 : 8
                     let hexb = swapBytes(m[2].slice(0, step))
-                    ctx.bottomFlashAddr = parseInt(hexb, 16)
-                    console.log(`ctx.bottomFlashAddr ${hexb}`)
+                    ctx.topFlashAddr = parseInt(hexb, 16)
                     readPointers(m[2].slice(step))
                 } else {
                     readPointers(m[2])
@@ -1045,8 +1044,8 @@ ${hexfile.hexPrelude()}
         let settingsSize = cres.configData.find(ce => ce.name === "SETTINGS_SIZE")
         let actualSettingsSize = settingsSize ? settingsSize.value : settingsSizeDefault ? settingsSizeDefault.value : 0
 
-        const bottomFlashAddr = hexfile.getBottomFlashAddress()
-        let flashUsableEnd = (bottomFlashAddr > 0 ? bottomFlashAddr : target.flashUsableEnd ? target.flashUsableEnd : target.flashEnd) - actualSettingsSize
+        const topFlashAddr = hexfile.getTopFlashAddress()
+        let flashUsableEnd = (topFlashAddr > 0 ? topFlashAddr : target.flashUsableEnd ? target.flashUsableEnd : target.flashEnd) - actualSettingsSize
 
         src = `; Interface tables: ${bin.itFullEntries}/${bin.itEntries} (${Math.round(100 * bin.itFullEntries / bin.itEntries)}%)\n` +
             `; Virtual methods: ${bin.numVirtMethods} / ${bin.numMethods}\n` +
