b69c67e00061975d422ea6d0f9c845ac53f27741
Eric Anderson <eanders@microsoft.com>
2025-04-01 13:16:25 -0700

    use a ref

--- webapp/src/editortoolbar.tsx
+++ webapp/src/editortoolbar.tsx
@@ -25,7 +25,7 @@ interface EditorToolbarState {
 
 export class EditorToolbar extends data.Component<ISettingsProps, EditorToolbarState> {
     protected compileTimeout: number;
-    compileBtnDropdown: sui.DropdownMenu;
+    private compileBtnDropdown: React.RefObject<sui.DropdownMenu>;
 
     constructor(props: ISettingsProps) {
         super(props);
@@ -41,6 +41,8 @@ export class EditorToolbar extends data.Component<ISettingsProps, EditorToolbarS
         this.toggleDebugging = this.toggleDebugging.bind(this);
         this.toggleCollapsed = this.toggleCollapsed.bind(this);
         this.cloudButtonClick = this.cloudButtonClick.bind(this);
+
+        this.compileBtnDropdown = React.createRef();
     }
 
     saveProjectName(name: string, view?: string) {
@@ -350,7 +352,7 @@ export class EditorToolbar extends data.Component<ISettingsProps, EditorToolbarS
         // Add the ... menu
         const usbIcon = pxt.appTarget.appTheme.downloadDialogTheme?.deviceIcon || "usb";
         el.push(
-            <sui.DropdownMenu key="downloadmenu" role="menuitem" icon={`${downloadButtonIcon} horizontal ${hwIconClasses}`} title={lf("Download options")} className={`${hwIconClasses} right attached editortools-btn hw-button button`} dataTooltip={tooltip} displayAbove={true} displayRight={displayRight} ref={ref => this.compileBtnDropdown = ref} onShow={
+            <sui.DropdownMenu key="downloadmenu" role="menuitem" icon={`${downloadButtonIcon} horizontal ${hwIconClasses}`} title={lf("Download options")} className={`${hwIconClasses} right attached editortools-btn hw-button button`} dataTooltip={tooltip} displayAbove={true} displayRight={displayRight} ref={this.compileBtnDropdown} onShow={
                 () => this.forceUpdate() // force update to refresh extMenuItems
             }>
                 {webUSBSupported && !packetioConnected && <sui.Item role="menuitem" icon={usbIcon} text={lf("Connect Device")} tabIndex={-1} onClick={this.onPairClick} />}
@@ -360,8 +362,8 @@ export class EditorToolbar extends data.Component<ISettingsProps, EditorToolbarS
                 {!extMenuItems && <sui.Item role="menuitem" icon="xicon file-download" text={downloadMenuText} tabIndex={-1} onClick={this.onFileDownloadClick} />}
                 {extMenuItems.map((props, index) => <sui.Item key={"ext" + index} role="menuitem" tabIndex={-1} {...props} onClick={(e) => {
                     props.onClick?.(e);
-                    this.compileBtnDropdown?.setInactive(e.currentTarget);
-                    this.compileBtnDropdown?.hide();
+                    this.compileBtnDropdown.current?.setInactive(e.currentTarget);
+                    this.compileBtnDropdown.current?.hide();
                 }}
                 />)}
                 {downloadHelp && <sui.Item role="menuitem" icon="help circle" text={lf("Help")} tabIndex={-1} onClick={this.onHelpClick} />}
