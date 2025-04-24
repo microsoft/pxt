18fbb2e9db6cad5a751e49f19ab464b805203dbc
Richard Knoll <riknoll@microsoft.com>
2025-04-24 15:47:04 -0700

    move flyout caching functionality into a block inflater

---
+++ pxtblocks/plugins/flyout/cachingFlyout.ts
@@ -0,0 +1,12 @@
+import * as Blockly from "blockly";
+import { MultiFlyoutRecyclableBlockInflater } from "./blockInflater";
+
+export class CachingFlyout extends Blockly.VerticalFlyout {
+    clearBlockCache() {
+        const inflater = this.getInflaterForType("block");
+
+        if (inflater instanceof MultiFlyoutRecyclableBlockInflater) {
+            inflater.clearCache();
+        }
+    }
+}
