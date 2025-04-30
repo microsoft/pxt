07f34effed855b0b05991c92ae85bda6d153efca
thsparks <thsparks@microsoft.com>
2025-04-30 15:03:42 -0700

    Hide hidden fields from block display string

--- pxtblocks/getBlockText.ts
+++ pxtblocks/getBlockText.ts
@@ -4,8 +4,16 @@ import { FieldCustom } from "./fields/field_utils";
 export function getBlockText(block: Blockly.BlockSvg): string {
   const fieldValues = [];
   for (const input of block.inputList) {
+    if (!input.isVisible()) {
+      continue;
+    }
+
     if (input.fieldRow.length > 0) {
       for (const field of input.fieldRow) {
+        if (!field.isVisible()) {
+          continue;
+        }
+
         const text = (field as FieldCustom & Blockly.Field).isFieldCustom_
           ? (field as any).getFieldDescription()
           : field.getText();
