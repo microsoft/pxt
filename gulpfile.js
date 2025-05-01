210c89e82ccd66e2dbdc7970b4628d376b1c4ad6
thsparks <thsparks@microsoft.com>
2025-05-01 13:37:08 -0700

    Revert "Temporarily override kiosk for theme builder in upload target"
    This reverts commit 0436b1d52c443ddaf49ba9311fed1ba647bb3aef.

--- gulpfile.js
+++ gulpfile.js
@@ -604,7 +604,7 @@ const multiplayer = createWebappTasks("multiplayer");
                       Kiosk
 *********************************************************/
 
-const kiosk = createWebappTasks("kiosk", "temp");
+const kiosk = createWebappTasks("kiosk");
 
 /********************************************************
                       Teacher Tool
@@ -622,7 +622,7 @@ const tutorialTool = createWebappTasks("tutorialtool");
                       Tutorial Tool
 *********************************************************/
 
-const themeBuilder = createWebappTasks("themebuilder", "kiosk");
+const themeBuilder = createWebappTasks("themebuilder");
 
 /********************************************************
                  Webapp build wrappers
