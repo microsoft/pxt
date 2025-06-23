8baccd3238bae71b1f0dab461df3cbfe1092589e
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-06-23 18:00:51 +0100

    Basic colour field based on dropdown grid plugin
    This give us more control over the format and naturally fixes the invalid
    initial selection for keyboard nav.
    
    Still needs styling to match as the CSS classes have changed.

--- pxtblocks/fields/field_colour.ts
+++ pxtblocks/fields/field_colour.ts
@@ -1,16 +1,25 @@
-/// <reference path="../../built/pxtlib.d.ts" />
-
-import * as Blockly from "blockly";
-import { FieldCustom, FieldCustomOptions } from "./field_utils";
-
-import { FieldColour } from "@blockly/field-colour";
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 /**
-     * The value modes:
-     *     hex - Outputs an HTML color string: "#ffffff" (with quotes)
-     *     rgb - Outputs an RGB number in hex: 0xffffff
-     *     index - Outputs the index of the color in the list of colors: 0
-     */
+ * @fileoverview Colour input field.
+ */
+
+import {
+    FieldGridDropdown
+} from '@blockly/field-grid-dropdown';
+import * as Blockly from 'blockly/core';
+import { FieldCustom, FieldCustomOptions } from './field_utils';
+
+/**
+ * The value modes:
+ *     hex - Outputs an HTML color string: "#ffffff" (with quotes)
+ *     rgb - Outputs an RGB number in hex: 0xffffff
+ *     index - Outputs the index of the color in the list of colors: 0
+ */
 export type FieldColourValueMode = "hex" | "rgb" | "index";
 
 export interface FieldColourNumberOptions extends FieldCustomOptions {
@@ -20,79 +29,91 @@ export interface FieldColourNumberOptions extends FieldCustomOptions {
     valueMode?: FieldColourValueMode;
 }
 
-export class FieldColorNumber extends FieldColour implements FieldCustom {
-    public isFieldCustom_ = true;
+/**
+ * Similar to Blockly's field_colour but with support for different colour
+ * formats.
+ */
+export class FieldColorNumber extends FieldGridDropdown implements FieldCustom {
 
-    protected colour_: string;
-    private valueMode_: FieldColourValueMode = "rgb";
-    protected colours_: string[];
+    isFieldCustom_ = true;
+
+    /**
+     * Used to tell if the field needs to be rendered the next time the block is
+     * rendered.  Colour fields are statically sized, and only need to be
+     * rendered at initialization.
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected override isDirty_ = false;
+
+    private allColoursCSSFormat_: string[];
+    private valueMode_: FieldColourValueMode;
 
     constructor(text: string, params: FieldColourNumberOptions, opt_validator?: Blockly.FieldValidator) {
-        super(text, opt_validator);
-
-        if (params.colours)
-            this.setColours(JSON.parse(params.colours));
-        else if (pxt.appTarget.runtime && pxt.appTarget.runtime.palette) {
-            let p = pxt.Util.clone(pxt.appTarget.runtime.palette);
-            p[0] = "#dedede";
-            let t;
-            if (pxt.appTarget.runtime.paletteNames) {
-                t = pxt.Util.clone(pxt.appTarget.runtime.paletteNames);
-                t[0] = lf("transparent");
-            }
-            this.setColours(p, t);
+        super(Blockly.Field.SKIP_SETUP as any, opt_validator, { primaryColour: "white", borderColour: "black", columns: !!params.columns ? parseInt(params.columns) : 7});
+        let allColoursValueFormat: string[];
+        let allColoursCSSFormat: string[];
+        let titles: string[] | undefined;
+        const valueMode = params.valueMode ?? "rgb";
+        if (params.colours) {
+            allColoursCSSFormat = JSON.parse(params.colours);
         }
-
-        // Set to first color in palette (for toolbox)
-        this.setValue(this.getColours_()[0]);
-
-        if (params.columns) this.setColumns(parseInt(params.columns));
-        if (params.valueMode) this.valueMode_ = params.valueMode;
-    }
-
-    setColours(colours: string[], titles?: string[]): FieldColour {
-        const s = super.setColours(colours, titles);
-        this.colours_ = colours;
-        return s;
-    }
-
-
-    doClassValidation_(colour: string) {
-        return "string" != typeof colour ? null : parseColour(colour, this.getColours_());
+        else if (pxt.appTarget.runtime && pxt.appTarget.runtime.palette) {
+            allColoursCSSFormat = pxt.Util.clone(pxt.appTarget.runtime.palette);
+            allColoursCSSFormat[0] = "#dedede";
+            if (pxt.appTarget.runtime.paletteNames) {
+                titles = pxt.Util.clone(pxt.appTarget.runtime.paletteNames);
+                titles[0] = lf("transparent");
+            }
+        }
+        allColoursValueFormat = allColoursCSSFormat.map(c => cssFormatToValueFormat(c, valueMode, allColoursCSSFormat))
+        this.allColoursCSSFormat_ = allColoursCSSFormat;
+        this.setOptions(makeSwatches(allColoursValueFormat, allColoursCSSFormat, titles));
+        this.setValue(text);
+        this.valueMode_ = valueMode;
     }
 
     /**
-     * Return the current colour.
-     * @param {boolean} opt_asHex optional field if the returned value should be a hex
-     * @return {string} Current colour in '#rrggbb' format.
+     * FieldDropdown has complex behaviors for normalizing options that aren't
+     * applicable here. Instead, just return the options as-is.
+     *
+     * @param options The options (colour swatches) to normalize.
+     * @returns The colour swatches as-is.
      */
-    getValue(opt_asHex?: boolean) {
-        if (opt_asHex) return this.value_;
-        switch (this.valueMode_) {
-            case "hex":
-                return `"${this.value_}"`;
-            case "rgb":
-                if (this.value_.indexOf('#') > -1) {
-                    return `0x${this.value_.replace(/^#/, '')}`;
-                }
-                else {
-                    return this.value_;
-                }
-            case "index":
-                if (!this.value_) return "-1";
-                const allColours = this.getColours_();
-                for (let i = 0; i < allColours.length; i++) {
-                    if (this.value_.toUpperCase() === allColours[i].toUpperCase()) {
-                        return i + "";
-                    }
-                }
-        }
-        return this.value_;
+    protected override trimOptions(options: Blockly.MenuOption[]) {
+        return { options };
     }
 
-    getFieldDescription(): string {
-        const value = this.getValue();
-        return value ? lf("color ${0}", value) : lf("color");
+    /**
+     * Create the block UI for this colour field.
+     *
+     * @internal
+     */
+    override initView() {
+        const constants = this.getConstants();
+        // This can't happen, but TypeScript thinks it can and lint forbids `!.`.
+        if (!constants) throw Error('Constants not found');
+        this.size_ = new Blockly.utils.Size(
+            constants.FIELD_COLOUR_DEFAULT_WIDTH,
+            constants.FIELD_COLOUR_DEFAULT_HEIGHT,
+        );
+        this.createBorderRect_();
+        this.getBorderRect().style['fillOpacity'] = '1';
+        this.getBorderRect().setAttribute('stroke', '#fff');
+        if (this.isFullBlockField()) {
+            this.clickTarget_ = (this.sourceBlock_ as Blockly.BlockSvg).getSvgRoot();
+        }
+    }
+
+    /**
+     * Shows the colour picker dropdown attached to the field.
+     *
+     * @param e The event that triggered display of the colour picker dropdown.
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected override showEditor_(e?: MouseEvent) {
+        super.showEditor_(e);
+        Blockly.DropDownDiv.getContentDiv().classList.add('blocklyFieldColour');
+        Blockly.DropDownDiv.repositionForWindowResize();
     }
 
     /**
@@ -112,18 +133,27 @@ export class FieldColorNumber extends FieldColour implements FieldCustom {
     }
 
     /**
-     * Set the colour.
-     * @param {string} colour The new colour in '#rrggbb' format.
+     * @returns True if the source block is a value block with a single editable
+     *     field.
+     * @internal
      */
-    doValueUpdate_(colour: string) {
-        super.doValueUpdate_(parseColour(colour, this.getColours_()))
-        // this.applyColour();
+    blockIsSimpleReporter(): boolean {
+        const block = this.getSourceBlock();
+        if (!block) throw new Blockly.UnattachedFieldError();
+
+        if (!block.outputConnection) return false;
+
+        for (const input of block.inputList) {
+            if (input.connection || input.fieldRow.length > 1) return false;
+        }
+        return true;
     }
 
-    getColours_(): string[] {
-        return this.colours_;
-    }
-
+    /**
+     * Updates text field to match the colour/style of the block.
+     *
+     * @internal
+     */
     override applyColour() {
         const block = this.getSourceBlock() as Blockly.BlockSvg | null;
         if (!block) throw new Blockly.UnattachedFieldError();
@@ -137,22 +167,186 @@ export class FieldColorNumber extends FieldColour implements FieldCustom {
 
         if (!this.isFullBlockField()) {
             borderRect.style.display = 'block';
-            borderRect.style.fill = this.getValue() as string;
+            borderRect.style.fill = this.getCSSValue();
         } else {
             borderRect.style.display = 'none';
             // In general, do *not* let fields control the color of blocks. Having the
             // field control the color is unexpected, and could have performance
             // impacts.
-            block.pathObject.svgPath.setAttribute('fill', parseColour(this.getValue(), this.getColours_()));
+            block.pathObject.svgPath.setAttribute('fill', this.getCSSValue());
             block.pathObject.svgPath.setAttribute('stroke', '#fff');
         }
     }
+
+    /**
+     * Returns the height and width of the field.
+     *
+     * This should *in general* be the only place render_ gets called from.
+     *
+     * @returns Height and width.
+     */
+    override getSize(): Blockly.utils.Size {
+        if (this.getConstants()?.FIELD_COLOUR_FULL_BLOCK) {
+            // In general, do *not* let fields control the color of blocks. Having the
+            // field control the color is unexpected, and could have performance
+            // impacts.
+            // Full block fields have more control of the block than they should
+            // (i.e. updating fill colour) so they always need to be rerendered.
+            this.render_();
+            this.isDirty_ = false;
+        }
+        return super.getSize();
+    }
+
+    /**
+     * Updates the colour of the block to reflect whether this is a full
+     * block field or not.
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected override render_() {
+        this.updateSize_();
+
+        const block = this.getSourceBlock() as Blockly.BlockSvg | null;
+        if (!block) throw new Blockly.UnattachedFieldError();
+        // Calling applyColour updates the UI (full-block vs non-full-block) for the
+        // colour field, and the colour of the field/block.
+        block.applyColour();
+    }
+
+    /**
+     * Updates the size of the field based on whether it is a full block field
+     * or not.
+     *
+     * @param margin margin to use when positioning the field.
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected updateSize_(margin?: number) {
+        const constants = this.getConstants();
+        if (!constants) return;
+        let totalWidth;
+        let totalHeight;
+        if (this.isFullBlockField()) {
+            const xOffset = margin ?? 0;
+            totalWidth = xOffset * 2;
+            totalHeight = constants.FIELD_TEXT_HEIGHT;
+        } else {
+            totalWidth = constants.FIELD_COLOUR_DEFAULT_WIDTH;
+            totalHeight = constants.FIELD_COLOUR_DEFAULT_HEIGHT;
+        }
+
+        this.size_.height = totalHeight;
+        this.size_.width = totalWidth;
+
+        this.positionBorderRect_();
+    }
+
+    /**
+     * Ensure that the input value is a valid colour.
+     *
+     * @param newValue The input value.
+     * @returns A valid colour, or null if invalid.
+     */
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected override doClassValidation_(
+        newValue: string,
+    ): string | null | undefined;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected override doClassValidation_(newValue?: string): string | null;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    protected override doClassValidation_(
+        newValue?: string,
+    ): string | null | undefined {
+        if (typeof newValue !== 'string') {
+            return null;
+        }
+        const cssFormat = valueFormatToCSSFormat(newValue, this.allColoursCSSFormat_);
+        const normalizedValueFormat = cssFormatToValueFormat(
+            cssFormat,
+            this.valueMode_,
+            this.allColoursCSSFormat_
+        );
+        return normalizedValueFormat;
+    }
+
+    /**
+     * Get the text for this field.  Used when the block is collapsed.
+     *
+     * @returns Text representing the value of this field.
+     */
+    override getText(): string {
+        let colour = this.getCSSValue();
+        // Try to use #rgb format if possible, rather than #rrggbb.
+        if (/^#(.)\1(.)\2(.)\3$/.test(colour)) {
+            colour = '#' + colour[1] + colour[3] + colour[5];
+        }
+        return colour;
+    }
+
+    getFieldDescription(): string {
+        const value = this.getCSSValue();
+        return value ? lf("color ${0}", value) : lf("color");
+    }
+
+    getCSSValue() {
+        return valueFormatToCSSFormat(this.getValue(), this.allColoursCSSFormat_);
+    }
+
 }
 
-function parseColour(colour: string, allColours: string[]) {
-    if (colour) {
-        const enumSplit = /Colors\.([a-zA-Z]+)/.exec(colour);
-        const hexSplit = /(0x|#)([0-9a-fA-F]+)/.exec(colour);
+/**
+* Creates a set of divs representing colour swatches for use in the picker.
+*
+* @param allColoursValueFormat An array of colours to create swatches for. The colours must
+*     be any legal CSS colour specifier.
+* @param titles A corresponding array of titles to be displayed as tooltips on
+*     the colour swatches.
+* @returns An array of pairs of DOM elements representing colour swatches and
+*     their corresponding colour.
+*/
+function makeSwatches(
+    allColoursValueFormat: string[],
+    allColoursCSSFormat: string[],
+    titles?: string[],
+): Blockly.MenuOption[] {
+    return allColoursValueFormat.map((colourValueFormat, index) => {
+        const swatch = document.createElement('div');
+        swatch.className = 'blocklyColourSwatch';
+        swatch.style.backgroundColor = allColoursCSSFormat[index];
+
+        if (titles && index < titles.length) {
+            swatch.title = titles[index];
+        }
+
+        return [swatch, colourValueFormat];
+    });
+}
+
+function cssFormatToValueFormat(value_: string, valueMode: FieldColourValueMode, allColoursCSSFormat: string[]) {
+    switch (valueMode) {
+        case "hex":
+            return `"${value_}"`;
+        case "rgb":
+            if (value_.indexOf('#') > -1) {
+                return `0x${value_.replace(/^#/, '')}`;
+            }
+            else {
+                return value_;
+            }
+        case "index":
+            if (!value_) return "-1";
+            for (let i = 0; i < allColoursCSSFormat.length; i++) {
+                if (value_.toUpperCase() === allColoursCSSFormat[i].toUpperCase()) {
+                    return i + "";
+                }
+            }
+    }
+    return value_;
+}
+
+function valueFormatToCSSFormat(value: string, allColoursCSSFormat: string[]): string {
+    if (value) {
+        const enumSplit = /Colors\.([a-zA-Z]+)/.exec(value);
+        const hexSplit = /(0x|#)([0-9a-fA-F]+)/.exec(value);
 
         if (enumSplit) {
             switch (enumSplit[1].toLocaleLowerCase()) {
@@ -167,7 +361,7 @@ function parseColour(colour: string, allColours: string[]) {
                 case "pink": return "#FF007F";
                 case "white": return "#FFFFFF";
                 case "black": return "#000000";
-                default: return colour;
+                default: return value;
             }
         } else if (hexSplit) {
             const hexLiteralNumber = hexSplit[2];
@@ -185,22 +379,68 @@ function parseColour(colour: string, allColours: string[]) {
             }
         }
 
-        if (allColours) {
-            const parsedAsInt = parseInt(colour);
+        const parsedAsInt = parseInt(value);
 
-            // Might be the index and not the color
-            if (!isNaN(parsedAsInt) && allColours[parsedAsInt] != undefined) {
-                return allColours[parsedAsInt];
-            } else {
-                return allColours[0];
-            }
+        // Might be the index and not the color
+        if (!isNaN(parsedAsInt) && allColoursCSSFormat[parsedAsInt] != undefined) {
+            return allColoursCSSFormat[parsedAsInt];
+        } else {
+            return allColoursCSSFormat[0];
         }
     }
-    return colour;
+    return value;
 }
 
+/**
+ * CSS for colour picker.
+ */
 Blockly.Css.register(`
-/* eslint-disable indent */
+.blocklyFieldColour .blocklyFieldGridItemSelected,
+.blocklyFieldGridItemSelected:hover {
+  border-color: #eee !important;
+  outline: 1px solid #333;
+  position: relative;
+}
+
+.blocklyColourSwatch {
+  width: 20px;
+  height: 20px;
+}
+
+.blocklyGridContainer {
+  padding: 0px;
+}
+
+.blocklyFieldColour .blocklyFieldGrid {
+  grid-gap: 0px;
+  row-gap: 4px;
+}
+
+.blocklyFieldColour .blocklyFieldGrid .blocklyGridItem {
+  border-radius: 0;
+  padding: 0;
+  border: 0.5px solid #888;
+  cursor: pointer;
+}
+
+.blocklyFieldColour .blocklyFieldGrid .blocklyFieldGridItem {
+  border: 0.5px solid #888;
+  padding: 0;
+  margin: 0;
+  border-radius: 0;
+}
+
+.blocklyFieldColour .blocklyFieldGrid .blocklyFieldGridItem:focus {
+  border-color: #eee;
+  box-shadow: 2px 2px 7px 2px rgba(0, 0, 0, 0.3);
+  position: relative;
+  border-radius: 0;
+  outline: none;
+}
+`);
+
+/*
+Blockly.Css.register(`
 table.blocklyColourTable {
     outline: none;
     border-radius: 11px;
@@ -238,5 +478,5 @@ table.blocklyColourTable>tr>td.blocklyColourHighlighted {
     outline: 1px solid #333;
     position: relative;
 }
-/* eslint-enable indent */
 `);
+*/
