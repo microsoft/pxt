f0fe8b0b9b2f99e7cffcbe7c80f685ea8e5b8135
Robert Knight <robert@microbit.org>
2026-03-13 11:38:34 +0000

    Remove comma for "[category], more" label
    The screen reader output is faster and less ambigious this way

--- webapp/src/toolbox.tsx
+++ webapp/src/toolbox.tsx
@@ -726,7 +726,7 @@ export class Toolbox extends data.Component<ToolboxProps, ToolboxState> {
                                     treeRow={subTreeRow}
                                     onCategoryClick={this.onCategoryClick}
                                     ariaLevel={1}
-                                    ariaLabel={Util.capitalize(`${treeRow.nameid === "led" ? treeRow.nameid.toUpperCase() : treeRow.nameid}, ${subTreeRow.name}`)}
+                                    ariaLabel={Util.capitalize(`${treeRow.nameid === "led" ? treeRow.nameid.toUpperCase() : treeRow.nameid} ${subTreeRow.name}`)}
                                 />
                             )}
                         </React.Fragment>
@@ -776,7 +776,7 @@ export class Toolbox extends data.Component<ToolboxProps, ToolboxState> {
                                                 treeRow={subTreeRow}
                                                 onCategoryClick={this.onCategoryClick}
                                                 ariaLevel={1}
-                                                ariaLabel={Util.capitalize(`${treeRow.nameid}, ${subTreeRow.name}`)}
+                                                ariaLabel={Util.capitalize(`${treeRow.nameid} ${subTreeRow.name}`)}
                                             />
                                         )}
                                     </React.Fragment>
