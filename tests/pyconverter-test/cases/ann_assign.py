733787032c9065df7c9563f26c87d3a679f4d83a
Richard Knoll <riknoll@microsoft.com>
2026-04-08 12:19:30 -0700

    only emit python annotated assigns when definition

---
+++ tests/pyconverter-test/cases/ann_assign.py
@@ -0,0 +1,16 @@
+class Foo:
+    def __init__(self):
+        self.l: List[Bar] = []
+
+class Bar:
+    def __init__(self):
+        pass
+
+foo: number = 9
+foo: number = 8
+
+bar: Bar = None
+bar: Bar = None
+
+bop: List[number] = []
+bop: List[number] = []
