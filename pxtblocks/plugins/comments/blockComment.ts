00caa470c71dcfa5c9249bf314d66ba64fa8622f
Robert Knight <robert@microbit.org>
2026-06-11 16:50:49 +0100

    Improve screen reader experience for comments

--- pxtblocks/plugins/comments/blockComment.ts
+++ pxtblocks/plugins/comments/blockComment.ts
@@ -59,6 +59,10 @@ export class CommentIcon extends Blockly.icons.Icon implements Blockly.IHasBubbl
         return CommentIcon.TYPE;
     }
 
+    protected override getAriaLabel(): string | null {
+        return lf("Open comment");
+    }
+
     override initView(pointerdownListener: (e: PointerEvent) => void): void {
         if (this.svgRoot) return; // Already initialized.
 
